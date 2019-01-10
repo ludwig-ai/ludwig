94325192cf25b86cf3f5a7a33732831fd6530ff8
piero <piero@uber.com>
2019-01-10 10:56:42 -0800

    Updated copyright

--- ludwig/utils/defaults.py
+++ ludwig/utils/defaults.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # coding=utf-8
-# Copyright 2019 The Ludwig Authors. All Rights Reserved.
+# Copyright (c) 2019 Uber Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
