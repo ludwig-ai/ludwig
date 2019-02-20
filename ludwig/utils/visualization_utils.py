6b948ea9f0b2e78558fb51d2edd4d9c3558ff505
Piero Molino <piero@uber.com>
2019-02-19 23:21:19 -0800

    Fix for issue #114, using TkAgg backend on OS X

--- ludwig/utils/visualization_utils.py
+++ ludwig/utils/visualization_utils.py
@@ -17,11 +17,15 @@
 from __future__ import absolute_import
 from __future__ import division
 
+from collections import Counter
+from sys import platform
+
 import copy
 import logging
-from collections import Counter
-
 import matplotlib as mpl
+
+if platform == "darwin":  # OS X
+    mpl.use('TkAgg')
 import matplotlib.patches as patches
 import matplotlib.path as path
 import matplotlib.patheffects as PathEffects
