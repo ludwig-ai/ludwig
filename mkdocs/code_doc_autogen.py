c0361aee48b994083715d0b4c874d27773dd99b1
Piero Molino <w4nderlust@gmail.com>
2019-03-13 00:15:12 -0700

    Added additional check on length of arguments in code_doc_autogen.py

--- mkdocs/code_doc_autogen.py
+++ mkdocs/code_doc_autogen.py
@@ -100,7 +100,7 @@ def get_function_signature(function, method=True):
     else:
         signature = inspect.getargspec(wrapped)
     defaults = signature.defaults
-    if method and signature.args[0] == 'self':
+    if method and len(signature.args) > 0 and signature.args[0] == 'self':
         args = signature.args[1:]
     else:
         args = signature.args
