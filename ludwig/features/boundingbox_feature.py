c615994e4999bd58e914613a6a32591a31584073
Julien St-Pierre Fortin <julienstpierrefortin@gmail.com>
2019-05-14 14:53:57 -0400

    Add skeleton for boundingbox feature

--- ludwig/features/boundingbox_feature.py
+++ ludwig/features/boundingbox_feature.py
@@ -63,3 +63,78 @@ class BoundingBoxBaseFeature(BaseFeature):
         data[feature['name']] = dataset_df[feature['name']].astype(
             np.float32).as_matrix()
 
+
+
+class BoundingBoxOutputFeature(NumericalBaseFeature, OutputFeature):
+    def __init__(self, feature):
+        super().__init__(feature)
+
+        self.loss = {'type': MEAN_SQUARED_ERROR}
+        self.clip = None
+        self.initializer = None
+        self.regularize = True
+
+        _ = self.overwrite_defaults(feature)
+
+    def _get_output_placeholder(self):
+        return tf.placeholder(
+            tf.int64,
+            [None],  # None is for dealing with variable batch size
+            name='{}_placeholder'.format(self.name)
+        )
+
+    def _get_predictions(
+            self,
+            hidden,
+            hidden_size,
+            regularizer=None
+    ):
+    	pass
+
+    def _get_loss(self, targets, predictions):
+    	pass
+
+    def _get_measures(self, targets, predictions):
+    	pass
+
+    def build_output(
+            self,
+            hidden,
+            hidden_size,
+            regularizer=None,
+            **kwargs
+    ):
+    	pass
+
+    @staticmethod
+    def update_model_definition_with_metadata(
+            output_feature,
+            feature_metadata,
+            *args,
+            **kwargs
+    ):
+        pass
+
+    @staticmethod
+    def calculate_overall_stats(
+            test_stats,
+            output_feature,
+            dataset,
+            train_set_metadata
+    ):
+        pass
+
+    @staticmethod
+    def postprocess_results(
+            output_feature,
+            result,
+            metadata,
+            experiment_dir_name,
+            skip_save_unprocessed_output=False
+    ):
+    	pass
+
+    @staticmethod
+    def populate_defaults(output_feature):
+    	pass
+
