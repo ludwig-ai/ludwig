90abff853af05459eb3e36ca4a6bd6bf731a6827
Julien St-Pierre Fortin <julienstpierrefortin@gmail.com>
2019-05-14 14:50:14 -0400

    Add skeleton for boundingbox feature

---
+++ ludwig/features/boundingbox_feature.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# coding=utf-8
+# Copyright (c) 2019 Uber Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import logging
+import os
+from collections import OrderedDict
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.ops.losses.losses_impl import Reduction
+
+from ludwig.constants import *
+from ludwig.features.base_feature import BaseFeature
+from ludwig.features.base_feature import InputFeature
+from ludwig.features.base_feature import OutputFeature
+from ludwig.models.modules.fully_connected_modules import fc_layer
+from ludwig.models.modules.initializer_modules import get_initializer
+from ludwig.models.modules.measure_modules import \
+    absolute_error as get_absolute_error
+from ludwig.models.modules.measure_modules import error as get_error
+from ludwig.models.modules.measure_modules import r2 as get_r2
+from ludwig.models.modules.measure_modules import \
+    squared_error as get_squared_error
+from ludwig.utils.misc import set_default_value
+
+
+
+class BoundingBoxBaseFeature(BaseFeature):
+    def __init__(self, feature):
+        super().__init__(feature)
+        self.type = NUMERICAL
+
+    preprocessing_defaults = {
+        'missing_value_strategy': FILL_WITH_CONST,
+        'fill_value': 0
+    }
+
+    @staticmethod
+    def get_feature_meta(column, preprocessing_parameters):
+        return {}
+
+    @staticmethod
+    def add_feature_data(
+            feature,
+            dataset_df,
+            data,
+            metadata,
+            preprocessing_parameters,
+    ):
+        data[feature['name']] = dataset_df[feature['name']].astype(
+            np.float32).as_matrix()
+
