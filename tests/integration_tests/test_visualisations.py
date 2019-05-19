87947cfdaa01a61da9dea111ff2bdd701bee3794
ivo <ivaylo.stefanov82@yahoo.com>
2019-05-19 17:03:55 +0300

    Test saving of confusion matrix visualisations as pdf and png.

--- tests/integration_tests/test_visualisations.py
+++ tests/integration_tests/test_visualisations.py
@@ -17,21 +17,14 @@
 import logging
 import shutil
 import subprocess
-import threading
 import glob
-from unittest import mock
-
-from pynput.keyboard import Key, Controller
 
 from ludwig.experiment import experiment
-from ludwig.utils import visualization_utils
-from ludwig.visualize import learning_curves
 from tests.integration_tests.utils import generate_data
 from tests.integration_tests.utils import text_feature
 
 # The following imports are pytest fixtures, required for running the tests
 from tests.fixtures.filenames import *
-# Globals needed for the thread communicating their results.
 
 
 def run_experiment(input_features, output_features, **kwargs):
@@ -60,9 +53,9 @@ def run_experiment(input_features, output_features, **kwargs):
 
     args = {
         'model_definition': model_definition,
-        'skip_save_processed_input': True,
-        'skip_save_progress': True,
-        'skip_save_unprocessed_output': True,
+        'skip_save_processed_input': False,
+        'skip_save_progress': False,
+        'skip_save_unprocessed_output': False,
     }
     args.update(kwargs)
 
@@ -100,7 +93,6 @@ def test_visualisation_learning_curves_output_pdf(csv_filename):
     pdf_figure_cnt = glob.glob(vis_output_pattern)
 
     assert 0 == result.returncode
-    # 5 pdf figures should be saved
     assert 5 == len(pdf_figure_cnt)
 
     shutil.rmtree(exp_dir_name, ignore_errors=True)
@@ -126,6 +118,41 @@ def test_visualisation_learning_curves_output_png(csv_filename):
     test_cmd = ['python', '-m', 'ludwig.visualize', '--visualization',
                 'learning_curves', '--training_statistics', train_stats,
                 '-od', exp_dir_name, '-ff', 'png']
+    result = subprocess.run(
+        test_cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    png_figure_cnt = glob.glob(vis_output_pattern)
+
+    assert 0 == result.returncode
+    assert 5 == len(png_figure_cnt)
+
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+
+
+def test_visualisation_confusion_matrix_output_pdf(csv_filename):
+    """It should be possible to save figures as pdf in the specified directory.
+
+    """
+    input_features = [text_feature(reduce_output=None, encoder='rnn')]
+    output_features = [text_feature(reduce_input=None, decoder='tagger')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    logging.info('seq to seq test, Encoder: {0}'.format(encoder))
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+    vis_output_pattern = exp_dir_name + '/*.pdf'
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth_metadata = experiment_source_data_name + '.json'
+    test_stats = exp_dir_name + '/test_statistics.json'
+    test_cmd = ['python', '-m', 'ludwig.visualize', '--visualization',
+                'confusion_matrix', '--test_statistics', test_stats,
+                '--ground_truth_metadata', ground_truth_metadata,
+                '-od', exp_dir_name]
 
     result = subprocess.run(
         test_cmd,
@@ -135,10 +162,60 @@ def test_visualisation_learning_curves_output_png(csv_filename):
     pdf_figure_cnt = glob.glob(vis_output_pattern)
 
     assert 0 == result.returncode
-    # 5 png figures should be saved
-    assert 5 == len(pdf_figure_cnt)
+    assert 2 == len(pdf_figure_cnt)
 
     shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
+
+
+def test_visualisation_confusion_matrix_output_png(csv_filename):
+    """It should be possible to save figures as png in the specified directory.
+
+    """
+    input_features = [text_feature(reduce_output=None, encoder='rnn')]
+    output_features = [text_feature(reduce_input=None, decoder='tagger')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    logging.info('seq to seq test, Encoder: {0}'.format(encoder))
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+    vis_output_pattern = exp_dir_name + '/*.png'
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth_metadata = experiment_source_data_name + '.json'
+    test_stats = exp_dir_name + '/test_statistics.json'
+    test_cmd = ['python', '-m', 'ludwig.visualize', '--visualization',
+                'confusion_matrix', '--test_statistics', test_stats,
+                '--ground_truth_metadata', ground_truth_metadata,
+                '-od', exp_dir_name, '-ff', 'png']
+
+    result = subprocess.run(
+        test_cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    png_figure_cnt = glob.glob(vis_output_pattern)
+
+    assert 0 == result.returncode
+
+    assert 2 == len(png_figure_cnt)
+
+    # clean up experiment files.
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
+
 
 if __name__ == '__main__':
     """
