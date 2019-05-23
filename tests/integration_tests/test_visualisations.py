0a6e33444fdf63ff1dd558605c3b1c6d6053d345
ivo <ivaylo.stefanov82@yahoo.com>
2019-05-23 23:29:34 +0300

    Test visualisation compare_classifiers_multimetric_output as pdf and png.

--- tests/integration_tests/test_visualisations.py
+++ tests/integration_tests/test_visualisations.py
@@ -820,6 +820,117 @@ def test_visualisation_compare_classifiers_changing_k_output_png(csv_filename):
         except OSError as e:  # if failed, report it back to the user
             print("Error: %s - %s." % (e.filename, e.strerror))
 
+def test_visualisation_compare_classifiers_multiclass_multimetric_output_pdf(
+        csv_filename
+):
+    """It should be possible to save figures as pdf in the specified directory.
+
+    """
+    input_features = [
+        text_feature(vocab_size=10, min_len=1, representation='sparse'),
+        categorical_feature(
+            vocab_size=10,
+            loss='sampled_softmax_cross_entropy'
+        )
+    ]
+    output_features = [categorical_feature(vocab_size=2, reduce_input='sum')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+    vis_output_pattern = exp_dir_name + '/*.pdf'
+    test_stats = exp_dir_name + '/test_statistics.json'
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth_metadata = experiment_source_data_name + '.json'
+    test_cmd = ['python',
+                '-m',
+                'ludwig.visualize',
+                '--visualization',
+                'compare_classifiers_multiclass_multimetric',
+                '--test_statistics',
+                test_stats,
+                test_stats,
+                '--ground_truth_metadata',
+                ground_truth_metadata,
+                '-od', exp_dir_name]
+
+    result = subprocess.run(
+        test_cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    pdf_figure_cnt = glob.glob(vis_output_pattern)
+
+    assert 0 == result.returncode
+    assert 4 == len(pdf_figure_cnt)
+
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
+
+def test_visualisation_compare_classifiers_multiclass_multimetric_output_png(
+        csv_filename
+):
+    """It should be possible to save figures as png in the specified directory.
+
+    """
+    input_features = [
+        text_feature(vocab_size=10, min_len=1, representation='sparse'),
+        categorical_feature(
+            vocab_size=10,
+            loss='sampled_softmax_cross_entropy'
+        )
+    ]
+    output_features = [categorical_feature(vocab_size=2, reduce_input='sum')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+    vis_output_pattern = exp_dir_name + '/*.png'
+    test_stats = exp_dir_name + '/test_statistics.json'
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth_metadata = experiment_source_data_name + '.json'
+    test_cmd = ['python',
+                '-m',
+                'ludwig.visualize',
+                '--visualization',
+                'compare_classifiers_multiclass_multimetric',
+                '--test_statistics',
+                test_stats,
+                test_stats,
+                '--ground_truth_metadata',
+                ground_truth_metadata,
+                '-od', exp_dir_name,
+                '-ff', 'png']
+
+    result = subprocess.run(
+        test_cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    png_figure_cnt = glob.glob(vis_output_pattern)
+
+    assert 0 == result.returncode
+    assert 4 == len(png_figure_cnt)
+
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
+
 
 if __name__ == '__main__':
     """
