3902cd07c3b9cbd0495b92f88542d29955e7a620
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2019-05-24 17:45:37 +0300

    Refactor visualise compare classifiers from prob to test both pdf and png outputs.

--- tests/integration_tests/test_visualisations.py
+++ tests/integration_tests/test_visualisations.py
@@ -17,11 +17,12 @@
 import glob
 import shutil
 import subprocess
+import json
 
 from ludwig.experiment import experiment
 from tests.fixtures.filenames import *
 from tests.integration_tests.utils import generate_data
-from tests.integration_tests.utils import text_feature
+from tests.integration_tests.utils import text_feature, categorical_feature
 
 
 # The following imports are pytest fixtures, required for running the tests
@@ -62,6 +63,15 @@ def run_experiment(input_features, output_features, **kwargs):
     exp_dir_name = experiment(**args)
 
     return exp_dir_name
+
+
+def get_output_field_name(experiment_dir):
+    """Helper function to extract output feature name."""
+    description_file = experiment_dir + '/description.json'
+    with open(description_file, 'rb') as f:
+        content = json.load(f)
+    field_name = content['model_definition']['output_features'][0]['name']
+    return field_name
 
 
 def test_visualisation_learning_curves_output_saved(csv_filename):
@@ -217,3 +227,86 @@ def test_visualisation_compare_performance_output_saved(csv_filename):
             os.remove(file)
         except OSError as e:  # if failed, report it back to the user
             print("Error: %s - %s." % (e.filename, e.strerror))
+
+
+def test_visualisation_compare_classifiers_from_prob_output_saved(csv_filename):
+    """Ensure pdf and png figures from the experiments can be saved."""
+    input_features = [
+        text_feature(vocab_size=10, min_len=1, representation='sparse'),
+        categorical_feature(
+            vocab_size=10,
+            loss='sampled_softmax_cross_entropy'
+        )
+    ]
+    output_features = [categorical_feature(vocab_size=2, reduce_input='sum')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+
+    vis_output_pattern_pdf = exp_dir_name + '/*.pdf'
+    vis_output_pattern_png = exp_dir_name + '/*.png'
+    field_name = get_output_field_name(exp_dir_name)
+    probability = exp_dir_name + '/{}_probabilities.npy'.format(field_name)
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth = experiment_source_data_name + '.hdf5'
+    test_cmd_pdf = ['python',
+                    '-m',
+                    'ludwig.visualize',
+                    '--visualization',
+                    'compare_classifiers_performance_from_prob',
+                    '--ground_truth',
+                    ground_truth,
+                    '--field',
+                    field_name,
+                    '--probabilities',
+                    probability,
+                    probability,
+                    '--model_names',
+                    'Model1',
+                    'Model2',
+                    '-od', exp_dir_name]
+    test_cmd_png = ['python',
+                    '-m',
+                    'ludwig.visualize',
+                    '--visualization',
+                    'compare_classifiers_performance_from_prob',
+                    '--ground_truth',
+                    ground_truth,
+                    '--field',
+                    field_name,
+                    '--probabilities',
+                    probability,
+                    probability,
+                    '--model_names',
+                    'Model1',
+                    'Model2',
+                    '-od', exp_dir_name,
+                    '-ff', 'png']
+
+    commands = [test_cmd_pdf, test_cmd_png]
+    vis_patterns = [vis_output_pattern_pdf, vis_output_pattern_png]
+
+    for command, viz_pattern in zip(commands, vis_patterns):
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        figure_cnt = glob.glob(viz_pattern)
+
+        assert 0 == result.returncode
+        assert 1 == len(figure_cnt)
+
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
+
+
