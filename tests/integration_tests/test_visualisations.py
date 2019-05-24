0c6b5b91b5bed387f1614bd39bde3e57404ef219
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2019-05-24 22:20:59 +0300

    Refactor visualise confidence treshholding data vs acc to test both pdf and png outputs.

--- tests/integration_tests/test_visualisations.py
+++ tests/integration_tests/test_visualisations.py
@@ -656,7 +656,6 @@ def test_visualisation_compare_classifiers_multiclass_multimetric_output_saved(
         assert 0 == result.returncode
         assert 4 == len(figure_cnt)
 
-
     shutil.rmtree(exp_dir_name, ignore_errors=True)
     shutil.rmtree('results', ignore_errors=True)
     for file in glob.glob(experiment_source_data_name + '.*'):
@@ -756,7 +755,7 @@ def test_visualisation_compare_classifiers_predictions_output_saved(
             print("Error: %s - %s." % (e.filename, e.strerror))
 
 
-def test_visualisation_compare_classifiers_predictions_distribution_output_saved(
+def test_visualisation_cmp_classifiers_predictions_distribution_output_saved(
         csv_filename
 ):
     """Ensure pdf and png figures from the experiments can be saved.
@@ -894,6 +893,91 @@ def test_visualisation_cconfidence_thresholding_output_saved(csv_filename):
                     'ludwig.visualize',
                     '--visualization',
                     'confidence_thresholding',
+                    '--ground_truth',
+                    ground_truth,
+                    '--field',
+                    field_name,
+                    '--probabilities',
+                    probability,
+                    probability,
+                    '--model_names',
+                    'Model1',
+                    'Model2',
+                    '-od', exp_dir_name,
+                    '-ff', 'png']
+    commands = [test_cmd_pdf, test_cmd_png]
+    vis_patterns = [vis_output_pattern_pdf, vis_output_pattern_png]
+
+    for command, viz_pattern in zip(commands, vis_patterns):
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        figure_cnt = glob.glob(viz_pattern)
+
+        assert 0 == result.returncode
+        assert 1 == len(figure_cnt)
+
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
+
+
+def test_visualisation_confidence_thresholding_data_vs_acc_output_saved(
+        csv_filename
+):
+    """Ensure pdf and png figures from the experiments can be saved.
+
+    :param csv_filename: csv fixture from tests.fixtures.filenames.csv_filename
+    :return: None
+    """
+    input_features = [
+        text_feature(vocab_size=10, min_len=1, representation='sparse'),
+        categorical_feature(
+            vocab_size=10,
+            loss='sampled_softmax_cross_entropy'
+        )
+    ]
+    output_features = [categorical_feature(vocab_size=2, reduce_input='sum')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+    vis_output_pattern_pdf = exp_dir_name + '/*.pdf'
+    vis_output_pattern_png = exp_dir_name + '/*.png'
+    field_name = get_output_field_name(exp_dir_name)
+    probability = exp_dir_name + '/{}_probabilities.npy'.format(field_name)
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth = experiment_source_data_name + '.hdf5'
+    test_cmd_pdf = ['python',
+                    '-m',
+                    'ludwig.visualize',
+                    '--visualization',
+                    'confidence_thresholding_data_vs_acc',
+                    '--ground_truth',
+                    ground_truth,
+                    '--field',
+                    field_name,
+                    '--probabilities',
+                    probability,
+                    probability,
+                    '--model_names',
+                    'Model1',
+                    'Model2',
+                    '-od', exp_dir_name]
+    test_cmd_png = ['python',
+                    '-m',
+                    'ludwig.visualize',
+                    '--visualization',
+                    'confidence_thresholding_data_vs_acc',
                     '--ground_truth',
                     ground_truth,
                     '--field',
