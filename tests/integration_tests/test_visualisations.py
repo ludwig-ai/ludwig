c69e98ace78ef9a1f76ed644711a482b2f127cd0
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2019-05-24 19:13:03 +0300

    Refactor visualise compare classifiers multiclass multimetric to test both pdf and png outputs.

--- tests/integration_tests/test_visualisations.py
+++ tests/integration_tests/test_visualisations.py
@@ -588,3 +588,79 @@ def test_visualisation_compare_classifiers_changing_k_output_pdf(csv_filename):
             os.remove(file)
         except OSError as e:  # if failed, report it back to the user
             print("Error: %s - %s." % (e.filename, e.strerror))
+
+
+def test_visualisation_compare_classifiers_multiclass_multimetric_output_saved(
+        csv_filename
+):
+    """Ensure pdf and png figures from the experiments can be saved.
+
+    :param csv_filename: csv fixture from tests.fixtures.filenames.csv_filename
+    :return: None
+    """
+    input_features = [
+        text_feature(vocab_size=10, min_len=1, representation='sparse'),
+        categorical_feature(
+            vocab_size=10,
+            loss='sampled_softmax_cross_entropy'
+        )
+    ]
+    output_features = [categorical_feature(vocab_size=2, reduce_input='sum')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+    vis_output_pattern_pdf = exp_dir_name + '/*.pdf'
+    vis_output_pattern_png = exp_dir_name + '/*.png'
+    test_stats = exp_dir_name + '/test_statistics.json'
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth_metadata = experiment_source_data_name + '.json'
+    test_cmd_pdf = ['python',
+                    '-m',
+                    'ludwig.visualize',
+                    '--visualization',
+                    'compare_classifiers_multiclass_multimetric',
+                    '--test_statistics',
+                    test_stats,
+                    test_stats,
+                    '--ground_truth_metadata',
+                    ground_truth_metadata,
+                    '-od', exp_dir_name]
+    test_cmd_png = ['python',
+                    '-m',
+                    'ludwig.visualize',
+                    '--visualization',
+                    'compare_classifiers_multiclass_multimetric',
+                    '--test_statistics',
+                    test_stats,
+                    test_stats,
+                    '--ground_truth_metadata',
+                    ground_truth_metadata,
+                    '-od', exp_dir_name,
+                    '-ff', 'png']
+
+    commands = [test_cmd_pdf, test_cmd_png]
+    vis_patterns = [vis_output_pattern_pdf, vis_output_pattern_png]
+
+    for command, viz_pattern in zip(commands, vis_patterns):
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        figure_cnt = glob.glob(viz_pattern)
+
+        assert 0 == result.returncode
+        assert 4 == len(figure_cnt)
+
+
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
