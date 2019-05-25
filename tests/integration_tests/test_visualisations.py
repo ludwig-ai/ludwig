864bcb2c76090adcd84625f78f96b6478ef6db9d
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2019-05-25 08:55:50 +0300

    Test visualise binary treshhold vs metric saves both pdf and png outputs.

--- tests/integration_tests/test_visualisations.py
+++ tests/integration_tests/test_visualisations.py
@@ -1368,3 +1368,84 @@ def test_vis_confidence_thresholding_2thresholds_3d_output_saved(
             os.remove(file)
         except OSError as e:  # if failed, report it back to the user
             print("Error: %s - %s." % (e.filename, e.strerror))
+
+
+
+def test_visualisation_binary_threshold_vs_metric_output_saved(
+        csv_filename
+):
+    """Ensure pdf and png figures from the experiments can be saved.
+
+    :param csv_filename: csv fixture from tests.fixtures.filenames.csv_filename
+    :return: None
+    """
+    input_features = [
+        text_feature(vocab_size=100, min_len=1, encoder='stacked_cnn'),
+        numerical_feature(),
+        categorical_feature(vocab_size=10, embedding_size=5),
+        set_feature(),
+        sequence_feature(vocab_size=10, max_len=10, encoder='embed')
+    ]
+    output_features = [
+        categorical_feature(vocab_size=2, reduce_input='sum'),
+        sequence_feature(vocab_size=10, max_len=5),
+        numerical_feature()
+    ]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    encoder = 'cnnrnn'
+    input_features[0]['encoder'] = encoder
+    exp_dir_name = run_experiment(input_features, output_features,
+                                  data_csv=rel_path)
+    vis_output_pattern_pdf = exp_dir_name + '/*.pdf'
+    vis_output_pattern_png = exp_dir_name + '/*.png'
+    field_name = get_output_field_name(exp_dir_name)
+    probability = exp_dir_name + '/{}_probabilities.npy'.format(field_name)
+    experiment_source_data_name = csv_filename.split('.')[0]
+    ground_truth = experiment_source_data_name + '.hdf5'
+    ground_truth_metadata = experiment_source_data_name + '.json'
+    test_cmd_pdf =     ['python',
+                        '-m',
+                        'ludwig.visualize',
+                        '--visualization',
+                        'binary_threshold_vs_metric',
+                        '--positive_label',
+                        '2',
+                        '--metrics',
+                        'accuracy',
+                        '--ground_truth',
+                        ground_truth,
+                        '--ground_truth_metadata',
+                        ground_truth_metadata,
+                        '--field',
+                        field_name,
+                        '--probabilities',
+                        probability,
+                        probability,
+                        '--model_names',
+                        'Model1',
+                        'Model2',
+                        '-od', exp_dir_name]
+    test_cmd_png = test_cmd_pdf.copy() + ['-ff', 'png']
+    commands = [test_cmd_pdf, test_cmd_png]
+    vis_patterns = [vis_output_pattern_pdf, vis_output_pattern_png]
+
+    for command, viz_pattern in zip(commands, vis_patterns):
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        figure_cnt = glob.glob(viz_pattern)
+
+        assert 0 == result.returncode
+        assert 1 == len(figure_cnt)
+
+    shutil.rmtree(exp_dir_name, ignore_errors=True)
+    shutil.rmtree('results', ignore_errors=True)
+    for file in glob.glob(experiment_source_data_name + '.*'):
+        try:
+            os.remove(file)
+        except OSError as e:  # if failed, report it back to the user
+            print("Error: %s - %s." % (e.filename, e.strerror))
