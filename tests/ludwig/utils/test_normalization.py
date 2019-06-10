15075a69e633415584c1c21927feed4d4722ceb4
lordeddard <imag.ruthvik@gmail.com>
2019-06-11 01:25:23 +0530

    Added more test for normalization

--- tests/ludwig/utils/test_normalization.py
+++ tests/ludwig/utils/test_normalization.py
@@ -14,31 +14,64 @@
 # limitations under the License.
 # ==============================================================================
 import pandas as pd
+import numpy as np
 
 from ludwig.features.numerical_feature import NumericalBaseFeature
 
 def numerical_feature():
-    return {'name': 'norm_test' , 'type': 'numerical'}
+    return {'name': 'x' , 'type': 'numerical'}
 
-column = pd.Series([
+data_df = pd.DataFrame(pd.Series([
     2,
     4,
     6,
     8,
     10
-])
+]), columns=['x'])
+
+data = pd.DataFrame(pd.Series([
+    2,
+    4,
+    6,
+    8,
+    10
+]), columns=['x'])
 
 feature_1 = NumericalBaseFeature(numerical_feature())
 feature_2 = NumericalBaseFeature(numerical_feature())
 
 def test_norm():
-    assert feature_1.get_feature_meta(
-        column, {'normalization': 'zscore'}
-    )['mean'] == 6
-    assert feature_2.get_feature_meta(
-        column, {'normalization': 'minmax'}
-    )['min'] == 2
-    assert feature_2.get_feature_meta(
-        column, {'normalization': 'minmax'}
-    )['max'] == 10
+    feature_1_meta = feature_1.get_feature_meta(
+        data_df['x'], {'normalization': 'zscore'}
+    )
+    feature_2_meta = feature_1.get_feature_meta(
+        data_df['x'], {'normalization': 'minmax'}
+    )
+    
+    assert feature_1_meta['mean'] == 6
+    assert feature_2_meta['min'] == 2
+    assert feature_2_meta['max'] == 10
+    
+    # value checks after normalization
+    feature_1.add_feature_data(
+        feature=numerical_feature(),
+        dataset_df=data_df,
+        data=data,
+        metadata={'x': feature_1_meta},
+        preprocessing_parameters={'normalization': 'zscore'}
+    )
+    assert np.allclose(np.array(data['x']), 
+        np.array([-1.26491106, -0.63245553,  0,  0.63245553,  1.26491106])
+    )
+
+    feature_2.add_feature_data(
+        feature=numerical_feature(),
+        dataset_df=data_df,
+        data=data,
+        metadata={'x': feature_2_meta},
+        preprocessing_parameters={'normalization': 'minmax'}
+    )
+    assert np.allclose(np.array(data['x']), 
+        np.array([0, 0.25, 0.5 , 0.75, 1])
+    )
 
