47bc4ca4b7b033ae659d5137a46f4ef17ceca434
Piero Molino <piero@uber.com>
2019-07-07 14:10:05 -0700

    More defensive way to obtain the datetime format in generate_datetime

--- ludwig/data/dataset_synthesyzer.py
+++ ludwig/data/dataset_synthesyzer.py
@@ -239,16 +239,20 @@ def generate_image(feature):
 
 
 def generate_datetime(feature):
-    """picking a format among 4 different types:
-    1. 01/22/90 10:00:00
-    2. 02/22/1990
-    3. 1990-22-01
-    4. 22/02/1989 10:45:56
+    """picking a format among different types.
+    If no format is specified, the first one is used.
     """
-
-    datetime_generation_format = DATETIME_FORMATS[
-        feature['preprocessing']['datetime_format']
-    ]
+    if 'datetime_format' in feature:
+        datetime_generation_format = DATETIME_FORMATS[
+            feature['datetime_format']
+        ]
+    elif ('preprocessing' in feature and
+            'datetime_format' in feature['preprocessing']):
+        datetime_generation_format = DATETIME_FORMATS[
+            feature['preprocessing']['datetime_format']
+        ]
+    else:
+        datetime_generation_format = DATETIME_FORMATS[0]
 
     y = random.randint(1, 99)
     Y = random.randint(1, 9999)
