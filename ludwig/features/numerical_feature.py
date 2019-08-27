Fix vector feature loss calculation

ludwig/features/numerical_feature.py
@@ -229,7 +229,8 @@ class NumericalOutputFeature(NumericalBaseFeature, OutputFeature):
                 train_loss = tf.compat.v1.losses.mean_squared_error(
                     labels=targets,
                     predictions=predictions,
-                    reduction=Reduction.NONE)
+                    reduction=Reduction.NONE
+                )
             elif self.loss['type'] == 'mean_absolute_error':
                 train_loss = tf.losses.absolute_difference(
                     labels=targets,

ludwig/features/vector_feature.py
@@ -212,16 +212,14 @@ class VectorOutputFeature(VectorBaseFeature, OutputFeature):
     def vector_loss(self, targets, predictions, logits):
         with tf.variable_scope('loss_{}'.format(self.name)):
             if self.loss['type'] == MEAN_SQUARED_ERROR:
-                squared_error_val = tf.reduce_sum(
+                train_loss = tf.reduce_sum(
                     get_squared_error(targets, predictions, self.name), axis=1
                 )
-                train_loss = tf.reduce_sum(squared_error_val)
 
             elif self.loss['type'] == MEAN_ABSOLUTE_ERROR:
-                absolute_error_val = tf.reduce_sum(
+                train_loss = tf.reduce_sum(
                     get_absolute_error(targets, predictions, self.name), axis=1
                 )
-                train_loss = tf.reduce_sum(absolute_error_val)
 
             elif self.loss['type'] == SOFTMAX_CROSS_ENTROPY:
                 train_loss = weighted_softmax_cross_entropy(