5cab1887c53ebc8de6e94aab6a577037323895e0
Ajay Prakash <ajaypr55@gmail.com>
2019-09-02 02:56:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
                       'spacy>=2.1',
                       'tqdm',
                       'tabulate>=0.7',
-                      'tensorflow==1.14.0'
+                      'tensorflow==1.14.0',
                       'PyYAML>=3.12',
                       'fastapi',
                       'pydantic==0.32.2',
