c002af097f39429d364ed86cef492809f72deafb
Jim Thompson <jimthompson5802@aol.com>
2019-12-23 12:14:37 -0500

    fix: resolve major issue flagged by deepsource.io

--- mkdocs/code_doc_autogen.py
+++ mkdocs/code_doc_autogen.py
@@ -399,15 +399,15 @@ def collect_class_methods(cls, methods):
 
 def render_function(function, method=True):
     subblocks = []
-    signature = get_function_signature(function, method=method)
+    _signature = get_function_signature(function, method=method)
     if method:
-        signature = signature.replace(
+        _signature = _signature.replace(
             clean_module_name(function.__module__) + '.', '')
     subblocks.append('## ' + function.__name__ + '\n')
-    subblocks.append(code_snippet(signature))
-    docstring = function.__doc__
-    if docstring:
-        subblocks.append(process_docstring(docstring))
+    subblocks.append(code_snippet(_signature))
+    _docstring = function.__doc__
+    if _docstring:
+        subblocks.append(process_docstring(_docstring))
     return '\n\n'.join(subblocks)
 
 
