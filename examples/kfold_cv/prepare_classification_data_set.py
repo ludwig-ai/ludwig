dadc8d128103216e0e28347ea91576ae6d004f88
Jim Thompson <jimthompson5802@aol.com>
2020-02-09 21:47:34 -0500

    refactor: code clean-up

--- examples/kfold_cv/prepare_classification_data_set.py
+++ examples/kfold_cv/prepare_classification_data_set.py
@@ -7,7 +7,6 @@
 #
 # Based on the [UCI Wisconsin Breast Cancer data set](https://archive.ics.uci.edu/ml/datasets/breast+cancer+wisconsin+(original))
 #
-import os
 import os.path
 import shutil
 import yaml
