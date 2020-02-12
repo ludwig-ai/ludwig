10f3e95fa68b171698ad7768534ae5f0e71970cc
piero <piero@uber.com>
2020-02-12 14:49:19 -0800

    Updated GPUOption and COnfigProto import to tf.compat.v1

--- ludwig/utils/tf_utils.py
+++ ludwig/utils/tf_utils.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 # ==============================================================================
 import tensorflow as tf
+from tensorflow.compat.v1 import GPUOptions, ConfigProto
 
 
 def sequence_length_3D(sequence):
@@ -52,17 +53,17 @@ def get_tf_config(gpus=None, gpu_fraction=1, horovod=None,
     if gpus is not None:
         if gpu_fraction > 0 and gpu_fraction < 1:
             # this is the source of freezing in tensorflow 1.3.1
-            gpu_options = tf.GPUOptions(
+            gpu_options = GPUOptions(
                 per_process_gpu_memory_fraction=gpu_fraction,
                 allow_growth=True)
         else:
-            gpu_options = tf.GPUOptions(allow_growth=True)
+            gpu_options = GPUOptions(allow_growth=True)
             # allow_growth=True is needed for a weird behavior with CUDA 10
             # https://github.com/tensorflow/tensorflow/issues/24828
         if isinstance(gpus, int):
             gpus = [gpus]
         gpu_options.visible_device_list = ','.join(str(g) for g in gpus)
-        tf_config = tf.ConfigProto(
+        tf_config = ConfigProto(
             allow_soft_placement=True,
             log_device_placement=False,
             intra_op_parallelism_threads=intra_op_parallelism_threads,
@@ -70,12 +71,12 @@ def get_tf_config(gpus=None, gpu_fraction=1, horovod=None,
             gpu_options=gpu_options
         )
     else:
-        tf_config = tf.compat.v1.ConfigProto(
+        tf_config = ConfigProto(
             allow_soft_placement=True,
             log_device_placement=False,
             intra_op_parallelism_threads=intra_op_parallelism_threads,
             inter_op_parallelism_threads=inter_op_parallelism_threads,
-            gpu_options=tf.compat.v1.GPUOptions(allow_growth=True)
+            gpu_options=GPUOptions(allow_growth=True)
         )
 
     if horovod is not None:
