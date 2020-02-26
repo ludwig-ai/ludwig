e1cc736f9bd23337fe3e4ed7f0608eefb3f80326
piero <piero@uber.com>
2020-02-25 16:17:14 -0800

    New test for savedmodel

---
+++ tests/integration_tests/test_savedmodel.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Uber Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import os
+import shutil
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from ludwig.api import LudwigModel
+from ludwig.constants import FULL
+from ludwig.data.preprocessing import preprocess_for_prediction
+from ludwig.globals import TRAIN_SET_METADATA_FILE_NAME
+from tests.integration_tests.utils import category_feature
+from tests.integration_tests.utils import generate_data
+from tests.integration_tests.utils import sequence_feature
+
+
+def test_savedmodel(csv_filename):
+    #######
+    # Setup
+    #######
+    dir_path = os.path.dirname(csv_filename)
+
+    # Single sequence input, single category output
+    sf = sequence_feature()
+    sf['encoder'] = 'parallel_cnn'
+    input_features = [sf]
+    input_feature_name = input_features[0]['name']
+    input_feature_tensor_name = '{}/{}_placeholder:0'.format(
+        input_feature_name,
+        input_feature_name
+    )
+    output_features = [category_feature(vocab_size=2)]
+    output_feature_name = output_features[0]['name']
+    output_feature_tensor_name = '{}/predictions_{}/predictions_{}:0'.format(
+        output_feature_name,
+        output_feature_name,
+        output_feature_name
+    )
+    predictions_column_name = '{}_predictions'.format(output_feature_name)
+    weight_tensor_name = '{}/fc_0/weights:0'.format(input_feature_name)
+
+    # Generate test data
+    data_csv_path = generate_data(input_features, output_features, csv_filename)
+
+    #############
+    # Train model
+    #############
+    model_definition = {
+        'input_features': input_features,
+        'output_features': output_features,
+        'training': {'epochs': 2}
+    }
+    ludwig_model = LudwigModel(model_definition)
+    ludwig_model.train(
+        data_csv=data_csv_path,
+        skip_save_training_description=True,
+        skip_save_training_statistics=True,
+        skip_save_model=True,
+        skip_save_progress=True,
+        skip_save_log=True,
+        skip_save_processed_input=True,
+    )
+    original_predictions_df = ludwig_model.predict(data_csv=data_csv_path)
+
+    ###################
+    # save Ludwig model
+    ###################
+    ludwigmodel_path = os.path.join(dir_path, 'ludwigmodel')
+    shutil.rmtree(ludwigmodel_path, ignore_errors=True)
+    ludwig_model.save(ludwigmodel_path)
+
+    #################
+    # save savedmodel
+    #################
+    savedmodel_path = os.path.join(dir_path, 'savedmodel')
+    shutil.rmtree(savedmodel_path, ignore_errors=True)
+
+    inputs = {}
+    outputs = {}
+
+    for feature in ludwig_model.model_definition['input_features']:
+        inputs[feature['name']] = getattr(ludwig_model.model, feature['name'])
+
+    for feature in ludwig_model.model_definition['output_features']:
+        outputs[feature['name']] = getattr(ludwig_model.model,
+                                           'predictions_' + feature['name'])
+
+    print('=== Inputs ===')
+    print(inputs)
+    print('=== Outputs ===')
+    print(outputs)
+
+    builder = tf.compat.v1.saved_model.builder.SavedModelBuilder(
+        savedmodel_path)
+
+    ludwig_model.model.initialize_session()
+
+    with ludwig_model.model.session as sess:
+        signature = tf.compat.v1.saved_model.signature_def_utils.predict_signature_def(
+            inputs=inputs,
+            outputs=outputs
+        )
+
+        builder.add_meta_graph_and_variables(
+            sess=sess,
+            tags=[tf.saved_model.SERVING],
+            signature_def_map={
+                tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
+                    signature
+            })
+
+        builder.save()
+
+    ludwig_model.close()
+
+    ###################################################
+    # load Ludwig model, obtain predictions and weights
+    ###################################################
+    ludwig_model = LudwigModel.load(ludwigmodel_path)
+    ludwig_prediction_df = ludwig_model.predict(data_csv=data_csv_path)
+    ludwig_weights = ludwig_model.model.collect_weights(
+        [weight_tensor_name]
+    )[weight_tensor_name]
+    ludwig_model.close()
+
+    #################################################
+    # load savedmodel, obtain predictions and weights
+    #################################################
+    train_set_metadata_json_fp = os.path.join(
+        ludwigmodel_path,
+        TRAIN_SET_METADATA_FILE_NAME
+    )
+
+    dataset, train_set_metadata = preprocess_for_prediction(
+        ludwigmodel_path,
+        split=FULL,
+        data_csv=data_csv_path,
+        train_set_metadata=train_set_metadata_json_fp,
+        evaluate_performance=False
+    )
+
+    with tf.compat.v1.Session() as sess:
+        tf.saved_model.loader.load(
+            sess,
+            [tf.saved_model.SERVING],
+            savedmodel_path
+        )
+
+        predictions = sess.run(
+            output_feature_tensor_name,
+            feed_dict={
+                input_feature_tensor_name: dataset.get(input_feature_name),
+            }
+        )
+
+        savedmodel_prediction_df = pd.DataFrame(
+            data=[train_set_metadata[input_feature_name]["idx2str"][p] for p in
+                  predictions], columns=[predictions_column_name])
+
+        savedmodel_weights = sess.run(weight_tensor_name)
+
+    #########
+    # Cleanup
+    #########
+    shutil.rmtree(ludwigmodel_path, ignore_errors=True)
+    shutil.rmtree(savedmodel_path, ignore_errors=True)
+
+    ###############################################
+    # Check if weights and predictions are the same
+    ###############################################
+    print(
+        "Are the weights identical?",
+        np.all(ludwig_weights == savedmodel_weights)
+    )
+    print(
+        "Are loaded model predictions identical to original ones?",
+        np.all(
+            original_predictions_df[predictions_column_name] == \
+            ludwig_prediction_df[predictions_column_name]
+        )
+    )
+    print(
+        "Are the predictions identical?",
+        np.all(
+            ludwig_prediction_df[predictions_column_name] == \
+            savedmodel_prediction_df[predictions_column_name]
+        )
+    )
+
+    assert np.all(ludwig_weights == savedmodel_weights)
+    assert np.all(
+        original_predictions_df[predictions_column_name] == \
+        ludwig_prediction_df[predictions_column_name]
+    )
+    assert np.all(
+        ludwig_prediction_df[predictions_column_name] == \
+        savedmodel_prediction_df[predictions_column_name]
+    )
