77fb94f88baed8a5d32123fdeb4e50236d29fb4e
w4nderlust <w4nderlust@gmail.com>
2020-05-14 18:18:20 -0700

    Added additional integration test for separated input csvs and df to the train API

--- tests/integration_tests/test_api.py
+++ tests/integration_tests/test_api.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+import os
 import shutil
 
 from ludwig.api import LudwigModel
@@ -66,6 +66,93 @@ def run_api_experiment(input_features, output_features, data_csv):
     shutil.rmtree(model.exp_dir_name, ignore_errors=True)
 
 
+def run_api_experiment_separated_datasets(
+        input_features,
+        output_features,
+        data_csv
+):
+    """
+    Helper method to avoid code repetition in running an experiment
+    :param input_features: input schema
+    :param output_features: output schema
+    :param data_csv: path to data
+    :return: None
+    """
+    model_definition = {
+        'input_features': input_features,
+        'output_features': output_features,
+        'combiner': {'type': 'concat', 'fc_size': 14},
+        'training': {'epochs': 2}
+    }
+
+    model = LudwigModel(model_definition)
+
+    # Training with dataframe
+    data_df = read_csv(data_csv)
+    train_df = data_df.sample(frac=0.8)
+    test_df = data_df.drop(train_df.index).sample(frac=0.5)
+    validation_df = data_df.drop(train_df.index).drop(test_df.index)
+
+    train_df.to_csv(data_csv + '.train')
+    validation_df.to_csv(data_csv + '.validation')
+    test_df.to_csv(data_csv + '.test')
+
+    # Training with csv
+    model.train(
+        data_train_csv=data_csv + '.train',
+        skip_save_processed_input=True,
+        skip_save_progress=True,
+        skip_save_unprocessed_output=True
+    )
+    model.train(
+        data_train_csv=data_csv + '.train',
+        data_validation_df=data_csv + '.validation',
+        skip_save_processed_input=True,
+        skip_save_progress=True,
+        skip_save_unprocessed_output=True
+    )
+    model.train(
+        data_train_csv=data_csv + '.train',
+        data_validation_df=data_csv + '.validation',
+        data_test_csv=data_csv + '.test',
+        skip_save_processed_input=True,
+        skip_save_progress=True,
+        skip_save_unprocessed_output=True
+    )
+
+    model.predict(data_csv=data_csv + '.test')
+
+    # Remove results/intermediate data saved to disk
+    os.remove(data_csv + '.train')
+    os.remove(data_csv + '.validation')
+    os.remove(data_csv + '.test')
+
+    shutil.rmtree(model.exp_dir_name, ignore_errors=True)
+
+    model.train(
+        data_train_df=train_df,
+        skip_save_processed_input=True,
+        skip_save_progress=True,
+        skip_save_unprocessed_output=True
+    )
+    model.train(
+        data_train_df=train_df,
+        data_validation_df=validation_df,
+        skip_save_processed_input=True,
+        skip_save_progress=True,
+        skip_save_unprocessed_output=True
+    )
+    model.train(
+        data_train_df=train_df,
+        data_validation_df=validation_df,
+        data_vtest_df=test_df,
+        skip_save_processed_input=True,
+        skip_save_progress=True,
+        skip_save_unprocessed_output=True
+    )
+    model.predict(data_df=data_df)
+    shutil.rmtree(model.exp_dir_name, ignore_errors=True)
+
 def test_api_intent_classification(csv_filename):
     # Single sequence input, single category output
     input_features = [sequence_feature(reduce_output='sum')]
@@ -76,3 +163,17 @@ def test_api_intent_classification(csv_filename):
     for encoder in ENCODERS:
         input_features[0]['encoder'] = encoder
         run_api_experiment(input_features, output_features, data_csv=rel_path)
+
+
+def test_api_intent_classification_separated(csv_filename):
+    # Single sequence input, single category output
+    input_features = [sequence_feature(reduce_output='sum')]
+    output_features = [category_feature(vocab_size=2, reduce_input='sum')]
+
+    # Generate test data
+    rel_path = generate_data(input_features, output_features, csv_filename)
+    for encoder in ENCODERS:
+        input_features[0]['encoder'] = encoder
+        run_api_experiment_separated_datasets(
+            input_features, output_features, data_csv=rel_path
+        )
