fe0245e92961d166fd5a83683571b99f6693abae
w4nderlust <w4nderlust@gmail.com>
2020-06-30 18:24:07 -0700

    Fix: most input features now work in test_model_save_reload_API

--- tests/integration_tests/test_model_training_options.py
+++ tests/integration_tests/test_model_training_options.py
@@ -13,7 +13,8 @@ from ludwig.experiment import full_experiment
 from ludwig.utils.data_utils import split_dataset_tvt, read_csv
 from tests.integration_tests.utils import binary_feature, numerical_feature, \
     category_feature, sequence_feature, date_feature, h3_feature, \
-    set_feature, generate_data, text_feature, vector_feature
+    set_feature, generate_data, text_feature, vector_feature, bag_feature, \
+    image_feature, audio_feature
 
 GeneratedData = namedtuple('GeneratedData',
                            'train_df validation_df test_df')
@@ -262,13 +263,13 @@ def test_model_save_reload_API(csv_filename, tmp_path):
         sequence_feature(vocab_size=3),
         text_feature(vocab_size=3),
         vector_feature(),
-        # image_feature(image_dest_folder),
-        # audio_feature(audio_dest_folder),
+        image_feature(image_dest_folder),
+        audio_feature(audio_dest_folder),
         # timeseries_feature(),
         date_feature(),
         h3_feature(),
         set_feature(vocab_size=3),
-        # bag_feature(vocab_size=3),
+        bag_feature(vocab_size=3),
     ]
 
     output_features = [
