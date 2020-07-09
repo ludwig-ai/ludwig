2cdc90f50ad8609922bbe0be1c8c6ccb54a436d4
w4nderlust <w4nderlust@gmail.com>
2020-07-08 17:52:04 -0700

    Cleanup Neuropod utils with imported constants

--- ludwig/utils/neuropod_utils.py
+++ ludwig/utils/neuropod_utils.py
@@ -9,10 +9,9 @@ import numpy as np
 from ludwig import __file__ as ludwig_path
 from ludwig.api import LudwigModel
 from ludwig.constants import CATEGORY, NUMERICAL, BINARY, SEQUENCE, TEXT, SET, \
-    VECTOR
+    VECTOR, PREDICTIONS, PROBABILITIES
 from ludwig.globals import MODEL_HYPERPARAMETERS_FILE_NAME, \
-    TRAIN_SET_METADATA_FILE_NAME, MODEL_WEIGHTS_FILE_NAME, LUDWIG_VERSION, \
-    MODEL_WEIGHTS_PROGRESS_FILE_NAME
+    TRAIN_SET_METADATA_FILE_NAME, MODEL_WEIGHTS_FILE_NAME, LUDWIG_VERSION
 from ludwig.utils.data_utils import load_json
 from ludwig.utils.print_utils import logging_level_registry, print_ludwig
 
@@ -50,30 +49,30 @@ def postprocess_for_neuropod(predicted, model_definition):
         if feature_type == BINARY:
             postprocessed[feature_name + "_predictions"] = \
                 np.expand_dims(
-                    predicted[feature_name]['predictions'].astype('str'), 1)
+                    predicted[feature_name][PREDICTIONS].astype('str'), 1)
             postprocessed[feature_name + "_probabilities"] = \
                 np.expand_dims(
-                    predicted[feature_name]['probabilities'].astype('float64'),
+                    predicted[feature_name][PROBABILITIES].astype('float64'),
                     1)
         elif feature_type == NUMERICAL:
             postprocessed[feature_name + "_predictions"] = \
                 np.expand_dims(
-                    predicted[feature_name]['predictions'].astype('float64'),
+                    predicted[feature_name][PREDICTIONS].astype('float64'),
                     1)
         elif feature_type == CATEGORY:
             postprocessed[feature_name + "_predictions"] = np.expand_dims(
-                np.array(predicted[feature_name]['predictions'], dtype='str'), 1
+                np.array(predicted[feature_name][PREDICTIONS], dtype='str'), 1
             )
             postprocessed[feature_name + "_probability"] = \
                 np.expand_dims(
                     predicted[feature_name]['probability'].astype('float64'),
                     1)
             postprocessed[feature_name + "_probabilities"] = \
-                predicted[feature_name]['probabilities'].astype('float64')
+                predicted[feature_name][PROBABILITIES].astype('float64')
         elif feature_type == SEQUENCE:
             predictions = list(map(
                 lambda x: ' '.join(x),
-                predicted[feature_name]['predictions']
+                predicted[feature_name][PREDICTIONS]
             ))
             postprocessed[feature_name + "_predictions"] = np.expand_dims(
                 np.array(predictions, dtype='str'), 1
@@ -81,7 +80,7 @@ def postprocess_for_neuropod(predicted, model_definition):
         elif feature_type == TEXT:
             predictions = list(map(
                 lambda x: ' '.join(x),
-                predicted[feature_name]['predictions']
+                predicted[feature_name][PREDICTIONS]
             ))
             postprocessed[feature_name + "_predictions"] = np.expand_dims(
                 np.array(predictions, dtype='str'), 1
@@ -89,7 +88,7 @@ def postprocess_for_neuropod(predicted, model_definition):
         elif feature_type == SET:
             predictions = list(map(
                 lambda x: ' '.join(x),
-                predicted[feature_name]['predictions']
+                predicted[feature_name][PREDICTIONS]
             ))
             postprocessed[feature_name + "_predictions"] = np.expand_dims(
                 np.array(predictions, dtype='str'), 1
@@ -102,13 +101,13 @@ def postprocess_for_neuropod(predicted, model_definition):
                 np.array(probability, dtype='str'), 1
             )
             postprocessed[feature_name + "_probabilities"] = \
-                predicted[feature_name]['probabilities'].astype('float64')
+                predicted[feature_name][PROBABILITIES].astype('float64')
         elif feature_type == VECTOR:
             postprocessed[feature_name + "_predictions"] = \
-                predicted[feature_name]['predictions'].astype('float64')
+                predicted[feature_name][PREDICTIONS].astype('float64')
         else:
             postprocessed[feature_name + "_predictions"] = np.expand_dims(
-                np.array(predicted[feature_name]['predictions'], dtype='str'), 1
+                np.array(predicted[feature_name][PREDICTIONS], dtype='str'), 1
             )
     # print(postprocessed, file=sys.stderr)
     return postprocessed
@@ -149,8 +148,7 @@ def export_neuropod(
         },
     ]
     for filename in os.listdir(ludwig_model_path):
-        if (MODEL_WEIGHTS_FILE_NAME in filename
-                and MODEL_WEIGHTS_PROGRESS_FILE_NAME not in filename):
+        if MODEL_WEIGHTS_FILE_NAME in filename:
             data_paths.append(
                 {
                     "path": os.path.join(
