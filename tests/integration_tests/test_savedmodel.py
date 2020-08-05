9389f0006c160b99e5d3391bdac7356da0eb0b73
Jim Thompson <jimthompson5802@gmail.com>
2020-08-04 20:08:48 -0400

    fix: put in work-around

--- tests/integration_tests/test_savedmodel.py
+++ tests/integration_tests/test_savedmodel.py
@@ -134,7 +134,7 @@ def test_savedmodel(csv_filename):
         {if_name: tf.convert_to_tensor(dataset.dataset[if_name])},
         # todo tf2: next is work-around allow matching function signature by
         # tf2, need to determine if this can be eliminated
-        #{of_name: tf.zeros([num_values, ], dtype=tf.int8)}
+        {of_name: tf.zeros([num_values, ], dtype=tf.int8)}
     )
 
     logits = restored_model(data_to_predict, False, None)
