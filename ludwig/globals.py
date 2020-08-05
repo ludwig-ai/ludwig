3dc523f15d28fff5e2b751ba881afd3788a90db9
w4nderlust <w4nderlust@gmail.com>
2020-08-04 19:46:00 -0700

    Increased version number to 0.3.0-a.2

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -16,7 +16,7 @@
 # ==============================================================================
 from ludwig.utils.horovod_utils import should_use_horovod
 
-LUDWIG_VERSION = '0.3.0-a.1'
+LUDWIG_VERSION = '0.3.0-a.2'
 
 MODEL_WEIGHTS_FILE_NAME = 'model_weights'
 MODEL_HYPERPARAMETERS_FILE_NAME = 'model_hyperparameters.json'
