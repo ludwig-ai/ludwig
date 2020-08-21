9371e3dd7f4ee43c987ae3a5570410e3c4e3b2a7
Jim Thompson <jimthompson5802@gmail.com>
2020-08-21 07:09:26 -0400

    refactor: add explicit parameter for tied, facilitates positive and negative testing

--- tests/integration_tests/test_input_feature_tied.py
+++ tests/integration_tests/test_input_feature_tied.py
@@ -7,33 +7,53 @@ from ludwig.models.ecd import build_inputs
 
 # InputFeatureOptions namedtuple structure:
 # feature_type: input feature type, e.g., numerical, category, etc.
-# feature_options: None or dictionary of required input feature specifications
-#   dictionary can be empty, None indicates not to tie the two input features
-InputFeatureOptions = namedtuple('InputFeatureOptions', 'feature_type feature_options')
+# feature_options: None or dictionary of required input feature specification
+# tie_features: boolean, True to tie features, False not to tie features
+InputFeatureOptions = namedtuple('InputFeatureOptions',
+                                 'feature_type feature_options tie_features')
 
 
 # note: vocab parameter, below, is made up to facilitate creating input encoders
 @pytest.mark.parametrize(
     'input_feature_options',
     [
-        InputFeatureOptions('numerical', {}),
+        # tie input features, encoders should be the same
+        InputFeatureOptions('numerical', None, True),
         InputFeatureOptions(
             'numerical',
-            {'preprocessing': {'normalization': 'zscore'}}
+            {'preprocessing': {'normalization': 'zscore'}},
+            True
         ),
-        InputFeatureOptions('binary', {}),
-        InputFeatureOptions('category', {'vocab': ['a', 'b', 'c']}),
-        InputFeatureOptions('set', {'vocab': ['a', 'b', 'c']}),
-        InputFeatureOptions('sequence', {'vocab': ['x', 'y', 'z']}),
-        InputFeatureOptions('text', {'vocab': ['a', 'b', 'c']}),
-        InputFeatureOptions('timeseries', {'vocab': ['a', 'b', 'c']}),
+        InputFeatureOptions('binary', None, True),
+        InputFeatureOptions('category', {'vocab': ['a', 'b', 'c']}, True),
+        InputFeatureOptions('set', {'vocab': ['a', 'b', 'c']}, True),
+        InputFeatureOptions('sequence', {'vocab': ['x', 'y', 'z']}, True),
+        InputFeatureOptions('text', {'vocab': ['a', 'b', 'c']}, True),
+        InputFeatureOptions('timeseries', {'vocab': ['a', 'b', 'c']}, True),
         InputFeatureOptions(
             'audio',
-            {'embedding_size': 64, 'length': 16, 'vocab': ['a', 'b', 'c']}
+            {'embedding_size': 64, 'length': 16, 'vocab': ['a', 'b', 'c']},
+            True
         ),
 
         # do not tie input features, encoders should be different
-        InputFeatureOptions('numerical', None)
+        InputFeatureOptions('numerical', None, False),
+        InputFeatureOptions(
+            'numerical',
+            {'preprocessing': {'normalization': 'zscore'}},
+            False
+        ),
+        InputFeatureOptions('binary', None, False),
+        InputFeatureOptions('category', {'vocab': ['a', 'b', 'c']}, False),
+        InputFeatureOptions('set', {'vocab': ['a', 'b', 'c']}, False),
+        InputFeatureOptions('sequence', {'vocab': ['x', 'y', 'z']}, False),
+        InputFeatureOptions('text', {'vocab': ['a', 'b', 'c']}, False),
+        InputFeatureOptions('timeseries', {'vocab': ['a', 'b', 'c']}, False),
+        InputFeatureOptions(
+            'audio',
+            {'embedding_size': 64, 'length': 16, 'vocab': ['a', 'b', 'c']},
+            False
+        ),
     ]
 )
 def test_encoder_tied_weights(input_feature_options):
@@ -56,12 +76,12 @@ def test_encoder_tied_weights(input_feature_options):
         input_feature_definitions[1].update(input_feature_options.feature_options)
 
     # add tied option to the second feature
-    if input_feature_options.feature_options is not None:
+    if input_feature_options.tie_features:
         input_feature_definitions[1]['tied'] = 'input_feature_1'
 
     input_features = build_inputs(input_feature_definitions)
 
-    if input_feature_options.feature_options is not None:
+    if input_feature_options.tie_features:
         # should be same encoder
         assert input_features['input_feature_1'].encoder_obj is \
             input_features['input_feature_2'].encoder_obj
