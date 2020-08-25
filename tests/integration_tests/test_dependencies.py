30093824b97894d4360500af2568a8ae08556d2d
Jim Thompson <jimthompson5802@gmail.com>
2020-08-24 23:57:10 -0400

    fix: issue in calculating the expected hidden size

--- tests/integration_tests/test_dependencies.py
+++ tests/integration_tests/test_dependencies.py
@@ -66,19 +66,22 @@ def test_multiple_dependencies(
         'feature_name': other_hidden_layer,
     }
 
+    # setup dummy output feature to be root of dependency list
     num_feature_defn = numerical_feature()
     num_feature_defn['loss'] = {'type': 'mean_squared_error'}
     num_feature_defn['dependencies'] = ['feature_name']
     if len(dependent_hidden_shape) > 2:
         num_feature_defn['reduce_dependencies'] = reduce_dependencies
-        # expected_hidden_size = HIDDEN_SIZE + SEQ_SIZE * OTHER_HIDDEN_SIZE
 
-    if reduce_dependencies == 'concat' and len(dependent_hidden_shape) > 2:
-        expected_hidden_size = HIDDEN_SIZE + OTHER_HIDDEN_SIZE * SEQ_SIZE
+    # Based on specification calculate expected resulting hidden size for
+    # with one dependencies
+    if reduce_dependencies == 'concat' and len(hidden_shape) == 2 and \
+            len(dependent_hidden_shape) == 3:
+            expected_hidden_size = HIDDEN_SIZE + OTHER_HIDDEN_SIZE * SEQ_SIZE
     else:
         expected_hidden_size = HIDDEN_SIZE + OTHER_HIDDEN_SIZE
 
-    # set up if multiple dependencies specified
+    # set up if multiple dependencies specified, setup second depdendent feature
     if dependent_hidden_shape2:
         other_hidden_layer2 = tf.random.normal(
             dependent_hidden_shape2,
@@ -88,10 +91,12 @@ def test_multiple_dependencies(
         num_feature_defn['dependencies'].append('feature_name2')
         if len(dependent_hidden_shape2) > 2:
             num_feature_defn['reduce_dependencies'] = reduce_dependencies
-            # expected_hidden_size += SEQ_SIZE * OTHER_HIDDEN_SIZE2
 
-        if reduce_dependencies == 'concat' and len(dependent_hidden_shape2) > 2:
-            expected_hidden_size += OTHER_HIDDEN_SIZE2 * SEQ_SIZE
+        # Based on specification calculate marginal increase in resulting
+        # hidden size with two dependencies
+        if reduce_dependencies == 'concat' and len(hidden_shape) == 2 and \
+                len(dependent_hidden_shape2) == 3:
+                expected_hidden_size += OTHER_HIDDEN_SIZE2 * SEQ_SIZE
         else:
             expected_hidden_size += OTHER_HIDDEN_SIZE2
 
@@ -102,6 +107,7 @@ def test_multiple_dependencies(
         other_dependencies
     )
 
+    # confirm size of resutling concat_dependencies() call
     if len(hidden_shape) > 2:
         assert results.shape.as_list() == \
                [BATCH_SIZE, SEQ_SIZE, expected_hidden_size]
