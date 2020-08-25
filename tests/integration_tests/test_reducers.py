50335ece04e920131f3243fabdcc5da1cff2ab67
Jim Thompson <jimthompson5802@gmail.com>
2020-08-24 22:18:06 -0400

    refactor: removed dependencies reduction test

--- tests/integration_tests/test_reducers.py
+++ tests/integration_tests/test_reducers.py
@@ -21,22 +21,4 @@ def test_reduction(reduce_output, csv_filename):
     run_experiment(input_features, output_features, data_csv=rel_path)
 
 
-def test_dependencies_reduction(csv_filename):
-    input_features = [
-        sequence_feature(reduce_output=None)
-    ]
-
-    output_features = [
-        category_feature(reduce_input=None, reduce_dependencies='sum'),
-        category_feature(reduce_input=None, reduce_dependencies='sum')
-    ]
-    feature1_name = output_features[0]['name']
-    output_features[1]['dependencies'] = [feature1_name]
-
-    rel_path = generate_data(input_features, output_features, csv_filename)
-    run_experiment(input_features, output_features, data_csv=rel_path)
-
-
-
-
 
