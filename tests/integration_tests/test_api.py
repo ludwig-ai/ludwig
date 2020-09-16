38af6be511505b2740b455ebc2664ad5b6b7df5e
Travis Addair <taddair@uber.com>
2020-09-16 16:57:53 -0700

    Added back LudwigModel.train_online

--- tests/integration_tests/test_api.py
+++ tests/integration_tests/test_api.py
@@ -207,3 +207,27 @@ def test_api_intent_classification_separated(csv_filename):
         run_api_experiment_separated_datasets(
             input_features, output_features, data_csv=rel_path
         )
+
+
+def test_api_train_online(csv_filename):
+    input_features = [sequence_feature(reduce_output='sum')]
+    output_features = [category_feature(vocab_size=2, reduce_input='sum')]
+    data_csv = generate_data(input_features, output_features, csv_filename)
+
+    model_definition = {
+        'input_features': input_features,
+        'output_features': output_features,
+        'combiner': {'type': 'concat', 'fc_size': 14},
+    }
+    model = LudwigModel(model_definition)
+
+    output_dir = None
+    try:
+        for i in range(2):
+            model.train_online(
+                dataset=data_csv,
+            )
+        model.predict(dataset=data_csv)
+    finally:
+        # Remove results/intermediate data saved to disk
+        shutil.rmtree(output_dir, ignore_errors=True)
