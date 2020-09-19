ae50c32f5a483a33338dd85671745d02423af094
Travis Addair <taddair@uber.com>
2020-09-19 16:21:47 -0700

    Error message

--- ludwig/data/preprocessing.py
+++ ludwig/data/preprocessing.py
@@ -491,7 +491,7 @@ def preprocess_for_training(
                                      shuffle_training=False,
                                      **kwargs)
         else:
-            raise ValueError('either data or data_train have to be not None')
+            raise ValueError('One of `dataset` or `training_set` must be not None')
 
     elif data_format in DICT_FORMATS:
         num_overrides = override_in_memory_flag(
