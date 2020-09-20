b594c675c9e1a2d78e9cad6ce9b1a1fb29fbc9f3
Jim Thompson <jimthompson5802@gmail.com>
2020-09-19 21:23:50 -0400

    fix: convergence issue by randomizing training data

--- examples/mnist/simple_model_training.py
+++ examples/mnist/simple_model_training.py
@@ -7,11 +7,12 @@
 # (https://ludwig-ai.github.io/ludwig-docs/examples/#image-classification-mnist).
 
 # Import required libraries
-
+import os
 import logging
 import shutil
 
 import yaml
+import pandas as pd
 
 from ludwig.api import LudwigModel
 
@@ -27,14 +28,24 @@ with open('./model_definition.yaml','r') as f:
 model = LudwigModel(model_definition,
                     logging_level=logging.INFO)
 
+current_working_directory = os.getcwd()
+
+# read in data set and randomize sequence of records
+train_df = pd.read_csv('./data/mnist_dataset_training.csv')
+train_df['image_path'] = current_working_directory + '/data/' + train_df['image_path']
+train_df = train_df.sample(train_df.shape[0])
+
+test_df = pd.read_csv('./data/mnist_dataset_testing.csv')
+test_df['image_path'] = current_working_directory + '/data/' + test_df['image_path']
+
 # initiate model training
 (
     train_stats,  #training statistics
     _,
     output_directory  # location for training results saved to disk
 ) = model.train(
-    training_set='./data/mnist_dataset_training.csv',
-    test_set='./data/mnist_dataset_testing.csv',
+    training_set=train_df,
+    test_set=test_df,
     experiment_name='simple_image_experiment',
     model_name='single_model',
     skip_save_processed_input=True
