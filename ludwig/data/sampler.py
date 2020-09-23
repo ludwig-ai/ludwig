0fbcc0b06ba085a0f341630ac64aae353edaed00
Travis Addair <taddair@uber.com>
2020-09-23 16:14:53 -0700

    Replaced param with dataset_size

--- ludwig/data/sampler.py
+++ ludwig/data/sampler.py
@@ -22,12 +22,12 @@ import numpy as np
 
 class DistributedSampler:
     """Adapted from `torch.utils.data.distributed.DistributedSampler`."""
-    def __init__(self, dataset, shuffle=True, seed=0, horovod=None):
-        self.dataset = dataset
+    def __init__(self, dataset_size, shuffle=True, seed=0, horovod=None):
+        self.dataset_size = dataset_size
         self.num_replicas = horovod.size() if horovod else 1
         self.rank = horovod.rank() if horovod else 0
         self.epoch = 0
-        self.num_samples = int(math.ceil(len(self.dataset) * 1.0 / self.num_replicas))
+        self.num_samples = int(math.ceil(self.dataset_size * 1.0 / self.num_replicas))
         self.total_size = self.num_samples * self.num_replicas
         self.shuffle = shuffle
         self.seed = seed
@@ -36,9 +36,9 @@ class DistributedSampler:
         if self.shuffle:
             # deterministically shuffle based on epoch and seed
             indices = np.random.RandomState(seed=self.seed + self.epoch)\
-                .permutation(len(self.dataset)).tolist()
+                .permutation(self.dataset_size).tolist()
         else:
-            indices = list(range(len(self.dataset)))
+            indices = list(range(self.dataset_size))
 
         # add extra samples to make it evenly divisible
         indices += indices[:(self.total_size - len(indices))]
