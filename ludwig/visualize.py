fab18eae6d83d38ed496b59bbe6559445226f419
Jim Thompson <jimthompson5802@gmail.com>
2020-09-26 19:25:24 -0400

    doc: docstring review calibration_multiclass

--- ludwig/visualize.py
+++ ludwig/visualize.py
@@ -2715,27 +2715,32 @@ def calibration_1_vs_all(
 
 
 def calibration_multiclass(
-        probabilities_per_model,
-        ground_truth,
-        labels_limit,
-        model_names=None,
-        output_directory=None,
-        file_format='pdf',
+        probabilities_per_model: List[np.array],
+        ground_truth: np.array,
+        labels_limit: int,
+        model_names: str = None,
+        output_directory: str = None,
+        file_format: str = 'pdf',
         **kwargs
-):
-    """Show models probability of predictions for each class of the the
+) -> None:
+    """Show models probability of predictions for each class of the
     specified output_feature_name.
 
     # Inputs
 
-    :param probabilities_per_model: (list) List of model probabilities
-    :param ground_truth: (ndarray) NumPy Array containing ground truth data
-    :param labels_limit: (int) Maximum numbers of labels.
-             If labels in dataset are higher than this number, "rare" label
-    :param model_names: (list, default: None) List of the names of the models to use as labels.
-    :param output_directory: (string, default: None) Directory where to save plots.
-             If not specified, plots will be displayed in a window
-    :param file_format: (string, default: 'pdf') File format of output plots - pdf or png
+    :param probabilities_per_model: (List[numpy.array]) list of model
+        probabilities.
+    :param ground_truth: (numpy.array) numpy.array containing ground truth data,
+        which are the numeric encoded values the category.
+    :param labels_limit: (int) upper limit on the numeric encoded label value.
+        Encoded numeric label values in dataset that are higher than
+        `label_limit` are considered to be "rare" labels.
+    :param model_names: (List[str], default: `None`) list of the names of the
+        models to use as labels.
+    :param output_directory: (str, default: `None`) directory where to save
+        plots. If not specified, plots will be displayed in a window
+    :param file_format: (str, default: `'pdf'`) file format of output plots -
+        `'pdf'` or `'png'`.
 
     # Return
 
