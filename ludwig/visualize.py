60545300633b4046f934b987bccc202dd37e1723
Jim Thompson <jimthompson5802@gmail.com>
2020-09-30 07:30:28 -0400

    doc: docstring review calibration_1_vs_all_cli

--- ludwig/visualize.py
+++ ludwig/visualize.py
@@ -742,19 +742,27 @@ def roc_curves_from_test_statistics_cli(
 
 
 def calibration_1_vs_all_cli(
-        probabilities,
-        ground_truth,
-        ground_truth_split,
-        output_feature_name,
-        **kwargs
-):
+        probabilities: Union[str, List[str]],
+        ground_truth: str,
+        ground_truth_split: int,
+        output_feature_name: str,
+        **kwargs: dict
+) -> None:
     """Load model data from files to be shown by calibration_1_vs_all_cli.
 
-    :param probabilities: Path to experiment probabilities file
-    :param ground_truth: Path to ground truth file
-    :param ground_truth_split: Type of ground truth split - train, val, test
-    :param output_feature_name: Name of the output feature to visualize
-    :param kwargs: model configuration arguments
+    # Inputs
+
+    :param probabilities: (Union[str, List[str]]) path to experiment
+        probabilities file
+    :param ground_truth: (str) path to ground truth file
+    :param ground_truth_split: (str) type of ground truth split -
+        `0` for training split, `1` for validation split or
+        2 for `'test'` split.
+    :param output_feature_name: (str) name of the output feature to visualize.
+    :param kwargs: (dict) parameters for the requested visualizations.
+
+    # Return
+
     :return None:
     """
     gt = load_from_file(ground_truth, output_feature_name, ground_truth_split)
