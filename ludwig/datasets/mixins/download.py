67810696dbc1dc1a7e39873a4a075a4b955f9b83
ANarayan <gostanfordtennis@gmail.com>
2020-10-26 21:49:20 -0700

    added fever and goemotions and new mixins

--- ludwig/datasets/mixins/download.py
+++ ludwig/datasets/mixins/download.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 # ==============================================================================
 import os
+import wget
 import tempfile
 
 from io import BytesIO
@@ -35,12 +36,35 @@ class ZipDownloadMixin:
         store that in the cache location.
         """
         os.makedirs(self.raw_temp_path, exist_ok=True)
-        with urlopen(self.download_url) as zipresp:
-            with ZipFile(BytesIO(zipresp.read())) as zfile:
-                zfile.extractall(self.raw_temp_path)
+        for url in self.download_urls:
+            with urlopen(url) as zipresp:
+                with ZipFile(BytesIO(zipresp.read())) as zfile:
+                    zfile.extractall(self.raw_temp_path)
+        os.rename(self.raw_temp_path, self.raw_dataset_path)
+
+    @property
+    def download_urls(self):
+        return self.config["download_urls"]
+
+class UncompressedFileDownloadMixin:
+    """Downloads the json file containing the training data and extracts the contents."""
+
+    config: dict
+    raw_dataset_path: str
+    raw_temp_path: str
+
+    def download_raw_dataset(self):
+        """
+        Download the raw dataset files and store in the cache location.
+        """
+        os.makedirs(self.raw_temp_path, exist_ok=True)
+        for url in self.download_url:
+            wget.download(url, out=self.raw_temp_path)
 
         os.rename(self.raw_temp_path, self.raw_dataset_path)
 
     @property
     def download_url(self):
-        return self.config["download_url"]
+        return self.config["download_urls"]
+
+
