35c54b5ea79dccb4cdd85fd04cc507018b63d17d
Jim Thompson <jimthompson5802@gmail.com>
2020-11-02 17:32:24 -0500

    refactor: update test to support _split.csv file

--- ludwig/visualize.py
+++ ludwig/visualize.py
@@ -982,6 +982,8 @@ def binary_threshold_vs_metric_cli(
         probabilities: Union[str, List[str]],
         ground_truth: str,
         ground_truth_split: int,
+        split_file: str,
+        ground_truth_metadata: str,
         output_feature_name: str,
         **kwargs: dict
 ) -> None:
@@ -995,6 +997,9 @@ def binary_threshold_vs_metric_cli(
     :param ground_truth_split: (str) type of ground truth split -
         `0` for training split, `1` for validation split or
         2 for `'test'` split.
+    :param split_file: (str, None) file path to csv file containing split values
+    :param ground_truth_metadata: (str) file path to feature metadata json file
+        created during training.
     :param output_feature_name: (str) name of the output feature to visualize.
     :param kwargs: (dict) parameters for the requested visualizations.
 
@@ -1002,12 +1007,26 @@ def binary_threshold_vs_metric_cli(
 
     :return None:
     """
-    gt = load_from_file(ground_truth, output_feature_name, ground_truth_split)
+
+    # retrieve feature metadata to convert raw predictions to encoded value
+    metadata = load_json(ground_truth_metadata)
+
+    # retrieve ground truth from source data set
+    ground_truth = _extract_ground_truth_values(
+        ground_truth,
+        output_feature_name,
+        ground_truth_split,
+        split_file
+    )
+    feature_metadata = metadata[output_feature_name]
+    vfunc = np.vectorize(_encode_categorical_feature)
+    ground_truth = vfunc(ground_truth, feature_metadata['str2idx'])
+
     probabilities_per_model = load_data_for_viz(
         'load_from_file', probabilities, dtype=float
     )
     binary_threshold_vs_metric(
-        probabilities_per_model, gt, **kwargs
+        probabilities_per_model, ground_truth, **kwargs
     )
 
 
