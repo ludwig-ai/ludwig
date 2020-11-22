bab3392c0a14581466fbd0bf8caa2a0e4a7e1d55
Saikat Kanjilal <sxk1969@hotmail.com>
2020-11-22 12:42:54 -0800

    next batch of changes

--- ludwig/datasets/titanic/__init__.py
+++ ludwig/datasets/titanic/__init__.py
@@ -19,7 +19,8 @@ import pandas as pd
 from ludwig.datasets.base_dataset import BaseDataset, DEFAULT_CACHE_LOCATION
 from ludwig.datasets.mixins.kaggle import KaggleMixin
 from ludwig.datasets.mixins.load import CSVLoadMixin
-from ludwig.datasets.mixins.process import IdentityProcessMixin
+
+NUM_LABELS = 10
 
 
 def load(cache_dir=DEFAULT_CACHE_LOCATION, split=False):
@@ -27,39 +28,26 @@ def load(cache_dir=DEFAULT_CACHE_LOCATION, split=False):
     return dataset.load(split=split)
 
 
-class Titanic(IdentityProcessMixin, KaggleMixin, CSVLoadMixin, BaseDataset):
-    """The Titanic dataset.
+class Titanic(CSVLoadMixin, KaggleMixin, BaseDataset):
+    """The Mnist dataset.
 
-    This dataset is constructed using the kaggle API.
-    For a detailed description of the dataset see here: https://www.kaggle.com/c/titanic-dataset/data
-
-    Like some of the other datasets this dataset is broken up into a titanic_train.csv
-    and a titanic_test.csv.
-
-    Here is a sample dataset structure:
-
-    passenger_id,pclass,name,sex,age,sibsp,parch,ticket,fare,cabin,embarked,boat,body,home.dest,survived
-    1216,3,"Smyth, Miss. Julia",female,,0,0,335432,7.7333,,Q,13,,,1
-    699,3,"Cacic, Mr. Luka",male,38.0,0,0,315089,8.6625,,S,,,Croatia,0
-    1267,3,"Van Impe, Mrs. Jean Baptiste (Rosalie Paula Govaert)",female,30.0,1,1,345773,24.15,,S,,,,0
-    449,2,"Hocking, Mrs. Elizabeth (Eliza Needs)",female,54.0,1,3,29105,23.0,,S,4,,"Cornwall / Akron, OH",1
-    576,2,"Veal, Mr. James",male,40.0,0,0,28221,13.0,,S,,,"Barre, Co Washington, VT",0
-
-    In the construction of this dataset, we again combine the train and the test datasets into 1 dataset
-    and add a split column to identify whether the dataset is a train or test dataset
-
-    This class pulls in an array of mixins for different types of functionality
-    which belongs in the workflow for ingesting and transforming
-    training data into a destination dataframe that can be use by Ludwig.
+    This pulls in an array of mixins for different types of functionality
+    which belongs in the workflow for ingesting and transforming training data into a destination
+    dataframe that can fit into Ludwig's training API.
     """
 
-    def __init__(self, cache_dir=DEFAULT_CACHE_LOCATION):
-        super().__init__(dataset_name='titanic', cache_dir=cache_dir)
+    config: dict
+    raw_temp_path: str
+    raw_dataset_path: str
+    processed_temp_path: str
+    processed_dataset_path: str
 
-    def output_training_and_test_data(self):
-        """The final method where we create a training and test file by iterating through
-        both of these files
-        """
+    def __init__(self, cache_dir=DEFAULT_CACHE_LOCATION):
+        super().__init__(dataset_name="titanic", cache_dir=cache_dir)
+
+    def process_downloaded_dataset(self):
+        """ The final method where we create a training and test file by iterating through
+        both of these files"""
         train_df = pd.read_csv(os.path.join(self.processed_temp_path, "titanic_train.csv"))
         test_df = pd.read_csv(os.path.join(self.processed_temp_path, "titanic_test.csv"))
         train_df["split"] = 0
@@ -67,5 +55,6 @@ class Titanic(IdentityProcessMixin, KaggleMixin, CSVLoadMixin, BaseDataset):
         final_df = pd.concat([train_df, train_df], axis=1)
         final_df.to_csv(self.processed_dataset_path)
 
-
-
+    @property
+    def download_url(self):
+        return self.config["download_url"]
---
+++ tests/ludwig/datasets/titanic/test_titanic_workflow.py
@@ -0,0 +1,76 @@
+import gzip
+import os
+import shutil
+import tempfile
+import pandas as pd
+from unittest import mock
+
+from ludwig.datasets.titanic import Titanic
+
+
+class FakeTitanicDataset(Titanic):
+    def __init__(self, cache_dir=None):
+        super().__init__(cache_dir=cache_dir)
+
+
+def test_download_titanic_dataset():
+    titanic_train_df = pd.DataFrame({
+        'passenger_id': [1216, 699, 234],
+        'pclass': [3, 3, 4],
+        'name': ['sai bo', 'bo staff', 'tae kwan nic'],
+        'sex': ["female", "male", "male"],
+        'age': [38, 28, 18],
+        'sibsp': [0,1,0],
+        'parch': [1,1,2],
+        'ticket': [335432, 315089, 322472],
+        'fare': [7.7333, 8.6625, 9.8765],
+        'cabin': [1, 2, 4],
+        'embarked': ["C", "Q", "S"],
+        'boat': [0, 0, 0],
+        'body': [0, 1],
+        'home.dest': ["Croatia", "Italy","Sweden"],
+        'survived': [0, 1, 0]
+    })
+
+    titanic_test_df = pd.DataFrame({
+        'passenger_id': [1216, 699, 234],
+        'pclass': [3, 3, 4],
+        'name': ['mo bo', 'bo bo bo', 'Rafael Nadal'],
+        'sex': ["female", "male", "male"],
+        'age': [28, 18, 30],
+        'sibsp': [0, 1, 0],
+        'parch': [1, 1, 2],
+        'ticket': [335412, 215089, 922472],
+        'fare': [17.7333, 18.6625, 19.8765],
+        'cabin': [2, 2, 1],
+        'embarked': ["Q", "Q", "C"],
+        'boat': [0, 0, 0],
+        'body': [0, 1],
+        'home.dest': ["Sweden", "Slovenia", "Italy"],
+        'survived': [0, 1, 0]
+    })
+    titanic_train_filename = "titanic_train.csv"
+    titanic_test_filename = "titanic_test.csv"
+    with tempfile.TemporaryDirectory() as source_dir:
+        archive_filename = os.path.join(source_dir, 'archive.zip')
+        titanic_train_df.to_csv(index=False)
+        titanic_test_df.to_csv(index=False)
+
+        config = {
+            'version': 1.0,
+            'split_filenames': {
+                'train_file': titanic_train_filename,
+                'test_file': titanic_test_filename
+            },
+            'csv_filename': 'fake_titanic.csv',
+        }
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with mock.patch('ludwig.datasets.base_dataset.read_config',
+                            return_value=config):
+                dataset = FakeTitanicDataset(tmpdir)
+                assert not dataset.is_downloaded()
+                assert not dataset.is_processed()
+                dataset.download()
+
+                assert dataset.is_downloaded()
