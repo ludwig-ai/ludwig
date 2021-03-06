214f7185b1c795bcc7068d4256a61e83e1e1688c
Travis Addair <tgaddair@gmail.com>
2021-03-06 13:51:34 -0800

    Test ray only

--- tests/integration_tests/test_ray.py
+++ tests/integration_tests/test_ray.py
@@ -120,7 +120,7 @@ def run_test_parquet(
             run_fn(config, data_parquet=dataset_parquet)
 
 
-@pytest.mark.distributed
+@pytest.mark.ray
 def test_ray_tabular():
     input_features = [
         sequence_feature(reduce_output='sum'),
