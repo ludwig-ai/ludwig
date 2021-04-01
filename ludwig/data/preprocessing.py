6bb9c7e187b29c907dc3fdb38d7e14fd1706aa14
Hao Zhang <zhisbug@users.noreply.github.com>
2021-03-31 22:38:02 -0400

    check dask dataframe at preprocessing (#1133)

--- ludwig/data/preprocessing.py
+++ ludwig/data/preprocessing.py
@@ -1095,7 +1095,8 @@ def build_metadata(dataset_df, features, global_preprocessing_parameters,
             fill_value = precompute_fill_value(
                 dataset_df,
                 feature,
-                preprocessing_parameters
+                preprocessing_parameters,
+                backend
             )
             if fill_value is not None:
                 preprocessing_parameters = {
@@ -1161,7 +1162,7 @@ def build_data(input_df, features, training_set_metadata, backend):
     return proc_df
 
 
-def precompute_fill_value(dataset_df, feature, preprocessing_parameters):
+def precompute_fill_value(dataset_df, feature, preprocessing_parameters, backend):
     missing_value_strategy = preprocessing_parameters['missing_value_strategy']
     if missing_value_strategy == FILL_WITH_CONST:
         return preprocessing_parameters['fill_value']
@@ -1173,8 +1174,7 @@ def precompute_fill_value(dataset_df, feature, preprocessing_parameters):
                 'Filling missing values with mean is supported '
                 'only for numerical types',
             )
-        return dataset_df[feature[COLUMN]].mean()
-
+        return backend.df_engine.compute(dataset_df[feature[COLUMN]].mean())
     # Otherwise, we cannot precompute the fill value for this dataset
     return None
 
