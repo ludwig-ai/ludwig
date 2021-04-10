f96b2707e60728e19866e0c9985fd7d20d924821
Nimit Sohoni <nims@stanford.edu>
2021-04-09 23:43:49 -0700

    Add timeseries datasets; minor cleanup

--- ludwig/datasets/yosemite/__init__.py
+++ ludwig/datasets/yosemite/__init__.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # coding=utf-8
-# Copyright (c) 2019 Uber Technologies, Inc.
+# Copyright (c) 2021 Uber Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,27 +17,19 @@
 from ludwig.datasets.base_dataset import BaseDataset, DEFAULT_CACHE_LOCATION
 from ludwig.datasets.mixins.download import UncompressedFileDownloadMixin
 from ludwig.datasets.mixins.load import CSVLoadMixin
-from ludwig.datasets.mixins.process import *
+from ludwig.datasets.mixins.process import IdentityProcessMixin
 
 
-def load(cache_dir=DEFAULT_CACHE_LOCATION, split=True):
-    dataset = AGNews(cache_dir=cache_dir)
+def load(cache_dir=DEFAULT_CACHE_LOCATION, split=False):
+    dataset = Yosemite(cache_dir=cache_dir)
     return dataset.load(split=split)
 
 
-class AGNews(UncompressedFileDownloadMixin, MultifileJoinProcessMixin,
-                 CSVLoadMixin, BaseDataset):
-    """The AGNews dataset"""
+class Yosemite(UncompressedFileDownloadMixin, IdentityProcessMixin,
+               CSVLoadMixin, BaseDataset):
+    """Yosemite temperatures dataset.
+
+    As found in https://github.com/ourownstory/neural_prophet.
+    """
     def __init__(self, cache_dir=DEFAULT_CACHE_LOCATION):
-        super().__init__(dataset_name="agnews", cache_dir=cache_dir)
-
-    def read_file(self, filetype, filename, header=0):
-        file_df = pd.read_csv(
-                os.path.join(self.raw_dataset_path, filename))
-        # class_index : number between 1-4 where 
-        # 1-World, 2-Sports, 3-Business, 4-Science/Tech
-        file_df.columns = ['class_index', 'title', 'description']
-        return file_df
-
-
-    
+        super().__init__(dataset_name="yosemite", cache_dir=cache_dir)
