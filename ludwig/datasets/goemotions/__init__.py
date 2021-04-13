c43bb46e8a6702b78cfdaff8a7084e91ae114c9d
Kanishk <36276423+kanishk16@users.noreply.github.com>
2021-04-13 19:40:48 +0530

    set split=True for goemotions datatset

--- ludwig/datasets/goemotions/__init__.py
+++ ludwig/datasets/goemotions/__init__.py
@@ -20,7 +20,7 @@ from ludwig.datasets.mixins.load import CSVLoadMixin
 from ludwig.datasets.mixins.process import *
 
 
-def load(cache_dir=DEFAULT_CACHE_LOCATION, split=False):
+def load(cache_dir=DEFAULT_CACHE_LOCATION, split=True):
     dataset = GoEmotions(cache_dir=cache_dir)
     return dataset.load(split=split)
 
