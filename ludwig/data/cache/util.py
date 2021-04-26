3d535428171eb630d9dc4d3dee819cc21543c1bf
Travis Addair <tgaddair@gmail.com>
2021-04-26 15:14:33 -0700

    Refactor

--- ludwig/data/cache/util.py
+++ ludwig/data/cache/util.py
@@ -5,16 +5,17 @@ from ludwig.utils.misc_utils import hash_dict
 
 
 def calculate_checksum(original_dataset, config):
-    info = {}
-    info['ludwig_version'] = ludwig.globals.LUDWIG_VERSION
-    info['dataset_checksum'] = checksum(original_dataset)
-    info['global_preprocessing'] = config['preprocessing']
     features = config.get('input_features', []) + \
                config.get('output_features', []) + \
                config.get('features', [])
-    info['feature_names'] = [feature[NAME] for feature in features]
-    info['feature_types'] = [feature[TYPE] for feature in features]
-    info['feature_preprocessing'] = [feature.get(PREPROCESSING, {})
-                                     for feature in features]
-    hash = hash_dict(info, max_length=None)
-    return hash.decode('ascii')
+    info = {
+        'ludwig_version': ludwig.globals.LUDWIG_VERSION,
+        'dataset_checksum': checksum(original_dataset),
+        'global_preprocessing': config['preprocessing'],
+        'feature_names': [feature[NAME] for feature in features],
+        'feature_types': [feature[TYPE] for feature in features],
+        'feature_preprocessing': [
+            feature.get(PREPROCESSING, {}) for feature in features
+        ],
+    }
+    return hash_dict(info, max_length=None).decode('ascii')
