4e2f14c69ea91eaeb7db7cbab662556fe04aa671
Travis Addair <tgaddair@gmail.com>
2021-05-29 22:29:08 -0700

    Reuse existing experiments

--- ludwig/contribs/mlflow.py
+++ ludwig/contribs/mlflow.py
@@ -12,17 +12,24 @@ except Exception as e:
 logger = logging.getLogger(__name__)
 
 
+def _get_or_create_experiment_id(experiment_name):
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    if experiment is not None:
+        return experiment.experiment_id
+    return mlflow.create_experiment(name=experiment_name)
+
+
 class MlflowCallback(Callback):
     def __init__(self):
         self.experiment_id = None
 
     def on_hyperopt_init(self, experiment_name):
-        self.experiment_id = mlflow.create_experiment(experiment_name)
+        self.experiment_id = _get_or_create_experiment_id(experiment_name)
 
     def on_train_init(self, experiment_name, **kwargs):
         if self.experiment_id is not None:
             return
-        self.experiment_id = mlflow.create_experiment(experiment_name)
+        self.experiment_id = _get_or_create_experiment_id(experiment_name)
 
     def on_train_start(self, config, **kwargs):
         config_flattened = json_normalize(config)
