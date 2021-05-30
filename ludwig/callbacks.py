509cbc0e4600b0161406b530c0dd50ad97866ea1
Travis Addair <tgaddair@gmail.com>
2021-05-30 12:51:10 -0700

    Added hparam

--- ludwig/callbacks.py
+++ ludwig/callbacks.py
@@ -25,6 +25,9 @@ class Callback(ABC):
     def on_hyperopt_init(self, experiment_name):
         pass
 
+    def on_hyperopt_trial_start(self, parameters):
+        pass
+
     def on_train_init(
         self,
         base_config,
