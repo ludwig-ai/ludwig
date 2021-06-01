515c5a56b0c7f141f05804242e6498283543a815
Travis Addair <tgaddair@gmail.com>
2021-06-01 11:13:02 -0700

    More tests

--- tests/integration_tests/test_cache_manager.py
+++ tests/integration_tests/test_cache_manager.py
@@ -11,8 +11,9 @@ from ludwig.data.dataset import PandasDatasetManager
 from tests.integration_tests.utils import sequence_feature, category_feature, LocalTestBackend
 
 
+@pytest.mark.parametrize('use_split', [True, False], ids=['split', 'no_split'])
 @pytest.mark.parametrize('use_cache_dir', [True, False], ids=['cache_dir', 'no_cache_dir'])
-def test_cache_dataset(use_cache_dir, tmpdir):
+def test_cache_dataset(use_cache_dir, use_split, tmpdir):
     dataset_manager = PandasDatasetManager(backend=LocalTestBackend())
     cache_dir = os.path.join(tmpdir, 'cache') if use_cache_dir else None
     manager = CacheManager(dataset_manager, cache_dir=cache_dir)
@@ -24,26 +25,45 @@ def test_cache_dataset(use_cache_dir, tmpdir):
         'preprocessing': {},
     }
 
-    dataset = os.path.join(tmpdir, 'dataset.csv')
-    Path(dataset).touch()
+    def touch(basename):
+        path = os.path.join(tmpdir, f'{basename}.csv')
+        Path(path).touch()
+        return path
 
-    cache_key = manager.get_cache_key(dataset, config)
+    dataset = training_set = test_set = validation_set = None
+    if not use_split:
+        dataset = touch('dataset')
+        cache_key = manager.get_cache_key(dataset, config)
+    else:
+        training_set = touch('train')
+        test_set = touch('test')
+        validation_set = touch('validation')
+        cache_key = manager.get_cache_key(training_set, config)
+
     training_set_metadata = {
         CHECKSUM: cache_key,
     }
 
-    cache = manager.get_dataset_cache(config, dataset)
+    cache = manager.get_dataset_cache(
+        config, dataset, training_set, test_set, validation_set
+    )
     cache_map = cache.cache_map
     assert len(cache_map) == 4
 
-    base_path = os.path.join(cache_dir, alphanum(cache_key)) if \
+    train_path = os.path.join(cache_dir, alphanum(cache_key)) if \
         use_cache_dir else \
         os.path.join(tmpdir, 'dataset')
+    test_path = val_path = train_path
 
-    assert cache_map[META] == f'{base_path}.meta.json'
-    assert cache_map[TRAINING] == f'{base_path}.training.hdf5'
-    assert cache_map[TEST] == f'{base_path}.test.hdf5'
-    assert cache_map[VALIDATION] == f'{base_path}.validation.hdf5'
+    if use_split and not use_cache_dir:
+        train_path = os.path.join(tmpdir, 'train')
+        test_path = os.path.join(tmpdir, 'test')
+        val_path = os.path.join(tmpdir, 'validation')
+
+    assert cache_map[META] == f'{train_path}.meta.json'
+    assert cache_map[TRAINING] == f'{train_path}.training.hdf5'
+    assert cache_map[TEST] == f'{test_path}.test.hdf5'
+    assert cache_map[VALIDATION] == f'{val_path}.validation.hdf5'
 
     for cache_path in cache_map.values():
         assert not os.path.exists(cache_path)
