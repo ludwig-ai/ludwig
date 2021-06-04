c77bfb07bdfe44a90995ea6a88b9fc95c06917e6
Piero Molino <w4nderlust@gmail.com>
2021-06-03 18:33:47 -0700

    Adding MLP-Mixer image encoder (#1178)

--- ludwig/modules/activation_modules.py
+++ ludwig/modules/activation_modules.py
@@ -10,3 +10,15 @@ def glu(x, n_units=None):
     and the other half is used as an input to GLU,
     """
     return x[:, :n_units] * tf.nn.sigmoid(x[:, n_units:])
+
+
+def gelu(features, approximate=False):
+    if approximate:
+        coeff = tf.cast(0.044715, features.dtype)
+        return 0.5 * features * (
+                1.0 + tf.math.tanh(0.7978845608028654 *
+                                   (features + coeff * tf.math.pow(features,
+                                                                   3))))
+    else:
+        return 0.5 * features * (1.0 + tf.math.erf(
+            features / tf.cast(1.4142135623730951, features.dtype)))
