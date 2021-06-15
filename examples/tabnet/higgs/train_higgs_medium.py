1a5152b1283c8b1ec9d145346b1f864f6d75c6d0
Travis Addair <tgaddair@gmail.com>
2021-06-14 20:36:10 -0700

    Added higgs boson tabnet examples (#1209)

---
+++ examples/tabnet/higgs/train_higgs_medium.py
@@ -0,0 +1,16 @@
+import logging
+
+from ludwig.api import LudwigModel
+from ludwig.datasets import higgs
+
+model = LudwigModel(
+    config='medium_config.yaml',
+    logging_level=logging.INFO,
+)
+
+higgs_df = higgs.load()
+model.train(
+    dataset=higgs_df,
+    experiment_name='higgs_medium',
+    model_name='higgs_tabnet_medium'
+)
