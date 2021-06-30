725f688d6cf40ae74353c26cca99527bbbee27d5
ANarayan <gostanfordtennis@gmail.com>
2021-06-30 10:10:42 -0700

    makes train an internal func. & adds output_dir param to auto_train

--- ludwig/automl/automl.py
+++ ludwig/automl/automl.py
@@ -11,9 +11,8 @@ Driver script which:
 from typing import Dict, Union
 
 import pandas as pd
-from ludwig.hyperopt.run import hyperopt
-
 from ludwig.automl.base_config import create_default_config
+from ludwig.hyperopt.run import hyperopt
 
 OUTPUT_DIR = "."
 
@@ -21,16 +20,16 @@ OUTPUT_DIR = "."
 def model_select(default_configs):
     """
     Performs model selection based on dataset.
-    Note: Current implementation returns tabnet by default. This will be 
+    Note: Current implementation returns tabnet by default. This will be
         improved in subsequent iterations
     """
     return default_configs['tabnet'], 'tabnet'
 
 
-def auto_train(dataset: str, target: str, time_limit_s: Union[int, float]):
+def auto_train(dataset: str, target: str, time_limit_s: Union[int, float], output_dir: str = OUTPUT_DIR):
     """
-    Main auto train API that first builds configs for each model type 
-    (e.g. concat, tabnet, transformer). Then selects model based on dataset 
+    Main auto train API that first builds configs for each model type
+    (e.g. concat, tabnet, transformer). Then selects model based on dataset
     attributes. And finally runs a hyperparameter optimization experiment.
 
     All batch and learning rate tuning is done @ training time.
@@ -39,16 +38,19 @@ def auto_train(dataset: str, target: str, time_limit_s: Union[int, float]):
     :param dataset: (str) filepath to dataset.
     :param target_name: (str) name of target feature
     :param time_limit_s: (int, float) total time allocated to auto_train. acts
-                                    as the stopping parameter
+                        as the stopping parameter
 
+    # Returns
+    :return: (str) path to best trained model
     """
 
     default_configs = create_default_config(dataset, target, time_limit_s)
     model_config, model_name = model_select(default_configs)
-    train(model_config, dataset, OUTPUT_DIR, model_name=model_name)
+    hyperopt_results = _train(model_config, dataset,
+                              output_dir, model_name=model_name)
 
 
-def train(
+def _train(
     config: Dict, dataset: Union[str, Dict, pd.DataFrame], output_dir: str, model_name: str
 ):
     hyperopt_results = hyperopt(
