cb6b97b5f1575dca14e605fc5af56f56f49b536a
amholler <86269492+amholler@users.noreply.github.com>
2021-07-13 12:13:41 -0700

    Add sample training (#1227)
    * Add 4 tabular datasets
    
    * Add laptop sanity-check config and train scripts for tabular datasets to be used in automl experiments
    
    Co-authored-by: Anne Holler <anne@vmware.com>

---
+++ automl/experiments/validation/synthetic_fraud/train_tabnet_sanity_laptop.py
@@ -0,0 +1,17 @@
+import logging
+
+from ludwig.api import LudwigModel
+from ludwig.datasets import synthetic_fraud
+
+model = LudwigModel(
+    config='config_tabnet_sanity_laptop.yaml',
+    logging_level=logging.INFO,
+)
+
+synthetic_fraud_df = synthetic_fraud.load()
+model.train(
+    dataset=synthetic_fraud_df,
+    experiment_name='synthetic_fraud_tabnet_sanity_laptop',
+    model_name='synthetic_fraud_tabnet_sanity_laptop',
+    skip_save_processed_input=True
+)
