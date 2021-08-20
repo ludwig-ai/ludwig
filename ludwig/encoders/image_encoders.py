50bf4ea264072550cac902f098d42c8c1d49b091
Shreya Rajpal <shreya.rajpal@gmail.com>
2021-08-20 13:23:11 -0700

    Added insurance lite dataset

--- ludwig/encoders/image_encoders.py
+++ ludwig/encoders/image_encoders.py
@@ -19,8 +19,16 @@ from abc import ABC
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import torch
+# import tensorflow as tf
+# import tensorflow_addons as tfa
+# from tensorflow.keras.layers import Dense, Dropout, Flatten
+# from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 
 from ludwig.encoders.base import Encoder
+from ludwig.modules.attention_modules import TransformerBlock
+from ludwig.modules.convolutional_modules import (Conv2DStack, ResNet,
+                                                  get_resnet_block_sizes)
+from ludwig.modules.fully_connected_modules import FCStack
 from ludwig.modules.mlp_mixer_modules import MLPMixer
 from ludwig.utils.registry import Registry, register, register_default
 from ludwig.modules.convolutional_modules import Conv2DStack, ResNet,\
@@ -296,3 +304,75 @@ class MLPMixerEncoder(ImageEncoder):
     @property
     def output_shape(self) -> torch.Size:
         return self._output_shape
+
+
+@register(name='vit')
+class ViTEncoder(ImageEncoder):
+    def __init__(
+            self,
+            image_size,
+            patch_size: int = 16,
+            num_layers: int = 8,
+            num_classes: int = 10,
+            d_model: int = 64,
+            num_heads: int = 4,
+            mlp_dim: int = 128,
+            channels: int = 3,
+            dropout: float = 0.1,
+    ):
+        super().__init__()
+        num_patches = (image_size // patch_size) ** 2
+        self.patch_dim = channels * patch_size ** 2
+
+        self.patch_size = patch_size
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.rescale = Rescaling(1./255)
+        self.pos_emb = self.add_weight(
+            "pos_emb", shape=(1, num_patches + 1, d_model)
+        )
+        self.class_emb = self.add_weight("class_emb", shape=(1, 1, d_model))
+        self.patch_proj = Dense(d_model)
+        self.enc_layers = [
+            TransformerBlock(d_model, num_heads, mlp_dim, dropout)
+            for _ in range(num_layers)
+        ]
+        self.mlp_head = tf.keras.Sequential(
+            [
+                Dense(mlp_dim, activation=tfa.activations.gelu),
+                Dropout(dropout),
+                Dense(num_classes),
+            ]
+        )
+
+    def extract_patches(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patches = tf.reshape(patches, [batch_size, -1, self.patch_dim])
+        return patches
+
+    def call(self, x, training):
+        batch_size = tf.shape(x)[0]
+        x = self.rescale(x)
+        patches = self.extract_patches(x)
+        x = self.patch_proj(patches)
+
+        class_emb = tf.broadcast_to(
+            self.class_emb, [batch_size, 1, self.d_model]
+        )
+        x = tf.concat([class_emb, x], axis=1)
+        x = x + self.pos_emb
+
+        for layer in self.enc_layers:
+            x = layer(x, training)
+
+        # First (class token) is used for classification
+        x = self.mlp_head(x[:, 0])
+        return x
