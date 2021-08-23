8817c9f8ad16d2a6220e219325bfced2b11e5627
Shreya Rajpal <shreya.rajpal@gmail.com>
2021-08-22 17:39:45 -0700

    Added training code

---
+++ examples/insurance_lite/train.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Simple Model Training Example
+#
+# This example is the API example for this Ludwig command line example
+# (https://ludwig-ai.github.io/ludwig-docs/examples/#kaggles-titanic-predicting-survivors).
+
+# Import required libraries
+import logging
+import os
+import shutil
+
+from ludwig.api import LudwigModel
+from ludwig.datasets import insurance_lite
+
+# clean out prior results
+shutil.rmtree('./results', ignore_errors=True)
+
+# Download and prepare the dataset
+dataset = insurance_lite.load()
+
+# Define Ludwig model object that drive model training
+model = LudwigModel(config='./config.yaml',
+                    logging_level=logging.INFO)
+
+training_set, val_set, test_set, train_metadata = model.preprocess(
+    dataset, random_seed=1
+)
+
+# initiate model training
+(
+    train_stats,  # dictionary containing training statistics
+    preprocessed_data,  # tuple Ludwig Dataset objects of pre-processed training data
+    output_directory # location of training results stored on disk
+ ) = model.train(
+    dataset=training_set,
+    experiment_name='simple_experiment',
+    model_name='simple_model'
+)
+
+# list contents of output directory
+print("contents of output directory:", output_directory)
+for item in os.listdir(output_directory):
+    print("\t", item)
