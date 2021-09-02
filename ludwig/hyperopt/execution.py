6cdda47af314f333782c190dcb65b78224ee76f9
Antoni Baum <antoni.baum@protonmail.com>
2021-09-02 00:38:22 +0000

    Fix

--- ludwig/hyperopt/execution.py
+++ ludwig/hyperopt/execution.py
@@ -837,7 +837,7 @@ class RayTuneExecutor(HyperoptExecutor):
                     checkpoint_files = []
                     for path in os.walk(save_path):
                         with open(path, "rb") as f:
-                            checkpoint_files.append((path, f))
+                            checkpoint_files.append((path, f.read()))
                     ray_queue.put((progress_tracker, save_path, checkpoint_files))
                     return
 
