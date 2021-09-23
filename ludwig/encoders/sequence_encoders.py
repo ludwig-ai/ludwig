10e95eee87dd2bc270e5c78686503a52f273fd05
Jim Thompson <jimthompson5802@gmail.com>
2021-09-22 21:21:07 -0400

    fix: correct input size specification for FCStack  corrects error with audio input feature in forward() call

--- ludwig/encoders/sequence_encoders.py
+++ ludwig/encoders/sequence_encoders.py
@@ -865,7 +865,7 @@ class StackedCNN(SequenceEncoder):
         if self.reduce_output is not None:
             logger.debug('  FCStack')
             self.fc_stack = FCStack(
-                num_filters,
+                self.conv1d_stack.output_shape[-1],
                 layers=fc_layers,
                 num_layers=num_fc_layers,
                 default_fc_size=fc_size,
