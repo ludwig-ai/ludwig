276979399e544a64e896d2e6402fce2ece82632d
Jim Thompson <jimthompson5802@gmail.com>
2021-09-22 22:06:13 -0400

    feature: port audio feature to torch including unit test

---
+++ tests/integration_tests/test_audio_feature.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import pytest
+import pandas as pd
+
+import torch
+
+from ludwig.features.audio_feature import AudioInputFeature
+from tests.integration_tests.utils import audio_feature
+from tests.integration_tests.utils import ENCODERS
+
+BATCH_SIZE = 2
+SEQ_SIZE = 20
+AUDIO_W_SIZE = 16
+FC_SIZE = 32
+
+
+@pytest.mark.parametrize(
+    'enc_encoder',
+    [
+        'stacked_cnn', 'parallel_cnn', 'stacked_parallel_cnn', 'rnn', 'cnnrnn',
+        'passthrough', 'null', 'none', 'None', None
+    ]
+)
+def test_audio_feature(enc_encoder):
+    # synthetic audio tensor
+    audio_tensor = torch.randn([BATCH_SIZE, SEQ_SIZE, AUDIO_W_SIZE],
+                               dtype=torch.float32)
+
+    # generate audio feature config
+    audio_feature_config = audio_feature(
+        folder='.',
+        encoder=enc_encoder,
+        max_sequence_length=SEQ_SIZE,
+        embedding_size=AUDIO_W_SIZE
+    )
+
+    # instantiate audio input feature object
+    audio_input_feature = AudioInputFeature(audio_feature_config)
+
+    # pass synthetic audio tensor through the audio input feature
+    encoder_output = audio_input_feature(audio_tensor)
+
+    # confirm correctness of the the audio encoder output
+    assert isinstance(encoder_output, dict)
+    assert 'encoder_output' in encoder_output
+    assert isinstance(encoder_output['encoder_output'], torch.Tensor)
+    assert encoder_output['encoder_output'].shape == (BATCH_SIZE, 256)
