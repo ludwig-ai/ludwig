6686140f9c10979d191f7939dd2f7bd5321d90a8
Justin Zhao <justin@predibase.com>
2021-10-04 22:43:35 -0400

    Enable more unit tests -- H3RNN is not passing yet.

--- tests/ludwig/encoders/test_h3_encoders.py
+++ tests/ludwig/encoders/test_h3_encoders.py
@@ -17,19 +17,21 @@ def test_h3_embed():
     assert outputs['encoder_output'].size()[1:] == embed.output_shape
 
 
-# def test_h3_weighted_sum():
-#     embed = h3_encoders.H3WeightedSum()
-#     inputs = torch.tensor([[2022, 6, 25, 5, 176, 9, 30, 59, 34259],
-#                            [2022, 6, 25, 5, 176, 9, 30, 59, 34259]],
-#                           dtype=torch.int32)
-#     outputs = embed(inputs)
-#     assert outputs['encoder_output'].size()[1:] == embed.output_shape
-#
-#
-# def test_h3_rnn_embed():
-#     date_embed = h3_encoders.H3RNNEmbed()
-#     inputs = torch.tensor([[2022, 6, 25, 5, 176, 9, 30, 59, 34259],
-#                            [2022, 6, 25, 5, 176, 9, 30, 59, 34259]],
-#                           dtype=torch.int32)
-#     outputs = date_embed(inputs)
-#     assert outputs['encoder_output'].size()[1:] == embed.output_shape
+def test_h3_weighted_sum():
+    embed = h3_encoders.H3WeightedSum()
+    inputs = torch.tensor(
+        [[2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7],
+         [2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7]],
+        dtype=torch.int32)
+    outputs = embed(inputs)
+    assert outputs['encoder_output'].size()[1:] == embed.output_shape
+
+
+def test_h3_rnn_embed():
+    embed = h3_encoders.H3RNN()
+    inputs = torch.tensor(
+        [[2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7],
+         [2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7]],
+        dtype=torch.int32)
+    outputs = embed(inputs)
+    assert outputs['encoder_output'].size()[1:] == embed.output_shape
