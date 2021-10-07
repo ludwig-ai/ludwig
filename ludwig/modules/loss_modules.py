29b103027cbc1e87a6adc8cc8970f34deb01e014
Shreya Rajpal <shreya.rajpal@gmail.com>
2021-10-07 00:24:33 -0700

    Ported metrics

--- ludwig/modules/loss_modules.py
+++ ludwig/modules/loss_modules.py
@@ -75,37 +75,26 @@ class BWCEWLoss:
         super().__init__()
         self.loss_fn = nn.BCEWithLogitsLoss(**kwargs)
 
-    def mean_confidence_penalty(self, probabilities, num_classes):
-        max_entropy = torch.log(torch.tensor(num_classes))
-        # clipping needed for avoiding log(0) = -inf
-        entropy_per_class = torch.maximum(
-            -probabilities * torch.log(torch.clamp(probabilities, 1e-10, 1)),
-            0
-        )
-        entropy = torch.sum(entropy_per_class, -1)
-        penalty = (max_entropy - entropy) / max_entropy
-        return torch.mean(penalty)
-
     def forward(self, predictions: torch.Tensor, target: torch.Tensor):
         logits = predictions[LOGITS]
         target = target.long()
         output = self.loss_fn(input, target)
         # robust lambda
         if self.robust_lambda > 0:
-            train_loss = (
-                                 1 - self.robust_lambda
-                         ) * output + self.robust_lambda / 2
+            train_loss = (1 - self.robust_lambda) * output + \
+                self.robust_lambda / 2
 
         train_mean_loss = torch.mean(train_loss)
-        #
+
         # confidence penalty
         if self.confidence_penalty > 0:
             # need to define logits
             probabilities = torch.sigmoid(logits)
-            mean_penalty = self.mean_confidence_penalty(probabilities, 2)
+            mean_penalty = utils.mean_confidence_penalty(probabilities, 2)
             train_mean_loss += self.confidence_penalty * mean_penalty
 
         return train_mean_loss
+
 
 # TODO torch: test behavior parity with tf
 class SoftmaxCrossEntropyLoss(nn.Module):
@@ -260,149 +249,4 @@ class SigmoidCrossEntropyLoss(nn.Module):
 #         loss = loss * mask
 #         loss = tf.reduce_sum(loss) / tf.reduce_sum(mask)
 #         return loss
-#
-#
-# def mean_confidence_penalty(probabilities, num_classes):
-#     max_entropy = tf.constant(np.log(num_classes), dtype=tf.float32)
-#     # clipping needed for avoiding log(0) = -inf
-#     entropy_per_class = tf.maximum(
-#         -probabilities
-#         * tf.math.log(tf.clip_by_value(probabilities, 1e-10, 1)),
-#         0,
-#     )
-#     entropy = tf.reduce_sum(entropy_per_class, -1)
-#     penalty = (max_entropy - entropy) / max_entropy
-#     return tf.reduce_mean(penalty)
-#
-#
-# # For categorical feature
-# def sampled_softmax_cross_entropy(
-#     labels,
-#     last_hidden,
-#     num_classes=1,
-#     decoder_weights=None,
-#     decoder_biases=None,
-#     sampler=None,
-#     negative_samples=0,
-#     class_counts=0,
-#     distortion=1,
-#     unique=False,
-#     **kwargs
-# ):
-#     labels = tf.cast(tf.expand_dims(labels, -1), tf.int64)
-#
-#     sampled_values = sample_values_from_classes(
-#         labels,
-#         sampler,
-#         num_classes,
-#         negative_samples,
-#         unique,
-#         class_counts,
-#         distortion,
-#     )
-#     train_loss = tf.nn.sampled_softmax_loss(
-#         weights=tf.transpose(decoder_weights),
-#         biases=decoder_biases,
-#         labels=labels,
-#         inputs=last_hidden,
-#         num_sampled=negative_samples,
-#         num_classes=num_classes,
-#         sampled_values=sampled_values,
-#     )
-#
-#     return train_loss
-#
-#
-# # custom class to support Laplace smoothing of Fixed Unigram candidate sampler
-# # Required because of zeros returned in the true_expected_count for
-# # <PAD> and <UNK> tokens in loss['class_counts'] list
-# class FixedUnigramCandidateSampler(
-#     collections.namedtuple(
-#         "FixedUnigramCandidateSampler",
-#         (
-#             "sampled_candidates",
-#             "true_expected_count",
-#             "sampled_expected_count",
-#         ),
-#     )
-# ):
-#     pass
-#
-#
-# # For sequence feature
-# def sequence_sampled_softmax_cross_entropy(
-#     targets, train_logits, decoder_weights, decoder_biases, num_classes, **loss
-# ):
-#     batch_max_targets_sequence_length = tf.shape(targets)[1]
-#     targets_sequence_length = sequence_length_2D(tf.cast(targets, tf.int64))
-#     batch_max_train_logits_sequence_length = tf.shape(train_logits)[1]
-#
-#     logits_pad_len = tf.maximum(
-#         0,
-#         batch_max_targets_sequence_length
-#         - batch_max_train_logits_sequence_length,
-#     )
-#     targets_pad_len = tf.maximum(
-#         0,
-#         batch_max_train_logits_sequence_length
-#         - batch_max_targets_sequence_length,
-#     )
-#
-#     padded_logits = tf.pad(train_logits, [[0, 0], [0, logits_pad_len], [0, 0]])
-#     padded_targets = tf.pad(targets, [[0, 0], [0, targets_pad_len]])
-#
-#     output_exp = tf.cast(tf.reshape(padded_targets, [-1, 1]), tf.int64)
-#     sampled_values = sample_values_from_classes(
-#         output_exp,
-#         loss["sampler"],
-#         num_classes,
-#         loss["negative_samples"],
-#         loss["unique"],
-#         loss["class_counts"],
-#         loss["distortion"],
-#     )
-#
-#     if loss["sampler"] == "fixed_unigram":
-#         # regenerate sampled_values structure for specified samplers
-#         # to handle any zero values in true_expected_count tensor
-#         sampled_values = FixedUnigramCandidateSampler(
-#             sampled_values.sampled_candidates,
-#             # add smoothing constant EPSILON to handle any zero values
-#             tf.add(sampled_values.true_expected_count, EPSILON),
-#             sampled_values.sampled_expected_count,
-#         )
-#
-#     def _sampled_loss(labels, logits):
-#         labels = tf.cast(labels, tf.int64)
-#         labels = tf.reshape(labels, [-1, 1])
-#         logits = tf.cast(logits, tf.float32)
-#
-#         return tf.cast(
-#             tf.nn.sampled_softmax_loss(
-#                 weights=tf.transpose(decoder_weights),
-#                 biases=decoder_biases,
-#                 labels=labels,
-#                 inputs=logits,
-#                 num_sampled=loss["negative_samples"],
-#                 num_classes=num_classes,
-#                 sampled_values=sampled_values,
-#             ),
-#             tf.float32,
-#         )
-#
-#     train_loss = tfa.seq2seq.sequence_loss(
-#         padded_logits,
-#         padded_targets,
-#         tf.sequence_mask(
-#             targets_sequence_length,
-#             tf.shape(padded_targets)[1],
-#             dtype=tf.float32,
-#         ),
-#         average_across_timesteps=True,
-#         average_across_batch=False,
-#         softmax_loss_function=_sampled_loss,
-#     )
-#
-#     return train_loss
-#
 #
--- ludwig/modules/metric_modules.py
+++ ludwig/modules/metric_modules.py
@@ -14,25 +14,23 @@
 # limitations under the License.
 # ==============================================================================
 from abc import abstractmethod
+from typing import Callable
 
-import numpy as np
-from torch import Tensor
-
-from ludwig.constants import *
-from ludwig.constants import PREDICTIONS, LOGITS
-from ludwig.modules.loss_modules import (
-        BWCEWLoss,
-    #     SequenceSoftmaxCrossEntropyLoss,
-    #     SequenceSampledSoftmaxCrossEntropyLoss,
-    #     SigmoidCrossEntropyLoss,
-    #     SoftmaxCrossEntropyLoss,
-    #     SampledSoftmaxCrossEntropyLoss,
-    rmspe_loss, SoftmaxCrossEntropyLoss, SigmoidCrossEntropyLoss
-)
 import torch
+import torchmetrics.functional as metrics_F
+from torch import Tensor
 from torchmetrics import Accuracy, AUROC, AverageMeter, IoU, MeanAbsoluteError,\
     MeanSquaredError, Metric, R2Score
-#from ludwig.utils.tf_utils import sequence_length_2D, to_sparse
+
+from ludwig.constants import *
+from ludwig.modules.loss_modules import BWCEWLoss, SigmoidCrossEntropyLoss,\
+    SoftmaxCrossEntropyLoss, rmspe_loss # SequenceSoftmaxCrossEntropyLoss,\
+    # SequenceSampledSoftmaxCrossEntropyLoss, SampledSoftmaxCrossEntropyLoss
+from ludwig.utils.torch_utils import sequence_length_2D
+from ludwig.utils.metric_utils import masked_corrected_predictions
+# from ludwig.utils.metric_utils import masked_sequence_corrected_predictions,\
+    # edit_distance
+# from ludwig.utils.tf_utils import to_sparse
 
 metrics = {
     ACCURACY,
@@ -227,7 +225,8 @@ class SigmoidCrossEntropyMetric(MeanMetric):
     def get_current_value(self, preds: Tensor, target: Tensor) -> Tensor:
         return self.sigmoid_cross_entropy_function(preds, target)
 
-#
+
+# TODO(shreya): After Sequence Losses
 # class SequenceLossMetric(tf.keras.metrics.Mean):
 #     def __init__(self, from_logits=True, name=None):
 #         super().__init__(name=name)
@@ -240,7 +239,8 @@ class SigmoidCrossEntropyMetric(MeanMetric):
 #         loss = self.loss_function(y, y_hat)
 #         super().update_state(loss)
 #
-#
+# 
+# TODO(shreya): After Sequence Losses
 # class SequenceSampledLossMetric(tf.keras.metrics.Mean):
 #     def __init__(
 #             self,
@@ -263,7 +263,8 @@ class SigmoidCrossEntropyMetric(MeanMetric):
 #         loss = self.loss_function(y, y_hat)
 #         super().update_state(loss)
 #
-#
+# 
+# TODO(shreya): After Sequence Losses
 # class SequenceLastAccuracyMetric(tf.keras.metrics.Accuracy):
 #     """
 #     Sequence accuracy based on last token in the sequence
@@ -302,6 +303,7 @@ class SigmoidCrossEntropyMetric(MeanMetric):
 #         mean = super().result()
 #         return np.exp(mean)
 
+
 # TODO(shreya): No PyTorch CUDA implementation available
 # class EditDistanceMetric(tf.keras.metrics.Mean):
 #     def __init__(self, name=None):
@@ -324,24 +326,19 @@ class SigmoidCrossEntropyMetric(MeanMetric):
 #         super().update_state(edit_distance_val)
 
 
-class TokenAccuracyMetric(tf.keras.metrics.Mean):
+# TODO(shreya): Confirm if its ok to inherit from MeanMetric.
+class TokenAccuracyMetric(MeanMetric):
     def __init__(self, name=None):
         super().__init__(name=name)
 
-    def update_state(self, y_true, y_pred):
-        # y_true: shape [batch_size, sequence_size]
-        # y_pred: shape [batch_size, sequence_size]
-
-        prediction_dtype = y_pred.dtype
-        y_true_tensor = tf.cast(y_true, dtype=prediction_dtype)
-        target_sequence_length = sequence_length_2D(y_true_tensor)
+    def get_current_value(self, preds: Tensor, target: Tensor) -> Tensor:
+        target = target.type(preds.dtype)
+        target_sequence_length = sequence_length_2D(target)
         masked_corrected_preds = masked_corrected_predictions(
-            y_true_tensor, y_pred, target_sequence_length
-        )
+            target, preds, target_sequence_length)
+        return masked_corrected_preds
 
-        super().update_state(masked_corrected_preds)
-#
-#
+
 # TODO(shreya): After Sequence Losses
 # class SequenceAccuracyMetric(tf.keras.metrics.Mean):
 #     def __init__(self, name=None):
@@ -364,7 +361,7 @@ class TokenAccuracyMetric(tf.keras.metrics.Mean):
 
 
 class CategoryAccuracy(Accuracy):
-    def __init__(self, name=None):
+    def __init__(self):
         super().__init__()
 
     def update(self, preds: torch.Tensor, target: torch.Tensor):
@@ -373,7 +370,7 @@ class CategoryAccuracy(Accuracy):
 
 
 class HitsAtKMetric(Accuracy):
-    def __init__(self, top_k: int = 3, name: str = None):
+    def __init__(self, top_k: int = 3):
         super().__init__(top_k=top_k)
 
     def update(self, preds: torch.Tensor, target: torch.Tensor):
@@ -392,7 +389,7 @@ class MSEMetric(MeanSquaredError):
     def __init__(self, **kwargs):
         super(MSEMetric, self).__init__(**kwargs)
 
-    def update(self, preds: torch.Tensor, target):
+    def update(self, preds: Tensor, target: Tensor):
         super().update(preds.detach(), target)
 
 
@@ -411,158 +408,62 @@ class JaccardMetric(Metric):
         return torch.mean(torch.stack(self.loss))
 
 
-def get_improved_fun(metric):
+def get_improved_fun(metric: str) -> Callable:
     if metric in min_metrics:
         return lambda x, y: x < y
     else:
         return lambda x, y: x > y
 
 
-def get_initial_validation_value(metric):
+def get_initial_validation_value(metric: str) -> float:
     if metric in min_metrics:
         return float("inf")
     else:
         return float("-inf")
 
 
-def get_best_function(metric):
+def get_best_function(metric: str) -> Callable:
     if metric in min_metrics:
         return min
     else:
         return max
 
 
-# def accuracy(targets, predictions, output_feature_name):
-#     correct_predictions = tf.equal(
-#         predictions,
-#         targets,
-#         name="correct_predictions_{}".format(output_feature_name),
-#     )
-#     accuracy = tf.reduce_mean(
-#         tf.cast(correct_predictions, tf.float32),
-#         name="accuracy_{}".format(output_feature_name),
-#     )
-#     return accuracy, correct_predictions
-#
-#
-def masked_corrected_predictions(
-        targets, predictions, targets_sequence_lengths
-):
-    truncated_preds = predictions[:, : targets.shape[1]]
-    paddings = tf.stack(
-        [[0, 0], [0, tf.shape(targets)[1] - tf.shape(truncated_preds)[1]]]
-    )
-    padded_truncated_preds = tf.pad(truncated_preds, paddings, name="ptp")
+def accuracy(preds: Tensor, target: Tensor) -> Tensor:
+    """
+    Returns:
+        Accuracy (float tensor of shape (1,)).
+    """
+    return metrics_F.accuracy(preds, target)
+    # correct_predictions = predictions == target
+    # accuracy = torch.mean(correct_predictions.type(torch.float32))
+    # return accuracy, correct_predictions
 
-    correct_preds = tf.equal(padded_truncated_preds, targets)
 
-    mask = tf.sequence_mask(
-        targets_sequence_lengths, maxlen=correct_preds.shape[1], dtype=tf.int32
-    )
+def perplexity(cross_entropy_loss):
+    # This seem weird but is correct:
+    # we are returning the cross entropy loss as it will be later summed,
+    # divided by the size of the dataset and finally exponentiated,
+    # because perplexity has a avg_exp aggregation strategy
+    # in the output config in SequenceOutputFeature.
+    # This implies that in Model update_output_stats_batch()
+    # the values read from the perplexity node will be summed
+    # and in Model update_output_stats() they will be divided
+    # by the set size first and exponentiated.
+    return cross_entropy_loss
 
-    _, masked_correct_preds = tf.dynamic_partition(correct_preds, mask, 2)
-    masked_correct_preds = tf.cast(masked_correct_preds, dtype=tf.float32)
 
-    return masked_correct_preds
-#
-#
-# def masked_sequence_corrected_predictions(
-#         targets, predictions, targets_sequence_lengths
-# ):
-#     truncated_preds = predictions[:, : targets.shape[1]]
-#     paddings = tf.stack(
-#         [[0, 0], [0, tf.shape(targets)[1] - tf.shape(truncated_preds)[1]]]
-#     )
-#     padded_truncated_preds = tf.pad(truncated_preds, paddings, name="ptp")
-#
-#     correct_preds = tf.equal(padded_truncated_preds, targets)
-#
-#     mask = tf.sequence_mask(
-#         targets_sequence_lengths, maxlen=correct_preds.shape[1], dtype=tf.int32
-#     )
-#
-#     one_masked_correct_prediction = (
-#         1.0
-#         - tf.cast(mask, tf.float32)
-#         + (tf.cast(mask, tf.float32) * tf.cast(correct_preds, tf.float32))
-#     )
-#     sequence_correct_preds = tf.reduce_prod(
-#         one_masked_correct_prediction, axis=-1
-#     )
-#
-#     return sequence_correct_preds
-#
-#
-# def hits_at_k(targets, predictions_logits, top_k, output_feature_name):
-#     with tf.device("/cpu:0"):
-#         hits_at_k = tf.nn.in_top_k(
-#             predictions_logits,
-#             targets,
-#             top_k,
-#             name="hits_at_k_{}".format(output_feature_name),
-#         )
-#         mean_hits_at_k = tf.reduce_mean(
-#             tf.cast(hits_at_k, tf.float32),
-#             name="mean_hits_at_k_{}".format(output_feature_name),
-#         )
-#     return hits_at_k, mean_hits_at_k
-#
-#
-# TODO(shreya): No PyTorch CUDA implementation available
-# def edit_distance(
-#         targets, target_seq_length, predictions_sequence,
-#         predictions_seq_length
-# ):
-#     predicts = to_sparse(
-#         predictions_sequence,
-#         predictions_seq_length,
-#         tf.shape(predictions_sequence)[1],
-#     )
-#     labels = to_sparse(targets, target_seq_length, tf.shape(targets)[1])
-#     edit_distance = tf.edit_distance(predicts, labels)
-#     mean_edit_distance = tf.reduce_mean(edit_distance)
-#     return edit_distance, mean_edit_distance
-#
-#
-# def perplexity(cross_entropy_loss):
-#     # This seem weird but is correct:
-#     # we are returning the cross entropy loss as it will be later summed,
-#     # divided by the size of the dataset and finally exponentiated,
-#     # because perplexity has a avg_exp aggregation strategy
-#     # in the output config in SequenceOutputFeature.
-#     # This implies that in Model update_output_stats_batch()
-#     # the values read from the perplexity node will be summed
-#     # and in Model update_output_stats() they will be divided
-#     # by the set size first and exponentiated.
-#     return cross_entropy_loss
-#
-#
-# def error(targets, predictions, output_feature_name):
-#     # return tf.get_variable('error_{}'.format(output_feature_name), initializer=tf.subtract(targets, predictions))
-#     return tf.subtract(
-#         targets, predictions, name="error_{}".format(output_feature_name)
-#     )
-#
-#
-# def absolute_error(targets, predictions, output_feature_name):
-#     # error = tf.get_variable('error_{}'.format(output_feature_name), initializer=tf.subtract(targets, predictions))
-#     error = tf.subtract(targets, predictions)
-#     return tf.abs(error, name="absolute_error_{}".format(output_feature_name))
-#
-#
-# def squared_error(targets, predictions, output_feature_name):
-#     # error = tf.get_variable('error_{}'.format(output_feature_name), initializer=tf.subtract(targets, predictions))
-#     error = tf.subtract(targets, predictions)
-#     return tf.pow(
-#         error, 2, name="squared_error_{}".format(output_feature_name)
-#     )
-#
-#
-# def r2(targets, predictions, output_feature_name):
-#     y_bar = tf.reduce_mean(targets)
-#     tot_ss = tf.reduce_sum(tf.pow(targets - y_bar, 2))
-#     res_ss = tf.reduce_sum(tf.pow(targets - predictions, 2))
-#     r2 = tf.subtract(
-#         1.0, res_ss / tot_ss, name="r2_{}".format(output_feature_name)
-#     )
-#     return r2
+def error(preds: Tensor, target: Tensor) -> Tensor:
+    return target - preds
+
+
+def absolute_error(preds: Tensor, target: Tensor) -> Tensor:
+    return torch.abs(target - preds)
+
+
+def squared_error(preds: Tensor, target: Tensor) -> Tensor:
+    return (target - preds) ** 2
+
+
+def r2(preds: Tensor, target: Tensor) -> Tensor:
+    return metrics_F.r2_score(preds, target)
