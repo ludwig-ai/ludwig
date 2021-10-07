702dfd5115920493b38a968a915f418a2feaa003
Justin Zhao <justin@predibase.com>
2021-10-06 22:43:31 -0400

    Port gpt2 encoder to pytorch

--- tests/ludwig/encoders/test_text_encoders.py
+++ tests/ludwig/encoders/test_text_encoders.py
@@ -39,3 +39,21 @@ def test_gpt_encoder(
     inputs = torch.rand((2, max_sequence_length)).type(gpt_encoder.input_dtype)
     outputs = gpt_encoder(inputs)
     assert outputs['encoder_output'].shape[1:] == gpt_encoder.output_shape
+
+
+@pytest.mark.parametrize('use_pretrained', [True, False])
+@pytest.mark.parametrize('reduce_output', [None, 'sum'])
+@pytest.mark.parametrize('max_sequence_length', [20])
+def test_gpt2_encoder(
+        use_pretrained: bool,
+        reduce_output: str,
+        max_sequence_length: int
+):
+    gpt_encoder = text_encoders.GPTEncoder(
+        use_pretrained=use_pretrained,
+        reduce_output=reduce_output,
+        max_sequence_length=max_sequence_length
+    )
+    inputs = torch.rand((2, max_sequence_length)).type(gpt_encoder.input_dtype)
+    outputs = gpt_encoder(inputs)
+    assert outputs['encoder_output'].shape[1:] == gpt_encoder.output_shape
