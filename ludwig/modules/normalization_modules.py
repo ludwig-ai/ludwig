5a2733eea90dbe0d578ebb44f8a1373dcec88e3b
Jim Thompson <jimthompson5802@gmail.com>
2021-10-10 18:15:21 -0400

    refactor: initial working version with unit test

--- ludwig/modules/normalization_modules.py
+++ ludwig/modules/normalization_modules.py
@@ -1,38 +1,41 @@
-from ludwig.utils.torch_utils import LudwigModule
+import torch
 
-# from tensorflow.keras.layers import BatchNormalization
+from ludwig.utils.torch_utils import LudwigModule
 
 
 class GhostBatchNormalization(LudwigModule):
     def __init__(
             self,
+            num_features: int,
             momentum: float = 0.9,
             epsilon: float = 1e-3,
             virtual_batch_size: int = None
     ):
         super().__init__()
+        self.num_features = num_features
         self.virtual_batch_size = virtual_batch_size
-        self.bn = BatchNormalization(momentum=momentum, epsilon=epsilon)
+        self.bn = torch.nn.BatchNorm1d(num_features, momentum=momentum,
+                                       eps=epsilon)
 
-    def call(self, x, training: bool = None, **kwargs):
-        if training and self.virtual_batch_size:
-            batch_size = x.shape[0]
+    def forward(self, inputs):
+        if self.training and self.virtual_batch_size:
+            batch_size = inputs.shape[0]
 
             q, r = divmod(batch_size, self.virtual_batch_size)
             num_or_size_splits = q
             if r != 0:
                 num_or_size_splits = [self.virtual_batch_size] * q + [r]
 
-            splits = tf.split(x, num_or_size_splits)
-            x = [self.bn(x, training=True) for x in splits]
-            return tf.concat(x, 0)
+            splits = torch.split(inputs, num_or_size_splits)
+            x = [self.bn(inputs) for x in splits]
+            return torch.cat(x, 0)
 
-        return self.bn(x, training=False)
+        return self.bn(inputs)
 
     @property
     def moving_mean(self):
-        return self.bn.moving_mean
+        return self.bn.running_mean
 
     @property
     def moving_variance(self):
-        return self.bn.moving_variance
+        return self.bn.running_var
