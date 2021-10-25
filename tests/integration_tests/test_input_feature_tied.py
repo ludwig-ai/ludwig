cceb8f2591e14ef2d4e78ce945a537b352297285
Justin Zhao <justin@predibase.com>
2021-10-25 18:10:09 -0500

    Add TODO to re-enable commented out tests.

--- tests/integration_tests/test_input_feature_tied.py
+++ tests/integration_tests/test_input_feature_tied.py
@@ -120,6 +120,7 @@ TiedUseCase = namedtuple('TiedUseCase', 'input_feature output_feature')
     [
         TiedUseCase(numerical_feature, numerical_feature),
         TiedUseCase(text_feature, category_feature),
+        # TODO(#1333): Re-enable.
         # TiedUseCase(sequence_feature, sequence_feature)
     ]
 )
