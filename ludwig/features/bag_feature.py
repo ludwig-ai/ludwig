c934851f60827b6105e170f911a20e0f6d6584d2
Justin Zhao <justin@predibase.com>
2021-10-25 16:42:27 -0500

    Revert overwrite_defaults to bag_feature.

--- ludwig/features/bag_feature.py
+++ ludwig/features/bag_feature.py
@@ -113,6 +113,7 @@ class BagInputFeature(BagFeatureMixin, InputFeature):
 
     def __init__(self, feature, encoder_obj=None):
         super().__init__(feature)
+        self.overwrite_defaults(feature)
         if encoder_obj:
             self.encoder_obj = encoder_obj
         else:
