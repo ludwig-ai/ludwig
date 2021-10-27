176fee77219dc6ea1d972a8ec73c6c6c5e3cc14f
Saikat Kanjilal <sxk1969@hotmail.com>
2021-10-27 16:59:06 -0700

    modified the generation of the randoms seed

--- tests/integration_tests/test_regularizers.py
+++ tests/integration_tests/test_regularizers.py
@@ -66,7 +66,7 @@ def test_encoder(test_case):
 
     # reproducible synthetic data set
     np.random.seed(RANDOM_SEED)
-    torch.randn(RANDOM_SEED)
+    torch.manual_seed(RANDOM_SEED)
 
     # create synthetic data for the test
     features = [
@@ -99,7 +99,7 @@ def test_encoder(test_case):
     for regularizer in [None, 'l1', 'l2', 'l1_l2']:
         # start with clean slate and make reproducible
         np.random.seed(RANDOM_SEED)
-        torch.randn(RANDOM_SEED)
+        torch.manual_seed(RANDOM_SEED)
 
         # setup kwarg for regularizer parms
         x_coder_kwargs = dict(
@@ -127,9 +127,9 @@ def test_encoder(test_case):
 
         # special handling for image feature
         if features[0]['type'] == 'image':
-            inputs = inputs.type(torch.float32)/255
+            inputs = torch.tensor(inputs.type(torch.float32)/255)
 
-        input_def_obj.encoder_obj(inputs)
+        input_def_obj.encoder_obj(torch.tensor(inputs))
         regularizer_loss = torch.sum(input_def_obj.encoder_obj.losses)
         regularizer_losses.append(regularizer_loss)
 
@@ -147,145 +147,3 @@ def test_encoder(test_case):
 
     # cleanup
     shutil.rmtree(IMAGE_DIR, ignore_errors=True)
-
-
-#
-# Regularization Decoder Tests
-#
-@pytest.mark.parametrize(
-    'test_case',
-    [
-        # regressor decoder
-        TestCase(
-            SyntheticData(BATCH_SIZE, numerical_feature, (), {}),
-            {
-                'decoder': 'regressor',
-                'loss': {'type': 'mean_squared_error'},
-                'num_fc_layers': 5
-            },
-            ['activity_regularizer', 'weights_regularizer', 'bias_regularizer']
-        ),
-        # Tagger Decoder
-        TestCase(
-            SyntheticData(BATCH_SIZE, sequence_feature, (),
-                          {'max_len': SEQ_SIZE}),
-            {'decoder': 'tagger'},
-            ['activity_regularizer', 'weights_regularizer', 'bias_regularizer']
-        ),
-        # Generator Decoder
-        TestCase(
-            SyntheticData(BATCH_SIZE, sequence_feature, (),
-                          {'max_len': SEQ_SIZE}),
-            {'decoder': 'generator', 'cell_type': 'gru'},
-            ['activity_regularizer', 'weights_regularizer', 'bias_regularizer']
-        ),
-    ]
-)
-def test_decoder(test_case):
-    # reproducible synthetic data set
-    np.random.seed(RANDOM_SEED)
-    torch.randn(RANDOM_SEED)
-
-    # create synthetic data for the test
-    features = [
-        test_case.syn_data.feature_generator(
-            *test_case.syn_data.feature_generator_args,
-            **test_case.syn_data.feature_generator_kwargs
-        )
-    ]
-    feature_name = features[0][NAME]
-    proc_column = compute_feature_hash(features[0])
-    features[0][PROC_COLUMN] = proc_column
-
-    data_generator = build_synthetic_dataset(BATCH_SIZE, features)
-    data_list = list(data_generator)
-    raw_data = [x[0] for x in data_list[1:]]
-    df = pd.DataFrame({data_list[0][0]: raw_data})
-
-    # create synthetic combiner layer
-    torch.randn(RANDOM_SEED)
-    combiner_outputs_rank2 = {
-        'combiner_output': torch.randn(
-            [BATCH_SIZE, HIDDEN_SIZE],
-            dtype=torch.float32
-        )
-    }
-
-    combiner_outputs_rank3 = {
-        'combiner_output': torch.randn(
-            [BATCH_SIZE, SEQ_SIZE, HIDDEN_SIZE],
-            dtype=torch.float32
-        ),
-        'encoder_output_state': torch.randn(
-            [BATCH_SIZE, HIDDEN_SIZE],
-            dtype=torch.float32
-        ),
-        'lengths': torch.torch_for_numpy.numpy(
-            np.array(BATCH_SIZE * [SEQ_SIZE]),
-            dtype=torch.int32
-        )
-    }
-
-    # minimal config sufficient to create output feature
-    config = {'input_features': [], 'output_features': features}
-    training_set, _, _, training_set_metadata = preprocess_for_training(
-        config,
-        training_set=df,
-        skip_save_processed_input=True,
-        random_seed=RANDOM_SEED
-    )
-
-    # run through each type of regularizer
-    regularizer_losses = []
-    for regularizer in [None, 'l1', 'l2', 'l1_l2']:
-        # start with clean slate and make reproducible
-        np.random.seed(RANDOM_SEED)
-        torch.randn(RANDOM_SEED)
-
-        # setup kwarg for regularizer parms
-        x_coder_kwargs = dict(
-            zip(test_case.regularizer_parm_names,
-                len(test_case.regularizer_parm_names) * [regularizer])
-        )
-
-        # combine other other keyword parameters
-        x_coder_kwargs.update(test_case.XCoder_other_parms)
-
-        features[0].update(x_coder_kwargs)
-        if features[0]['type'] in SEQUENCE_TYPES:
-            features[0]['num_classes'] = training_set_metadata[feature_name][
-                                             'vocab_size'] + 1
-            training_set.dataset[proc_column] = \
-                training_set.dataset[proc_column].astype(np.int32)
-            combiner_outputs = combiner_outputs_rank3
-        else:
-            combiner_outputs = combiner_outputs_rank2
-
-        output_def_obj = build_single_output(features[0], None, None)
-
-        targets = training_set.dataset[proc_column]
-        if len(targets.shape) == 1:
-            targets = targets.reshape(-1, 1)
-
-        output_def_obj(
-            (
-                (combiner_outputs, None),
-                targets
-            ),
-            training=True,
-            mask=None
-        )
-        regularizer_loss = torch.sum(output_def_obj.decoder_obj.losses)
-        regularizer_losses.append(regularizer_loss)
-
-    # check loss regularization loss values
-    # None should be zero
-    assert regularizer_losses[0] == 0
-
-    # l1, l2 and l1_l2 should be greater than zero
-    assert np.all([t > 0.0 for t in regularizer_losses[1:]])
-
-    # # using default setting l1 + l2 == l1_l2 losses
-    assert np.isclose(
-        regularizer_losses[1].numpy() + regularizer_losses[2].numpy(),
-        regularizer_losses[3].numpy())
