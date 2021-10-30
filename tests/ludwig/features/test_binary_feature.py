034d61e776344e9421b3f98202862ea7f614f04c
Jim Thompson <jimthompson5802@gmail.com>
2021-10-30 15:38:45 -0400

    test: add dense encoder test case for input feature add output feature test case

--- tests/ludwig/features/test_binary_feature.py
+++ tests/ludwig/features/test_binary_feature.py
@@ -2,37 +2,32 @@ import pytest
 
 import torch
 
-from ludwig.features.binary_feature import BinaryInputFeature
-from tests.integration_tests.utils import binary_feature
+from ludwig.features.binary_feature import BinaryInputFeature, \
+    BinaryOutputFeature
+from tests.integration_tests.utils import binary_feature, \
+    setup_input_feature_test, setup_output_feature_test
 
 BATCH_SIZE = 2
-SEQ_SIZE = 20
-DEFAULT_FC_SIZE = 256
+FC_SIZE = 64
+HIDDEN_SIZE = 16
 
 
-@pytest.mark.parametrize(
-    'enc_encoder',
-    [
-        'passthrough'
-    ]
-)
-def test_binary_feature(enc_encoder):
-    # synthetic binary tensor
-    binary_tensor = torch.randn([BATCH_SIZE, SEQ_SIZE],
-                               dtype=torch.float32)
+@pytest.mark.parametrize('enc_encoder', ['passthrough', 'dense'])
+def test_binary_input_feature(enc_encoder: str) -> None:
+    feature_to_test = binary_feature(encoder=enc_encoder, fc_size=FC_SIZE)
 
-    # generate binary feature config
-    binary_feature_config = binary_feature(
-        folder='.',
-        encoder=enc_encoder,
-        max_sequence_length=SEQ_SIZE
+    # setup synthetic tensor and feature definition
+    input_tensor, feature_definition = setup_input_feature_test(
+        batch_size=BATCH_SIZE,
+        feature_definition=feature_to_test,
+        feature_class=BinaryInputFeature
     )
 
     # instantiate binary input feature object
-    binary_input_feature = BinaryInputFeature(binary_feature_config)
+    input_feature_obj = BinaryInputFeature(feature_definition)
 
     # pass synthetic binary tensor through the input feature
-    encoder_output = binary_input_feature(binary_tensor)
+    encoder_output = input_feature_obj(input_tensor)
 
     # confirm correctness of the the binary encoder output
     assert isinstance(encoder_output, dict)
@@ -40,7 +35,47 @@ def test_binary_feature(enc_encoder):
     assert isinstance(encoder_output['encoder_output'], torch.Tensor)
     if enc_encoder == 'passthrough':
         assert encoder_output['encoder_output'].shape \
-               == (BATCH_SIZE, 1, SEQ_SIZE)
+               == (BATCH_SIZE, 1)
     else:
         assert encoder_output['encoder_output'].shape \
-               == (BATCH_SIZE, DEFAULT_FC_SIZE)
+               == (BATCH_SIZE, FC_SIZE)
+
+
+@pytest.mark.parametrize('num_fc_layers', [0, 1])
+def test_binary_output_feature(num_fc_layers: int) -> None:
+    feature_to_test = binary_feature(
+        num_fc_layers=num_fc_layers,
+        fc_size=FC_SIZE,
+        loss={'type': 'cross_entropy'}
+    )
+
+    # setup synthetic tensor and feature definition
+    # ignore generated synthentic data
+    input_for_output_feature, feature_definition = setup_output_feature_test(
+        batch_size=BATCH_SIZE,
+        hidden_size=HIDDEN_SIZE,
+        feature_definition=feature_to_test,
+        feature_class=BinaryOutputFeature
+    )
+
+    # instantiate binary output feature object
+    input_feature_obj = BinaryOutputFeature(feature_definition)
+
+    # pass synthetic binary tensor through the input feature
+    # specify no dependencies
+    feature_output = input_feature_obj(input_for_output_feature)
+
+    # confirm correctness of the the binary encoder output structure
+    assert isinstance(feature_output, dict)
+
+    # confirm expected components of feature output
+    assert 'logits' in feature_output
+    assert feature_output['logits'].shape == (BATCH_SIZE,)
+
+    assert 'last_hidden' in feature_output
+    if num_fc_layers == 0:
+        assert feature_output['last_hidden'].shape \
+               == (BATCH_SIZE, HIDDEN_SIZE)
+    else:
+        assert feature_output['last_hidden'].shape \
+               == (BATCH_SIZE, FC_SIZE)
