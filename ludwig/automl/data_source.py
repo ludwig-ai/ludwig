d2d2d517e6f164807f4f4dd27b1618269d9fafdc
Travis Addair <tgaddair@gmail.com>
2021-11-20 20:37:11 -0800

    Remove NaN from distinct values

--- ludwig/automl/data_source.py
+++ ludwig/automl/data_source.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from typing import List
+from typing import List, Tuple
 
 from ludwig.automl.utils import avg_num_tokens
 from ludwig.utils.image_utils import is_image_score
@@ -13,23 +13,23 @@ class DataSource(ABC):
         raise NotImplementedError()
 
     @abstractmethod
-    def get_dtype(self, column) -> str:
+    def get_dtype(self, column: str) -> str:
         raise NotImplementedError()
 
     @abstractmethod
-    def get_distinct_values(self, column, max_values_to_return) -> (int, List[str]):
+    def get_distinct_values(self, column: str, max_values_to_return: int) -> Tuple[int, List[str]]:
         raise NotImplementedError()
 
     @abstractmethod
-    def get_nonnull_values(self, column) -> int:
+    def get_nonnull_values(self, column: str) -> int:
         raise NotImplementedError()
 
     @abstractmethod
-    def get_avg_num_tokens(self, column) -> int:
+    def get_avg_num_tokens(self, column: str) -> int:
         raise NotImplementedError()
 
     @abstractmethod
-    def is_string_type(self, dtype) -> bool:
+    def is_string_type(self, dtype: str) -> bool:
         raise NotImplementedError()
 
     @abstractmethod
@@ -45,24 +45,24 @@ class DataframeSource(DataSource):
     def columns(self) -> List[str]:
         return self.df.columns
 
-    def get_dtype(self, column) -> str:
+    def get_dtype(self, column: str) -> str:
         return self.df[column].dtype.name
 
-    def get_distinct_values(self, column, max_values_to_return) -> (int, List[str]):
-        unique_values = self.df[column].unique()
+    def get_distinct_values(self, column, max_values_to_return: int) -> Tuple[int, List[str]]:
+        unique_values = self.df[column].dropna().unique()
         num_unique_values = len(unique_values)
-        return (num_unique_values, unique_values[:max_values_to_return])
+        return num_unique_values, unique_values[:max_values_to_return]
 
-    def get_nonnull_values(self, column) -> int:
+    def get_nonnull_values(self, column: str) -> int:
         return len(self.df[column].notnull())
 
-    def get_image_values(self, column, sample_size=10) -> int:
+    def get_image_values(self, column: str, sample_size: int = 10) -> int:
         return int(sum(is_image_score(None, x) for x in self.df[column].head(sample_size)))
 
-    def get_avg_num_tokens(self, column) -> int:
+    def get_avg_num_tokens(self, column: str) -> int:
         return avg_num_tokens(self.df[column])
 
-    def is_string_type(self, dtype) -> bool:
+    def is_string_type(self, dtype: str) -> bool:
         return dtype in ['str', 'string', 'object']
 
     def __len__(self) -> int:
