871f5eb952c685d1109c6f3724a940a071aceeb4
Shreya Rajpal <shreya.rajpal@gmail.com>
2021-11-30 22:43:42 -0800

    Fixed tests in test_encoder.py

--- ludwig/modules/mlp_mixer_modules.py
+++ ludwig/modules/mlp_mixer_modules.py
@@ -1,4 +1,3 @@
-# coding=utf-8
 # Copyright (c) 2021 Linuf Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,13 +22,12 @@ from ludwig.utils.torch_utils import LudwigModule
 
 
 class MLP(LudwigModule):
-    def __init__(
-            self,
-            in_features: Union[int, Tuple[int]],
-            hidden_size: int,
-            out_features: Union[int, Tuple[int]] = None,
-            dropout: float = 0.0
-    ):
+
+    def __init__(self,
+                 in_features: Union[int, Tuple[int]],
+                 hidden_size: int,
+                 out_features: Union[int, Tuple[int]] = None,
+                 dropout: float = 0.0):
         """
         """
         super().__init__()
@@ -39,10 +37,8 @@ class MLP(LudwigModule):
         self._input_shape = in_features
         self._output_shape = out_features
 
-        self.linear1 = nn.Linear(
-            in_features=in_features, out_features=hidden_size)
-        self.linear2 = nn.Linear(
-            in_features=hidden_size, out_features=out_features)
+        self.linear1 = nn.Linear(in_features=in_features, out_features=hidden_size)
+        self.linear2 = nn.Linear(in_features=hidden_size, out_features=out_features)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
 
@@ -60,29 +56,20 @@ class MLP(LudwigModule):
 
 
 class MixerBlock(LudwigModule):
-    def __init__(
-            self,
-            embed_size: int,
-            n_patches: int,
-            token_dim: int,
-            channel_dim: int,
-            dropout: float = 0.0
-    ):
+
+    def __init__(self,
+                 embed_size: int,
+                 n_patches: int,
+                 token_dim: int,
+                 channel_dim: int,
+                 dropout: float = 0.0):
         super().__init__()
         self._input_shape = (n_patches, embed_size)
         self._output_shape = (n_patches, embed_size)
 
-        self.mlp1 = MLP(
-            in_features=n_patches,
-            hidden_size=token_dim,
-            dropout=dropout
-        )
+        self.mlp1 = MLP(in_features=n_patches, hidden_size=token_dim, dropout=dropout)
 
-        self.mlp2 = MLP(
-            in_features=embed_size,
-            hidden_size=channel_dim,
-            dropout=dropout
-        )
+        self.mlp2 = MLP(in_features=embed_size, hidden_size=channel_dim, dropout=dropout)
 
         self.layernorm1 = nn.LayerNorm(normalized_shape=embed_size)
         self.layernorm2 = nn.LayerNorm(normalized_shape=embed_size)
@@ -121,40 +108,36 @@ class MLPMixer(LudwigModule):
     """
 
     def __init__(
-            self,
-            img_height: int,
-            img_width: int,
-            in_channels: int,
-            patch_size: int = 16,
-            embed_size: int = 512,
-            token_size: int = 2048,
-            channel_dim: int = 256,
-            num_layers: int = 8,
-            dropout: float = 0.0,
-            avg_pool: bool = True,
+        self,
+        img_height: int,
+        img_width: int,
+        in_channels: int,
+        patch_size: int = 16,
+        embed_size: int = 512,
+        token_size: int = 2048,
+        channel_dim: int = 256,
+        num_layers: int = 8,
+        dropout: float = 0.0,
+        avg_pool: bool = True,
     ):
         super().__init__()
         assert (img_height % patch_size == 0) and (img_width % patch_size == 0)
 
         self._input_shape = (in_channels, img_height, img_width)
-        n_patches = int(img_height * img_width / (patch_size ** 2))
+        n_patches = int(img_height * img_width / (patch_size**2))
 
-        self.patch_conv = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=embed_size,
-            kernel_size=patch_size,
-            stride=patch_size
-        )
+        self.patch_conv = nn.Conv2d(in_channels=in_channels,
+                                    out_channels=embed_size,
+                                    kernel_size=patch_size,
+                                    stride=patch_size)
 
-        self.mixer_blocks = [
-            MixerBlock(
-                embed_size=embed_size,
-                n_patches=n_patches,
-                token_dim=token_size,
-                channel_dim=channel_dim,
-                dropout=dropout)
-            for _ in range(num_layers)
-        ]
+        self.mixer_blocks = nn.ModuleList([
+            MixerBlock(embed_size=embed_size,
+                       n_patches=n_patches,
+                       token_dim=token_size,
+                       channel_dim=channel_dim,
+                       dropout=dropout) for _ in range(num_layers)
+        ])
 
         self.layer_norm = nn.LayerNorm(normalized_shape=embed_size)
 
