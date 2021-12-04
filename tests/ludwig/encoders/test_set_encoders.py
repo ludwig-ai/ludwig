46f073059fcab0b2da6c45111525f66b2a886ebc
Shreya Rajpal <shreya.rajpal@gmail.com>
2021-12-03 19:50:58 -0800

    Moved set embed and inputs to correct device in tests/ludwig/encoders/test_set_encoders.py

--- tests/ludwig/encoders/test_set_encoders.py
+++ tests/ludwig/encoders/test_set_encoders.py
@@ -1,24 +1,18 @@
 from typing import List
-import pytest
 
+import pytest
 import torch
 
 from ludwig.encoders.set_encoders import SetSparseEncoder
 
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-@pytest.mark.parametrize('vocab', [['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']])
-@pytest.mark.parametrize('embedding_size', [10])
-@pytest.mark.parametrize('representation', ['sparse'])
-def test_set_encoder(
-        vocab: List[str],
-        embedding_size: int,
-        representation: str
-):
-    set_encoder = SetSparseEncoder(
-        vocab=vocab,
-        representation=representation,
-        embedding_size=embedding_size
-    )
-    inputs = torch.randint(0, 2, size=(2, len(vocab))).bool()
+
+@pytest.mark.parametrize("vocab", [["a", "b", "c", "d", "e", "f", "g", "h"]])
+@pytest.mark.parametrize("embedding_size", [10])
+@pytest.mark.parametrize("representation", ["sparse"])
+def test_set_encoder(vocab: List[str], embedding_size: int, representation: str):
+    set_encoder = SetSparseEncoder(vocab=vocab, representation=representation, embedding_size=embedding_size).to(DEVICE)
+    inputs = torch.randint(0, 2, size=(2, len(vocab))).bool().to(DEVICE)
     outputs = set_encoder(inputs)
     assert outputs.shape[1:] == set_encoder.output_shape
