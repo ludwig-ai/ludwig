30d0e0358b2602816e7a9b28122c87a0e4ee3bd6
Animesh Kumar <animesh@predibase.com>
2021-12-06 13:56:58 -0800

    Addressed Shreya's comments

--- tests/ludwig/encoders/test_h3_encoders.py
+++ tests/ludwig/encoders/test_h3_encoders.py
@@ -5,14 +5,14 @@ import torch
 from ludwig.encoders import h3_encoders
 
 logger = logging.getLogger(__name__)
-
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def test_h3_embed():
     embed = h3_encoders.H3Embed()
     inputs = torch.tensor(
         [[2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7],
          [2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7]],
-        dtype=torch.int32).to('cuda' if torch.cuda.is_available() else 'cpu')
+        dtype=torch.int32).to(DEVICE)
     outputs = embed(inputs)
     assert outputs['encoder_output'].size()[1:] == embed.output_shape
 
@@ -22,16 +22,16 @@ def test_h3_weighted_sum():
     inputs = torch.tensor(
         [[2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7],
          [2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7]],
-        dtype=torch.int32).to('cuda' if torch.cuda.is_available() else 'cpu')
+        dtype=torch.int32).to(DEVICE)
     outputs = embed(inputs)
     assert outputs['encoder_output'].size()[1:] == embed.output_shape
 
 
-# def test_h3_rnn_embed():
-#     embed = h3_encoders.H3RNN()
-#     inputs = torch.tensor(
-#         [[2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7],
-#          [2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7]],
-#         dtype=torch.int32).to('cuda' if torch.cuda.is_available() else 'cpu')
-#     outputs = embed(inputs)
-#     assert outputs['encoder_output'].size()[1:] == embed.output_shape
+def test_h3_rnn_embed():
+    embed = h3_encoders.H3RNN().to(DEVICE)
+    inputs = torch.tensor(
+        [[2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7],
+         [2, 0, 14, 102, 7, 0, 3, 5, 0, 5, 5, 0, 5, 7, 7, 7, 7, 7, 7]],
+        dtype=torch.int32).to('cuda' if torch.cuda.is_available() else 'cpu')
+    outputs = embed(inputs)
+    assert outputs['encoder_output'].size()[1:] == embed.output_shape
