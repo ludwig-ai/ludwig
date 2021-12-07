5b84bc004abd2fc9625f1abbe647a8055d3e5361
Shreya Rajpal <ShreyaR@users.noreply.github.com>
2021-12-07 11:12:50 -0800

    [torch] Fix GPU failures in test_regularizers.py #1572

--- tests/integration_tests/test_regularizers.py
+++ tests/integration_tests/test_regularizers.py
@@ -5,59 +5,44 @@ import numpy as np
 import pytest
 import torch
 
-
-from ludwig.utils.data_utils import read_csv
-from ludwig.data.preprocessing import preprocess_for_training
-from tests.integration_tests.utils import binary_feature, category_feature, \
-    date_feature, generate_data, image_feature, numerical_feature, \
-    sequence_feature, set_feature, LocalTestBackend
 from ludwig.api import LudwigModel
+from ludwig.data.preprocessing import preprocess_for_training
+from ludwig.utils.data_utils import read_csv
+from tests.integration_tests.utils import (
+    binary_feature,
+    category_feature,
+    date_feature,
+    generate_data,
+    image_feature,
+    LocalTestBackend,
+    numerical_feature,
+    sequence_feature,
+    set_feature,
+)
 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 32
 RANDOM_SEED = 42
 IMAGE_DIR = tempfile.mkdtemp()
 
 
 @pytest.mark.parametrize(
-    'input_features,output_features',
+    "input_features,output_features",
     [
         (
-            [numerical_feature(
-                num_layers=2,
-                encoder='dense',
-                preprocessing={'normalization': 'zscore'}
-            )],
-            [numerical_feature()]
+            [numerical_feature(num_layers=2, encoder="dense", preprocessing={"normalization": "zscore"})],
+            [numerical_feature()],
         ),
+        ([image_feature(IMAGE_DIR, encoder="stacked_cnn")], [numerical_feature()]),
+        ([image_feature(IMAGE_DIR, encoder="resnet")], [category_feature()]),
         (
-            [image_feature(IMAGE_DIR, encoder='stacked_cnn')],
-            [numerical_feature()]
+            [category_feature(representation="dense")],
+            [numerical_feature(decoder="regressor", loss={"type": "mean_squared_error"}, num_fc_layers=5)],
         ),
-        (
-            [image_feature(IMAGE_DIR, encoder='resnet')],
-            [category_feature()]
-        ),
-        (
-            [category_feature(representation='dense')],
-            [numerical_feature(
-                decoder='regressor',
-                loss={'type': 'mean_squared_error'},
-                num_fc_layers=5
-            )]
-        ),
-        (
-            [date_feature()],
-            [binary_feature()]
-        ),
-        (
-            [sequence_feature(encoder='parallel_cnn', cell_type='gru')],
-            [binary_feature()]
-        ),
-        (
-            [set_feature()],
-            [set_feature()]
-        )
-    ]
+        ([date_feature()], [binary_feature()]),
+        ([sequence_feature(encoder="parallel_cnn", cell_type="gru")], [binary_feature()]),
+        ([set_feature()], [set_feature()]),
+    ],
 )
 def test_regularizers(
     input_features,
@@ -68,48 +53,38 @@ def test_regularizers(
     torch.manual_seed(RANDOM_SEED)
     random.seed(0)
 
-    data_file = generate_data(
-        input_features,
-        output_features,
-        num_examples=BATCH_SIZE
-    )
+    data_file = generate_data(input_features, output_features, num_examples=BATCH_SIZE)
     data_df = read_csv(data_file)
 
     regularizer_losses = []
-    for regularization_type in [None, 'l1', 'l2', 'l1_l2']:
+    for regularization_type in [None, "l1", "l2", "l1_l2"]:
 
         config = {
-            'input_features': input_features,
-            'output_features': output_features,
-            'combiner': {'type': 'concat', 'fc_size': 14},
-            'training': {
-                'epochs': 2,
-                'regularization_type': regularization_type,
-                'regularization_lambda': 0.1
-            }
+            "input_features": input_features,
+            "output_features": output_features,
+            "combiner": {"type": "concat", "fc_size": 14},
+            "training": {"epochs": 2, "regularization_type": regularization_type, "regularization_lambda": 0.1},
         }
 
         backend = LocalTestBackend()
         model = LudwigModel(config, backend=backend)
-        processed_data_df, _, _, _ = preprocess_for_training(
-            config, data_df, backend=backend
-        )
+        processed_data_df, _, _, _ = preprocess_for_training(config, data_df, backend=backend)
         with processed_data_df.initialize_batcher(batch_size=BATCH_SIZE) as batcher:
             batch = batcher.next_batch()
 
         _, _, _ = model.train(
-                training_set=data_df,
-                skip_save_processed_input=True,
-                skip_save_progress=True,
-                skip_save_unprocessed_output=True
+            training_set=data_df,
+            skip_save_processed_input=True,
+            skip_save_progress=True,
+            skip_save_unprocessed_output=True,
         )
 
         inputs = {
-            i_feat.feature_name: batch[i_feat.proc_column]
+            i_feat.feature_name: torch.from_numpy(batch[i_feat.proc_column]).to(DEVICE)
             for i_feat in model.model.input_features.values()
         }
         targets = {
-            o_feat.feature_name: batch[o_feat.proc_column]
+            o_feat.feature_name: torch.from_numpy(batch[o_feat.proc_column]).to(DEVICE)
             for o_feat in model.model.output_features.values()
         }
         predictions = model.model((inputs, targets))
@@ -117,17 +92,13 @@ def test_regularizers(
         loss, _ = model.model.train_loss(targets, predictions, regularization_type, 0.1)
         regularizer_losses.append(loss)
 
-    print(regularizer_losses)
-
     # Regularizer_type=None has lowest regularizer loss
     assert min(regularizer_losses) == regularizer_losses[0]
 
     # l1, l2 and l1_l2 should be greater than zero
-    assert np.all([t - regularizer_losses[0] > 0.0 for t in regularizer_losses[1:]])
+    assert torch.all(torch.tensor([t - regularizer_losses[0] > 0.0 for t in regularizer_losses[1:]]))
 
     # using default setting l1 + l2 == l1_l2 losses
     assert torch.isclose(
-        regularizer_losses[1] + regularizer_losses[2] - regularizer_losses[0],
-        regularizer_losses[3],
-        rtol=0.1
+        regularizer_losses[1] + regularizer_losses[2] - regularizer_losses[0], regularizer_losses[3], rtol=0.1
     )
