1816f916b15ca01744edcb2c80262d22a85cca12
Daniel Treiman <daniel@predibase.com>
2021-12-13 16:02:19 -0800

    Implemented LudwigModule's input_shape instead of infer_output_shape.

--- ludwig/modules/reduction_modules.py
+++ ludwig/modules/reduction_modules.py
@@ -18,39 +18,56 @@ import torch
 
 from ludwig.modules.attention_modules import FeedForwardAttentionReducer
 from ludwig.utils.misc_utils import get_from_registry
+from ludwig.utils.torch_utils import LudwigModule
 from ludwig.utils.torch_utils import sequence_length_3D
 
 logger = logging.getLogger(__name__)
 
 
-class SequenceReducer(torch.nn.Module):
-    def __init__(self, reduce_mode=None, **kwargs):
+class SequenceReducer(LudwigModule):
+    """Reduces the sequence dimension of an input tensor according to the specified reduce_mode.  Any additional kwargs
+    are passed on to the reduce mode's constructor.  If using reduce_mode=="attention", the input_size kwarg must also
+    be specified.
+
+    A sequence is a tensor of 2 or more dimensions, where the shape is [batch size x sequence length x ...].
+
+    :param reduce_mode: The reduction mode, one of {"last", "sum", "mean", "max", "concat", "attention", "none"}
+    :param max_sequence_length The maximum sequence length.
+    :param embedding_size The size of each sequence element/embedding vector, or None if input is a sequence of scalars.
+    """
+    def __init__(self, reduce_mode: str = None, max_sequence_length: int = None, embedding_size: int = None, **kwargs):
         super().__init__()
         # save as private variable for debugging
         self._reduce_mode = reduce_mode
-
+        self._max_sequence_length = max_sequence_length
+        self._embedding_size = embedding_size
+        # If embedding size specified and mode is attention, use embedding size as attention module input size
+        # unless the input_size kwarg is provided.
+        if reduce_mode == "attention" and embedding_size and 'input_size' not in kwargs:
+            kwargs['input_size'] = embedding_size
         # use registry to find required reduction function
         self._reduce_obj = get_from_registry(reduce_mode, reduce_mode_registry)(**kwargs)
 
     def forward(self, inputs, mask=None):
+        """Forward pass of reducer.
+
+            :param inputs: A tensor of 2 or more dimensions, where the shape is [batch size x sequence length x ...].
+            :param mask: A mask tensor of 2 dimensions [batch size x sequence length].  Not yet implemented.
+
+            :return: The input after applying the reduction operation to sequence dimension.
+        """
         return self._reduce_obj(inputs, mask=mask)
 
-    def infer_output_shape(self, input_shape):
-        """Infers output shape from input using the specified reduction mode.
-
-        :param input_shape: The shape of the input omitting any batch dimensions,
-                            which is typically [sequence length x embedding size].
-
-        :param return: The output shape after reduction.
-        """
-        if self._reduce_mode in {None, "none", "None"}:
-            return input_shape
-        elif self._reduce_mode == "concat":
-            if len(input_shape) > 1:
-                return input_shape[:-2] + (input_shape[-1] * input_shape[-2],)
-            return input_shape
+    @property
+    def input_shape(self) -> torch.Size:
+        """Returns size of the input tensor without the batch dimension."""
+        if self._max_sequence_length is None:
+            raise RuntimeError(
+                "SequenceReducer must be initialized with max_sequence_length to get input_shape or output_shape.")
+        elif self._embedding_size is None:
+            return torch.Size([self._max_sequence_length])
         else:
-            return input_shape[1:]  # Reduce sequence dimension).
+            return torch.Size([self._max_sequence_length, self._embedding_size])
 
 
 class ReduceLast(torch.nn.Module):
--- tests/ludwig/modules/test_reduction_modules.py
+++ tests/ludwig/modules/test_reduction_modules.py
@@ -9,16 +9,18 @@ from ludwig.modules import reduction_modules
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-@pytest.mark.parametrize("reduce_mode", ["last", "sum", "mean", "avg", "max", "concat", None])
+@pytest.mark.parametrize("reduce_mode", ["last", "sum", "mean", "avg", "max", "concat", "attention", None])
 @pytest.mark.parametrize("test_input_shape", [(16, 1, 4), (4, 10, 16)])
 def test_sequence_reducer(reduce_mode: str, test_input_shape: Tuple[int, ...]):
-    batch_size = test_input_shape[0]
-    max_sequence_length = test_input_shape[1]
-    sequence_reducer = reduction_modules.SequenceReducer(reduce_mode=reduce_mode).to(DEVICE)
+    (batch_size, max_sequence_length, embedding_size) = test_input_shape
+    sequence_reducer = reduction_modules.SequenceReducer(
+        reduce_mode=reduce_mode,
+        max_sequence_length=max_sequence_length,
+        embedding_size=embedding_size).to(DEVICE)
     inputs = torch.zeros(test_input_shape)
     # Generates random sequence of random length for each instance in batch.
     for batch_index in range(batch_size):
         sequence_length = torch.randint(max_sequence_length, (1,))
-        inputs[batch_index, :sequence_length] = torch.rand((sequence_length,) + test_input_shape[2:])
+        inputs[batch_index, :sequence_length] = torch.rand((sequence_length, embedding_size))
     outputs = sequence_reducer(inputs.to(DEVICE))
-    assert outputs.shape[1:] == sequence_reducer.infer_output_shape(test_input_shape[1:])
+    assert outputs.shape[1:] == sequence_reducer.output_shape
