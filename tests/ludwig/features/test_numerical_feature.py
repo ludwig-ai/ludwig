e13bd0db04ad8a6c2aea2da35f7db9b874ec3fdf
Saikat Kanjilal <sxk1969@hotmail.com>
2021-12-15 10:41:48 -0800

    initial cut around numerical tests

--- tests/ludwig/features/test_numerical_feature.py
+++ tests/ludwig/features/test_numerical_feature.py
@@ -15,29 +15,7 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 def numerical_config():
     return {
         "name": "numerical_column_name",
-        "type": "numerical",
-        "vocab": ["a", "b", "c"],
-        "representation": "dense",
-        "encoder": "embed",
-        "embedding_size": 50,
-        "embeddings_trainable": True,
-        "pretrained_embeddings": None,
-        "embeddings_on_cpu": False,
-        "fc_layers": None,
-        "num_fc_layers": 0,
-        "fc_size": 0,
-        "use_bias": True,
-        "weights_initializer": "uniform",
-        "bias_initializer": "zeros",
-        "weights_regularizer": None,
-        "bias_regularizer": None,
-        "activity_regularizer": None,
-        "norm": None,
-        "norm_params": None,
-        "activation": "relu",
-        "dropout": 0.0,
-        "reduce_output": "sum",
-        "tied_weights": None,
+        "type": "numerical"
     }
 
 
@@ -54,7 +32,7 @@ def test_numerical_input_feature(
     input_feature_obj = build_single_input(numerical_def, None).to(DEVICE)
 
     # check one forward pass through input feature
-    input_tensor = torch.randint(0, 3, size=(BATCH_SIZE,), dtype=torch.int32).to(DEVICE)
+    input_tensor = torch.rand(2, dtype=torch.float32).to(DEVICE)
 
     encoder_output = input_feature_obj(input_tensor)
     assert encoder_output["encoder_output"].shape == (BATCH_SIZE, *input_feature_obj.output_shape)
