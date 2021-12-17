7f7ed35e6c5ec120212b3530495a6d39ca68439a
ksbrar <kabir@brar.xyz>
2021-12-17 16:02:41 -0500

    Fix validation ranges for early_stop - match test expectations?

--- ludwig/models/trainer.py
+++ ludwig/models/trainer.py
@@ -105,7 +105,7 @@ class TrainerConfig:
     learning_rate: float = schema.FloatRange(default=0.001)  # , min=0.0, max=1.0) # TODO: What are the bounds here?
     batch_size: int = schema.NonNegativeInteger(default=128)
     eval_batch_size: Optional[int] = schema.NonNegativeInteger(None)
-    early_stop: int = schema.PositiveInteger(5)
+    early_stop: int = schema.NonNegativeInteger(5)
     reduce_learning_rate_on_plateau: float = schema.FloatRange(default=0.0, min=0.0, max=1.0)
     reduce_learning_rate_on_plateau_patience: int = schema.NonNegativeInteger(5)
     reduce_learning_rate_on_plateau_rate: float = schema.FloatRange(default=0.5, min=0.0, max=1.0)
