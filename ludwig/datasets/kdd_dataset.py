ae9cce4fc6467c37f9c4f1ac80d398064a3024ee
Anne Holler <anne@vmware.com>
2022-01-23 11:12:03 -0800

    [crossport from master] For kdd datasets, do not include unlabeled test data by default

--- ludwig/datasets/kdd_dataset.py
+++ ludwig/datasets/kdd_dataset.py
@@ -35,9 +35,10 @@ class KDDCup2009Dataset(UncompressedFileDownloadMixin,
     https://www.kdd.org/kdd-cup/view/kdd-cup-2009/Data
     """
 
-    def __init__(self, task_name, cache_dir=DEFAULT_CACHE_LOCATION):
+    def __init__(self, task_name, cache_dir=DEFAULT_CACHE_LOCATION, include_test_download=False):
         super().__init__(dataset_name="kdd_" + task_name, cache_dir=cache_dir)
         self.task_name = task_name
+        self.include_test_download = include_test_download
 
     def process_downloaded_dataset(self, header=0):
         zip_file = ZipFile(
@@ -45,12 +46,6 @@ class KDDCup2009Dataset(UncompressedFileDownloadMixin,
         )
         train_df = pd.read_csv(zip_file.open("orange_small_train.data"),
                                sep='\t')
-
-        zip_file = ZipFile(
-            os.path.join(self.raw_dataset_path, "orange_small_test.data.zip")
-        )
-        test_df = pd.read_csv(zip_file.open("orange_small_test.data"),
-                              sep='\t')
 
         train_df = process_categorical_features(train_df, categorical_features)
         train_df = process_numerical_features(train_df, categorical_features)
@@ -87,10 +82,17 @@ class KDDCup2009Dataset(UncompressedFileDownloadMixin,
         processed_val_df['target'] = targets.iloc[val_idcs]
         processed_val_df['split'] = 1
 
-        test_df['target'] = ''
-        test_df['split'] = 2
-
-        df = pd.concat([processed_train_df, processed_val_df, test_df])
+        if self.include_test_download:
+            zip_file = ZipFile(
+                os.path.join(self.raw_dataset_path, "orange_small_test.data.zip")
+            )
+            test_df = pd.read_csv(zip_file.open("orange_small_test.data"),
+                                  sep='\t')
+            test_df['target'] = ''  # no ground truth labels for test download
+            test_df['split'] = 2
+            df = pd.concat([processed_train_df, processed_val_df, test_df])
+        else:
+            df = pd.concat([processed_train_df, processed_val_df])
 
         makedirs(self.processed_temp_path, exist_ok=True)
         df.to_csv(os.path.join(self.processed_temp_path, self.csv_filename),
