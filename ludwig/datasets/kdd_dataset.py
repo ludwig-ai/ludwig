404474b1631905784028b9b4e100bdc48e9bbabb
amholler <86269492+amholler@users.noreply.github.com>
2022-01-25 12:29:24 -0800

    For kdd datasets, fetch w/binary and do not include unlabeled test data by default (#1703)
    Co-authored-by: Anne Holler <anne@vmware.com>

--- ludwig/datasets/kdd_dataset.py
+++ ludwig/datasets/kdd_dataset.py
@@ -19,13 +19,13 @@ from zipfile import ZipFile
 import pandas as pd
 
 from ludwig.datasets.base_dataset import BaseDataset, DEFAULT_CACHE_LOCATION
-from ludwig.datasets.mixins.download import UncompressedFileDownloadMixin
+from ludwig.datasets.mixins.download import BinaryFileDownloadMixin
 from ludwig.datasets.mixins.load import CSVLoadMixin
 from ludwig.datasets.mixins.process import MultifileJoinProcessMixin
 from ludwig.utils.fs_utils import makedirs, rename
 
 
-class KDDCup2009Dataset(UncompressedFileDownloadMixin, MultifileJoinProcessMixin, CSVLoadMixin, BaseDataset):
+class KDDCup2009Dataset(BinaryFileDownloadMixin, MultifileJoinProcessMixin, CSVLoadMixin, BaseDataset):
     """The KDD Cup 2009 dataset base class.
 
     Additional Details:
@@ -33,16 +33,14 @@ class KDDCup2009Dataset(UncompressedFileDownloadMixin, MultifileJoinProcessMixin
     https://www.kdd.org/kdd-cup/view/kdd-cup-2009/Data
     """
 
-    def __init__(self, task_name, cache_dir=DEFAULT_CACHE_LOCATION):
+    def __init__(self, task_name, cache_dir=DEFAULT_CACHE_LOCATION, include_test_download=False):
         super().__init__(dataset_name="kdd_" + task_name, cache_dir=cache_dir)
         self.task_name = task_name
+        self.include_test_download = include_test_download
 
     def process_downloaded_dataset(self, header=0):
         zip_file = ZipFile(os.path.join(self.raw_dataset_path, "orange_small_train.data.zip"))
         train_df = pd.read_csv(zip_file.open("orange_small_train.data"), sep="\t")
-
-        zip_file = ZipFile(os.path.join(self.raw_dataset_path, "orange_small_test.data.zip"))
-        test_df = pd.read_csv(zip_file.open("orange_small_test.data"), sep="\t")
 
         train_df = process_categorical_features(train_df, categorical_features)
         train_df = process_numerical_features(train_df, categorical_features)
@@ -71,10 +69,14 @@ class KDDCup2009Dataset(UncompressedFileDownloadMixin, MultifileJoinProcessMixin
         processed_val_df["target"] = targets.iloc[val_idcs]
         processed_val_df["split"] = 1
 
-        test_df["target"] = ""
-        test_df["split"] = 2
-
-        df = pd.concat([processed_train_df, processed_val_df, test_df])
+        if self.include_test_download:
+            zip_file = ZipFile(os.path.join(self.raw_dataset_path, "orange_small_test.data.zip"))
+            test_df = pd.read_csv(zip_file.open("orange_small_test.data"), sep="\t")
+            test_df["target"] = ""  # no ground truth labels for test download
+            test_df["split"] = 2
+            df = pd.concat([processed_train_df, processed_val_df, test_df])
+        else:
+            df = pd.concat([processed_train_df, processed_val_df])
 
         makedirs(self.processed_temp_path, exist_ok=True)
         df.to_csv(os.path.join(self.processed_temp_path, self.csv_filename), index=False)
