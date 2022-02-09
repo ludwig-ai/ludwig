7999e06f75b19a0e6427c71f6d22068b16932261
Connor <connor@predibase.com>
2022-02-09 14:28:17 -0800

    Passing test_balance_data

--- tests/ludwig/utils/test_class_balancing.py
+++ tests/ludwig/utils/test_class_balancing.py
@@ -5,91 +5,48 @@ from ludwig.data.preprocessing import balance_data
 from ludwig.backend import initialize_backend, RAY
 from ludwig.backend.base import Backend, LocalBackend
 
-fraud_inputs = [
-    {"name": "Time", "type": "numerical", "output_flag": False},
-    {"name": "V1", "type": "numerical", "output_flag": False},
-    {"name": "V2", "type": "numerical", "output_flag": False},
-    {"name": "V3", "type": "numerical", "output_flag": False},
-    {"name": "V4", "type": "numerical", "output_flag": False},
-    {"name": "V5", "type": "numerical", "output_flag": False},
-    {"name": "V6", "type": "numerical", "output_flag": False},
-    {"name": "V7", "type": "numerical", "output_flag": False},
-    {"name": "V8", "type": "numerical", "output_flag": False},
-    {"name": "V9", "type": "numerical", "output_flag": False},
-    {"name": "V10", "type": "numerical", "output_flag": False},
-    {"name": "V11", "type": "numerical", "output_flag": False},
-    {"name": "V12", "type": "numerical", "output_flag": False},
-    {"name": "V13", "type": "numerical", "output_flag": False},
-    {"name": "V14", "type": "numerical", "output_flag": False},
-    {"name": "V15", "type": "numerical", "output_flag": False},
-    {"name": "V16", "type": "numerical", "output_flag": False},
-    {"name": "V17", "type": "numerical", "output_flag": False},
-    {"name": "V18", "type": "numerical", "output_flag": False},
-    {"name": "V19", "type": "numerical", "output_flag": False},
-    {"name": "V20", "type": "numerical", "output_flag": False},
-    {"name": "V21", "type": "numerical", "output_flag": False},
-    {"name": "V22", "type": "numerical", "output_flag": False},
-    {"name": "V23", "type": "numerical", "output_flag": False},
-    {"name": "V24", "type": "numerical", "output_flag": False},
-    {"name": "V25", "type": "numerical", "output_flag": False},
-    {"name": "V26", "type": "numerical", "output_flag": False},
-    {"name": "V27", "type": "numerical", "output_flag": False},
-    {"name": "V28", "type": "numerical", "output_flag": False},
-    {"name": "Amount", "type": "numerical", "output_flag": False},
-]
-
-fraud_outputs = [
-    {"name": "Class", "type": "binary", "output_flag": True}
-]
+DFS = {
+    "test_df_1": pd.DataFrame({"Index": np.arange(0, 200, 1),
+                               "random_1": np.random.randint(0, 50, 200),
+                               "random_2": np.random.choice(['Type A', 'Type B', 'Type C', 'Type D'], 200),
+                               "Label": np.random.choice(2, 200, p=[0.9, 0.1])})
+}
 
 CONFIGS = {
     "test_oversample": {
-        "input_features": fraud_inputs,
-        "output_features": fraud_outputs,
+        "input_features": [
+            {"name": "Index", "proc_column": "Index_1234", "type": "numerical", "output_flag": False},
+            {"name": "random_1", "proc_column": "random_1_2345", "type": "numerical", "output_flag": False},
+            {"name": "random_2", "proc_column": "random_2_3456", "type": "numerical", "output_flag": False}],
+        "output_features": [
+            {"name": "Label", "proc_column": "Label_4567", "type": "numerical", "output_flag": True}
+        ],
         "preprocessing": {
+            "undersample_majority": None,
             "oversample_minority": 0.5
         }
     },
     "test_undersample": {
-        "input_features": fraud_inputs,
-        "output_features": fraud_outputs,
+        "input_features": [
+            {"name": "Index", "proc_column": "Index_1234", "type": "numerical", "output_flag": False},
+            {"name": "random_1", "proc_column": "random_1_2345", "type": "numerical", "output_flag": False},
+            {"name": "random_2", "proc_column": "random_2_3456", "type": "numerical", "output_flag": False}],
+        "output_features": [
+            {"name": "Label", "proc_column": "Label_4567", "type": "numerical", "output_flag": True}
+        ],
         "preprocessing": {
-            "undersample_majority": 0.5
+            "undersample_majority": 0.5,
+            "oversample_minority": None
         }
     }
 }
 
-DFS = {
-    "test_df_1": pd.DataFrame({"Index": np.arange(0, 100, 1),
-                               "random_1": np.random.randint(0, 50, 100),
-                               "random_2": np.random.choice(['Type A', 'Type B', 'Type C', 'Type D'], 100),
-                               "Label": np.random.choice(2, 100, p=[0.8, 0.2])}),
-    "test_df_2": pd.read_csv("./creditcard.csv")
-}
-
 TEST_COLS = {
-    "df1_cols": {"Index": DFS["test_df_1"]["Index"],
-                 "random_1": DFS["test_df_1"]["random_1"],
-                 "random_2": DFS["test_df_1"]["random_2"],
-                 "Label": DFS["test_df_1"]["Label"]
-                 },
-    "df2_cols": {"V1": DFS["test_df_2"]["V1"], "V2": DFS["test_df_2"]["V2"],
-                 "V3": DFS["test_df_2"]["V3"], "V4": DFS["test_df_2"]["V4"],
-                 "V5": DFS["test_df_2"]["V5"], "V6": DFS["test_df_2"]["V6"],
-                 "V7": DFS["test_df_2"]["V7"], "V8": DFS["test_df_2"]["V8"],
-                 "V9": DFS["test_df_2"]["V9"], "V10": DFS["test_df_2"]["V10"],
-                 "V11": DFS["test_df_2"]["V11"], "V12": DFS["test_df_2"]["V12"],
-                 "V13": DFS["test_df_2"]["V13"], "V14": DFS["test_df_2"]["V14"],
-                 "V15": DFS["test_df_2"]["V15"], "V16": DFS["test_df_2"]["V16"],
-                 "V17": DFS["test_df_2"]["V17"], "V18": DFS["test_df_2"]["V18"],
-                 "V19": DFS["test_df_2"]["V19"], "V20": DFS["test_df_2"]["V20"],
-                 "V21": DFS["test_df_2"]["V21"], "V22": DFS["test_df_2"]["V22"],
-                 "V23": DFS["test_df_2"]["V23"], "V24": DFS["test_df_2"]["V24"],
-                 "V25": DFS["test_df_2"]["V25"], "V26": DFS["test_df_2"]["V26"],
-                 "V27": DFS["test_df_2"]["V27"], "V28": DFS["test_df_2"]["V28"],
-                 "Amount": DFS["test_df_2"]["Amount"], "Class": DFS["test_df_2"]["Class"],
-                 "Time": DFS["test_df_2"]["Time"],
-                 "split": pd.Series(np.random.choice(3, len(DFS["test_df_2"]), p=(0.7, 0.1, 0.2)))
+    "df1_cols": {"Index_1234": DFS["test_df_1"]["Index"],
+                 "random_1_2345": DFS["test_df_1"]["random_1"],
+                 "random_2_3456": DFS["test_df_1"]["random_2"],
+                 "Label_4567": DFS["test_df_1"]["Label"],
+                 "split": pd.Series(np.random.choice(3, len(DFS["test_df_1"]), p=(0.7, 0.1, 0.2)))
                  }
 }
 
@@ -98,38 +55,29 @@ REMOTE_BACKEND = initialize_backend(RAY)
 
 
 @pytest.mark.parametrize(
-    "config, df, cols, backend",
+    "config, backend",
     [
-        ("test_oversample", "test_df_1", "df1_cols", LOCAL_BACKEND),
-        ("test_undersample", "test_df_1", "df1_cols", LOCAL_BACKEND),
-        ("test_oversample", "test_df_2", "df2_cols", LOCAL_BACKEND),
-        ("test_undersample", "test_df_2", "df2_cols", LOCAL_BACKEND),
-        ("test_oversample", "test_df_1", "df1_cols", REMOTE_BACKEND),
-        ("test_undersample", "test_df_1", "df1_cols", REMOTE_BACKEND),
-        ("test_oversample", "test_df_2", "df2_cols", REMOTE_BACKEND),
-        ("test_undersample", "test_df_2", "df2_cols", REMOTE_BACKEND)
-    ]
+        ("test_oversample", LOCAL_BACKEND),
+        ("test_undersample", LOCAL_BACKEND),
+        ("test_oversample", REMOTE_BACKEND),
+        ("test_undersample", REMOTE_BACKEND),
+    ],
 )
-def test_balance_data_synthetic(config, df, cols, backend):
-    test_proc_cols, test_df = balance_data(DFS[df],
-                                           TEST_COLS[cols],
+def test_balance_data(config, backend):
+    test_proc_cols, test_df = balance_data(DFS['test_df_1'],
+                                           TEST_COLS['df1_cols'],
                                            CONFIGS[config]['input_features'] + CONFIGS[config]['output_features'],
                                            CONFIGS[config]['preprocessing'],
                                            backend,
                                            )
-    assert len(test_proc_cols) == 4
-    assert len(test_df) == 100
+    balanced_train = test_df[test_df.split == 0]
+    new_class_balance = round(balanced_train["Label_4567"].value_counts()[
+                                  balanced_train["Label_4567"].value_counts().idxmin()] / \
+                              balanced_train["Label_4567"].value_counts()[
+                                  balanced_train["Label_4567"].value_counts().idxmax()], 1)
+    assert len(test_proc_cols) == 5
+    assert new_class_balance == 0.5
 
-@pytest.mark.parametrize
-def test_balance_data_fraud(config, df, cols, backend):
-    test_proc_cols, test_df = balance_data(DFS[df],
-                                           TEST_COLS[cols],
-                                           CONFIGS[config]['input_features'] + CONFIGS[config]['output_features'],
-                                           CONFIGS[config]['preprocessing'],
-                                           backend,
-                                           )
-    assert len(test_proc_cols) == 4
-    assert len(test_df) == 100
 
 def test_build_dataset(config):
     assert True is True
