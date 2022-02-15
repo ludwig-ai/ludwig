a5339b988fd28fd611cdaa08b64f4c6ff0e3f229
amholler <86269492+amholler@users.noreply.github.com>
2022-02-15 11:03:10 -0800

    [crossport from tf-legacy] Address issues when AutoML tune_for_memory parameter set true (#1752)
    * [crossport from tf-legacy] Address issues when AutoML tune_for_memory parameter set true
    
    * Fix memory usage to return bytes (as intended) not bits, and update to allow reduction of upper bound to match min acceptable value
    
    Co-authored-by: Anne Holler <anne@vmware.com>

--- ludwig/automl/auto_tune_config.py
+++ ludwig/automl/auto_tune_config.py
@@ -61,7 +61,7 @@ def get_machine_memory():
         @ray.remote(num_gpus=1)
         def get_remote_gpu():
             gpus = GPUtil.getGPUs()
-            total_mem_mb = gpus[0].memory_total
+            total_mem_mb = gpus[0].memoryTotal
             return total_mem_mb * BYTES_PER_MiB
 
         @ray.remote(num_cpus=1)
@@ -77,7 +77,7 @@ def get_machine_memory():
             machine_mem = ray.get(get_remote_cpu.remote())
     else:  # not using ray cluster
         if GPUtil.getGPUs():
-            machine_mem = GPUtil.getGPUs()[0].memory_total * BYTES_PER_MiB
+            machine_mem = GPUtil.getGPUs()[0].memoryTotal * BYTES_PER_MiB
         else:
             machine_mem = psutil.virtual_memory().total
 
@@ -91,8 +91,8 @@ def compute_memory_usage(config, training_set_metadata) -> int:
     total_size = 0
     batch_size = config[TRAINER][BATCH_SIZE]
     for tnsr in model_tensors:
-        total_size += tnsr[1].numpy().size * batch_size
-    total_bytes = total_size * 32  # assumes 32-bit precision
+        total_size += tnsr[1].detach().numpy().size * batch_size
+    total_bytes = total_size * 4  # assumes 32-bit precision = 4 bytes
     return total_bytes
 
 
@@ -107,10 +107,11 @@ def sub_new_params(config: dict, new_param_vals: dict):
 
 def get_new_params(current_param_values, hyperparam_search_space, params_to_modify):
     for param, _ in params_to_modify.items():
-        if hyperparam_search_space[param][SPACE] == "choice":
-            current_param_values[param] = hyperparam_search_space[param]["categories"][-1]
-        else:
-            current_param_values[param] = hyperparam_search_space[param]["upper"]
+        if param in hyperparam_search_space:
+            if hyperparam_search_space[param][SPACE] == "choice":
+                current_param_values[param] = hyperparam_search_space[param]["categories"][-1]
+            else:
+                current_param_values[param] = hyperparam_search_space[param]["upper"]
     return current_param_values
 
 
@@ -119,12 +120,16 @@ def memory_tune_config(config, dataset):
     raw_config = merge_with_defaults(config)
     training_set_metadata = get_trainingset_metadata(raw_config, dataset)
     modified_hyperparam_search_space = copy.deepcopy(raw_config[HYPEROPT]["parameters"])
-    params_to_modify = RANKED_MODIFIABLE_PARAM_LIST[get_model_name(raw_config)]
-    param_list = list(params_to_modify.keys())
     current_param_values = {}
-    max_memory = get_machine_memory()
+    param_list = []
+    model_name = get_model_name(raw_config)
+    if model_name in RANKED_MODIFIABLE_PARAM_LIST:
+        params_to_modify = RANKED_MODIFIABLE_PARAM_LIST[model_name]
+        if len(params_to_modify.keys()) > 0:
+            param_list = list(params_to_modify.keys())
+            max_memory = get_machine_memory()
 
-    while param_list is not None:
+    while param_list:
         # compute memory utilization
         current_param_values = get_new_params(current_param_values, modified_hyperparam_search_space, params_to_modify)
         temp_config = sub_new_params(raw_config, current_param_values)
@@ -139,7 +144,7 @@ def memory_tune_config(config, dataset):
             if param_space == "choice":
                 if (
                     len(modified_hyperparam_search_space[param]["categories"]) > 2
-                    and modified_hyperparam_search_space[param]["categories"][-2] > min_value
+                    and modified_hyperparam_search_space[param]["categories"][-2] >= min_value
                 ):
                     modified_hyperparam_search_space[param]["categories"] = modified_hyperparam_search_space[param][
                         "categories"
