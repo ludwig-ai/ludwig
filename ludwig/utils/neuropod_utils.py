1d3ba6ec50fc8987109527233f8ac71682909a33
Travis Addair <tgaddair@gmail.com>
2022-03-01 11:15:24 -0800

    Removed deprecated code paths

--- ludwig/utils/neuropod_utils.py
+++ ludwig/utils/neuropod_utils.py
@@ -1,32 +1,13 @@
 import importlib.util
 import logging
 import os
-import shutil
 import tempfile
 from typing import Any, Dict, List
 
-import numpy as np
 import torch
 
-from ludwig import __file__ as ludwig_path
 from ludwig.api import LudwigModel
-from ludwig.backend import LOCAL_BACKEND, LocalBackend
-from ludwig.constants import (
-    BINARY,
-    CATEGORY,
-    NAME,
-    NUMBER,
-    PREDICTIONS,
-    PROBABILITIES,
-    PROBABILITY,
-    SEQUENCE,
-    SET,
-    TEXT,
-    TYPE,
-    VECTOR,
-)
-from ludwig.globals import MODEL_HYPERPARAMETERS_FILE_NAME, MODEL_WEIGHTS_FILE_NAME, TRAIN_SET_METADATA_FILE_NAME
-from ludwig.utils.data_utils import load_json
+from ludwig.constants import NAME
 
 logger = logging.getLogger(__name__)
 
@@ -89,14 +70,6 @@ def generate_neuropod_torchscript(model: LudwigModel):
                 )
             )
 
-        print(
-            INFERENCE_MODULE_TEMPLATE.format(
-                input_signature=_get_input_signature(config),
-                input_dict=_get_input_dict(config),
-                output_dicts=_get_output_dicts(config),
-            )
-        )
-
         spec = importlib.util.spec_from_file_location("generated.ts", ts_path)
         gen_ts = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(gen_ts)
@@ -110,11 +83,9 @@ def _get_input_spec(model: LudwigModel) -> List[Dict[str, Any]]:
     spec = []
     for feature_name, feature in model.model.input_features.items():
         metadata = model.training_set_metadata[feature_name]
-        print(feature, feature.get_preproc_input_dtype(metadata))
         spec.append(
             {"name": feature.feature_name, "dtype": feature.get_preproc_input_dtype(metadata), "shape": ("batch_size",)}
         )
-    print("INPUT SPEC", spec)
     return spec
 
 
@@ -129,7 +100,6 @@ def _get_output_spec(model: LudwigModel) -> List[Dict[str, Any]]:
                 "shape": ("batch_size",),
             }
         )
-    print("OUTPUT SPEC", spec)
     return spec
 
 
@@ -147,184 +117,3 @@ def export_neuropod(model: LudwigModel, neuropod_path: str, neuropod_model_name=
         input_spec=_get_input_spec(model),
         output_spec=_get_output_spec(model),
     )
-
-
-class LudwigNeuropodModelWrapper:
-    def __init__(self, data_root, backend):
-        self.ludwig_model = LudwigModel.load(data_root, backend=backend)
-
-    def __call__(self, **kwargs):
-        data_dict = kwargs
-        for key in data_dict:
-            data_dict[key] = np.squeeze(data_dict[key], axis=1)
-        predicted, _ = self.ludwig_model.predict(dataset=data_dict, return_type="dict")
-        # print(predicted, file=sys.stderr)
-        return postprocess_for_neuropod(predicted, self.ludwig_model.config)
-
-
-def get_model(data_root):
-    return LudwigNeuropodModelWrapper(data_root, backend=LOCAL_BACKEND)
-
-
-def get_test_model(data_root):
-    class LocalTestBackend(LocalBackend):
-        @property
-        def supports_multiprocessing(self):
-            return False
-
-    return LudwigNeuropodModelWrapper(data_root, backend=LocalTestBackend())
-
-
-def postprocess_for_neuropod(predicted, config):
-    postprocessed = {}
-    for output_feature in config["output_features"]:
-        feature_name = output_feature[NAME]
-        feature_type = output_feature[TYPE]
-        if feature_type == BINARY:
-            postprocessed[feature_name + "_predictions"] = np.expand_dims(
-                predicted[feature_name][PREDICTIONS].astype("str"), 1
-            )
-            postprocessed[feature_name + "_probability"] = np.expand_dims(
-                predicted[feature_name][PROBABILITY].astype("float64"), 1
-            )
-            postprocessed[feature_name + "_probabilities"] = predicted[feature_name][PROBABILITIES].astype("float64")
-        elif feature_type == NUMBER:
-            postprocessed[feature_name + "_predictions"] = np.expand_dims(
-                predicted[feature_name][PREDICTIONS].astype("float64"), 1
-            )
-        elif feature_type == CATEGORY:
-            postprocessed[feature_name + "_predictions"] = np.expand_dims(
-                np.array(predicted[feature_name][PREDICTIONS], dtype="str"), 1
-            )
-            postprocessed[feature_name + "_probability"] = np.expand_dims(
-                predicted[feature_name][PROBABILITY].astype("float64"), 1
-            )
-            postprocessed[feature_name + "_probabilities"] = predicted[feature_name][PROBABILITIES].astype("float64")
-        elif feature_type == SEQUENCE or feature_type == TEXT:
-            predictions = list(map(lambda x: " ".join(x), predicted[feature_name][PREDICTIONS]))
-            postprocessed[feature_name + "_predictions"] = np.expand_dims(np.array(predictions, dtype="str"), 1)
-        elif feature_type == SET:
-            predictions = list(map(lambda x: " ".join(x), predicted[feature_name][PREDICTIONS]))
-            postprocessed[feature_name + "_predictions"] = np.expand_dims(np.array(predictions, dtype="str"), 1)
-            probability = list(map(lambda x: " ".join([str(e) for e in x]), predicted[feature_name]["probability"]))
-            postprocessed[feature_name + "_probability"] = np.expand_dims(np.array(probability, dtype="str"), 1)
-            postprocessed[feature_name + "_probabilities"] = predicted[feature_name][PROBABILITIES].astype("float64")
-        elif feature_type == VECTOR:
-            postprocessed[feature_name + "_predictions"] = predicted[feature_name][PREDICTIONS].astype("float64")
-        else:
-            postprocessed[feature_name + "_predictions"] = np.expand_dims(
-                np.array(predicted[feature_name][PREDICTIONS], dtype="str"), 1
-            )
-    return postprocessed
-
-
-def _export_neuropod(
-    ludwig_model_path,
-    neuropod_path,
-    neuropod_model_name="ludwig_model",
-    entrypoint="get_model",
-):
-    try:
-        from neuropod.backends.python.packager import create_python_neuropod
-    except ImportError:
-        raise RuntimeError('The "neuropod" package is not installed in your environment.')
-
-    data_paths = [
-        {
-            "path": os.path.join(ludwig_model_path, MODEL_HYPERPARAMETERS_FILE_NAME),
-            "packaged_name": MODEL_HYPERPARAMETERS_FILE_NAME,
-        },
-        {
-            "path": os.path.join(ludwig_model_path, TRAIN_SET_METADATA_FILE_NAME),
-            "packaged_name": TRAIN_SET_METADATA_FILE_NAME,
-        },
-        {"path": os.path.join(ludwig_model_path, "checkpoint"), "packaged_name": "checkpoint"},
-    ]
-    for filename in os.listdir(ludwig_model_path):
-        if MODEL_WEIGHTS_FILE_NAME in filename:
-            data_paths.append({"path": os.path.join(ludwig_model_path, filename), "packaged_name": filename})
-
-    logger.debug(f"data_paths: {data_paths}")
-
-    ludwig_config = load_json(os.path.join(ludwig_model_path, MODEL_HYPERPARAMETERS_FILE_NAME))
-    training_set_metadata = load_json(os.path.join(ludwig_model_path, TRAIN_SET_METADATA_FILE_NAME))
-
-    input_spec = []
-    for feature in ludwig_config["input_features"]:
-        input_spec.append({NAME: feature[NAME], "dtype": "str", "shape": (None, 1)})
-    logger.debug(f"input_spec: {input_spec}")
-
-    output_spec = []
-    for feature in ludwig_config["output_features"]:
-        feature_type = feature[TYPE]
-        feature_name = feature[NAME]
-        if feature_type == BINARY:
-            output_spec.append({"name": feature_name + "_predictions", "dtype": "str", "shape": (None, 1)})
-            output_spec.append({"name": feature_name + "_probability", "dtype": "float64", "shape": (None, 1)})
-            output_spec.append({"name": feature_name + "_probabilities", "dtype": "float64", "shape": (None, 2)})
-        elif feature_type == NUMBER:
-            output_spec.append({"name": feature_name + "_predictions", "dtype": "float64", "shape": (None, 1)})
-        elif feature_type == CATEGORY:
-            output_spec.append({"name": feature_name + "_predictions", "dtype": "str", "shape": (None, 1)})
-            output_spec.append({"name": feature_name + "_probability", "dtype": "float64", "shape": (None, 1)})
-            output_spec.append(
-                {
-                    "name": feature_name + "_probabilities",
-                    "dtype": "float64",
-                    "shape": (None, training_set_metadata[feature[NAME]]["vocab_size"]),
-                }
-            )
-        elif feature_type == SEQUENCE or feature_type == TEXT:
-            output_spec.append({"name": feature_name + "_predictions", "dtype": "str", "shape": (None, 1)})
-        elif feature_type == SET:
-            output_spec.append({"name": feature_name + "_predictions", "dtype": "str", "shape": (None, 1)})
-            output_spec.append({"name": feature_name + "_probability", "dtype": "str", "shape": (None, 1)})
-            output_spec.append(
-                {
-                    "name": feature_name + "_probabilities",
-                    "dtype": "float64",
-                    "shape": (None, training_set_metadata[feature[NAME]]["vocab_size"]),
-                }
-            )
-        elif feature_type == VECTOR:
-            output_spec.append(
-                {
-                    "name": feature["name"] + "_predictions",
-                    "dtype": "float64",
-                    "shape": (None, training_set_metadata[feature[NAME]]["vector_size"]),
-                }
-            )
-        else:
-            output_spec.append({"name": feature["name"] + "_predictions", "dtype": "str", "shape": (None, 1)})
-    logger.debug(f"output_spec: {output_spec}")
-
-    if os.path.exists(neuropod_path):
-        if os.path.isfile(neuropod_path):
-            logger.warning(f"Removing file: {neuropod_path}")
-            os.remove(neuropod_path)
-        else:
-            logger.warning(f"Removing directory: {neuropod_path}")
-            shutil.rmtree(neuropod_path, ignore_errors=True)
-
-    from pathlib import Path
-
-    path = Path(ludwig_path)
-    logger.debug(f"python_root: {path.parent.parent}")
-
-    create_python_neuropod(
-        neuropod_path=neuropod_path,
-        model_name=neuropod_model_name,
-        data_paths=data_paths,
-        code_path_spec=[
-            {
-                "python_root": path.parent.parent,
-                "dirs_to_package": ["ludwig"],  # Package everything in the python_root
-            }
-        ],
-        entrypoint_package="ludwig.utils.neuropod_utils",
-        entrypoint=entrypoint,
-        skip_virtualenv=True,
-        input_spec=input_spec,
-        output_spec=output_spec,
-    )
-    logger.info(f"Neuropod saved to: {neuropod_path}")
--- tests/integration_tests/test_neuropod.py
+++ tests/integration_tests/test_neuropod.py
@@ -12,187 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import itertools
 import os
-import shutil
-import tempfile
 from typing import List, Union
 
 import numpy as np
 import pandas as pd
-import pytest
 import torch
 
 from ludwig.api import LudwigModel
-from ludwig.constants import BINARY, NAME, PREDICTIONS, SEQUENCE, SET, TEXT, TRAINER
+from ludwig.constants import NAME, PREDICTIONS, TRAINER
 from ludwig.utils.neuropod_utils import export_neuropod
-from ludwig.utils.strings_utils import str2bool
 from tests.integration_tests.utils import (
-    audio_feature,
-    bag_feature,
     binary_feature,
     category_feature,
-    date_feature,
     generate_data,
-    h3_feature,
-    image_feature,
     LocalTestBackend,
     number_feature,
-    sequence_feature,
-    set_feature,
-    text_feature,
-    timeseries_feature,
-    vector_feature,
 )
-
-
-@pytest.mark.skip(reason="Issue #1451: Use torchscript.")
-def test_neuropod(csv_filename):
-    #######
-    # Setup
-    #######
-    with tempfile.TemporaryDirectory() as tmpdir:
-        dir_path = tmpdir
-        data_csv_path = os.path.join(tmpdir, csv_filename)
-        image_dest_folder = os.path.join(tmpdir, "generated_images")
-        audio_dest_folder = os.path.join(tmpdir, "generated_audio")
-
-        input_features = [
-            binary_feature(),
-            number_feature(),
-            category_feature(vocab_size=3),
-            sequence_feature(vocab_size=3),
-            text_feature(vocab_size=3),
-            vector_feature(),
-            image_feature(image_dest_folder),
-            audio_feature(audio_dest_folder),
-            timeseries_feature(),
-            date_feature(),
-            h3_feature(),
-            set_feature(vocab_size=3),
-            bag_feature(vocab_size=3),
-        ]
-
-        output_features = [
-            binary_feature(),
-            number_feature(),
-            category_feature(vocab_size=3),
-            sequence_feature(vocab_size=3),
-            text_feature(vocab_size=3),
-            set_feature(vocab_size=3),
-            vector_feature(),
-        ]
-
-        # Generate test data
-        data_csv_path = generate_data(input_features, output_features, data_csv_path)
-
-        #############
-        # Train model
-        #############
-        config = {"input_features": input_features, "output_features": output_features, TRAINER: {"epochs": 2}}
-        ludwig_model = LudwigModel(config, backend=LocalTestBackend())
-        ludwig_model.train(
-            dataset=data_csv_path,
-            skip_save_training_description=True,
-            skip_save_training_statistics=True,
-            skip_save_progress=True,
-            skip_save_log=True,
-            skip_save_processed_input=True,
-            output_directory=dir_path,
-        )
-
-        data_df = pd.read_csv(data_csv_path)
-        original_predictions_df, _ = ludwig_model.predict(dataset=data_df)
-
-        ###################
-        # save Ludwig model
-        ###################
-        ludwigmodel_path = os.path.join(dir_path, "ludwigmodel")
-        shutil.rmtree(ludwigmodel_path, ignore_errors=True)
-        ludwig_model.save(ludwigmodel_path)
-
-        ################
-        # build neuropod
-        ################
-        neuropod_path = os.path.join(dir_path, "neuropod")
-        shutil.rmtree(neuropod_path, ignore_errors=True)
-        export_neuropod(ludwigmodel_path, neuropod_path=neuropod_path, entrypoint="get_test_model")
-
-        ########################
-        # predict using neuropod
-        ########################
-        if_dict = {
-            input_feature["name"]: np.expand_dims(
-                np.array([str(x) for x in data_df[input_feature["name"]].tolist()], dtype="str"), 1
-            )
-            for input_feature in input_features
-        }
-
-        from neuropod.loader import load_neuropod
-
-        neuropod_model = load_neuropod(neuropod_path, _always_use_native=False)
-        preds = neuropod_model.infer(if_dict)
-
-        for key in preds:
-            preds[key] = np.squeeze(preds[key])
-
-        #########
-        # cleanup
-        #########
-        # Delete the temporary data created
-        for path in [ludwigmodel_path, neuropod_path, image_dest_folder, audio_dest_folder]:
-            if os.path.exists(path):
-                if os.path.isfile(path):
-                    os.remove(path)
-                else:
-                    shutil.rmtree(path, ignore_errors=True)
-
-        ########
-        # checks
-        ########
-        for output_feature in output_features:
-            output_feature_name = output_feature["name"]
-            output_feature_type = output_feature["type"]
-
-            if (
-                output_feature_name + "_predictions" in preds
-                and output_feature_name + "_predictions" in original_predictions_df
-            ):
-                neuropod_pred = preds[output_feature_name + "_predictions"].tolist()
-                if output_feature_type == BINARY:
-                    neuropod_pred = [str2bool(x) for x in neuropod_pred]
-                if output_feature_type in {SEQUENCE, TEXT, SET}:
-                    neuropod_pred = [x.split() for x in neuropod_pred]
-
-                original_pred = original_predictions_df[output_feature_name + "_predictions"].tolist()
-
-                assert neuropod_pred == original_pred
-
-            if (
-                output_feature_name + "_probability" in preds
-                and output_feature_name + "_probability" in original_predictions_df
-            ):
-                neuropod_prob = preds[output_feature_name + "_probability"].tolist()
-                if output_feature_type in {SEQUENCE, TEXT, SET}:
-                    neuropod_prob = [[float(n) for n in x.split()] for x in neuropod_prob]
-                if any(isinstance(el, list) for el in neuropod_prob):
-                    neuropod_prob = np.array(list(itertools.zip_longest(*neuropod_prob, fillvalue=0))).T
-
-                original_prob = original_predictions_df[output_feature_name + "_probability"].tolist()
-                if any(isinstance(el, list) for el in original_prob):
-                    original_prob = np.array(list(itertools.zip_longest(*original_prob, fillvalue=0))).T
-
-                assert np.allclose(neuropod_prob, original_prob)
-
-            if (
-                output_feature_name + "_probabilities" in preds
-                and output_feature_name + "_probabilities" in original_predictions_df
-            ):
-                neuropod_prob = preds[output_feature_name + "_probabilities"].tolist()
-
-                original_prob = original_predictions_df[output_feature_name + "_probabilities"].tolist()
-
-                assert np.allclose(neuropod_prob, original_prob)
 
 
 def test_neuropod_torchscript(csv_filename, tmpdir):
