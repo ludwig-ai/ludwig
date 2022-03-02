3d66d812bde2f0754f30ddedadb38bced652f642
Daniel Treiman <daniel@predibase.com>
2022-03-01 16:07:22 -0800

    Removes __main__ from test_losses.py

--- ludwig/utils/entmax/__init__.py
+++ ludwig/utils/entmax/__init__.py
@@ -12,3 +12,22 @@ from ludwig.utils.entmax.losses import (
     SparsemaxLoss,
 )
 from ludwig.utils.entmax.root_finding import entmax_bisect, EntmaxBisect, sparsemax_bisect, SparsemaxBisect
+
+__all__ = [
+    "entmax15",
+    "Entmax15",
+    "sparsemax",
+    "Sparsemax",
+    "entmax15_loss",
+    "Entmax15Loss",
+    "entmax_bisect_loss",
+    "EntmaxBisectLoss",
+    "sparsemax_bisect_loss",
+    "sparsemax_loss",
+    "SparsemaxBisectLoss",
+    "SparsemaxLoss",
+    "entmax_bisect",
+    "EntmaxBisect",
+    "sparsemax_bisect",
+    "SparsemaxBisect",
+]
