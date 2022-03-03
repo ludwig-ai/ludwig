86c2f6bc58053786e5b34ba8d79b63c38ec582bc
Justin Zhao <justinxzhao@gmail.com>
2022-03-03 15:35:23 -0500

    Enable feature names with periods in them.

--- tests/ludwig/features/test_feature_utils.py
+++ tests/ludwig/features/test_feature_utils.py
@@ -19,7 +19,29 @@ def test_ludwig_feature_dict():
     assert feature_dict.items() == [("to", to_module), ("type", type_module)]
     assert feature_dict["to"] == to_module
 
-    feature_dict.update({"1": torch.nn.Module()})
+    feature_dict.update({"to_empty": torch.nn.Module()})
 
     assert len(feature_dict) == 3
-    assert [key for key in feature_dict] == ["to", "type", "1"]
+    assert [key for key in feature_dict] == ["to", "type", "to_empty"]
+
+
+def test_ludwig_feature_dict_with_periods():
+    feature_dict = feature_utils.LudwigFeatureDict()
+
+    to_module = torch.nn.Module()
+    type_module = torch.nn.Module()
+
+    feature_dict["to."] = to_module
+    feature_dict["type"] = type_module
+
+    assert iter(feature_dict) is not None
+    assert next(feature_dict) is not None
+    assert len(feature_dict) == 2
+    assert feature_dict.keys() == ["to.", "type"]
+    assert feature_dict.items() == [("to.", to_module), ("type", type_module)]
+    assert feature_dict["to."] == to_module
+
+    feature_dict.update({"to_empty": torch.nn.Module()})
+
+    assert len(feature_dict) == 3
+    assert [key for key in feature_dict] == ["to.", "type", "to_empty"]
