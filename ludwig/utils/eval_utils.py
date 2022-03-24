Fix references to numerical, and handle case where float-like number is an integer, like 1.0 (#1846)

ludwig/utils/eval_utils.py
@@ -49,7 +49,7 @@ class ConfusionMatrix:
         #     if conditions.dtype.char == 'S':  # it's an array of strings
         #         self.labels_dict = {str(label): idx for idx, label in
         #                             enumerate(np.unique([predictions, conditions]))}
-        #     else:  # numerical
+        #     else:  # number
         #         max_label = np.concatenate([predictions, conditions]).max()
         #         self.labels_dict = {str(i): i for i in range(max_label + 1)}
         #         labels = [str(i) for i in range(max_label + 1)]