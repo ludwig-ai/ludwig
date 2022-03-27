d68969591bf5e6555dd711ee9c12e55e7a29a069
Justin Zhao <justinxzhao@gmail.com>
2022-03-26 22:09:11 -0400

    Fix trainer_utils test.

--- tests/ludwig/utils/test_trainer_utils.py
+++ tests/ludwig/utils/test_trainer_utils.py
@@ -11,25 +11,13 @@ def test_progress_tracker_empty():
         {"name": "category_feature", "input_size": 10, "num_classes": 3}, {}
     )
 
-    progress_tracker = trainer_utils.ProgressTracker(
+    progress_tracker = trainer_utils.get_new_progress_tracker(
         batch_size=5,
-        epoch=0,
-        steps=0,
-        last_improvement_steps=0,
-        last_learning_rate_reduction_steps=0,
-        last_increase_batch_size_steps=0,
-        learning_rate=0.01,
         best_eval_metric=0,
         best_reduce_learning_rate_eval_metric=0,
-        last_reduce_learning_rate_eval_metric_improvement=0,
         best_increase_batch_size_eval_metric=0,
-        last_increase_batch_size_eval_metric_improvement=0,
-        num_reductions_learning_rate=0,
-        num_increases_batch_size=0,
+        learning_rate=0.01,
         output_features=output_features,
-        last_improvement=0,
-        last_learning_rate_reduction=0,
-        last_increase_batch_size=0,
     )
 
     assert progress_tracker.log_metrics() == {
@@ -50,25 +38,13 @@ def test_progress_tracker():
         {"name": "category_feature", "input_size": 10, "num_classes": 3}, {}
     )
 
-    progress_tracker = trainer_utils.ProgressTracker(
+    progress_tracker = trainer_utils.get_new_progress_tracker(
         batch_size=5,
-        epoch=0,
-        steps=0,
-        last_improvement_steps=0,
-        last_learning_rate_reduction_steps=0,
-        last_increase_batch_size_steps=0,
-        learning_rate=0.01,
         best_eval_metric=0,
         best_reduce_learning_rate_eval_metric=0,
-        last_reduce_learning_rate_eval_metric_improvement=0,
         best_increase_batch_size_eval_metric=0,
-        last_increase_batch_size_eval_metric_improvement=0,
-        num_reductions_learning_rate=0,
-        num_increases_batch_size=0,
+        learning_rate=0.01,
         output_features=output_features,
-        last_improvement=0,
-        last_learning_rate_reduction=0,
-        last_increase_batch_size=0,
     )
 
     progress_tracker.vali_metrics[COMBINED][LOSS].append(TrainerMetric(epoch=1, step=10, value=0.1))
