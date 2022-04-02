555722b63af9562400029845946e4db8a13c9819
Jim Thompson <jimthompson5802@gmail.com>
2022-04-02 14:41:10 -0400

    refactor: incorporate reviewer comments Add positive and negative tests Add docstring for tests Add typing info

--- tests/integration_tests/test_reproducibility.py
+++ tests/integration_tests/test_reproducibility.py
@@ -10,8 +10,6 @@ import torch
 from ludwig.api import LudwigModel
 from ludwig.data.dataset_synthesizer import cli_synthesize_dataset
 
-RANDOM_SEED = 1919
-
 INPUT_FEATURES = [
     {"name": "num_1", "type": "number"},
     {"name": "num_2", "type": "number"},
@@ -27,83 +25,153 @@ CONFIG = {
 
 
 @pytest.fixture(scope="module")
-def raw_dataset_fp():
+def raw_dataset_fp() -> str:
+    """Generates dataset to be used in this test.
+
+    Returns (str):  file path string for dataset to use in this tests
+    """
     with tempfile.TemporaryDirectory() as tmpdir:
         raw_fp = os.path.join(tmpdir, "raw_data.csv")
-        random.seed(RANDOM_SEED)
-        cli_synthesize_dataset(20, INPUT_FEATURES + OUTPUT_FEATURES, raw_fp)
+        random.seed(42)
+        cli_synthesize_dataset(64, INPUT_FEATURES + OUTPUT_FEATURES, raw_fp)
         yield raw_fp
 
 
-def test_preprocess(raw_dataset_fp):
+@pytest.mark.parametrize('second_seed_offset', [0, 1])
+@pytest.mark.parametrize('random_seed', [1919, 31])
+def test_preprocess(raw_dataset_fp: str, random_seed: int, second_seed_offset: int) -> None:
+    """Test reproducibility of train/validation/test splits.
+
+    Args:
+        raw_dataset_fp (str): file path for data to be used as part of this test
+        random_seed(int): random seed integer to use for test
+        second_seed_offset(int): zero to use same random seed for second test, non-zero to use a different
+            seed for the second run.
+
+    Returns: None
+    """
     # define Ludwig model
     model1 = LudwigModel(config=CONFIG)
 
     # preprocess the raw data set, specify seed
-    preprocessed_data1 = model1.preprocess(raw_dataset_fp, random_seed=RANDOM_SEED)
+    preprocessed_data1 = model1.preprocess(raw_dataset_fp, random_seed=random_seed)
 
     # invoke torch random functions
-    torch.manual_seed(RANDOM_SEED + 1)
+    torch.manual_seed(random_seed + 1)
     torch.rand((5,))
 
     # define Ludwig model
     model2 = LudwigModel(config=CONFIG)
     # preprocess same raw data set with same seed
-    preprocessed_data2 = model2.preprocess(raw_dataset_fp, random_seed=RANDOM_SEED)
+    preprocessed_data2 = model2.preprocess(raw_dataset_fp, random_seed=random_seed + second_seed_offset)
 
     # confirm data splits are reproducible
     for i in range(3):
         for k in preprocessed_data1[i].dataset:
-            assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
+            if second_seed_offset == 0:
+                # same seeds should result in same output
+                assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
+            else:
+                # non-zero second_seed_offset uses different seeds and should result in different output
+                with pytest.raises(AssertionError):
+                    assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
 
 
-def test_train(raw_dataset_fp):
+@pytest.mark.parametrize('second_seed_offset', [0, 1])
+@pytest.mark.parametrize('random_seed', [1919, 31])
+def test_train(raw_dataset_fp: str, random_seed: int, second_seed_offset: int) -> None:
+    """Test reproducibility of training API.
+
+    Args:
+        raw_dataset_fp (str): file path for data to be used as part of this test
+        random_seed(int): random seed integer to use for test
+        second_seed_offset(int): zero to use same random seed for second test, non-zero to use a different
+            seed for the second run.
+
+    Returns: None
+    """
     # define Ludwig model
     model1 = LudwigModel(config=CONFIG, logging_level=logging.WARN)
     training_statistics1, preprocessed_data1, _ = model1.train(
-        dataset=raw_dataset_fp, random_seed=RANDOM_SEED, skip_save_progress=True, skip_save_processed_input=True
+        dataset=raw_dataset_fp,
+        random_seed=random_seed,
+        skip_save_progress=True,
+        skip_save_processed_input=True
     )
 
     # invoke torch random functions
-    torch.manual_seed(RANDOM_SEED + 1)
+    torch.manual_seed(random_seed + 1)
     torch.rand((5,))
 
     model2 = LudwigModel(config=CONFIG, logging_level=logging.WARN)
     training_statistics2, preprocessed_data2, _ = model2.train(
-        dataset=raw_dataset_fp, random_seed=RANDOM_SEED, skip_save_progress=True, skip_save_processed_input=True
+        dataset=raw_dataset_fp,
+        random_seed=random_seed + second_seed_offset,
+        skip_save_progress=True,
+        skip_save_processed_input=True
     )
 
     # confirm data splits are reproducible
     for i in range(3):
         for k in preprocessed_data1[i].dataset:
-            assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
+            if second_seed_offset == 0:
+                # same seeds should result in same output
+                assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
+                assert training_statistics1 == training_statistics2
+            else:
+                # non-zero second_seed_offset uses different seeds and should result in different output
+                with pytest.raises(AssertionError):
+                    assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
+                with pytest.raises(AssertionError):
+                    assert training_statistics1 == training_statistics2
 
-    # check for equality of training statistics
-    assert training_statistics1 == training_statistics2
 
+@pytest.mark.parametrize('second_seed_offset', [0, 1])
+@pytest.mark.parametrize('random_seed', [1919, 31])
+def test_experiment(raw_dataset_fp: str, random_seed: int, second_seed_offset: int) -> None:
+    """Test reproducibility of experiment API.
 
-def test_experiment(raw_dataset_fp):
+    Args:
+        raw_dataset_fp (str): file path for data to be used as part of this test
+        random_seed(int): random seed integer to use for test
+        second_seed_offset(int): zero to use same random seed for second test, non-zero to use a different
+            seed for the second run.
+
+    Returns: None
+    """
     # define Ludwig model
     model1 = LudwigModel(config=CONFIG, logging_level=logging.WARN)
 
     evaluation_statistics1, training_statistics1, preprocessed_data1, _ = model1.experiment(
-        dataset=raw_dataset_fp, random_seed=RANDOM_SEED, skip_save_processed_input=True
+        dataset=raw_dataset_fp,
+        random_seed=random_seed,
+        skip_save_processed_input=True
     )
 
     # invoke torch random functions
-    torch.manual_seed(RANDOM_SEED + 1)
+    torch.manual_seed(random_seed + 1)
     torch.rand((5,))
 
     model2 = LudwigModel(config=CONFIG, logging_level=logging.WARN)
     evaluation_statistics2, training_statistics2, preprocessed_data2, _ = model2.experiment(
-        dataset=raw_dataset_fp, random_seed=RANDOM_SEED, skip_save_processed_input=True
+        dataset=raw_dataset_fp,
+        random_seed=random_seed + second_seed_offset,
+        skip_save_processed_input=True
     )
 
     # confirm data splits are reproducible
     for i in range(3):
         for k in preprocessed_data1[i].dataset:
-            assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
-
-    # check for equality of training statistics
-    assert training_statistics1 == training_statistics2
-    assert evaluation_statistics1 == evaluation_statistics2
+            if second_seed_offset == 0:
+                # same seeds should result in same output
+                assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
+                assert training_statistics1 == training_statistics2
+                assert evaluation_statistics1 == evaluation_statistics2
+            else:
+                # non-zero second_seed_offset uses different seeds and should result in different output
+                with pytest.raises(AssertionError):
+                    assert np.all(preprocessed_data1[i].dataset[k] == preprocessed_data2[i].dataset[k])
+                with pytest.raises(AssertionError):
+                    assert training_statistics1 == training_statistics2
+                with pytest.raises(AssertionError):
+                    assert evaluation_statistics1 == evaluation_statistics2
