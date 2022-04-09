45c1bcb7253d58fb11d2950ed2f82b6d6184a389
Julian Bright <brightsparc@gmail.com>
2022-04-09 13:20:20 +1000

    Update test to use native dict with numpy value given pd.to_dict() does the right thing

--- tests/ludwig/utils/test_data_utils.py
+++ tests/ludwig/utils/test_data_utils.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 import dask.dataframe as dd
+import numpy as np
 import pandas as pd
 
 from ludwig.data.cache.types import CacheableDataframe
@@ -89,6 +90,5 @@ def test_figure_data_format_dataset():
 
 
 def test_hash_dict_numpy_types():
-    df = pd.DataFrame([{"text": "t", "int": 1, "float": 0.1, "bool": True}])
-    d = df.to_dict()
-    assert hash_dict(d) == b"OCq5a6"
+    d = {"float32": np.float32(1)}
+    assert hash_dict(d) == b"uqtgWB"
