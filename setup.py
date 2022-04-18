0fb0b6966152fb2343779da04e127bb39c8bef24
Geoffrey Angus <geoffrey@predibase.com>
2022-04-18 10:28:24 -0700

    added psutil to requirements.txt and reflected changes in setup.py

--- setup.py
+++ setup.py
@@ -19,14 +19,8 @@ extra_requirements = {}
 with open(path.join(here, "requirements_audio.txt"), encoding="utf-8") as f:
     extra_requirements["audio"] = [line.strip() for line in f if line]
 
-with open(path.join(here, "requirements_image.txt"), encoding="utf-8") as f:
-    extra_requirements["image"] = [line.strip() for line in f if line]
-
 with open(path.join(here, "requirements_serve.txt"), encoding="utf-8") as f:
     extra_requirements["serve"] = [line.strip() for line in f if line]
-
-with open(path.join(here, "requirements_text.txt"), encoding="utf-8") as f:
-    extra_requirements["text"] = [line.strip() for line in f if line]
 
 with open(path.join(here, "requirements_viz.txt"), encoding="utf-8") as f:
     extra_requirements["viz"] = [line.strip() for line in f if line]
