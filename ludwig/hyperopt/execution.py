103dfff201f9604df3608b17ce1d232b386912fe
Travis Addair <tgaddair@gmail.com>
2022-04-19 09:15:35 -0700

    Placement groups

--- ludwig/hyperopt/execution.py
+++ ludwig/hyperopt/execution.py
@@ -672,11 +672,12 @@ class RayTuneExecutor(HyperoptExecutor):
             )
 
         if _is_ray_backend(backend):
-            # we can't set Trial actor's CPUs to 0 so we just go very low
+            # for now, we do not do distributed training on cpu (until spread scheduling is implemented for Ray Train)
+            # but we do want to enable it when GPUs are specified
             resources_per_trial = PlacementGroupFactory(
-                [{"CPU": 0}] + ([{"CPU": 0, "GPU": 1}] * self._gpu_resources_per_trial_non_none)
+                [{}] + ([{"CPU": 0, "GPU": 1}] * self._gpu_resources_per_trial_non_none)
                 if self._gpu_resources_per_trial_non_none
-                else [{"CPU": 0}] + [{"CPU": 1}] * self._cpu_resources_per_trial_non_none
+                else [{}] + [{"CPU": self._cpu_resources_per_trial_non_none}]
             )
 
         if has_remote_protocol(output_directory):
