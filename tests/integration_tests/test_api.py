c44be8e846dc60357ce5c725c7f1afc2df11a005
Geoffrey Angus <geoffrey@predibase.com>
2022-04-20 16:35:15 -0700

    make test_api compatible with torch==1.10.0

--- tests/integration_tests/test_api.py
+++ tests/integration_tests/test_api.py
@@ -15,6 +15,7 @@
 import os
 import shutil
 import tempfile
+from unicodedata import category
 from unittest import mock
 
 import pytest
@@ -522,14 +523,7 @@ def test_api_callbacks_checkpoints_per_epoch(csv_filename, epochs, batch_size, n
 
 def test_api_save_torchscript(tmpdir):
     """Tests successful saving and loading of model in TorchScript format."""
-    input_features = [
-        text_feature(
-            vocab_size=3,
-            preprocessing={
-                "tokenizer": "sentencepiece_tokenizer",
-            },
-        )
-    ]
+    input_features = [category_feature(vocab_size=5)]
     output_features = [category_feature(vocab_size=5, reduce_input="sum")]
 
     data_csv = generate_data(input_features, output_features, os.path.join(tmpdir, "dataset.csv"))
