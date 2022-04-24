fc5f47df163425847a0596b0d4740333de713804
Joppe Geluykens <joppe@predibase.com>
2022-04-24 12:01:13 +0000

    incorporate api changes

--- ludwig/models/lightgbm.py
+++ ludwig/models/lightgbm.py
@@ -3,7 +3,7 @@ import os
 import lightgbm as lgb
 import torch
 
-from ludwig.constants import BINARY, CATEGORY, COMBINED, LOSS, NUMERICAL
+from ludwig.constants import BINARY, CATEGORY, COMBINED, LOSS, NUMBER
 from ludwig.globals import MODEL_WEIGHTS_FILE_NAME, TRAINING_CHECKPOINTS_DIR_PATH
 from ludwig.models.hummingbird import convert_to_pytorch
 from ludwig.models.trainer import Trainer
@@ -11,27 +11,27 @@ from ludwig.utils.checkpoint_utils import Checkpoint, CheckpointManager
 
 
 class LightGBMTrainer(Trainer):
-    def train(self, model, training_set, validation_set=None, test_set=None, save_path="model", **kwargs):
+    def train(self, training_set, validation_set=None, test_set=None, save_path="model", **kwargs):
         # TODO: construct new datasets by running encoders (for text, image)
 
         # TODO: only single task currently
-        if len(model.output_features) > 1:
+        if len(self.model.output_features) > 1:
             raise ValueError("Only single task currently supported")
 
         output_params = {}
-        for feature in model.output_features.values():
-            if feature.type == CATEGORY:
+        for feature in self.model.output_features.values():
+            if feature.type() == CATEGORY:
                 output_params = {
                     "objective": "multiclass",
                     "metric": "multi_logloss",
                     "num_class": feature.num_classes,
                 }
-            elif feature.type == BINARY:
+            elif feature.type() == BINARY:
                 output_params = {
                     "objective": "binary",
                     "metric": "binary_logloss",
                 }
-            elif feature.type == NUMERICAL:
+            elif feature.type() == NUMBER:
                 # TODO: regression
                 output_params = {}
             else:
@@ -49,8 +49,8 @@ class LightGBMTrainer(Trainer):
             **output_params,
         }
 
-        X_train = training_set.to_df(model.input_features.values())
-        y_train = training_set.to_df(model.output_features.values())
+        X_train = training_set.to_df(self.model.input_features.values())
+        y_train = training_set.to_df(self.model.output_features.values())
 
         # create dataset for lightgbm
         # if you want to re-use data, remember to set free_raw_data=False
@@ -58,29 +58,29 @@ class LightGBMTrainer(Trainer):
 
         eval_sets = []
         if validation_set is not None:
-            X_val = validation_set.to_df(model.input_features.values())
-            y_val = validation_set.to_df(model.output_features.values())
+            X_val = validation_set.to_df(self.model.input_features.values())
+            y_val = validation_set.to_df(self.model.output_features.values())
             lgb_val = lgb.Dataset(X_val, y_val, reference=lgb_train, free_raw_data=False)
             eval_sets.append(lgb_val)
 
         if test_set is not None:
-            X_test = test_set.to_df(model.input_features.values())
-            y_test = test_set.to_df(model.output_features.values())
+            X_test = test_set.to_df(self.model.input_features.values())
+            y_test = test_set.to_df(self.model.output_features.values())
             lgb_test = lgb.Dataset(X_test, y_test, reference=lgb_train, free_raw_data=False)
             eval_sets.append(lgb_test)
 
-        features_names = list(model.input_features.keys())
+        features_names = list(self.model.input_features.keys())
 
         categorical_features = [
             i
-            for i, feature in enumerate(model.input_features.values())
+            for i, feature in enumerate(self.model.input_features.values())
             if feature.type == CATEGORY or feature.type == BINARY
         ]
 
         # TODO: update training metrics
-        (train_metrics, vali_metrics, test_metrics) = self.initialize_training_metrics(model.output_features)
+        (train_metrics, vali_metrics, test_metrics) = self.initialize_training_metrics(self.model.output_features)
 
-        for output_feature_name, output_feature in model.output_features.items():
+        for output_feature_name, output_feature in self.model.output_features.items():
             for metric in output_feature.metric_functions:
                 train_metrics[output_feature_name][metric].append(0.0)
                 vali_metrics[output_feature_name][metric].append(0.0)
@@ -99,15 +99,15 @@ class LightGBMTrainer(Trainer):
         )
 
         # convert to pytorch for inference, fine tuning
-        model = convert_to_pytorch(gbm, model)
+        self.model = convert_to_pytorch(gbm, self.model)
 
         os.makedirs(save_path, exist_ok=True)
         training_checkpoints_path = os.path.join(save_path, TRAINING_CHECKPOINTS_DIR_PATH)
-        checkpoint = Checkpoint(model=model)
+        checkpoint = Checkpoint(model=self.model)
         checkpoint_manager = CheckpointManager(checkpoint, training_checkpoints_path, device=self.device, max_to_keep=1)
         checkpoint_manager.save(1)
 
         model_weights_path = os.path.join(save_path, MODEL_WEIGHTS_FILE_NAME)
-        torch.save(model.state_dict(), model_weights_path)
+        torch.save(self.model.state_dict(), model_weights_path)
 
-        return model, train_metrics, vali_metrics, test_metrics
+        return self.model, train_metrics, vali_metrics, test_metrics
