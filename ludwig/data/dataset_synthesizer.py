3d28abbfca5329dbf6dbc6af0ca25e1187e590cf
connor-mccorm <connor@predibase.com>
2022-04-26 12:03:43 -0700

    Switched type setting to torch.float32

--- ludwig/data/dataset_synthesizer.py
+++ ludwig/data/dataset_synthesizer.py
@@ -261,7 +261,7 @@ def generate_audio(feature):
     sampling_rate = 16000
     num_samples = int(audio_length * sampling_rate)
     audio = np.sin(np.arange(num_samples) / 100 * 2 * np.pi) * 2 * (np.random.random(num_samples) - 0.5)
-    audio_tensor = torch.tensor(np.array([audio]).astype('float32'))
+    audio_tensor = torch.tensor(np.array([audio])).type(torch.float32)
 
     audio_filename = uuid.uuid4().hex[:10].upper() + ".wav"
 
