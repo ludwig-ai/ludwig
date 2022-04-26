cabedd6459610afee0dffc4b8849ae831d78d3f0
Geoffrey Angus <geoffrey@predibase.com>
2022-04-26 14:10:06 -0700

    cleaner comment in test

--- tests/ludwig/utils/test_tokenizers.py
+++ tests/ludwig/utils/test_tokenizers.py
@@ -2,7 +2,7 @@ from ludwig.utils.tokenizers import tokenizer_registry
 
 
 def test_space_punct_tokenizer_parity():
-    """Tests that the new implementation of space_punct tokenizer is equivalent to the old one."""
+    """Tests that the torchscript-compatible implementation of space_punct tokenizer is equivalent to the regex one."""
 
     sentences = ["this is123, a_sent-ence", "...test test?", "hello..world", "", "    ", ".", "ç–12¡™£•"]
     legacy_tokenizer = tokenizer_registry["legacy_space_punct"]()
