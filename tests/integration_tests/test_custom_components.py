4294d87e7d20a39908503c2f6ae217101443d63f
Travis Addair <tgaddair@gmail.com>
2022-05-10 15:40:00 -0700

    Fix redundant import (#2019)

--- tests/integration_tests/test_custom_components.py
+++ tests/integration_tests/test_custom_components.py
@@ -7,7 +7,7 @@ from marshmallow_dataclass import dataclass
 from torch import nn, Tensor
 
 from ludwig.api import LudwigModel
-from ludwig.combiners.combiners import BaseCombinerConfig, Combiner, register_combiner
+from ludwig.combiners.combiners import Combiner, register_combiner
 from ludwig.constants import NUMBER, TRAINER
 from ludwig.decoders.base import Decoder
 from ludwig.decoders.registry import register_decoder
