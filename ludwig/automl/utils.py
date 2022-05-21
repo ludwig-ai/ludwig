6525fdafdd94d5eb2a0108bbd1c279694ad8d91a
hungcs <hung@predibase.com>
2022-05-20 18:01:24 -0700

    import one

--- ludwig/automl/utils.py
+++ ludwig/automl/utils.py
@@ -4,8 +4,8 @@ import os
 from dataclasses import dataclass
 from typing import Dict, List
 
-import numpy as np
 from dataclasses_json import dataclass_json, LetterCase
+from numpy import nan_to_num
 from pandas import Series
 
 from ludwig.constants import COMBINER, CONFIG, HYPEROPT, NAME, NUMBER, PARAMETERS, SAMPLER, TRAINER, TYPE
@@ -59,7 +59,7 @@ def avg_num_tokens(field: Series) -> int:
     if len(field) > 5000:
         field = field.sample(n=5000, random_state=40)
     unique_entries = field.unique()
-    avg_words = round(np.nan_to_num(Series(unique_entries).str.split().str.len().mean()))
+    avg_words = round(nan_to_num(Series(unique_entries).str.split().str.len().mean()))
     return avg_words
 
 
