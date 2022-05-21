48f2b94af5521b031b031ecb825380523dedbffb
hungcs <hung@predibase.com>
2022-05-20 18:13:10 -0700

    tes

--- tests/ludwig/automl/test_utils.py
+++ tests/ludwig/automl/test_utils.py
@@ -6,6 +6,12 @@ from ludwig.automl.utils import avg_num_tokens
 np.asarray([None])
 
 
-@pytest.mark.parametrize("field,expected", [np.asarray([None], 0), np.asarray(["string1", "string2", "string3"], 1)])
+@pytest.mark.parametrize(
+    "field,expected",
+    [
+        (np.asarray([None]), 0),
+        (np.asarray(["string1", "string2", "string3"]), 1)
+    ]
+)
 def test_avg_num_tokens(field, expected):
     assert avg_num_tokens(field) == expected
