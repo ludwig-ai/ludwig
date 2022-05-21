8200fb1f131c548b60d245c351a8e79b5f6a5f28
hungcs <hung@predibase.com>
2022-05-20 17:59:39 -0700

    add unit tests

---
+++ tests/ludwig/automl/test_utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+
+from ludwig.automl.utils import avg_num_tokens
+
+np.asarray([None])
+
+
+@pytest.mark.parametrize(
+    "field,expected",
+    [
+        np.asarray([None], 0),
+        np.asarray(["string1", "string2", "string3"], 1)
+    ]
+)
+def test_avg_num_tokens(field, expected):
+    assert avg_num_tokens(field) == expected
