d213164c45440a39e0ef9e4f5ac5d41f82e12e5d
Geoffrey Angus <geoffrey@predibase.com>
2022-05-25 13:05:28 -0700

    re-added deflaked test

--- tests/integration_tests/test_class_imbalance_feature.py
+++ tests/integration_tests/test_class_imbalance_feature.py
@@ -134,13 +134,12 @@ def run_test_imbalance_local(
     ["oversample_minority", "undersample_majority"],
 )
 @pytest.mark.distributed
-@pytest.mark.skip(reason="Flaky")
 def test_imbalance_ray(balance):
     config = {
         "input_features": [
-            {"name": "Index", "column": "Index", "type": "numerical"},
-            {"name": "random_1", "column": "random_1", "type": "numerical"},
-            {"name": "random_2", "column": "random_2", "type": "numerical"},
+            {"name": "Index", "column": "Index", "type": "number"},
+            {"name": "random_1", "column": "random_1", "type": "number"},
+            {"name": "random_2", "column": "random_2", "type": "category"},
         ],
         "output_features": [{"name": "Label", "column": "Label", "type": "binary"}],
         "trainer": {"epochs": 2, "batch_size": 8},
