c47e5a5ef1b04f278784878e57e9f991771c3272
Joppe Geluykens <joppe@predibase.com>
2022-05-25 15:23:38 +0000

    test: verify binary predictions + add non-supported output

--- tests/integration_tests/test_lightgbm.py
+++ tests/integration_tests/test_lightgbm.py
@@ -2,12 +2,30 @@ import os
 import shutil
 import tempfile
 
+import pytest
+
 from ludwig.api import LudwigModel
-from ludwig.backend import initialize_backend
-from tests.integration_tests.utils import binary_feature, category_feature, generate_data, number_feature
+from tests.integration_tests.utils import binary_feature, category_feature, generate_data, number_feature, text_feature
 
 
-def test_lightgbm(tmpdir):
+def test_gbm_output_not_supported(tmpdir):
+    with tempfile.TemporaryDirectory() as outdir:
+        input_features = [number_feature(), category_feature(reduce_output="sum")]
+        output_features = [text_feature()]
+
+        csv_filename = os.path.join(tmpdir, "training.csv")
+        data_csv = generate_data(input_features, output_features, csv_filename)
+        val_csv = shutil.copyfile(data_csv, os.path.join(tmpdir, "validation.csv"))
+        test_csv = shutil.copyfile(data_csv, os.path.join(tmpdir, "test.csv"))
+
+        config = {"model_type": "gbm", "input_features": input_features, "output_features": output_features}
+
+        model = LudwigModel(config)
+        with pytest.raises(ValueError, match="Output feature must be numerical, categorical, or binary"):
+            model.train(training_set=data_csv, validation_set=val_csv, test_set=test_csv, output_directory=outdir)
+
+
+def test_gbm(tmpdir):
     with tempfile.TemporaryDirectory() as outdir:
         input_features = [number_feature(), category_feature(reduce_output="sum")]
         output_features = [binary_feature()]
@@ -21,22 +39,19 @@ def test_lightgbm(tmpdir):
             "model_type": "gbm",
             "input_features": input_features,
             "output_features": output_features,
-            "combiner": {"type": "concat", "fc_size": 14},
-            "training": {"epochs": 2},
+            "trainer": {"num_boosting_rounds": 1},
         }
 
-        backend_config = {
-            "type": "local",
-        }
-        backend = initialize_backend(backend_config)
-
-        model = LudwigModel(config, backend=backend)
+        model = LudwigModel(config)
         _, _, output_directory = model.train(
             training_set=data_csv,
             validation_set=val_csv,
             test_set=test_csv,
             output_directory=outdir,
         )
-        preds = model.predict(dataset=test_csv, output_directory=output_directory)
+        preds, _ = model.predict(dataset=test_csv, output_directory=output_directory)
 
-    assert preds is not None
+    assert preds[output_features[0]["name"] + "_probabilities"].apply(sum).mean() == pytest.approx(1.0)
+
+
+# TODO: test ray backend
