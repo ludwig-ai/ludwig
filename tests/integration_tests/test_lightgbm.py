6f477bd37560daa115f1ec4ab008a8e989d770c5
Joppe Geluykens <joppe@predibase.com>
2022-05-25 17:51:02 +0000

    test: use local and ray backend

--- tests/integration_tests/test_lightgbm.py
+++ tests/integration_tests/test_lightgbm.py
@@ -5,12 +5,26 @@ import tempfile
 import pytest
 
 from ludwig.api import LudwigModel
+from ludwig.backend import initialize_backend
 from tests.integration_tests.utils import binary_feature, category_feature, generate_data, number_feature, text_feature
 
+
+@pytest.fixture(params=["local", "ray"], scope="module")
+def backend_config(request):
+    backend_type = request.param
+    if backend_type == "local":
+        return {"type": "local"}
+    else:
+        num_actors = 2
+        return {
+            "type": "ray",
+            "processor": {"parallelism": num_actors},
+            "trainer": {"num_actors": num_actors},
+        }
+
+
 # TODO: parametrize to include ray backend
-
-
-def test_gbm_output_not_supported(tmpdir):
+def test_gbm_output_not_supported(tmpdir, backend_config):
     """Test that an error is raised when the output feature is not supported by the model."""
     with tempfile.TemporaryDirectory() as outdir:
         input_features = [number_feature(), category_feature(reduce_output="sum")]
@@ -23,12 +37,13 @@ def test_gbm_output_not_supported(tmpdir):
 
         config = {"model_type": "gbm", "input_features": input_features, "output_features": output_features}
 
-        model = LudwigModel(config)
+        backend = initialize_backend(backend_config)
+        model = LudwigModel(config, backend=backend)
         with pytest.raises(ValueError, match="Output feature must be numerical, categorical, or binary"):
             model.train(training_set=data_csv, validation_set=val_csv, test_set=test_csv, output_directory=outdir)
 
 
-def test_gbm_multiple_outputs(tmpdir):
+def test_gbm_multiple_outputs(tmpdir, backend_config):
     """Test that an error is raised when the model is trained with multiple outputs."""
     with tempfile.TemporaryDirectory() as outdir:
         input_features = [number_feature(), category_feature(reduce_output="sum")]
@@ -47,20 +62,16 @@ def test_gbm_multiple_outputs(tmpdir):
             "model_type": "gbm",
             "input_features": input_features,
             "output_features": output_features,
-            "trainer": {"num_boosting_rounds": 1},
+            "trainer": {"num_boost_round": 1},
         }
 
-        model = LudwigModel(config)
+        backend = initialize_backend(backend_config)
+        model = LudwigModel(config, backend=backend)
         with pytest.raises(ValueError, match="Only single task currently supported"):
-            model.train(
-                training_set=data_csv,
-                validation_set=val_csv,
-                test_set=test_csv,
-                output_directory=outdir,
-            )
+            model.train(training_set=data_csv, validation_set=val_csv, test_set=test_csv, output_directory=outdir)
 
 
-def test_gbm_binary(tmpdir):
+def test_gbm_binary(tmpdir, backend_config):
     """Test that the GBM model can train and predict a binary variable."""
     with tempfile.TemporaryDirectory() as outdir:
         input_features = [number_feature(), category_feature(reduce_output="sum")]
@@ -76,15 +87,20 @@ def test_gbm_binary(tmpdir):
             "model_type": "gbm",
             "input_features": input_features,
             "output_features": output_features,
-            "trainer": {"num_boosting_rounds": 1},
+            "trainer": {"num_boost_round": 1},
         }
 
-        model = LudwigModel(config)
+        backend = initialize_backend(backend_config)
+        model = LudwigModel(config, backend=backend)
         _, _, output_directory = model.train(
             training_set=data_csv,
             validation_set=val_csv,
             test_set=test_csv,
             output_directory=outdir,
+            skip_save_processed_input=True,
+            skip_save_progress=True,
+            skip_save_unprocessed_output=True,
+            skip_save_log=True,
         )
         preds, _ = model.predict(dataset=test_csv, output_directory=output_directory)
 
@@ -119,6 +135,10 @@ def test_gbm_category(tmpdir):
             validation_set=val_csv,
             test_set=test_csv,
             output_directory=outdir,
+            skip_save_processed_input=True,
+            skip_save_progress=True,
+            skip_save_unprocessed_output=True,
+            skip_save_log=True,
         )
         preds, _ = model.predict(dataset=test_csv, output_directory=output_directory)
 
