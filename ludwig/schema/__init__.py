39ad2a817cd59f8fb2d21df049a55746d49669cb
Travis Addair <tgaddair@gmail.com>
2022-06-07 17:17:48 -0700

    Fixed combiner schema creation

--- ludwig/schema/__init__.py
+++ ludwig/schema/__init__.py
@@ -20,11 +20,11 @@
 from jsonschema import Draft7Validator, validate
 from jsonschema.validators import extend
 
+from ludwig.combiners.combiners import get_combiner_schema
 from ludwig.constants import COMBINER, HYPEROPT, PREPROCESSING, TRAINER
 from ludwig.decoders.registry import get_decoder_classes
 from ludwig.encoders.registry import get_encoder_classes
 from ludwig.features.feature_registries import input_type_registry, output_type_registry
-from ludwig.schema.combiners.utils import get_combiner_jsonschema
 from ludwig.schema.trainer import get_trainer_jsonschema
 from ludwig.schema.utils import create_cond
 
@@ -66,7 +66,7 @@ def get_schema():
                     "required": ["name", "type"],
                 },
             },
-            COMBINER: get_combiner_jsonschema(),
+            COMBINER: get_combiner_schema(),
             TRAINER: get_trainer_jsonschema(),
             PREPROCESSING: {},
             HYPEROPT: {},
