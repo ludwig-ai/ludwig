edfab0384ff846b63379369e3ec6ba2831ad67db
Joppe Geluykens <joppe@predibase.com>
2022-06-12 16:18:11 +0000

    make gbm imports optional

--- ludwig/models/registry.py
+++ ludwig/models/registry.py
@@ -1,8 +1,20 @@
+import logging
+
 from ludwig.constants import MODEL_ECD, MODEL_GBM
 from ludwig.models.ecd import ECD
-from ludwig.models.gbm import GBM
 
 model_type_registry = {
     MODEL_ECD: ECD,
-    MODEL_GBM: GBM,
 }
+
+try:
+    import lightgbm  # noqa: F401
+
+    from ludwig.models.gbm import GBM
+
+    model_type_registry[MODEL_GBM] = GBM
+except ImportError:
+    logging.warning(
+        "Importing GBM requirements failed. Not loading GBM model type. "
+        "If you want to use GBM, install Ludwig's 'tree' extra."
+    )
--- ludwig/trainers/__init__.py
+++ ludwig/trainers/__init__.py
@@ -1,3 +1,14 @@
 # register trainers
+import logging
+
 import ludwig.trainers.trainer  # noqa: F401
-import ludwig.trainers.trainer_lightgbm  # noqa: F401
+
+try:
+    import lightgbm  # noqa: F401
+
+    import ludwig.trainers.trainer_lightgbm  # noqa: F401
+except ImportError:
+    logging.warning(
+        "Importing GBM requirements failed. Not loading LightGBM trainer. "
+        "If you want to use LightGBM, install Ludwig's 'tree' extra."
+    )
