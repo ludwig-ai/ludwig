1c9a2e51aa937e72de28db1296fd0281ca0eb597
Travis Addair <tgaddair@gmail.com>
2022-06-13 09:58:42 -0700

    Fixed stratify test

--- tests/ludwig/data/test_split.py
+++ tests/ludwig/data/test_split.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timedelta
+from random import randrange
 from unittest.mock import Mock
 
 import numpy as np
@@ -109,11 +112,12 @@ def test_stratify_split():
             return 1
         return 2
 
-    df["category"] = df["C"].map(get_category).astype(np.int8)
+    df["category"] = df.index.map(get_category).astype(np.int8)
 
     probs = (0.7, 0.1, 0.2)
     split_params = {
-        "type": "random",
+        "type": "stratify",
+        "column": "category",
         "probabilities": probs,
     }
     splitter = get_splitter(**split_params)
@@ -129,3 +133,48 @@ def test_stratify_split():
             actual = np.sum(split["category"] == idx)
             expected = int(r * p)
             assert np.isclose(actual, expected, atol=5)
+
+
+@pytest.mark.parametrize(
+    ("df_engine",),
+    [
+        pytest.param(PandasEngine(), id="pandas"),
+        pytest.param(DaskEngine(_use_ray=False), id="dask", marks=pytest.mark.distributed),
+    ],
+)
+def test_datetime_split(df_engine):
+    nrows = 100
+    npartitions = 10
+
+    df = pd.DataFrame(np.random.randint(0, 100, size=(nrows, 3)), columns=["A", "B", "C"])
+    if isinstance(df_engine, DaskEngine):
+        df = df_engine.df_lib.from_pandas(df, npartitions=npartitions)
+
+    def random_date(*args, **kwargs):
+        start = datetime.strptime("1/1/1990 1:30 PM", "%m/%d/%Y %I:%M %p")
+        end = datetime.strptime("1/1/2022 4:50 AM", "%m/%d/%Y %I:%M %p")
+        delta = end - start
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = randrange(int_delta)
+        return str(start + timedelta(seconds=random_second))
+
+    df["datetime"] = df["C"].map(random_date)
+
+    probs = (0.7, 0.1, 0.2)
+    split_params = {
+        "type": "random",
+        "probabilities": probs,
+    }
+    splitter = get_splitter(**split_params)
+
+    backend = Mock()
+    backend.df_engine = df_engine
+    splits = splitter.split(df, backend)
+
+    assert len(splits) == 3
+    for split, p in zip(splits, probs):
+        if isinstance(df_engine, DaskEngine):
+            # Dask splitting is not exact, so apply soft constraint here
+            assert np.isclose(len(split), int(nrows * p), atol=5)
+        else:
+            assert len(split) == int(nrows * p)
