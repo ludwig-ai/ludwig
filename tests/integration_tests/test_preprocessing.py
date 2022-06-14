28ddd3f7630074fab98f48fcebac4b4221fc7bcd
Geoffrey Angus <geoffrey@predibase.com>
2022-06-14 12:14:42 -0700

    Fixes dtype of SPLIT column if already provided

--- tests/integration_tests/test_preprocessing.py
+++ tests/integration_tests/test_preprocessing.py
@@ -85,3 +85,38 @@ def test_strip_whitespace_category(csv_filename, tmpdir):
 
     # expect values containing whitespaces to be properly mapped to vocab_size unique values
     assert len(np.unique(train_ds.dataset[cat_feat[PROC_COLUMN]])) == cat_feat["vocab_size"]
+
+
+@pytest.mark.parametrize("backend", ["local", "ray"])
+@pytest.mark.distributed
+def test_with_split(backend, csv_filename, tmpdir):
+    num_examples = 10
+    train_set_size = int(num_examples * 0.8)
+    val_set_size = int(num_examples * 0.1)
+    test_set_size = int(num_examples * 0.1)
+
+    input_features = [sequence_feature(reduce_output="sum")]
+    output_features = [category_feature(vocab_size=5, reduce_input="sum")]
+    data_csv = generate_data(
+        input_features, output_features, os.path.join(tmpdir, csv_filename), num_examples=num_examples
+    )
+    data_df = pd.read_csv(data_csv)
+    data_df["split"] = [0] * train_set_size + [1] * val_set_size + [2] * test_set_size
+    data_df.to_csv(data_csv, index=False)
+    config = {
+        "input_features": input_features,
+        "output_features": output_features,
+        "trainer": {
+            "epochs": 2,
+        },
+    }
+
+    with init_backend(backend):
+        model = LudwigModel(config, backend=backend)
+        train_set, val_set, test_set, _ = model.preprocess(
+            data_csv,
+            skip_save_processed_input=False,
+        )
+        assert len(train_set) == train_set_size
+        assert len(val_set) == val_set_size
+        assert len(test_set) == test_set_size
