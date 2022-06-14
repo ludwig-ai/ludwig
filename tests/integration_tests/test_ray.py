e71c69fb9ca746e7d1025ed4cbae44c41a3461e6
Daniel Treiman <daniel@predibase.com>
2022-06-13 20:11:24 -0700

    Adds ray test of calibration.

--- tests/integration_tests/test_ray.py
+++ tests/integration_tests/test_ray.py
@@ -521,3 +521,18 @@ def test_ray_progress_bar():
         output_features,
         df_engine="dask",
     )
+
+
+@pytest.mark.parametrize("calibration", ["temperature_scaling", "matrix_scaling"])
+@pytest.mark.distributed
+def test_ray_categorical_calibration(calibration):
+    input_features = [
+        number_feature(normalization="zscore"),
+        set_feature(),
+        binary_feature(),
+    ]
+    output_features = [category_feature(vocab_size=3, calibration=calibration)]
+    run_test_with_features(
+        input_features,
+        output_features,
+    )
