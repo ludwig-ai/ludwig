73164b4da3787e6410c37429413382d561ef46d6
Arnav Garg <arnav@predibase.com>
2022-06-17 08:22:35 +0000

    Fix docstring typo

--- ludwig/hyperopt/run.py
+++ ludwig/hyperopt/run.py
@@ -401,7 +401,7 @@ def update_hyperopt_params_with_defaults(hyperopt_params):
 def get_shared_params_dict(features: Dict[str, Any]) -> Dict[str, Set]:
     """
     Generates a mapping of feature type to the corresponding set of features
-    clwithout an encoder.
+    without an encoder.
 
     They may be considered for potential shared parameter search spaces depending
     on the parameter space defined later within the hyperopt config.
