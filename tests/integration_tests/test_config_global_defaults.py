032d0fe04756bc41d93f7633e2b5c78a600412a5
Arnav Garg <arnav@predibase.com>
2022-06-21 07:00:11 +0000

    Added simple test that takes in default parameters

--- tests/integration_tests/test_config_global_defaults.py
+++ tests/integration_tests/test_config_global_defaults.py
@@ -1,10 +1,32 @@
+import logging
+import tempfile
 from typing import Dict, Tuple
 
-from ludwig.constants import COMBINER, INPUT_FEATURES, OUTPUT_FEATURES, TRAINER, TYPE
-from tests.integration_tests.utils import category_feature, generate_data, text_feature
+import pytest
+
+from ludwig.constants import (
+    CATEGORY,
+    COMBINER,
+    DECODER,
+    DEFAULTS,
+    ENCODER,
+    FILL_WITH_CONST,
+    INPUT_FEATURES,
+    LOSS,
+    OUTPUT_FEATURES,
+    PREPROCESSING,
+    SOFTMAX_CROSS_ENTROPY,
+    TRAINER,
+    TYPE,
+)
+from tests.integration_tests.utils import category_feature, generate_data, run_experiment, text_feature
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logging.getLogger("ludwig").setLevel(logging.INFO)
 
 
-def _prepared_data(csv_filename: str) -> Tuple[Dict, str]:
+def _prepare_data(csv_filename: str) -> Tuple[Dict, str]:
     input_features = [
         text_feature(name="title", cell_type="rnn", reduce_output="sum"),
         text_feature(name="summary", cell_type="rnn"),
@@ -14,7 +36,7 @@ def _prepared_data(csv_filename: str) -> Tuple[Dict, str]:
 
     output_features = [category_feature(vocab_size=2, reduce_input="sum")]
 
-    rel_path = generate_data(input_features, output_features, csv_filename)
+    dataset = generate_data(input_features, output_features, csv_filename)
 
     config = {
         INPUT_FEATURES: input_features,
@@ -23,4 +45,22 @@ def _prepared_data(csv_filename: str) -> Tuple[Dict, str]:
         TRAINER: {"epochs": 2, "learning_rate": 0.001},
     }
 
-    return config, rel_path
+    return config, dataset
+
+
+@pytest.mark.distributed
+@pytest.mark.parametrize("backend", ["local", "ray"])
+def test_run_global_default_parameters(backend, csv_filename):
+    config, dataset = _prepare_data(csv_filename)
+
+    config[DEFAULTS] = {
+        CATEGORY: {
+            PREPROCESSING: {"missing_value_strategy": FILL_WITH_CONST, "fill_value": "<UNK>"},
+            ENCODER: {TYPE: "sparse"},
+            DECODER: {"norm_params": None, "dropout": 0, "use_bias": True},
+            LOSS: {TYPE: SOFTMAX_CROSS_ENTROPY, "confidence_penalty": 0},
+        }
+    }
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        run_experiment(config=config, backend=backend, dataset=dataset, output_dir=tmpdir)
