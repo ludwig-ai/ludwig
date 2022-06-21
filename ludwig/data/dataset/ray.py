f6b7ce03f04ccf2fd61fd029f2242788ec17de08
Arnav Garg <arnav@predibase.com>
2022-06-21 20:40:59 +0000

    Removing dead code

--- ludwig/data/dataset/ray.py
+++ ludwig/data/dataset/ray.py
@@ -173,7 +173,6 @@ class RayDatasetShard(Dataset):
 
     @lru_cache(1)
     def __len__(self):
-        # TODO(travis): find way to avoid calling this, as it's expensive
         return self.dataset_shard_size
 
     @property
