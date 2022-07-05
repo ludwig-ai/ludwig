2d085028eeea026b2cc19066894476fb566f7d59
Justin Zhao <justinxzhao@gmail.com>
2022-07-05 18:23:28 -0400

    Use zipped file + wget to load old Ludwig model from a zipped file, hosted on S3.

--- tests/regression_tests/model/test_old_models.py
+++ tests/regression_tests/model/test_old_models.py
@@ -1,34 +1,18 @@
 import os
+import zipfile
 
-import boto3
 import pandas as pd
+import wget
 
 from ludwig.api import LudwigModel
 
 
-def download_directory_from_s3(bucket_name: str, remote_directory_name: str, tmpdir: str):
-    """Downloads a directory from S3 to the same relative path, under tmpdir.
-
-    Based on https://stackoverflow.com/questions/49772151/download-a-folder-from-s3-using-boto3.
-    """
-    s3_resource = boto3.resource("s3")
-    bucket = s3_resource.Bucket(bucket_name)
-    for obj in bucket.objects.filter(Prefix=remote_directory_name):
-        local_dir = os.path.join(tmpdir, os.path.dirname(obj.key))
-        if not os.path.exists(local_dir):
-            os.makedirs(local_dir)
-        bucket.download_file(obj.key, os.path.join(local_dir, os.path.basename(obj.key)))
-
-
 def test_model_loaded_from_old_config_prediction_works(tmpdir):
     # Titanic model based on 0.5.3.
-    # Model config:
-    # https://predibase-public-us-west-2.s3.us-west-2.amazonaws.com/ludwig_unit_tests/simple_experiment_simple_model/
-    # model/model_hyperparameters.json
-    download_directory_from_s3(
-        "predibase-public-us-west-2", "ludwig_unit_tests/simple_experiment_simple_model/model/", tmpdir
-    )
-    ludwig_model = LudwigModel.load(os.path.join(tmpdir, "ludwig_unit_tests/simple_experiment_simple_model/model"))
+    old_model_url = "https://predibase-public-us-west-2.s3.us-west-2.amazonaws.com/ludwig_unit_tests/old_model.zip"
+    old_model_filename = wget.download(old_model_url, tmpdir)
+    with zipfile.ZipFile(old_model_filename, "r") as zip_ref:
+        zip_ref.extractall(tmpdir)
     example_data = {
         "PassengerId": 892,
         "Pclass": 3,
@@ -44,6 +28,7 @@ def test_model_loaded_from_old_config_prediction_works(tmpdir):
     }
     test_set = pd.DataFrame(example_data, index=[0])
 
+    ludwig_model = LudwigModel.load(os.path.join(tmpdir, "old_model/model"))
     predictions, _ = ludwig_model.predict(dataset=test_set)
 
     assert predictions.to_dict()["Survived_predictions"] == {0: False}
