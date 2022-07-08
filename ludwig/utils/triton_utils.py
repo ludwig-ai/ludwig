db994c6f840b2c19dfa813c4bdba7d69cdcf24d9
Wael Abid <wael@predibase.com>
2022-07-08 01:39:50 -0700

    adding basic logic and dataclasses

--- ludwig/utils/triton_utils.py
+++ ludwig/utils/triton_utils.py
@@ -1,12 +1,21 @@
 import importlib.util
 import os
 import tempfile
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Dict, Tuple, Union, List
 
 import torch
+import pandas as pd
 
+from dataclasses import dataclass
 from ludwig.api import LudwigModel
 from ludwig.constants import NAME
+from ludwig.models.inference import InferenceModule, PREPROCESSOR, PREDICTOR, POSTPROCESSOR, INFERENCE_STAGES
+from ludwig.features.category_feature import CategoryInputFeature
+from ludwig.utils.torch_utils import DEVICE
+from ludwig.utils.types import TorchscriptPreprocessingInput, TorchAudioTuple
+
+INPUT = "INPUT"
+OUTPUT = "OUTPUT"
 
 INFERENCE_MODULE_TEMPLATE = """
 from typing import Any, Dict, List, Union
@@ -48,6 +57,24 @@ output [{output_spec}
 instance_group [{instance_spec}
 ]
 """
+
+TRITON_PREPROCESS_CONFIG_TEMPLATE = """
+name: "ensemble_preprocess"
+platform: "pytorch_libtorch"
+max_batch_size: 0
+input [{input_spec}
+]
+output [{output_spec}
+]
+instance_group [{instance_spec}
+]
+"""
+
+
+
+class EnsemblePreprocessingConfig:
+    pass
+
 
 
 def _get_input_signature(config: Dict[str, Any]) -> str:
@@ -99,7 +126,10 @@ def generate_triton_torchscript(model: LudwigModel) -> torch.jit.ScriptModule:
 
 
 def _get_type_map(dtype: str) -> str:
-    """Return the Triton API type mapped to numpy type."""
+    """Return the Triton API type mapped to numpy type.
+
+    todo (Wael): add pytorch types and what they correspond to.
+    """
     # see: https://github.com/triton-inference-server/server/blob/main/docs/model_configuration.md
     return {
         "bool": "BOOL",
@@ -162,14 +192,122 @@ def _get_model_config(model: LudwigModel) -> str:
     return config
 
 
+def single_to_input(s: pd.Series, feature) -> Union[List[str], torch.Tensor]:
+    """Transform input for a feature to be compatible with what's required by TorchScript.
+    """
+    if s.dtype == "object" or type(feature) is CategoryInputFeature:
+        return s.astype('str').to_list()
+    return torch.from_numpy(s.to_numpy())
+
+def all_to_input(df: pd.DataFrame, model: LudwigModel):
+    """Transform input for all features to be compatible with what's required by TorchScript.
+    """
+    return {name: single_to_input(df[feature.column], feature) for name, feature in model.model.input_features.items()}
+
+
+def to_triton_dimension(content: Union[List[str], List[torch.Tensor], List[TorchAudioTuple], torch.Tensor]):
+    if isinstance(content, list) and content:
+        if isinstance(content[0], str):
+            return (len(content))
+        """
+        todo (Wael): check these and add other types
+        
+        if isinstance(content[0], torch.Tensor):
+            return (len(content))
+        if isinstance(content[0], TorchAudioTuple):
+            return (len(content))
+        """
+    elif isinstance(content, torch.Tensor):
+        return tuple(content.size())
+
+def to_triton_type(content: Union[List[str], List[torch.Tensor], List[TorchAudioTuple], torch.Tensor]):
+    if isinstance(content, list) and content:
+        if isinstance(content[0], str):
+            return _get_type_map("string")
+        """
+        todo (Wael): check these and add other types
+
+        if isinstance(content[0], torch.Tensor):
+            return _get_type_map(content.dtype)
+        if isinstance(content[0], TorchAudioTuple):
+            return _get_type_map(content.dtype)
+        """
+    elif isinstance(content, torch.Tensor):
+        return _get_type_map(content.dtype)
+
+
+@dataclass
+class TritonConfigFeature:
+    name: str
+    content: Union[TorchscriptPreprocessingInput, torch.Tensor]
+    inference_stage: str
+    kind: str
+    order: int
+
+    def __post_init__(self):
+        # get Triton type
+        self.type = to_triton_type(self.content)
+        # get dimension
+        self.dimension = to_triton_dimension(self.content)
+        # get ensemble_scheduling output_map key (same as "name" in input/output)
+        self.key = f"{self.kind}__{self.order}"
+        # get ensemble_scheduling output_map value
+        self.value = f"{self.name}_{self.inference_stage}"
+        pass
+
+@dataclass
+class TritonEnsembleConfig:
+    """
+    will store triton config template, call the proper functions to populate it.
+    could store path and have a function for save.
+    """
+    config_template: str
+    pass
+
+@dataclass
+class TritonConfig:
+    """
+    will store triton config template, call the proper functions to populate it.
+    could store path and have a function for save.
+    """
+
+    config_template: str
+    pass
+
+@dataclass
+class TritonModel:
+    """
+    will store wrapper class template, call the proper functions to populate it.
+    will return torchscript of each part of the inference pipeline.
+    could store path and have a function for save.
+    """
+    wrapper_template: str
+    pass
+
+@dataclass
+class TritonMaster: # change name
+    input_data_example: Dict[str, Union[TorchscriptPreprocessingInput, torch.Tensor]]
+    output_data_example: Dict[str, Union[TorchscriptPreprocessingInput, torch.Tensor]]
+    inference_stage: str
+
+    def __post_init__(self):
+        if self.inference_stage in INFERENCE_STAGES:
+            self.input_features = [TritonConfigFeature(feature_name, content, self.inference_stage, INPUT, i) for i, (feature_name, content) in
+                                   enumerate(self.input_data_example.items())]
+            self.output_features = [TritonConfigFeature(feature_name, content, self.inference_stage, OUTPUT, i) for i, (feature_name, content) in
+                                   enumerate(self.output_data_example.items())]
+        else:
+            raise ValueError("Invalid inference stage. Choose one of {}".format(INFERENCE_STAGES))
+
 def export_triton(
-    model: LudwigModel, output_path: str, model_name: str = "ludwig_model", model_version: Union[int, str] = 1
+    model: LudwigModel, data_example: pd.DataFrame, output_path: str, model_name: str = "ludwig_model", model_version: Union[int, str] = 1
 ) -> Tuple[str, str]:
     """Exports a torchscript model to a output path that serves as a repository for Triton Inference Server.
 
     # Inputs
-
     :param model: (LudwigModel) A ludwig model.
+    :param data_example: (pd.DataFrame) an example from the dataset.
+        Used to get dimensions throughout the pipeline.
     :param output_path: (str) The output path for the model repository.
     :param model_name: (str) The optional model name.
     :param model_name: (Union[int,str]) The optional model verison.
@@ -177,6 +315,23 @@ def export_triton(
     # Return
     :return: (str, str) The saved model path, and config path.
     """
+
+
+    inference_module = InferenceModule.from_ludwig_model(model.model, model.config, model.training_set_metadata, DEVICE)
+    data_example = all_to_input(data_example.head(1), model)
+
+    preprocessor_output = inference_module.preprocessor_forward(data_example)
+    preprocessor_triton_config = TritonMaster(data_example, preprocessor_output, PREPROCESSOR)
+
+    predictor_output = inference_module.predictor_forward(preprocessor_output)
+    predictor_triton_config = TritonMaster(preprocessor_output, predictor_output, PREDICTOR)
+
+    postprocessor_output = inference_module.postprocessor_forward(predictor_output)
+    postprocessor_triton_config = TritonMaster(predictor_output, postprocessor_output, POSTPROCESSOR)
+
+
+
+
     model_ts = generate_triton_torchscript(model)
     model_dir = os.path.join(output_path, model_name, str(model_version))
     os.makedirs(model_dir, exist_ok=True)
