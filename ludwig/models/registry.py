f4cc2f9568c2e7473c86a066ba21ee51ae1f7ccb
Travis Addair <tgaddair@gmail.com>
2022-07-10 08:37:01 -0700

    Removed import warnings when LightGBM model not specified

--- ludwig/models/registry.py
+++ ludwig/models/registry.py
@@ -3,18 +3,21 @@ import logging
 from ludwig.constants import MODEL_ECD, MODEL_GBM
 from ludwig.models.ecd import ECD
 
+
+def gbm(*args, **kwargs):
+    try:
+        from ludwig.models.gbm import GBM
+    except ImportError:
+        logging.warning(
+            "Importing GBM requirements failed. Not loading GBM model type. "
+            "If you want to use GBM, install Ludwig's 'tree' extra."
+        )
+        raise
+
+    return GBM(*args, **kwargs)
+
+
 model_type_registry = {
     MODEL_ECD: ECD,
+    MODEL_GBM: gbm,
 }
-
-try:
-    import lightgbm  # noqa: F401
-
-    from ludwig.models.gbm import GBM
-
-    model_type_registry[MODEL_GBM] = GBM
-except ImportError:
-    logging.warning(
-        "Importing GBM requirements failed. Not loading GBM model type. "
-        "If you want to use GBM, install Ludwig's 'tree' extra."
-    )
