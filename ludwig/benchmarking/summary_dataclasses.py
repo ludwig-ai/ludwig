70daff18ede4599919f00c20067ced322dc79cbb
Wael Abid <wael@predibase.com>
2022-07-14 15:47:18 -0700

    replaced `__post_init__`with builder/getter functions

--- ludwig/benchmarking/summary_dataclasses.py
+++ ludwig/benchmarking/summary_dataclasses.py
@@ -1,12 +1,13 @@
 import os
+
+from typing import Union, List, Dict, Any
 from dataclasses import dataclass
-from typing import List
+import ludwig.modules.metric_modules
+from ludwig.utils.data_utils import load_json
+from ludwig.modules.metric_registry import get_metric_classes, metric_feature_registry
+from ludwig.globals import MODEL_HYPERPARAMETERS_FILE_NAME
 
 from globals import REPORT_JSON
-
-from ludwig.globals import MODEL_HYPERPARAMETERS_FILE_NAME
-from ludwig.modules.metric_registry import get_metric_classes
-from ludwig.utils.data_utils import load_json
 
 
 @dataclass
@@ -18,31 +19,13 @@ class ExperimentSummary:
     output_feature_name: LudwigModel output feature name.
     metric_to_values: dictionary that maps from metric name to their values.
     metric_names: names of metrics for the output feature.
-    empty: True if unable to load metrics.
     """
-
     experiment_local_directory: str
-
-    def __post_init__(self):
-        self.errors: List[Exception] = []
-        try:
-            self.config = load_json(os.path.join(self.experiment_local_directory, MODEL_HYPERPARAMETERS_FILE_NAME))
-            report = load_json(os.path.join(self.experiment_local_directory, REPORT_JSON))
-        except Exception as error:
-            self.errors.append(error)
-            self.empty = True
-            return
-
-        performance_metrics = report["evaluate"]["performance_metrics"]
-        self.output_feature_type: str = self.config["output_features"][0]["type"]
-        self.output_feature_name: str = self.config["output_features"][0]["name"]
-        metric_dict = performance_metrics[self.output_feature_name]
-        full_metric_names = get_metric_classes(self.output_feature_type)
-        self.metric_to_values: dict = {
-            metric_name: metric_dict[metric_name] for metric_name in full_metric_names if metric_name in metric_dict
-        }
-        self.metric_names: set = set(self.metric_to_values.keys())
-        self.empty = False
+    config: Dict[str, Any]
+    output_feature_type: str
+    output_feature_name: str
+    metric_to_values: Dict[str, Union[float, int]]
+    metric_names: set
 
 
 @dataclass
@@ -55,14 +38,11 @@ class MetricDiff:
     diff: experimental_value - base_value.
     diff_percentage: percentage of change the metric with respect to base_value.
     """
-
     name: str
     base_value: float
     experimental_value: float
-
-    def __post_init__(self):
-        self.diff = self.experimental_value - self.base_value
-        self.diff_percentage = 100 * self.diff / self.base_value if self.base_value != 0 else None
+    diff: float
+    diff_percentage: float
 
 
 @dataclass
@@ -76,29 +56,66 @@ class ExperimentsDiff:
     base_summary: `ExperimentSummary` of the base_experiment.
     experimental_summary: `ExperimentSummary` of the experimental_experiment.
     metrics: `List[MetricDiff]` containing diffs for metric of the two experiments.
-    empty: True if we're unable to load either of the `ExperimentSummary`.
     """
-
     dataset_name: str
     base_experiment_name: str
     experimental_experiment_name: str
     local_directory: str
+    base_summary: ExperimentSummary
+    experimental_summary: ExperimentSummary
+    metrics: List[MetricDiff]
 
-    def __post_init__(self):
-        self.base_summary: ExperimentSummary = ExperimentSummary(
-            os.path.join(self.local_directory, self.dataset_name, self.base_experiment_name)
-        )
-        self.experimental_summary: ExperimentSummary = ExperimentSummary(
-            os.path.join(self.local_directory, self.dataset_name, self.experimental_experiment_name)
-        )
 
-        if self.base_summary.empty or self.experimental_summary.empty:
-            self.empty = True
-            return
+def build_experiment_summary(experiment_local_directory: str) -> ExperimentSummary:
+    config = load_json(os.path.join(experiment_local_directory, MODEL_HYPERPARAMETERS_FILE_NAME))
+    report = load_json(os.path.join(experiment_local_directory, REPORT_JSON))
+    performance_metrics = report["evaluate"]["performance_metrics"]
+    output_feature_type: str = config["output_features"][0]["type"]
+    output_feature_name: str = config["output_features"][0]["name"]
+    metric_dict = performance_metrics[output_feature_name]
+    full_metric_names = get_metric_classes(output_feature_type)
+    metric_to_values: dict = {metric_name: metric_dict[metric_name] for metric_name in full_metric_names if
+                              metric_name in metric_dict}
+    metric_names: set = set(metric_to_values.keys())
 
-        shared_metrics = set(self.base_summary.metric_names).intersection(set(self.experimental_summary.metric_names))
-        self.metrics: List[MetricDiff] = [
-            MetricDiff(name, self.base_summary.metric_to_values[name], self.experimental_summary.metric_to_values[name])
-            for name in shared_metrics
-        ]
-        self.empty = False
+    return ExperimentSummary(experiment_local_directory=experiment_local_directory,
+                             config=config,
+                             output_feature_name=output_feature_name,
+                             output_feature_type=output_feature_type,
+                             metric_to_values=metric_to_values,
+                             metric_names=metric_names,
+                             )
+
+
+def build_metric_diff(name: str, base_value: float, experimental_value: float) -> MetricDiff:
+    diff = experimental_value - base_value
+    diff_percentage = 100 * diff / base_value if base_value != 0 else None
+
+    return MetricDiff(name=name,
+                      base_value=base_value,
+                      experimental_value=experimental_value,
+                      diff=diff,
+                      diff_percentage=diff_percentage,
+                      )
+
+
+def build_experiments_diff(dataset_name, base_experiment_name, experimental_experiment_name, local_directory):
+    base_summary: ExperimentSummary = build_experiment_summary(
+        os.path.join(local_directory, dataset_name, base_experiment_name))
+    experimental_summary: ExperimentSummary = build_experiment_summary(
+        os.path.join(local_directory, dataset_name, experimental_experiment_name))
+
+    shared_metrics = set(base_summary.metric_names).intersection(set(experimental_summary.metric_names))
+
+    metrics: List[MetricDiff] = [
+        build_metric_diff(name, base_summary.metric_to_values[name], experimental_summary.metric_to_values[name]) for
+        name in shared_metrics]
+
+    return ExperimentsDiff(dataset_name=dataset_name,
+                           base_experiment_name=base_experiment_name,
+                           experimental_experiment_name=experimental_experiment_name,
+                           local_directory=local_directory,
+                           base_summary=base_summary,
+                           experimental_summary=experimental_summary,
+                           metrics=metrics,
+                           )
