e3d928e1a18be9800ebb248615c92481ce9820f7
Geoffrey Angus <geoffrey@predibase.com>
2022-07-15 17:00:04 -0700

    starting work on custom datasource starting from file data source

---
+++ ludwig/backend/datasource.py
@@ -0,0 +1,183 @@
+import logging
+import pathlib
+import posixpath
+from typing import (
+    Callable,
+    Optional,
+    List,
+    Tuple,
+    Union,
+    Any,
+    Dict,
+    Iterator,
+    Iterable,
+    TYPE_CHECKING,
+)
+import urllib.parse
+
+from ray.data.datasource.partitioning import PathPartitionFilter
+
+if TYPE_CHECKING:
+    import pyarrow
+
+from ray.types import ObjectRef
+from ray.data.block import Block, BlockAccessor
+from ray.data.context import DatasetContext
+from ray.data.impl.arrow_block import ArrowRow
+from ray.data.impl.block_list import BlockMetadata
+from ray.data.impl.output_buffer import BlockOutputBuffer
+from ray.data.datasource.datasource import Datasource, ReadTask, WriteResult
+from ray.data.datasource.file_based_datasource import (
+    _resolve_paths_and_filesystem,
+    _wrap_s3_serialization_workaround,
+    _S3FileSystemWrapper,
+)
+from ray.data.datasource.file_meta_provider import (
+    BaseFileMetadataProvider,
+    DefaultFileMetadataProvider,
+)
+from ray.util.annotations import DeveloperAPI
+from ray.data.impl.util import _check_pyarrow_version
+from ray.data.impl.remote_fn import cached_remote_fn
+
+logger = logging.getLogger(__name__)
+
+
+class FileBasedDatasource(Datasource[Union[ArrowRow, Any]]):
+    """File-based datasource, for reading and writing files.
+
+    This class should not be used directly, and should instead be subclassed
+    and tailored to particular file formats. Classes deriving from this class
+    must implement _read_file().
+
+    Current subclasses:
+        JSONDatasource, CSVDatasource, NumpyDatasource, BinaryDatasource
+    """
+
+    def prepare_read(
+        self,
+        parallelism: int,
+        paths: Union[str, List[str]],
+        filesystem: Optional["pyarrow.fs.FileSystem"] = None,
+        schema: Optional[Union[type, "pyarrow.lib.Schema"]] = None,
+        open_stream_args: Optional[Dict[str, Any]] = None,
+        meta_provider: BaseFileMetadataProvider = DefaultFileMetadataProvider(),
+        partition_filter: PathPartitionFilter = None,
+        # TODO(ekl) deprecate this once read fusion is available.
+        _block_udf: Optional[Callable[[Block], Block]] = None,
+        **reader_args,
+    ) -> List[ReadTask]:
+        """Creates and returns read tasks for a file-based datasource."""
+        _check_pyarrow_version()
+        import numpy as np
+
+        paths, filesystem = _resolve_paths_and_filesystem(paths, filesystem)
+        paths, file_sizes = meta_provider.expand_paths(paths, filesystem)
+        if partition_filter is not None:
+            paths = partition_filter(paths)
+
+        read_stream = self._read_stream
+
+        filesystem = _wrap_s3_serialization_workaround(filesystem)
+
+        if open_stream_args is None:
+            open_stream_args = {}
+
+        def read_files(
+            read_paths: List[str],
+            fs: Union["pyarrow.fs.FileSystem", _S3FileSystemWrapper],
+        ) -> Iterable[Block]:
+            logger.debug(f"Reading {len(read_paths)} files.")
+            if isinstance(fs, _S3FileSystemWrapper):
+                fs = fs.unwrap()
+            ctx = DatasetContext.get_current()
+            output_buffer = BlockOutputBuffer(block_udf=_block_udf, target_max_block_size=ctx.target_max_block_size)
+            for read_path in read_paths:
+                compression = open_stream_args.pop("compression", None)
+                if compression is None:
+                    import pyarrow as pa
+
+                    try:
+                        # If no compression manually given, try to detect
+                        # compression codec from path.
+                        compression = pa.Codec.detect(read_path).name
+                    except (ValueError, TypeError):
+                        # Arrow's compression inference on the file path
+                        # doesn't work for Snappy, so we double-check ourselves.
+                        import pathlib
+
+                        suffix = pathlib.Path(read_path).suffix
+                        if suffix and suffix[1:] == "snappy":
+                            compression = "snappy"
+                        else:
+                            compression = None
+                if compression == "snappy":
+                    # Pass Snappy compression as a reader arg, so datasource subclasses
+                    # can manually handle streaming decompression in
+                    # self._read_stream().
+                    reader_args["compression"] = compression
+                    reader_args["filesystem"] = fs
+                elif compression is not None:
+                    # Non-Snappy compression, pass as open_input_stream() arg so Arrow
+                    # can take care of streaming decompression for us.
+                    open_stream_args["compression"] = compression
+                with self._open_input_source(fs, read_path, **open_stream_args) as f:
+                    for data in read_stream(f, read_path, **reader_args):
+                        output_buffer.add_block(data)
+                        if output_buffer.has_next():
+                            yield output_buffer.next()
+            output_buffer.finalize()
+            if output_buffer.has_next():
+                yield output_buffer.next()
+
+        # fix https://github.com/ray-project/ray/issues/24296
+        parallelism = min(parallelism, len(paths))
+
+        read_tasks = []
+        for read_paths, file_sizes in zip(np.array_split(paths, parallelism), np.array_split(file_sizes, parallelism)):
+            if len(read_paths) <= 0:
+                continue
+
+            meta = meta_provider(
+                read_paths,
+                schema,
+                rows_per_file=self._rows_per_file(),
+                file_sizes=file_sizes,
+            )
+            read_task = ReadTask(lambda read_paths=read_paths: read_files(read_paths, filesystem), meta)
+            read_tasks.append(read_task)
+
+        return read_tasks
+
+    def _rows_per_file(self):
+        """Returns the number of rows per file, or None if unknown."""
+        return None
+
+    def _read_stream(self, f: "pyarrow.NativeFile", path: str, **reader_args) -> Iterator[Block]:
+        """Streaming read a single file, passing all kwargs to the reader.
+
+        By default, delegates to self._read_file().
+        """
+        yield self._read_file(f, path, **reader_args)
+
+    def _read_file(self, f: "pyarrow.NativeFile", path: str, **reader_args) -> Block:
+        """Reads a single file, passing all kwargs to the reader.
+
+        This method should be implemented by subclasses.
+        """
+        raise NotImplementedError("Subclasses of FileBasedDatasource must implement _read_file().")
+
+    def _open_input_source(
+        self,
+        filesystem: "pyarrow.fs.FileSystem",
+        path: str,
+        **open_args,
+    ) -> "pyarrow.NativeFile":
+        """Opens a source path for reading and returns the associated Arrow NativeFile.
+
+        The default implementation opens the source path as a sequential input stream.
+
+        Implementations that do not support streaming reads (e.g. that require random
+        access) should override this method.
+        """
+        return filesystem.open_input_stream(path, **open_args)
