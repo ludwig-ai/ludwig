c646a45871889460f3e076b1a0e21d1ee38a6de2
hungcs <85463385+hungcs@users.noreply.github.com>
2022-07-18 13:38:46 -0700

    Support passing ground_truth as df for visualizations (#2281)
    https://github.com/ludwig-ai/ludwig/pull/2281

--- ludwig/utils/data_utils.py
+++ ludwig/utils/data_utils.py
@@ -61,7 +61,11 @@ logger = logging.getLogger(__name__)
 DATASET_SPLIT_URL = "dataset_{}_fp"
 DATA_PROCESSED_CACHE_DIR = "data_processed_cache_dir"
 DATA_TRAIN_HDF5_FP = "data_train_hdf5_fp"
+
 DATA_TRAIN_PARQUET_FP = "data_train_parquet_fp"
+DATA_VALIDATION_PARQUET_FP = "data_validation_parquet_fp"
+DATA_TEST_PARQUET_FP = "data_test_parquet_fp"
+
 HDF5_COLUMNS_KEY = "columns"
 DICT_FORMATS = {"dict", "dictionary", dict}
 DATAFRAME_FORMATS = {"dataframe", "df", pd.DataFrame} | DASK_DF_FORMATS
