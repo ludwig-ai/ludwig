82bcaf58efaa9c8573e9f758b476b2d28700a295
Daniel Treiman <daniel@predibase.com>
2022-07-21 16:05:26 -0700

    Updates comment.

--- ludwig/modules/tabnet_modules.py
+++ ludwig/modules/tabnet_modules.py
@@ -109,7 +109,7 @@ class TabNet(LudwigModule):
         total_entropy = 0.0
 
         if batch_size != 1 or not self.training:
-            # Skip batch normalization if the batch size is 1.
+            # Skip batch normalization training if the batch size is 1.
             features = self.batch_norm(features)  # [b_s, i_s]
         masked_features = features
 
