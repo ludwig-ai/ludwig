51aeeab9962f8a5f792eae2aa1518f3e053c7c1e
Magdy Saleh <magdy@predibase.com>
2022-07-22 17:07:56 +0200

    clean up

--- ludwig/data/postprocessing.py
+++ ludwig/data/postprocessing.py
@@ -48,11 +48,13 @@ def postprocess(
     def postprocess_batch(df):
         for of_name, output_feature in output_features.items():
             predictions = output_feature.postprocess_predictions(
-                predictions,
+                df,
                 training_set_metadata[of_name],
                 output_directory=output_directory,
                 backend=backend,
             )
+
+    predictions = backend.df_engine.map_batches(predictions, postprocess_batch)
 
     # Save any new columns but do not save the original columns again
     if not skip_save_unprocessed_output:
