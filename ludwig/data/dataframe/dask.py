dad760523b1fb4b91883cc71d974e85132f7c65f
Geoffrey Angus <geoffrey@predibase.com>
2022-07-29 12:21:12 -0700

    added preliminary check for empty partitions to improve speed

--- ludwig/data/dataframe/dask.py
+++ ludwig/data/dataframe/dask.py
@@ -117,6 +117,9 @@ class DaskEngine(DataFrameEngine):
     def remove_empty_partitions(self, df):
         # Reference: https://stackoverflow.com/questions/47812785/remove-empty-partitions-in-dask
         ll = list(df.map_partitions(len).compute())
+        if all([ll_i > 0 for ll_i in ll]):
+            return df
+
         df_delayed = df.to_delayed()
         df_delayed_new = list()
         empty_partition = None
@@ -125,8 +128,7 @@ class DaskEngine(DataFrameEngine):
                 empty_partition = df.get_partition(ix)
             else:
                 df_delayed_new.append(df_delayed[ix])
-        if empty_partition is not None:
-            df = dd.from_delayed(df_delayed_new, meta=empty_partition)
+        df = dd.from_delayed(df_delayed_new, meta=empty_partition)
         return df
 
     def to_parquet(self, df, path, index=False):
