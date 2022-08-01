423350c9878209c2af11d961cfc24fbfbb47d08a
connor-mccorm <connor@predibase.com>
2022-08-01 11:10:15 -0700

    Started config object

---
+++ ludwig/schema/config_object.py
@@ -0,0 +1,15 @@
+from ludwig.schema.combiners.base import BaseCombinerConfig
+
+
+class Config:
+    """
+    This class is the implementation of the config object that replaces the need for a config dictionary throughout the
+    project.
+    """
+
+    pass
+    # input_features =
+    # output_features =
+    # combiner =
+    # trainer =
+    # hyperopt =
