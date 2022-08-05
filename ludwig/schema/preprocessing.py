1bcee3870a9eb512f356c9dc6b3fda8a76f7c1e7
Kabir Brar <kabir@brar.xyz>
2022-08-05 19:56:45 -0400

    refactor: Replace bespoke marshmallow fields that accept multiple types with a new 'combinatorial' `OneOfField` that accepts other fields as arguments. (#2285)
    * initial commit.
    
    * fix validation.
    
    * apply oneof field in other schemas.
    
    * fix
    
    * remove commented out code.
    
    * none fix
    
    * add test
    
    * fixes
    
    * fixes
    
    * fixes

--- ludwig/schema/preprocessing.py
+++ ludwig/schema/preprocessing.py
@@ -187,28 +187,26 @@ class BinaryPreprocessingConfig(schema_utils.BaseMarshmallowConfig):
         description="What strategy to follow when there's a missing value in a binary column",
     )
 
-    fill_value: Union[int, float, str] = schema_utils.NumericOrStringOptionsField(
-        strings_utils.all_bool_strs(),
+    fill_value: Union[int, float, str] = schema_utils.OneOfOptionsField(
         default=None,
-        default_numeric=None,
-        default_option=None,
-        allow_none=False,
-        min=0,
-        max=1,
+        allow_none=True,
         description="The value to replace missing values with in case the missing_value_strategy is fill_with_const",
+        field_options=[
+            schema_utils.FloatRange(default=None, allow_none=True, min=0, max=1),
+            schema_utils.StringOptions(options=strings_utils.all_bool_strs(), default=None),
+        ],
     )
 
-    computed_fill_value: Union[int, float, str] = schema_utils.NumericOrStringOptionsField(
-        strings_utils.all_bool_strs(),
+    computed_fill_value: Union[int, float, str] = schema_utils.OneOfOptionsField(
         default=None,
-        default_numeric=None,
-        default_option=None,
         allow_none=False,
-        min=0,
-        max=1,
         description="The internally computed fill value to replace missing values with in case the "
         "missing_value_strategy is fill_with_mode or fill_with_mean",
         parameter_metadata=PREPROCESSING_METADATA["computed_fill_value"],
+        field_options=[
+            schema_utils.FloatRange(default=None, allow_none=True, min=0, max=1),
+            schema_utils.StringOptions(options=strings_utils.all_bool_strs(), default=None),
+        ],
     )
 
     fallback_true_label: str = schema_utils.String(
