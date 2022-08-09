8f2a96ae577481d7e97b3798e6f27c71d96b814f
Wael Abid <wael@predibase.com>
2022-08-09 09:23:51 -0700

    exporting cpu and cuda memory usage

--- ludwig/benchmarking/reporting.py
+++ ludwig/benchmarking/reporting.py
@@ -4,6 +4,7 @@ from torch.autograd import DeviceType
 from torch.autograd.profiler_util import MemRecordsAcc, _format_memory, _format_time
 from typing import Any, Dict, Tuple
 from statistics import mean
+from collections import Counter, defaultdict
 
 from ludwig.constants import CACHE, EVAL_TAG, EXPERIMENT_RUN, TRAIN_TAG
 from ludwig.utils.data_utils import load_json, save_json
@@ -38,59 +39,72 @@ def create_metrics_report(experiment_name: str) -> Tuple[Dict[str, Any], str]:
     save_json(merged_file_path, full_report)
     return full_report, merged_file_path
 
-def get_devices_usage(p, info):
-    kineto_results = p.profiler.kineto_results
-    mem_records = [[evt, False] for evt in kineto_results.events() if evt.name() == "[memory]"]
-    main_events = [[evt, False] for evt in kineto_results.events() if "ludwig" in evt.name()]
+def get_device_name(event):
+    if event.device_type() in [DeviceType.CPU, DeviceType.MKLDNN, DeviceType.IDEEP]:
+        return "cpu", event.nbytes()
+    elif event.device_type() in [DeviceType.CUDA, DeviceType.HIP]:
+        return f"cuda_{event.device_index()}", event.nbytes()
+
+def get_devices_usage(kineto_events, info):
+    mem_records = [[evt, False] for evt in kineto_events if evt.name() == "[memory]"]
+    main_events = [evt for evt in kineto_events if "ludwig" in evt.name()]
     mem_records_acc = MemRecordsAcc(mem_records)
     for evt in main_events:
-        info[evt[0].name()] = {"code_block_tag": evt[0].name()}
-        print(evt[0].name())
-        cpu_so_far, cuda_so_far = 0, 0
-        cpu, cuda = [0], [0]
-        for mem_record in mem_records_acc.in_interval(evt[0].start_us(), evt[0].start_us() + evt[0].duration_us()):
-            # mem_record[0].device_index(): 255 is CPU, 0, 1, 2, etc. is CUDA.
-            if mem_record[0].device_type() in [DeviceType.CPU, DeviceType.MKLDNN, DeviceType.IDEEP]:
-                cpu.append(cpu_so_far + mem_record[0].nbytes())
-                cpu_so_far += mem_record[0].nbytes()
-            if mem_record[0].device_type() in [DeviceType.CUDA, DeviceType.HIP]:
-                cuda.append(cuda_so_far + mem_record[0].nbytes())
-                cuda_so_far += mem_record[0].nbytes()
-        print(_format_memory(mean(cpu)), _format_memory(max(cpu)), _format_memory(mean(cuda)), _format_memory(max(cuda)))
-        # export graphs of usage data
-        # put cuda usage in corresponding cuda device id
-        info[evt[0].name()]["average_cpu_memory_usage"] = mean(cpu)
-        info[evt[0].name()]["max_cpu_memory_usage"] = max(cpu)
-        info[evt[0].name()]["average_cuda_memory_usage"] = mean(cuda)
-        info[evt[0].name()]["max_cuda_memory_usage"] = max(cuda)
+        event_name = evt.name()
+        memory_so_far = defaultdict(int)
+        memory_lists = defaultdict(list)
+        for mem_record in mem_records_acc.in_interval(evt.start_us(), evt.start_us() + evt.duration_us()):
+            device, nbytes = get_device_name(mem_record[0])
+            memory_so_far[device] += nbytes
+            memory_lists[device].append(memory_so_far[device])
+        run_usage_info = {}
+        for device in memory_lists:
+            memory_lists[device].append(0) # just in case we have an empty list
+            run_usage_info[f"average_{device}_memory_usage"] = mean(memory_lists[device])
+            run_usage_info[f"max_{device}_memory_usage"] = max(memory_lists[device])
+            run_usage_info[f"average_{device}_memory_usage_str"] = _format_memory(mean(memory_lists[device]))
+            run_usage_info[f"max_{device}_memory_usage_str"] = _format_memory(max(memory_lists[device]))
+        info[event_name]["runs"].append(run_usage_info)
     return info
 
-def get_device_timing(p, info):
-    function_events = p.profiler.function_events
+
+def get_device_timing(function_events, info):
     main_events = [evt for evt in function_events if "ludwig" in evt.name]
     for evt in main_events:
-        print(evt.name)
-        # evt.self_cpu_time_total
-        # evt.cuda_time_total
-        # evt.self_cuda_time_total
-        # evt.cpu_time_total
-        # evt.cpu_time_str
-        # evt.cuda_time_str
-        # evt.cpu_time_total_str
-        # evt.cuda_time_total_str
-        # evt.self_cpu_time_total_str
-        # evt.self_cuda_time_total_str
-        # evt.cpu_time
-        # evt.cuda_time
+        info[evt.name]["num_calls"] += 1
+        info[evt.name]["self_cpu_time_total"] = evt.self_cpu_time_total
+        info[evt.name]["self_cpu_time_total_str"] = evt.self_cpu_time_total_str
+        info[evt.name]["cuda_time_total"] = evt.cuda_time_total
+        info[evt.name]["cuda_time_total_str"] = evt.cuda_time_total_str
+        info[evt.name]["self_cuda_time_total"] = evt.self_cuda_time_total
+        info[evt.name]["self_cuda_time_total_str"] = evt.self_cuda_time_total_str
+        info[evt.name]["cpu_time_total"] = evt.cpu_time_total
+        info[evt.name]["cpu_time_total_str"] = evt.cpu_time_total_str
+        info[evt.name]["cpu_time"] = evt.cpu_time
+        info[evt.name]["cpu_time_str"] = evt.cpu_time_str
+        info[evt.name]["cuda_time"] = evt.cuda_time
+        info[evt.name]["cuda_time_str"] = evt.cuda_time_str
         # I think evt.device_index can give the GPU number
         # name, trace_name, key seem to return the same thing.
     return info
 
+def initialize_stats_dict(function_events):
+    info = dict()
+    for event_name in [evt.name for evt in function_events if "ludwig" in evt.name]:
+        info[event_name] = {"code_block_tag": event_name}
+        info[event_name]["runs"] = []
+    return info
 
 def export_metrics_from_torch_profiler(p: profile, experiment_name: str):
-    info = dict()
-    info = get_devices_usage(p, info)
-    info = get_device_timing(p, info)
+    # events in both of these lists are in chronological order.
+    kineto_events = p.profiler.kineto_results.events()
+    function_events = p.profiler.function_events
+    assert Counter([evt.name for evt in function_events if "ludwig" in evt.name]) == Counter(
+        [evt.name() for evt in kineto_events if "ludwig" in evt.name()])
+
+    info = initialize_stats_dict(function_events)
+    info = get_devices_usage(kineto_events, info)
+    info = get_device_timing(function_events, info)
     for code_block_tag, report in info.items():
         file_path = os.path.join(os.getcwd(), experiment_name, "metrics_report", "{}.json".format(code_block_tag))
         save_json(file_path, report)
