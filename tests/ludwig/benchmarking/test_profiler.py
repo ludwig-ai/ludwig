c1f4777faa985f38bbdbaa6d5d5a81388701ea5a
Wael Abid <wael@predibase.com>
2022-08-19 03:44:06 -0700

    test for LudwigProfiler

---
+++ tests/ludwig/benchmarking/test_profiler.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+import time
+
+import torch
+import numpy as np
+import pandas as pd
+
+from ludwig.api import LudwigModel
+from ludwig.benchmarking.profiler import LudwigProfiler
+from ludwig.constants import TRAINER
+
+
+def test_resource_usage_tracker(tmpdir):
+
+    @LudwigProfiler(tag="test_function", output_dir=tmpdir, use_torch_profiler=False, logging_interval=0.1)
+    def func(duration):
+        time.sleep(duration)
+        x = torch.Tensor(2, 3)
+        y = torch.rand(2, 3)
+        torch.add(x, y)
+
+    train_df = pd.DataFrame(np.random.normal(0, 1, size=(100, 3)), columns=["input_1", "input_2", "output_1"])
+    eval_df = pd.DataFrame(np.random.normal(0, 1, size=(20, 3)), columns=["input_1", "input_2", "output_1"])
+
+    config = {
+        "input_features": [{"name": "input_1", "type": "number"}, {"name": "input_2", "type": "number"}],
+        "output_features": [{"name": "output_1", "type": "number"}],
+        "combiner": {"type": "concat", "output_size": 14},
+        TRAINER: {"epochs": 1},
+    }
+
+    model = LudwigModel(config=config, backend="local")
+
+    with LudwigProfiler(tag="train", output_dir=tmpdir, use_torch_profiler=False, logging_interval=0.1):
+        model.train(
+            dataset=train_df,
+            output_directory=tmpdir,
+            skip_save_training_description=True,
+            skip_save_training_statistics=True,
+            skip_save_model=True,
+            skip_save_progress=True,
+            skip_save_log=True,
+            skip_save_processed_input=True,
+        )
+
+    assert os.path.exists(os.path.join(tmpdir, "system_resource_usage", "train", "run_0.json"))
+
+    with LudwigProfiler(tag="evaluate", output_dir=tmpdir, use_torch_profiler=True, logging_interval=0.1):
+        model.evaluate(dataset=eval_df)
+        func(0.1)
+
+    assert os.path.exists(os.path.join(tmpdir, "system_resource_usage", "evaluate", "run_0.json"))
+    assert os.path.exists(os.path.join(tmpdir, "torch_ops_resource_usage", "evaluate", "run_0.json"))
+
+    func(0.25)
+    func(0.5)
+    assert os.path.exists(os.path.join(tmpdir, "system_resource_usage", "test_function", "run_0.json"))
+    assert os.path.exists(os.path.join(tmpdir, "system_resource_usage", "test_function", "run_1.json"))
+    assert os.path.exists(os.path.join(tmpdir, "system_resource_usage", "test_function", "run_2.json"))
+
+    shutil.rmtree(tmpdir)
