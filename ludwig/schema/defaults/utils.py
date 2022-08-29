44fff64727351084514da52e140fe9461a98fd33
connor-mccorm <connor@predibase.com>
2022-08-29 10:40:37 -0700

    Added defaults schema, need to debug

--- ludwig/schema/defaults/utils.py
+++ ludwig/schema/defaults/utils.py
@@ -2,6 +2,27 @@ from marshmallow import fields, ValidationError
 from dataclasses import field
 
 from ludwig.features.feature_registries import input_type_registry, output_type_registry
+import ludwig.schema.utils as schema_utils
+
+
+def get_defaults_conds():
+    """Returns a JSON schema of conditionals to validate against encoder types for specific feature types."""
+    conds = []
+    for feat in input_type_registry.keys():
+        input_feature_cls = input_type_registry.get(feat).get_schema_cls()
+        output_feature_cls = output_type_registry.get(feat, None)
+        if output_feature_cls:
+            output_feature_cls = output_feature_cls.get_schema_cls()
+        input_props = schema_utils.unload_jsonschema_from_marshmallow_class(input_feature_cls)["properties"]
+        output_props = schema_utils.unload_jsonschema_from_marshmallow_class(output_feature_cls)["properties"]
+        combined_props = {**input_props, **output_props}
+        combined_props.pop("type")
+        defaults_cond = schema_utils.create_cond(
+            {"type": feat},
+            combined_props,
+        )
+        conds.append(defaults_cond)
+    return conds
 
 
 def DefaultsDataclassField(feature_type: str):
@@ -20,33 +41,40 @@ def DefaultsDataclassField(feature_type: str):
                 return None
             if isinstance(value, dict):
                 input_feature_class = input_type_registry[feature_type]
-                output_feature_class = output_type_registry[feature_type]
+                output_feature_class = output_type_registry.get(feature_type, None)
                 try:
-                    return split_class.get_schema_cls().Schema().load(value)
+                    input_schema = input_feature_class.get_schema_cls().Schema().load(value)
+                    if output_feature_class:
+                        output_schema = output_feature_class.get_schema_cls().Schema().load(value)
+                        combined = input_schema + output_schema
+                    else:
+                        combined = input_schema
+                    return combined
                 except (TypeError, ValidationError) as error:
                     raise ValidationError(
-                        f"Invalid split params: {value}, see `{split_class}` definition. Error: {error}"
+                        f"Invalid params: {value}, see `{attr}` definition. Error: {error}"
                     )
             raise ValidationError(
-                f"Invalid params for splitter: {value}, expected dict with at least a valid `type` attribute."
+                f"Invalid params: {value}"
             )
-            raise ValidationError("Field should be None or dict")
 
         @staticmethod
         def _jsonschema_type_mapping():
             return {
                 "type": "object",
                 "properties": {
-                    "type": {"type": "string", "enum": list(split_config_registry.data.keys()), "default": default},
+                    "type": {"type": "string", "enum": list(input_type_registry.data.keys()), "default": None},
                 },
-                "title": "split_options",
-                "allOf": get_split_conds()
+                "title": "defaults_options",
+                "allOf": get_defaults_conds()
             }
 
     try:
-        splitter = split_config_registry.data[default]
-        load_default = splitter.Schema().load({"type": default})
-        dump_default = splitter.Schema().dump({"type": default})
+        input_cls = input_type_registry[feature_type]
+        output_cls = output_type_registry.get(feature_type, None)
+        defaults = input_cls + output_cls
+        load_default = defaults.Schema().load({"type": feature_type})
+        dump_default = defaults.Schema().dump({"type": feature_type})
 
         return field(
             metadata={
@@ -59,4 +87,4 @@ def DefaultsDataclassField(feature_type: str):
             default_factory=lambda: load_default,
         )
     except Exception as e:
-        raise ValidationError(f"Unsupported splitter type: {default}. See split_registry. " f"Details: {e}")
+        raise ValidationError(f"Unsupported splitter type: {feature_type}. See split_registry. " f"Details: {e}")
