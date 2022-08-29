59135acf608e58223c391fe77d02d28938186138
Wael Abid <wael@predibase.com>
2022-08-29 12:20:20 -0700

    changing to ray backend

--- ludwig/benchmarking/benchmark.py
+++ ludwig/benchmarking/benchmark.py
@@ -4,7 +4,8 @@ import logging
 import os
 import shutil
 from typing import Any, Dict, Union
-
+from pprint import pprint
+import ray
 from ludwig.api import LudwigModel
 from ludwig.benchmarking.utils import export_artifacts, load_from_module
 from ludwig.contrib import add_contrib_callback_args
@@ -19,14 +20,13 @@ def setup_experiment(experiment: Dict[str, str]) -> Dict[Any, Any]:
     experiment: dictionary containing the dataset name, config path, and experiment name.
     Returns a Ludwig config.
     """
-    shutil.rmtree(os.path.join(experiment["dataset_name"]), ignore_errors=True)
+    shutil.rmtree(os.path.join(experiment["experiment_name"]), ignore_errors=True)
     model_config = load_yaml(experiment["config_path"])
-    model_config["backend"] = {}
-    model_config["backend"]["type"] = "local"
     return model_config
 
 
-def benchmark_one_local(experiment: Dict[str, str], export_artifacts_dict: Dict[str, Union[str, bool]]) -> None:
+@ray.remote
+def benchmark_one(experiment: Dict[str, str], export_artifacts_dict: Dict[str, Union[str, bool]]) -> None:
     """Run a Ludwig exepriment and track metrics given a dataset name.
 
     experiment: dictionary containing the dataset name, config path, and experiment name.
@@ -45,7 +45,7 @@ def benchmark_one_local(experiment: Dict[str, str], export_artifacts_dict: Dict[
     model = LudwigModel(config=model_config, logging_level=logging.ERROR)
     _, _, _, output_directory = model.experiment(
         dataset=dataset,
-        output_directory=experiment["dataset_name"],
+        output_directory=experiment["experiment_name"],
         skip_save_processed_input=True,
         skip_save_unprocessed_output=True,
         skip_save_predictions=True,
@@ -63,15 +63,17 @@ def benchmark(bench_config_path: str) -> None:
         corresponding Ludwig configs, as well as export options.
     """
     config = load_yaml(bench_config_path)
+    tasks = []
     for experiment in config["datasets"]:
         try:
             if "experiment_name" not in experiment:
                 experiment["experiment_name"] = config["global_experiment_name"]
-            benchmark_one_local(experiment, export_artifacts_dict=config["export"][0])
+            tasks.append(benchmark_one.remote(experiment, config["export"][0]))
         except Exception:
             logging.exception(
                 f"Experiment *{experiment['experiment_name']}* on dataset *{experiment['dataset_name']}* failed"
             )
+    ray.get(tasks)
 
 
 def cli(sys_argv):
