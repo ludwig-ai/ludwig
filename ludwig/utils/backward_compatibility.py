2fbfdf18a7a6714de1ff0803685ed3d875bc55ea
connor-mccorm <connor@predibase.com>
2022-08-30 19:57:11 -0700

    Removed merge with defaults, fixed more tests

--- ludwig/utils/backward_compatibility.py
+++ ludwig/utils/backward_compatibility.py
@@ -196,6 +196,7 @@ def _upgrade_encoder_decoder_params(feature: Dict[str, Any], input_feature: bool
         "name",
         "type",
         "column",
+        "proc_column",
         "decoder",
         "num_classes",
         "preprocessing",
@@ -244,10 +245,11 @@ def _upgrade_encoder_decoder_params(feature: Dict[str, Any], input_feature: bool
             nested_params.append(k)
             warn = True
 
-    if module_type in feature:
-        feature[module_type].update(module)
-    else:
-        feature[module_type] = module
+    if module:
+        if module_type in feature:
+            feature[module_type].update(module)
+        else:
+            feature[module_type] = module
 
     for k in nested_params:
         del feature[k]
