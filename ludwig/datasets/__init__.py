3e7b93dfc0eb7357233cd181cbc4d27b5b60f1dc
Daniel Treiman <daniel@predibase.com>
2022-09-12 12:18:18 -0700

    Upgrades datasets tests.

--- ludwig/datasets/__init__.py
+++ ludwig/datasets/__init__.py
@@ -12,7 +12,7 @@ from ludwig.globals import LUDWIG_VERSION
 from ludwig.utils.print_utils import print_ludwig
 
 
-def _load_dataset_config(config_filename: str):
+def load_dataset_config(config_filename: str):
     """Loads a dataset config."""
     config_path = os.path.join(os.path.dirname(configs.__file__), config_filename)
     with open(config_path) as f:
@@ -25,16 +25,21 @@ def _get_dataset_configs() -> Dict[str, DatasetConfig]:
     import importlib.resources
 
     config_files = [f for f in importlib.resources.contents(configs) if f.endswith(".yaml")]
-    config_objects = [_load_dataset_config(f) for f in config_files]
+    config_objects = [load_dataset_config(f) for f in config_files]
     return {c.name: c for c in config_objects}
 
 
-def get_dataset(dataset_name, cache_dir=None):
-    """Gets an instance of the dataset loader for a given dataset."""
+def get_dataset_config(dataset_name) -> DatasetConfig:
+    """Get the config for a dataset."""
     configs = _get_dataset_configs()
     if dataset_name not in configs:
         raise AttributeError(f"No config found for dataset {dataset_name}")
-    config = configs[dataset_name]
+    return configs[dataset_name]
+
+
+def get_dataset(dataset_name, cache_dir=None) -> Any:
+    """Gets an instance of the dataset loader for a dataset."""
+    config = get_dataset_config(dataset_name)
     class_name = config.loader.split(".")[-1]
     module_name = "." + ".".join(config.loader.split(".")[:-1])
     loader_module = importlib.import_module(module_name, package="ludwig.datasets.loaders")
@@ -45,14 +50,17 @@ def get_dataset(dataset_name, cache_dir=None):
 
 
 def list_datasets() -> List[str]:
+    """Returns a list of the names of all available datasets."""
     return sorted(_get_dataset_configs().keys())
 
 
 def describe_dataset(dataset_name: str) -> str:
+    """Returns the description of the dataset."""
     return _get_dataset_configs()[dataset_name].description
 
 
 def download_dataset(dataset_name: str, output_dir: str = "."):
+    """Downloads the dataset to the specified directory."""
     dataset = get_dataset(dataset_name)
     dataset.export(output_dir)
 
@@ -101,7 +109,7 @@ def __getattr__(name: str) -> Any:
 
          from ludwig.datasets import titanic
 
-    returns an instance of DatasetLoader with the titanic config.
+    returns an instance of DatasetLoader configured to load titanic.
 
     If you want to download a dataset in a non-default ludwig cache directory, there are two options:
         1. set the LUDWIG_CACHE environment variable to your desired path before importing the dataset
--- tests/ludwig/datasets/test_datasets.py
+++ tests/ludwig/datasets/test_datasets.py
@@ -1,148 +1,103 @@
 import os
+from unittest import mock
 
-# from ludwig.datasets.base_dataset import BaseDataset
-from ludwig.datasets.dataset import Dataset
+import pandas as pd
+import pytest
 
-# from unittest import mock
+import ludwig.datasets
+from ludwig.datasets.dataset_config import DatasetConfig
+from ludwig.datasets.loaders.dataset_loader import DatasetState
 
-# import pandas as pd
-# import pytest
-
-
-# from ludwig.datasets.mixins.download import UncompressedFileDownloadMixin, ZipDownloadMixin
-# from ludwig.datasets.mixins.load import CSVLoadMixin
-# from ludwig.datasets.mixins.process import IdentityProcessMixin, MultifileJoinProcessMixin
-
-# SUPPORTED_UNCOMPRESSED_FILETYPES = ["json", "jsonl", "tsv", "csv"]
-#
-#
-# class FakeCSVDataset(ZipDownloadMixin, IdentityProcessMixin, CSVLoadMixin, BaseDataset):
-#     def __init__(self, cache_dir=None):
-#         super().__init__(dataset_name="fake", cache_dir=cache_dir)
-#
-#
-# class FakeMultiFileDataset(UncompressedFileDownloadMixin, MultifileJoinProcessMixin, CSVLoadMixin, BaseDataset):
-#     def __init__(self, cache_dir=None):
-#         super().__init__(dataset_name="multifiles", cache_dir=cache_dir)
-#
-#
-# def test_load_csv_dataset(tmpdir):
-#     input_df = pd.DataFrame(
-#         {"name": ["Raphael", "Donatello"], "mask": ["red", "purple"], "weapon": ["sai", "bo staff"], "split": [0, 1]}
-#     )
-#
-#     extracted_filename = "input.csv"
-#     compression_opts = dict(method="zip", archive_name=extracted_filename)
-#
-#     archive_filename = os.path.join(tmpdir, "archive.zip")
-#     input_df.to_csv(archive_filename, index=False, compression=compression_opts)
-#
-#     config = dict(
-#         version=1.0,
-#         download_urls=["file://" + archive_filename],
-#         csv_filename=extracted_filename,
-#     )
-#
-#     with mock.patch("ludwig.datasets.base_dataset.read_config", return_value=config):
-#         dataset = FakeCSVDataset(tmpdir)
-#
-#         assert not dataset.is_downloaded()
-#         assert not dataset.is_processed()
-#
-#         output_df = dataset.load()
-#         pd.testing.assert_frame_equal(input_df, output_df)
-#
-#         assert dataset.is_downloaded()
-#         assert dataset.is_processed()
-#
-#
-# @pytest.mark.parametrize("f_type", SUPPORTED_UNCOMPRESSED_FILETYPES)
-# def test_multifile_join_dataset(tmpdir, f_type):
-#     if f_type != "jsonl":
-#         train_df = pd.DataFrame(
-#             {"name": ["Raphael", "Donatello"], "mask": ["red", "purple"], "weapon": ["sai", "bo staff"]}
-#         )
-#
-#         test_df = pd.DataFrame({"name": ["Jack", "Bob"], "mask": ["green", "yellow"], "weapon": ["knife", "gun"]})
-#
-#         val_df = pd.DataFrame({"name": ["Tom"], "mask": ["pink"], "weapon": ["stick"]})
-#     else:
-#         train_df = pd.DataFrame([{"name": "joe"}, {"mask": "green"}, {"weapon": "stick"}])
-#         test_df = pd.DataFrame([{"name": "janice"}, {"mask": "black"}, {"weapon": "gun"}])
-#         val_df = pd.DataFrame([{"name": "sara"}, {"mask": "pink"}, {"weapon": "gun"}])
-#
-#     # filetypes = ['json', 'tsv', 'jsonl']
-#     train_filename = "train." + f_type
-#     test_filename = "test." + f_type
-#     val_filename = "val." + f_type
-#     train_filepath = os.path.join(tmpdir, train_filename)
-#     test_filepath = os.path.join(tmpdir, test_filename)
-#     val_filepath = os.path.join(tmpdir, val_filename)
-#
-#     if f_type == "json":
-#         train_df.to_json(train_filepath)
-#         test_df.to_json(test_filepath)
-#         val_df.to_json(val_filepath)
-#     elif f_type == "jsonl":
-#         train_df.to_json(train_filepath, orient="records", lines=True)
-#         test_df.to_json(test_filepath, orient="records", lines=True)
-#         val_df.to_json(val_filepath, orient="records", lines=True)
-#     elif f_type == "tsv":
-#         train_df.to_csv(train_filepath, sep="\t")
-#         test_df.to_csv(test_filepath, sep="\t")
-#         val_df.to_csv(val_filepath, sep="\t")
-#     else:
-#         train_df.to_csv(train_filepath)
-#         test_df.to_csv(test_filepath)
-#         val_df.to_csv(val_filepath)
-#
-#     config = {
-#         "version": 1.0,
-#         "download_urls": ["file://" + train_filepath, "file://" + test_filepath, "file://" + val_filepath],
-#         "split_filenames": {"train_file": train_filename, "test_file": test_filename, "val_file": val_filename},
-#         "download_file_type": f_type,
-#         "csv_filename": "fake.csv",
-#     }
-#
-#     with mock.patch("ludwig.datasets.base_dataset.read_config", return_value=config):
-#         dataset = FakeMultiFileDataset(tmpdir)
-#
-#         assert not dataset.is_downloaded()
-#         assert not dataset.is_processed()
-#
-#         output_df = dataset.load()
-#         assert output_df.shape[0] == train_df.shape[0] + test_df.shape[0] + val_df.shape[0]
-#
-#         assert dataset.is_downloaded()
-#         assert dataset.is_processed()
+SUPPORTED_UNCOMPRESSED_FILETYPES = ["json", "jsonl", "tsv", "csv"]
 
 
-def test_dead_links():
-    # Iterate through all datasets, ensure links are valid and reachable.
-    from ludwig.datasets.registry import dataset_registry
+def test_load_csv_dataset(tmpdir):
+    input_df = pd.DataFrame(
+        {"name": ["Raphael", "Donatello"], "mask": ["red", "purple"], "weapon": ["sai", "bo staff"], "split": [0, 1]}
+    )
 
-    for name, dataset in dataset_registry.items():
-        print(f"Checking dataset {name}")
+    extracted_filename = "input.csv"
+    compression_opts = dict(method="zip", archive_name=extracted_filename)
+
+    archive_filename = os.path.join(tmpdir, "archive.zip")
+    input_df.to_csv(archive_filename, index=False, compression=compression_opts)
+
+    config = DatasetConfig(
+        version=1.0,
+        name="fake_csv_dataset",
+        download_urls=["file://" + archive_filename],
+    )
+
+    ludwig.datasets._get_dataset_configs.cache_clear()
+    with mock.patch("ludwig.datasets.load_dataset_config", return_value=config):
+        dataset = ludwig.datasets.get_dataset("fake_csv_dataset", cache_dir=tmpdir)
+
+        assert not dataset.state == DatasetState.DOWNLOADED
+        assert not dataset.state == DatasetState.TRANSFORMED
+
+        output_df = dataset.load()
+        pd.testing.assert_frame_equal(input_df, output_df)
+
+        assert dataset.state == DatasetState.TRANSFORMED
 
 
-def test_new_datasets(tmpdir):
-    from ludwig.datasets.registry import dataset_registry
+@pytest.mark.parametrize("f_type", SUPPORTED_UNCOMPRESSED_FILETYPES)
+def test_multifile_join_dataset(tmpdir, f_type):
+    if f_type != "jsonl":
+        train_df = pd.DataFrame(
+            {"name": ["Raphael", "Donatello"], "mask": ["red", "purple"], "weapon": ["sai", "bo staff"]}
+        )
 
-    assert "yosemite" in dataset_registry
-    yosemite_dataset = dataset_registry["yosemite"]
-    assert isinstance(yosemite_dataset, Dataset)
-    yosemite_dataset.cache_dir = os.path.join(tmpdir, "yosemite")
-    df = yosemite_dataset.load()
-    assert df is not None
+        test_df = pd.DataFrame({"name": ["Jack", "Bob"], "mask": ["green", "yellow"], "weapon": ["knife", "gun"]})
 
+        val_df = pd.DataFrame({"name": ["Tom"], "mask": ["pink"], "weapon": ["stick"]})
+    else:
+        train_df = pd.DataFrame([{"name": "joe"}, {"mask": "green"}, {"weapon": "stick"}])
+        test_df = pd.DataFrame([{"name": "janice"}, {"mask": "black"}, {"weapon": "gun"}])
+        val_df = pd.DataFrame([{"name": "sara"}, {"mask": "pink"}, {"weapon": "gun"}])
 
-def test_new_datasets_kaggle(tmpdir):
-    from ludwig.datasets.registry import dataset_registry
+    # filetypes = ['json', 'tsv', 'jsonl']
+    train_filename = "train." + f_type
+    test_filename = "test." + f_type
+    val_filename = "val." + f_type
+    train_filepath = os.path.join(tmpdir, train_filename)
+    test_filepath = os.path.join(tmpdir, test_filename)
+    val_filepath = os.path.join(tmpdir, val_filename)
 
-    assert "twitter_bots" in dataset_registry
-    twitter_bots_dataset = dataset_registry["twitter_bots"]
-    assert twitter_bots_dataset.is_kaggle_dataset
+    if f_type == "json":
+        train_df.to_json(train_filepath)
+        test_df.to_json(test_filepath)
+        val_df.to_json(val_filepath)
+    elif f_type == "jsonl":
+        train_df.to_json(train_filepath, orient="records", lines=True)
+        test_df.to_json(test_filepath, orient="records", lines=True)
+        val_df.to_json(val_filepath, orient="records", lines=True)
+    elif f_type == "tsv":
+        train_df.to_csv(train_filepath, sep="\t")
+        test_df.to_csv(test_filepath, sep="\t")
+        val_df.to_csv(val_filepath, sep="\t")
+    else:
+        train_df.to_csv(train_filepath)
+        test_df.to_csv(test_filepath)
+        val_df.to_csv(val_filepath)
 
+    config = DatasetConfig(
+        version=1.0,
+        name="fake_multifile_dataset",
+        download_urls=["file://" + train_filepath, "file://" + test_filepath, "file://" + val_filepath],
+        train_filenames=train_filename,
+        validation_filenames=val_filename,
+        test_filenames=test_filename,
+    )
 
-if __name__ == "__main__":
-    test_new_datasets("/tmp")
+    ludwig.datasets._get_dataset_configs.cache_clear()
+    with mock.patch("ludwig.datasets.load_dataset_config", return_value=config):
+        dataset = ludwig.datasets.get_dataset("fake_multifile_dataset", cache_dir=tmpdir)
+
+        assert not dataset.state == DatasetState.DOWNLOADED
+        assert not dataset.state == DatasetState.TRANSFORMED
+
+        output_df = dataset.load()
+        assert output_df.shape[0] == train_df.shape[0] + test_df.shape[0] + val_df.shape[0]
+
+        assert dataset.state == DatasetState.TRANSFORMED
