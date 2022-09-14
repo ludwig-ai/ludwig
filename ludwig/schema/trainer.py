57e66c24165bde6d32d3aa99cc4642fa102b63b3
Joppe Geluykens <joppe@predibase.com>
2022-09-14 20:41:57 +0000

    reduce lightgbm log output

--- ludwig/schema/trainer.py
+++ ludwig/schema/trainer.py
@@ -529,7 +529,7 @@ class GBMTrainerConfig(BaseTrainerConfig):
         description="Smoothing factor applied to tree nodes in the GBM trainer.",
     )
 
-    verbose: int = schema_utils.IntegerRange(default=0, min=-1, max=2, description="Verbosity level for GBM trainer.")
+    verbose: int = schema_utils.IntegerRange(default=-1, min=-1, max=2, description="Verbosity level for GBM trainer.")
 
     # LightGBM IO params
     max_bin: int = schema_utils.PositiveInteger(
