3fd3546991ec114bbeeb65adac20cc2768a8a1be
Daniel Treiman <daniel@predibase.com>
2022-09-16 17:24:37 -0700

    Adds mnist, titanic examples as default configs.

--- tests/ludwig/datasets/model_configs/train_all_model_configs.py
+++ tests/ludwig/datasets/model_configs/train_all_model_configs.py
@@ -9,11 +9,13 @@
 import pandas as pd
 
 from ludwig import datasets
+from ludwig.api import LudwigModel
 
 
 def train_all_datasets():
     dataset_names = []
     has_config = []
+    output_directories = []
     model_metrics = []
     model_performance = []
 
@@ -21,17 +23,29 @@ def train_all_datasets():
     for dataset_name in datasets.list_datasets():
         dataset = datasets.get_dataset(dataset_name)
         config = dataset.default_model_config
-        dataset_names.append(dataset_name)
         if config:
-            has_config.append(True)
             # Train model on config
+            df = dataset.load()
+            model = LudwigModel(config)
+            train_stats, _, output_directory = model.train(dataset=df, model_name=dataset_name)
+
+            # Get metric for first output feature (assuming
+
+            dataset_names.append(dataset_name)
+            has_config.append(True)
+            output_directories.append(output_directory)
+            model_metrics.append(None)
+            model_performance.append(None)
         else:
+            dataset_names.append(dataset_name)
             has_config.append(False)
+            model_metrics.append(None)
             model_performance.append(None)
     results = pd.DataFrame(
         {
             "dataset": dataset_names,
             "has_config": has_config,
+            "output_directory": output_directories,
             "metric": model_metrics,
             "performance": model_performance,
         }
