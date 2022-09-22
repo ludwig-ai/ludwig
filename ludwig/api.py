6f8f18a76db711f3878f384d356d7de834c270ba
Magdy Saleh <magdy@predibase.com>
2022-09-22 14:38:38 -0400

    pull stuff from master

--- ludwig/api.py
+++ ludwig/api.py
@@ -35,7 +35,7 @@ import pandas as pd
 import torch
 from tabulate import tabulate
 
-from ludwig.backend import Backend, initialize_backend
+from ludwig.backend import Backend, initialize_backend, provision_preprocessing_workers
 from ludwig.callbacks import Callback
 from ludwig.constants import (
     AUTO,
@@ -676,17 +676,18 @@ class LudwigModel:
             self.config.get(PREPROCESSING, {}), self.config.get(DEFAULTS, {})
         )
 
-        training_dataset, _, _, training_set_metadata = preprocess_for_training(
-            self.config,
-            training_set=dataset,
-            training_set_metadata=training_set_metadata,
-            data_format=data_format,
-            skip_save_processed_input=True,
-            preprocessing_params=preprocessing_params,
-            backend=self.backend,
-            random_seed=random_seed,
-            callbacks=self.callbacks,
-        )
+        with provision_preprocessing_workers(self.backend):
+            training_dataset, _, _, training_set_metadata = preprocess_for_training(
+                self.config,
+                training_set=dataset,
+                training_set_metadata=training_set_metadata,
+                data_format=data_format,
+                skip_save_processed_input=True,
+                preprocessing_params=preprocessing_params,
+                backend=self.backend,
+                random_seed=random_seed,
+                callbacks=self.callbacks,
+            )
 
         if not self.training_set_metadata:
             self.training_set_metadata = training_set_metadata
@@ -795,7 +796,6 @@ class LudwigModel:
             converted_postproc_predictions = convert_predictions(
                 postproc_predictions, self.model.output_features, return_type=return_type, backend=self.backend
             )
-
             if self.backend.is_coordinator():
                 if not skip_save_predictions:
                     save_prediction_outputs(
@@ -1290,20 +1290,21 @@ class LudwigModel:
             self.config.get(PREPROCESSING, {}), self.config.get(DEFAULTS, {})
         )
 
-        preprocessed_data = preprocess_for_training(
-            self.config,
-            dataset=dataset,
-            training_set=training_set,
-            validation_set=validation_set,
-            test_set=test_set,
-            training_set_metadata=training_set_metadata,
-            data_format=data_format,
-            skip_save_processed_input=skip_save_processed_input,
-            preprocessing_params=preprocessing_params,
-            backend=self.backend,
-            random_seed=random_seed,
-            callbacks=self.callbacks,
-        )
+        with provision_preprocessing_workers(self.backend):
+            preprocessed_data = preprocess_for_training(
+                self.config,
+                dataset=dataset,
+                training_set=training_set,
+                validation_set=validation_set,
+                test_set=test_set,
+                training_set_metadata=training_set_metadata,
+                data_format=data_format,
+                skip_save_processed_input=skip_save_processed_input,
+                preprocessing_params=preprocessing_params,
+                backend=self.backend,
+                random_seed=random_seed,
+                callbacks=self.callbacks,
+            )
 
         (proc_training_set, proc_validation_set, proc_test_set, training_set_metadata) = preprocessed_data
 
