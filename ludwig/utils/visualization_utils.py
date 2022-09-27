6e541205809e9ba3a4d71ee03dfa70d6c288ffdd
Arnav Garg <arnav@predibase.com>
2022-09-27 22:00:07 +0000

    fix comment

--- ludwig/utils/visualization_utils.py
+++ ludwig/utils/visualization_utils.py
@@ -1402,7 +1402,7 @@ def hyperopt_pair_plot(hyperopt_results_df, metric, title, filename):
     params.remove(metric)
     num_param = len(params)
 
-    # Can't create a pair plot with only 1 parameter
+    # Pair plot is empty if there's only 1 parameter, so skip creating a pair plot
     if num_param == 1:
         return
 
