e7b7f5786c5afa32c5756f494b217deee309b55c
Joppe Geluykens <joppe@predibase.com>
2022-09-28 13:35:44 +0000

    return Resources object instead of dict

--- ludwig/utils/automl/ray_utils.py
+++ ludwig/utils/automl/ray_utils.py
@@ -1,6 +1,7 @@
 import os
 
 from ludwig.backend.ray import initialize_ray
+from ludwig.utils.system_utils import Resources
 
 try:
     import ray
@@ -8,13 +9,10 @@ except ImportError:
     raise ImportError(" ray is not installed. " "In order to use auto_train please run " "pip install ludwig[ray]")
 
 
-def get_available_resources() -> dict:
+def get_available_resources() -> Resources:
     # returns total number of gpus and cpus
     resources = ray.cluster_resources()
-    gpus = resources.get("GPU", 0)
-    cpus = resources.get("CPU", 0)
-    resources = {"gpu": gpus, "cpu": cpus}
-    return resources
+    return Resources(cpus=resources.get("CPU", 0), gpus=resources.get("GPU", 0))
 
 
 def _ray_init():
