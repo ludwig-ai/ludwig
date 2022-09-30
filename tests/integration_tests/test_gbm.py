47f4e345a634533ad589dd76c70e165ac86b685b
Joppe Geluykens <joppe@predibase.com>
2022-09-30 15:08:27 +0000

    add test coverage for prediction matching

--- tests/integration_tests/test_gbm.py
+++ tests/integration_tests/test_gbm.py
@@ -1,6 +1,8 @@
 import os
 
+import numpy as np
 import pytest
+import torch
 from marshmallow import ValidationError
 
 from ludwig.api import LudwigModel
@@ -145,6 +147,82 @@ def test_local_gbm_non_number_inputs(tmpdir, local_backend):
 @pytest.mark.distributed
 def test_ray_gbm_non_number_inputs(tmpdir, ray_backend, ray_cluster_4cpu):
     run_test_gbm_non_number_inputs(tmpdir, ray_backend)
+
+
+def run_test_hummingbird_conversion_binary(tmpdir, backend_config):
+    input_features = [number_feature(), category_feature(encoder={"reduce_output": "sum"})]
+    output_features = [binary_feature()]
+
+    preds_hb, model = _train_and_predict_gbm(input_features, output_features, tmpdir, backend_config)
+
+    _, _, test, _ = model.preprocess(os.path.join(tmpdir, "training.csv"))
+    test_inputs = test.to_df(model.model.input_features.values())
+
+    output_feature = output_features[0]
+
+    probs_hb = preds_hb[f'{output_feature["name"]}_probabilities']
+    probs_lgbm = model.model.lgbm_model.predict_proba(test_inputs)
+
+    # sanity check Hummingbird probabilities equal to LightGBM probabilities
+    assert np.allclose(np.stack(probs_hb.values), probs_lgbm, atol=1e-8)
+
+
+def run_test_hummingbird_conversion_regression(tmpdir, backend_config):
+    input_features = [number_feature(), category_feature(encoder={"reduce_output": "sum"})]
+    output_features = [number_feature()]
+
+    preds_hb, model = _train_and_predict_gbm(input_features, output_features, tmpdir, backend_config)
+
+    _, _, test, _ = model.preprocess(os.path.join(tmpdir, "training.csv"))
+    test_inputs = test.to_df(model.model.input_features.values())
+
+    preds_lgbm = model.model.lgbm_model.predict(test_inputs)
+
+    assert np.allclose(preds_hb.values, preds_lgbm)
+
+
+def run_test_hummingbird_conversion_category(vocab_size, tmpdir, backend_config):
+    input_features = [number_feature(), category_feature(encoder={"reduce_output": "sum"})]
+    output_features = [category_feature(decoder={"vocab_size": vocab_size})]
+
+    preds_hb, model = _train_and_predict_gbm(input_features, output_features, tmpdir, backend_config)
+
+    _, _, test, _ = model.preprocess(os.path.join(tmpdir, "training.csv"))
+    test_inputs = test.to_df(model.model.input_features.values())
+
+    output_feature = next(iter(model.model.output_features.values()))
+
+    probs_hb = preds_hb[f"{output_feature.column}_probabilities"]
+
+    if output_feature.num_classes == 2:
+        # LightGBM binary classifier transforms logits using sigmoid, so we need to invert the
+        # transformation to get the logits back, and then use softmax to get the probabilities to match
+        # Ludwig's category feature prediction behavior.
+        probs_lgbm = torch.softmax(torch.logit(torch.from_numpy(model.model.lgbm_model.predict_proba(test_inputs))), -1)
+    else:
+        # Otherwise, just compare to the raw probabilities from LightGBM
+        probs_lgbm = model.model.lgbm_model.predict_proba(test_inputs)
+
+    # sanity check Hummingbird probabilities equal to LightGBM probabilities
+    assert np.allclose(np.stack(probs_hb.values), probs_lgbm, atol=1e-4)
+
+
+def test_local_hummingbird_conversion_binary(tmpdir, local_backend):
+    run_test_hummingbird_conversion_binary(tmpdir, local_backend)
+
+
+def test_local_hummingbird_conversion_regression(tmpdir, local_backend):
+    run_test_hummingbird_conversion_regression(tmpdir, local_backend)
+
+
+@pytest.mark.parametrize("vocab_size", [2, 3])
+def test_local_hummingbird_conversion_category(vocab_size, tmpdir, local_backend):
+    run_test_hummingbird_conversion_category(vocab_size, tmpdir, local_backend)
+
+
+# TODO: tests for loss going down
+
+# TODO: saving, loading, continuing training
 
 
 def run_test_gbm_category(vocab_size, tmpdir, backend_config):
