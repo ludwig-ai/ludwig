30bacb834936a541224070b1f3fad02f62fc2a28
Wael Abid <wael@predibase.com>
2022-10-03 14:55:49 -0700

    update early stop policy message

--- ludwig/trainers/trainer.py
+++ ludwig/trainers/trainer.py
@@ -811,9 +811,9 @@ class Trainer(BaseTrainer):
                         logger.info("Early stopping policy: None")
                     else:
                         logger.info(
-                            f"Early stopping policy: {self.early_stop} round(s) of evaluation, or {early_stopping_steps} "
-                            f"step(s), approximately {int(early_stopping_steps / batcher.steps_per_epoch)} "
-                            "epoch(s).\n"
+                            f"Early stopping policy: {self.early_stop} round(s) of evaluation, or "
+                            f"{early_stopping_steps} step(s), approximately "
+                            f"{int(early_stopping_steps / batcher.steps_per_epoch)} epoch(s).\n"
                         )
 
                     logger.info(f"Starting with step {progress_tracker.steps}, epoch: {progress_tracker.epoch}")
