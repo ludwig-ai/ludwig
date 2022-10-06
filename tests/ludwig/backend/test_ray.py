707f84527014b1540cbf30c62b82f49150dcf444
Travis Addair <tgaddair@gmail.com>
2022-10-06 12:13:19 -0700

    More tests

--- tests/ludwig/backend/test_ray.py
+++ tests/ludwig/backend/test_ray.py
@@ -1,3 +1,4 @@
+import copy
 from unittest.mock import patch
 
 import pytest
@@ -16,6 +17,7 @@ pytestmark = pytest.mark.distributed
 @pytest.mark.parametrize(
     "trainer_config,cluster_resources,num_nodes,expected_kwargs",
     [
+        # Prioritize using the GPU when available over multi-node
         (
             {},
             {"CPU": 4, "GPU": 1},
@@ -30,6 +32,7 @@ pytestmark = pytest.mark.distributed
                 },
             ),
         ),
+        # Use one worker per node for CPU, chck NIC override
         (
             {"nics": [""]},
             {"CPU": 4, "GPU": 0},
@@ -44,12 +47,46 @@ pytestmark = pytest.mark.distributed
                 },
             ),
         ),
+        # Allow explicitly setting GPU usage for autoscaling clusters
+        (
+            {"use_gpu": True, "num_workers": 2},
+            {"CPU": 4, "GPU": 0},
+            1,
+            dict(
+                backend=HorovodConfig(),
+                num_workers=2,
+                use_gpu=True,
+                resources_per_worker={
+                    "CPU": 0,
+                    "GPU": 1,
+                },
+            ),
+        ),
+        # Allow overriding resources_per_worker
+        (
+            {"resources_per_worker": {"CPU": 2, "GPU": 1}},
+            {"CPU": 4, "GPU": 2},
+            2,
+            dict(
+                backend=HorovodConfig(),
+                num_workers=2,
+                use_gpu=True,
+                resources_per_worker={
+                    "CPU": 2,
+                    "GPU": 1,
+                },
+            ),
+        ),
     ],
 )
 def test_get_trainer_kwargs(trainer_config, cluster_resources, num_nodes, expected_kwargs):
     with patch("ludwig.backend.ray.ray.cluster_resources", return_value=cluster_resources):
         with patch("ludwig.backend.ray._num_nodes", return_value=num_nodes):
-            actual_kwargs = get_trainer_kwargs(**trainer_config)
+            trainer_config_copy = copy.deepcopy(trainer_config)
+            actual_kwargs = get_trainer_kwargs(**trainer_config_copy)
+
+            # Function should not modify the original input
+            assert trainer_config_copy == trainer_config
 
             actual_backend = actual_kwargs.pop("backend")
             expected_backend = expected_kwargs.pop("backend")
