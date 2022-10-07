99dfae0737f6dfca9d35871d7684dd71b064b291
Geoffrey Angus <geoffrey@predibase.com>
2022-10-07 12:59:12 -0700

    comment description of constant

--- ludwig/constants.py
+++ ludwig/constants.py
@@ -164,7 +164,9 @@ EPOCHS = "epochs"
 BATCH_SIZE = "batch_size"
 EVAL_BATCH_SIZE = "eval_batch_size"
 DEFAULT_BATCH_SIZE = 128
-MAX_POSSIBLE_BATCH_SIZE = 1099511627776  # 2^40
+MAX_POSSIBLE_BATCH_SIZE = (
+    1099511627776  # 2^40. Used for `max_batch_size` config param. Not a hard constraint for `batch_size` config param.
+)
 LEARNING_RATE = "learning_rate"
 USE_BIAS = "use_bias"
 BIAS = "bias"
