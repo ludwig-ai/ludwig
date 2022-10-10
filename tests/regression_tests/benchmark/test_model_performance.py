21387b25a7882e8cfa60098ade4e9ba792de0007
Wael Abid <wael@predibase.com>
2022-10-10 00:34:28 -0700

    using jsons assert

--- tests/regression_tests/benchmark/test_model_performance.py
+++ tests/regression_tests/benchmark/test_model_performance.py
@@ -1,10 +1,11 @@
 import os
 
+import numpy as np
 import pytest
 
 from ludwig.benchmarking.benchmark import benchmark
 from ludwig.constants import MODEL_ECD, MODEL_GBM
-from ludwig.utils.data_utils import load_json, load_yaml
+from ludwig.utils.data_utils import load_json
 
 BENCHMARKING_CONFIG = """
 experiment_name: {experiment_name}
@@ -21,58 +22,11 @@ experiments:
     config_path: {config_path}
 """
 
-dataset_name_to_metric = {
-    "ames_housing": "r2",
-    "mercedes_benz_greener": "r2",
-    "protein": "r2",
-    "naval": "r2",
-    "sarcos": "r2",
-    "adult_census_income": "accuracy",
-}
-
-dataset_to_expected_performance = {
-    "ames_housing": 0.75,
-    "mercedes_benz_greener": 0.48,
-    "adult_census_income": 0.81,
-    "protein": 0.47,
-    "sarcos": 0.94,
-    "naval": 0.65,
-}
-
-dataset_to_expected_preprocessing_time = {  # in microseconds
-    "ames_housing": 6e5,
-    "mercedes_benz_greener": 3.5e6,
-    "adult_census_income": 2e6,
-    "protein": 7e5,
-    "sarcos": 2.4e6,
-    "naval": 6e5,
-}
-
-dataset_to_expected_training_time = {  # in microseconds
-    "ames_housing": 3e7,
-    "mercedes_benz_greener": 1.3e8,
-    "adult_census_income": 6e7,
-    "protein": 1.25e8,
-    "sarcos": 3e8,
-    "naval": 8e8,
-}
-
-dataset_to_expected_evaluation_time = {  # in microseconds
-    "ames_housing": 1.3e5,
-    "mercedes_benz_greener": 4.5e5,
-    "adult_census_income": 6e5,
-    "protein": 2.5e5,
-    "sarcos": 3.5e5,
-    "naval": 1.3e5,
-}
-
 
 @pytest.mark.benchmark
 @pytest.mark.parametrize("model_type", [MODEL_GBM, MODEL_ECD])
 @pytest.mark.parametrize(
-    "dataset",
-    ["adult_census_income", "protein", "sarcos", "naval"]
-    # "dataset", ["ames_housing", "mercedes_benz_greener", "adult_census_income", "protein", "sarcos", "naval"]
+    "dataset", ["ames_housing", "mercedes_benz_greener", "adult_census_income", "protein", "sarcos", "naval"]
 )
 def test_performance(model_type, dataset, tmpdir):
     benchmark_directory = "/".join(__file__.split("/")[:-1])
@@ -90,44 +44,11 @@ def test_performance(model_type, dataset, tmpdir):
     with open(benchmarking_config_fp, "w") as f:
         f.write(benchmarking_config)
 
-    for _ in range(1):
-        benchmark(benchmarking_config_fp)
+    benchmark(benchmarking_config_fp)
 
-        test_statistics_fp = os.path.join(tmpdir, dataset, experiment_name, "experiment_run", "test_statistics.json")
-        test_statistics = load_json(test_statistics_fp)
-        output_feature_name = load_yaml(config_path)["output_features"][0]["name"]
-        metric_name = dataset_name_to_metric[dataset]
+    test_statistics_fp = os.path.join(tmpdir, dataset, experiment_name, "experiment_run", "test_statistics.json")
+    test_statistics = load_json(test_statistics_fp)
+    expected_test_statistics_fp = os.path.join(benchmark_directory, "expected_metrics", f"{dataset}_{model_type}.yaml")
+    expected_test_statistics = load_json(expected_test_statistics_fp)
 
-        # preprocessing_resource_usage_fp = os.path.join(
-        #     tmpdir, dataset, experiment_name, "system_resource_usage", "preprocessing", "run_0.json"
-        # )
-        # training_resource_usage_fp = os.path.join(
-        #     tmpdir, dataset, experiment_name, "system_resource_usage", "training", "run_0.json"
-        # )
-        # evaluation_resource_usage_fp = os.path.join(
-        #     tmpdir, dataset, experiment_name, "system_resource_usage", "evaluation", "run_0.json"
-        # )
-        #
-        # preprocessing_resource_usage = load_json(preprocessing_resource_usage_fp)
-        # training_resource_usage = load_json(training_resource_usage_fp)
-        # evaluation_resource_usage = load_json(evaluation_resource_usage_fp)
-
-        from pprint import pprint
-
-        print()
-        print(dataset, model_type)
-        pprint(test_statistics)
-        # print(metric_name, test_statistics[output_feature_name][metric_name])
-        # print("preprocessing_time", preprocessing_resource_usage["total_execution_time"])
-        # print("training_time", training_resource_usage["total_execution_time"])
-        # print("evaluation_time", evaluation_resource_usage["total_execution_time"])
-        print()
-
-        import shutil
-
-        shutil.rmtree(os.path.join(tmpdir, dataset), ignore_errors=True)
-
-    # assert test_statistics[output_feature_name][metric_name] > dataset_to_expected_performance[dataset]
-    # assert preprocessing_resource_usage["total_execution_time"] < dataset_to_expected_preprocessing_time[dataset]
-    # assert training_resource_usage["total_execution_time"] < dataset_to_expected_training_time[dataset]
-    # assert evaluation_resource_usage["total_execution_time"] < dataset_to_expected_evaluation_time[dataset]
+    np.testing.assert_equal(test_statistics, expected_test_statistics)
