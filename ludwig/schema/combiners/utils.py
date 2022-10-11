8d5acca001aabb4230f8d701b2593020b9af7881
connor-mccorm <connor@predibase.com>
2022-10-11 10:09:17 -0700

    revert title change

--- ludwig/schema/combiners/utils.py
+++ ludwig/schema/combiners/utils.py
@@ -27,7 +27,7 @@ def get_combiner_jsonschema():
                 "type": "string",
                 "enum": combiner_types,
                 "default": "concat",
-                "title": "Combiner Type",
+                "title": "type",
                 "description": "Select the combiner type.",
                 "parameter_metadata": parameter_metadata
             },
