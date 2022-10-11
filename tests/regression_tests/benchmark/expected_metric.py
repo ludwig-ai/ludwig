08070bdbceaa976e016f3cfc7025a5f090ea6ab1
Wael Abid <wael@predibase.com>
2022-10-11 14:55:19 -0700

    addressing comments

---
+++ tests/regression_tests/benchmark/expected_metric.py
@@ -0,0 +1,41 @@
+from typing import Union
+from enum import Enum
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+
+class MetricRegressionDirection(Enum):
+    """Which direction is considered a regression."""
+
+    LOWER = -1
+    HIGHER = 1
+
+
+@dataclass_json
+@dataclass
+class ExpectedMetric:
+    # Output feature name.
+    output_feature_name: str
+
+    # Metric name.
+    metric_name: str
+
+    # Expected metric value.
+    expected_value: Union[int, float]
+
+    # Which direction is considered a regression.
+    regression_direction: int
+
+    # The percentage change that would trigger a notification/failure.
+    percent_change_sensitivity: float
+
+    def __post_init__(self):
+        if self.regression_direction == "LOWER":
+            self.regression_direction = -1
+        elif self.regression_direction == "HIGHER":
+            self.regression_direction = 1
+        else:
+            raise ValueError(
+                "Regression direction in the expected performance YAML file should be one" "of 'LOWER', 'HIGHER'."
+            )
+
--- tests/regression_tests/benchmark/test_model_performance.py
+++ tests/regression_tests/benchmark/test_model_performance.py
@@ -4,69 +4,61 @@ import pytest
 from typing import List
 from expected_metric import ExpectedMetric, MetricRegressionDirection
 from ludwig.benchmarking.benchmark import benchmark
-from ludwig.constants import MODEL_ECD, MODEL_GBM
-from ludwig.utils.data_utils import load_json, load_yaml
+from ludwig.utils.data_utils import load_yaml
 
-BENCHMARKING_CONFIG_TEMPLATE = """
-experiment_name: {experiment_name}
-hyperopt: false
-export:
-  export_artifacts: true
-  export_base_path: {export_base_path}
-profiler:
-  enable: false
-  use_torch_profiler: false
-  logging_interval: 0.1
-experiments:
-  - dataset_name: {dataset}
-    config_path: {config_path}
-"""
+
+def get_test_config_paths() -> List[str]:
+    """Return list of the config filenames used for benchmarking."""
+    benchmark_directory = "/".join(__file__.split("/")[:-1])
+    return [config_fp for config_fp in os.listdir(benchmark_directory)]
+
+
+def get_dataset_from_config_path(config_path: str) -> str:
+    """path/to/config/<dataset>.<descriptors>.yaml -> dataset"""
+    return os.path.basename(config_path).split(".")[0]
 
 
 @pytest.mark.benchmark
-@pytest.mark.parametrize("model_type", [MODEL_GBM, MODEL_ECD])
-@pytest.mark.parametrize("dataset", ["ames_housing", "mercedes_benz_greener", "adult_census_income", "sarcos"])
-def test_performance(model_type, dataset, tmpdir):
+@pytest.mark.parametrize("config_filename", get_test_config_paths())
+def test_performance(config_filename, tmpdir):
     benchmark_directory = "/".join(__file__.split("/")[:-1])
-    experiment_name = "regression_test"
-    config_path = os.path.join(benchmark_directory, "configs", f"{dataset}_{model_type}.yaml")
+    config_path = os.path.join(benchmark_directory, "configs", config_filename)
+    expected_test_statistics_fp = os.path.join(benchmark_directory, "expected_metrics", config_filename)
 
-    benchmarking_config = BENCHMARKING_CONFIG_TEMPLATE.format(
-        experiment_name=experiment_name,
-        export_base_path=tmpdir,
-        dataset=dataset,
-        config_path=config_path,
-    )
+    benchmarking_config = {
+        "experiment_name": "regression_test",
+        "export": {
+            "export_artifacts": True,
+            "export_base_path": tmpdir
+        },
+        "experiments": [
+            {
+                "dataset_name": get_dataset_from_config_path(config_path),
+                "config_path": config_path
+            }
+        ]
+    }
 
-    benchmarking_config_fp = tmpdir + "/benchmarking_config.yaml"
-    with open(benchmarking_config_fp, "w") as f:
-        f.write(benchmarking_config)
+    benchmarking_artifacts = benchmark(benchmarking_config)
 
-    benchmark(benchmarking_config_fp)
-
-    test_statistics_fp = os.path.join(tmpdir, dataset, experiment_name, "experiment_run", "test_statistics.json")
-    test_statistics = load_json(test_statistics_fp)
-
-    expected_test_statistics_fp = os.path.join(benchmark_directory, "expected_metrics", f"{dataset}_{model_type}.yaml")
-    expected_metrics_dict = load_yaml(expected_test_statistics_fp)
-
-    expected_metrics: List[ExpectedMetric] = [
-        ExpectedMetric.from_dict(expected_metric) for expected_metric in expected_metrics_dict["metrics"]
-    ]
-
-    for expected_metric in expected_metrics:
-        tolerance = (
-            expected_metric.percent_change_sensitivity
-            * expected_metric.expected_value
-            * expected_metric.regression_direction
-        )
-        if expected_metric.regression_direction == MetricRegressionDirection.LOWER.value:
-            assert (
-                test_statistics[expected_metric.output_feature_name][expected_metric.metric_name]
-                >= expected_metric.expected_value + tolerance
+    for artifact in benchmarking_artifacts:
+        expected_metrics_dict = load_yaml(expected_test_statistics_fp)
+        expected_metrics: List[ExpectedMetric] = [
+            ExpectedMetric.from_dict(expected_metric) for expected_metric in expected_metrics_dict["metrics"]
+        ]
+        for expected_metric in expected_metrics:
+            tolerance = (
+                expected_metric.percent_change_sensitivity
+                * expected_metric.expected_value
+                * expected_metric.regression_direction
             )
-        else:
-            assert (
-                test_statistics[expected_metric.output_feature_name][expected_metric.metric_name]
-                <= expected_metric.expected_value + tolerance
-            )
+            if expected_metric.regression_direction == MetricRegressionDirection.LOWER.value:
+                assert (
+                    artifact.test_statistics[expected_metric.output_feature_name][expected_metric.metric_name]
+                    >= expected_metric.expected_value + tolerance
+                )
+            else:
+                assert (
+                    artifact.test_statistics[expected_metric.output_feature_name][expected_metric.metric_name]
+                    <= expected_metric.expected_value + tolerance
+                )
