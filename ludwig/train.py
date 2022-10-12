10dbed2b5a606d6a544b1464c9d21187b91c4d31
Arnav Garg <arnav@predibase.com>
2022-10-12 20:18:58 +0000

    Add comment with todo

--- ludwig/train.py
+++ ludwig/train.py
@@ -157,6 +157,8 @@ def train_cli(
     if HYPEROPT in config:
         if not query_yes_no(HYPEROPT_WARNING + CONTINUE_PROMPT):
             exit(1)
+        # Stop gap: remove hyperopt from the config to prevent interference with training step sizes
+        # TODO: Need to investigate why the presence of hyperopt in the config interferes with training step sizes
         config.pop(HYPEROPT)
 
     if model_load_path:
