605d7dff0ab1a994d13d8bff309be61304f58624
Wael Abid <wael@predibase.com>
2022-10-13 14:25:50 -0700

    split test into two functions

--- tests/ludwig/benchmarking/test_benchmarking.py
+++ tests/ludwig/benchmarking/test_benchmarking.py
@@ -1,30 +1,28 @@
 import os
-from contextlib import nullcontext as does_not_raise
-
 import pytest
+
+from contextlib import nullcontext as does_not_raise
 
 from ludwig.benchmarking.utils import validate_benchmarking_config
 from ludwig.utils.data_utils import load_yaml
 
 
-def get_benchamrking_config_validity_pair():
+def get_benchamrking_configs(validity="valid"):
     local_dir = "/".join(__file__.split("/")[:-1])
-    invalid_bench_configs = [
-        os.path.join(local_dir, "example_files", "invalid", config_fp)
-        for config_fp in os.listdir(os.path.join(local_dir, "example_files", "invalid"))
-    ]
-    valid_bench_configs = [
-        os.path.join(local_dir, "example_files", "valid", config_fp)
-        for config_fp in os.listdir(os.path.join(local_dir, "example_files", "valid"))
-    ]
-    return [(config_fp, does_not_raise()) for config_fp in valid_bench_configs] + [
-        (config_fp, pytest.raises(ValueError)) for config_fp in invalid_bench_configs
-    ]
+    return [os.path.join(local_dir, "example_files", validity, config_fp) for config_fp in os.listdir(os.path.join(local_dir, "example_files", validity))]
 
 
-@pytest.mark.parametrize("benchmarking_config_fp,error_expectation", get_benchamrking_config_validity_pair())
-def test_benchmarking(benchmarking_config_fp, error_expectation):
+@pytest.mark.parametrize("benchmarking_config_fp", get_benchamrking_configs("valid"))
+def test_valid_benchmarking_configs_valid(benchmarking_config_fp):
     benchmarking_config = load_yaml(benchmarking_config_fp)
 
-    with error_expectation:
+    with does_not_raise():
+        validate_benchmarking_config(benchmarking_config)
+
+
+@pytest.mark.parametrize("benchmarking_config_fp", get_benchamrking_configs("invalid"))
+def test_invalid_benchmarking_configs_valid(benchmarking_config_fp):
+    benchmarking_config = load_yaml(benchmarking_config_fp)
+
+    with pytest.raises(ValueError):
         validate_benchmarking_config(benchmarking_config)
