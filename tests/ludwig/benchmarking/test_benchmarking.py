d5490620062e6d62c69222e6849421773c424f97
Wael Abid <wael@predibase.com>
2022-10-13 14:27:11 -0700

    formatting

--- tests/ludwig/benchmarking/test_benchmarking.py
+++ tests/ludwig/benchmarking/test_benchmarking.py
@@ -1,15 +1,18 @@
 import os
-import pytest
-
 from contextlib import nullcontext as does_not_raise
+
+import pytest
 
 from ludwig.benchmarking.utils import validate_benchmarking_config
 from ludwig.utils.data_utils import load_yaml
 
 
-def get_benchamrking_configs(validity="valid"):
+def get_benchamrking_configs(validity):
     local_dir = "/".join(__file__.split("/")[:-1])
-    return [os.path.join(local_dir, "example_files", validity, config_fp) for config_fp in os.listdir(os.path.join(local_dir, "example_files", validity))]
+    return [
+        os.path.join(local_dir, "example_files", validity, config_fp)
+        for config_fp in os.listdir(os.path.join(local_dir, "example_files", validity))
+    ]
 
 
 @pytest.mark.parametrize("benchmarking_config_fp", get_benchamrking_configs("valid"))
