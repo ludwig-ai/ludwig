b718c6d601cd4f700929350de09840546cd96aef
Justin <justinxzhao@gmail.com>
2022-10-12 22:05:44 -0400

    Update version to 0.6.2 (#2624)

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.6.1"
+LUDWIG_VERSION = "0.6.2"
 
 MODEL_WEIGHTS_FILE_NAME = "model_weights"
 MODEL_HYPERPARAMETERS_FILE_NAME = "model_hyperparameters.json"
