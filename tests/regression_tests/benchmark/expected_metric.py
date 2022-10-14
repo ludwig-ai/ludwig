828ca0fbcc04c26aec2dccbd22be0c72031ac48e
Wael Abid <wael@predibase.com>
2022-10-14 12:37:00 -0700

    address comments

--- tests/regression_tests/benchmark/expected_metric.py
+++ tests/regression_tests/benchmark/expected_metric.py
@@ -1,15 +1,7 @@
 from dataclasses import dataclass
-from enum import Enum
 from typing import Union
 
 from dataclasses_json import dataclass_json
-
-
-class MetricRegressionDirection(Enum):
-    """Which direction is considered a regression."""
-
-    LOWER = -1
-    HIGHER = 1
 
 
 @dataclass_json
@@ -24,18 +16,5 @@ class ExpectedMetric:
     # Expected metric value.
     expected_value: Union[int, float]
 
-    # Which direction is considered a regression.
-    regression_direction: int
-
     # The percentage change that would trigger a notification/failure.
-    percent_change_sensitivity: float
-
-    def __post_init__(self):
-        if self.regression_direction == "LOWER":
-            self.regression_direction = -1
-        elif self.regression_direction == "HIGHER":
-            self.regression_direction = 1
-        else:
-            raise ValueError(
-                "Regression direction in the expected performance YAML file should be one" "of 'LOWER', 'HIGHER'."
-            )
+    tolerance_percentage: float
--- tests/regression_tests/benchmark/test_model_performance.py
+++ tests/regression_tests/benchmark/test_model_performance.py
@@ -2,15 +2,15 @@ import os
 from typing import List
 
 import pytest
-from expected_metric import ExpectedMetric, MetricRegressionDirection
+from expected_metric import ExpectedMetric
 
 from ludwig.benchmarking.benchmark import benchmark
 from ludwig.utils.data_utils import load_yaml
 
 
-def get_test_config_paths() -> List[str]:
+def get_test_config_filenames() -> List[str]:
     """Return list of the config filenames used for benchmarking."""
-    benchmark_directory = "/".join(__file__.split("/")[:-1])
+    benchmark_directory = "/".join(__file__.split("/")[:-1] + ["configs"])
     return [config_fp for config_fp in os.listdir(benchmark_directory)]
 
 
@@ -20,38 +20,43 @@ def get_dataset_from_config_path(config_path: str) -> str:
 
 
 @pytest.mark.benchmark
-@pytest.mark.parametrize("config_filename", get_test_config_paths())
+@pytest.mark.parametrize("config_filename", get_test_config_filenames())
 def test_performance(config_filename, tmpdir):
     benchmark_directory = "/".join(__file__.split("/")[:-1])
     config_path = os.path.join(benchmark_directory, "configs", config_filename)
     expected_test_statistics_fp = os.path.join(benchmark_directory, "expected_metrics", config_filename)
+    dataset_name = get_dataset_from_config_path(config_path)
+
+    if not os.path.exists(expected_test_statistics_fp):
+        raise FileNotFoundError(
+            """No corresponding expected metrics found for benchmarking config '{config_path}'.
+            Please add a new metrics YAML file '{expected_test_statistics_fp}'. Suggested content:
+
+            metrics:
+              - output_feature_name: <YOUR_OUTPUT_FEATURE e.g. SalePrice>
+                metric_name: <YOUR METRIC NAME e.g. accuracy>
+                expected_value: <A FLOAT VALUE>
+                tolerance_percent: 0.15"""
+        )
+    expected_metrics_dict = load_yaml(expected_test_statistics_fp)
 
     benchmarking_config = {
         "experiment_name": "regression_test",
         "export": {"export_artifacts": True, "export_base_path": tmpdir},
-        "experiments": [{"dataset_name": get_dataset_from_config_path(config_path), "config_path": config_path}],
+        "experiments": [{"dataset_name": dataset_name, "config_path": config_path}],
     }
-
     benchmarking_artifacts = benchmark(benchmarking_config)
 
-    for artifact in benchmarking_artifacts:
-        expected_metrics_dict = load_yaml(expected_test_statistics_fp)
-        expected_metrics: List[ExpectedMetric] = [
-            ExpectedMetric.from_dict(expected_metric) for expected_metric in expected_metrics_dict["metrics"]
-        ]
-        for expected_metric in expected_metrics:
-            tolerance = (
-                expected_metric.percent_change_sensitivity
-                * expected_metric.expected_value
-                * expected_metric.regression_direction
-            )
-            if expected_metric.regression_direction == MetricRegressionDirection.LOWER.value:
-                assert (
-                    artifact.test_statistics[expected_metric.output_feature_name][expected_metric.metric_name]
-                    >= expected_metric.expected_value + tolerance
-                )
-            else:
-                assert (
-                    artifact.test_statistics[expected_metric.output_feature_name][expected_metric.metric_name]
-                    <= expected_metric.expected_value + tolerance
-                )
+    experiment_artifact = benchmarking_artifacts[dataset_name]
+    expected_metrics: List[ExpectedMetric] = [
+        ExpectedMetric.from_dict(expected_metric) for expected_metric in expected_metrics_dict["metrics"]
+    ]
+    for expected_metric in expected_metrics:
+        tolerance = expected_metric.tolerance_percentage * expected_metric.expected_value
+        output_feature_name = expected_metric.output_feature_name
+        metric_name = expected_metric.metric_name
+        experiment_metric_value = experiment_artifact.test_statistics[output_feature_name][metric_name]
+        assert abs(expected_metric.expected_value - experiment_metric_value) <= tolerance, (
+            f"The obtained {metric_name} value ({experiment_metric_value}) was not within"
+            f" {100 * expected_metric.tolerance_percentage}% of the expected value ({expected_metric.expected_value})."
+        )
