89470c910d2cd6703a868e4bf9a049435c5eb20a
connor-mccorm <connor@predibase.com>
2022-10-18 14:14:06 -0700

    Switch defaults to use mixins and improve test

--- tests/ludwig/schema/test_validate_config_misc.py
+++ tests/ludwig/schema/test_validate_config_misc.py
@@ -1,7 +1,19 @@
 import pytest
 from jsonschema.exceptions import ValidationError
 
-from ludwig.constants import DECODER, ENCODER, LOSS, MODEL_ECD, MODEL_GBM, MODEL_TYPE, PREPROCESSING, TRAINER
+from ludwig.constants import (
+    CATEGORY,
+    DECODER,
+    DEFAULTS,
+    ENCODER,
+    LOSS,
+    MODEL_ECD,
+    MODEL_GBM,
+    MODEL_TYPE,
+    NAME,
+    PREPROCESSING,
+    TRAINER,
+)
 from ludwig.features.audio_feature import AudioFeatureMixin
 from ludwig.features.bag_feature import BagFeatureMixin
 from ludwig.features.binary_feature import BinaryFeatureMixin
@@ -300,6 +312,11 @@ def test_validate_defaults_schema():
     }
 
     validate_config(config)
+
+    config[DEFAULTS][CATEGORY][NAME] = "TEST"
+
+    with pytest.raises(ValidationError):
+        validate_config(config)
 
 
 def test_validate_no_trainer_type():
