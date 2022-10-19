bbfcc4cd0ef62b3fb105572b7e84392330fc3465
Justin Zhao <justinxzhao@gmail.com>
2022-10-19 05:18:15 +0000

    Remove custom Ludwig schemas, implement type inference on the out of the box whylogs profile, add a test for type inference.

--- ludwig/profiling/dataset_profile.py
+++ ludwig/profiling/dataset_profile.py
@@ -1,34 +1,145 @@
-import logging
 import time
-from typing import Dict, Union
+from typing import Any, Dict, Set, Union
 
 import whylogs as why
 from whylogs.core.proto import ColumnMessage
 from whylogs.core.view.column_profile_view import ColumnProfileView
 from whylogs.core.view.dataset_profile_view import DatasetProfileView
 
+from ludwig.constants import AUDIO, BINARY, CATEGORY, IMAGE, NUMBER, TEXT
 from ludwig.profiling import dataset_profile_pb2
-from ludwig.profiling.why_schema import LudwigWhySchema
+from ludwig.utils import strings_utils
+from ludwig.utils.audio_utils import is_audio_score
+
+# from ludwig.profiling.why_schema import LudwigWhySchema
 from ludwig.utils.data_utils import load_dataset
+from ludwig.utils.image_utils import is_image_score
 from ludwig.utils.types import DataFrame
 
 
 def get_dataset_profile_view(dataset: Union[str, DataFrame]) -> DatasetProfileView:
     """Returns whylogs dataset profile view."""
     dataframe = load_dataset(dataset)
-    results = why.log(pandas=dataframe, schema=LudwigWhySchema())
+    results = why.log(pandas=dataframe)
     profile = results.profile()
     profile_view = profile.view()
-    logging.debug(f"Dataset profiled: {profile_view.to_pandas()}")
     return profile_view
 
 
-def get_dataset_profile_proto(profile_view: DatasetProfileView) -> dataset_profile_pb2.DatasetProfile:
+def get_num_distinct_values(column_profile_summary):
+    return int(column_profile_summary["cardinality/est"])
+
+
+def get_num_nonnull_values(column_profile_summary):
+    return column_profile_summary["counts/n"] - column_profile_summary["counts/null"]
+
+
+def get_pct_null_values(column_profile_summary):
+    return column_profile_summary["counts/null"] / column_profile_summary["counts/n"]
+
+
+def get_distinct_values(column_profile_summary) -> Set[str]:
+    if "frequent_items/frequent_strings" not in column_profile_summary:
+        return {}
+    frequent_items = column_profile_summary["frequent_items/frequent_strings"]
+    if not frequent_items:
+        # Can be an empty list if the feature is non-string.
+        return {}
+    return {frequent_item.value for frequent_item in frequent_items}
+
+
+def get_pct_distinct_values(column_profile_summary) -> float:
+    return get_num_distinct_values(column_profile_summary) / column_profile_summary["counts/n"]
+
+
+def get_distinct_values_balance(column_profile_summary):
+    if "frequent_items/frequent_strings" not in column_profile_summary:
+        return -1
+    frequent_items = column_profile_summary["frequent_items/frequent_strings"]
+    if not frequent_items:
+        # Can be an empty list if the feature is non-string.
+        return -1
+
+    max_occurence = frequent_items[0].est
+    min_occurence = frequent_items[-1].est
+    return min_occurence / max_occurence
+
+
+def are_values_images(distinct_values: Set[str], feature_name: str):
+    overall_image_score = 0
+    for value in distinct_values:
+        is_image_score(None, value, column=feature_name)
+        if overall_image_score > 3:
+            return True
+
+    if overall_image_score > 0.5 * len(distinct_values):
+        return True
+    return False
+
+
+def are_values_audio(distinct_values: Set[str], feature_name: str):
+    overall_audio_score = 0
+    for value in distinct_values:
+        is_audio_score(value)
+        if overall_audio_score > 3:
+            return True
+
+    if overall_audio_score > 0.5 * len(distinct_values):
+        return True
+    return False
+
+
+def get_ludwig_type_from_column_profile_summary(feature_name: str, column_profile_summary: Dict[str, Any]) -> str:
+    distinct_values = get_distinct_values(column_profile_summary)
+
+    # Check for unstructured types.
+    if are_values_images(distinct_values, feature_name):
+        return IMAGE
+    if are_values_audio(distinct_values, feature_name):
+        return AUDIO
+
+    if column_profile_summary["types/boolean"]:
+        # True booleans.
+        return BINARY
+    if column_profile_summary["types/fractional"]:
+        # True fractionals.
+        return NUMBER
+    if column_profile_summary["types/integral"]:
+        # True integers.
+        # Use CATEGORY if percentage of distinct values is sufficiently low.
+        if get_pct_distinct_values(column_profile_summary) < 0.5:
+            return CATEGORY
+        return NUMBER
+    if column_profile_summary["types/string"]:
+        # TODO: Check for DATE.
+        # Check for NUMBER, CATEGORY, BINARY.
+        if get_num_distinct_values(column_profile_summary) == 2:
+            return BINARY
+        if get_pct_distinct_values(column_profile_summary) < 0.5:
+            return CATEGORY
+        if strings_utils.are_all_numbers(distinct_values):
+            return NUMBER
+    # Fallback to TEXT.
+    return TEXT
+
+
+def get_ludwig_type_map_from_column_profile_summaries(column_profile_summaries: Dict[str, Any]) -> Dict[str, str]:
+    ludwig_type_map = {}
+    for feature_name, column_profile_summary in column_profile_summaries.items():
+        ludwig_type_map[feature_name] = get_ludwig_type_from_column_profile_summary(
+            feature_name, column_profile_summary
+        )
+    return ludwig_type_map
+
+
+def get_dataset_profile_proto(df: DataFrame, profile_view: DatasetProfileView) -> dataset_profile_pb2.DatasetProfile:
+    """Returns a Ludwig DatasetProfile from a whylogs DatasetProfileView."""
     profile_view_pandas = profile_view.to_pandas()
 
     dataset_profile = dataset_profile_pb2.DatasetProfile()
     dataset_profile.timestamp = int(time.time())
     dataset_profile.num_examples = profile_view_pandas.iloc[0]["counts/n"]
+    dataset_profile.size_bytes = sum(df.memory_usage(deep=True))
     for column_name, column_profile_view in profile_view.get_columns().items():
         feature_profile = dataset_profile_pb2.FeatureProfile()
         # Ideally, this line of code would simply be:
@@ -43,14 +154,24 @@ def get_dataset_profile_proto(profile_view: DatasetProfileView) -> dataset_profi
     return dataset_profile
 
 
-def get_column_profile_views_from_proto(
+def get_column_profile_summaries_from_proto(
     dataset_profile_proto: dataset_profile_pb2.DatasetProfile,
-) -> Dict[str, ColumnProfileView]:
+) -> Dict[str, Dict[str, Any]]:
     """Returns a mapping of feature name to ColumnProfileView."""
     column_profile_views: Dict[str, ColumnProfileView] = {}
     for feature_name, feature_profile in dataset_profile_proto.feature_profiles.items():
         whylogs_metrics_proto = ColumnMessage()
         # Extra copy+deserialization to avoid TypeError.
         whylogs_metrics_proto.ParseFromString(feature_profile.whylogs_metrics.SerializeToString())
-        column_profile_views[feature_name] = ColumnProfileView.from_protobuf(whylogs_metrics_proto)
+        column_profile_view: ColumnProfileView = ColumnProfileView.from_protobuf(whylogs_metrics_proto)
+        column_profile_views[feature_name] = column_profile_view.to_summary_dict()
     return column_profile_views
+
+
+def get_column_profile_summaries(
+    pandas_df: DataFrame,
+) -> Dict[str, Dict[str, Any]]:
+    """Get WhyLogs column summaries directly from a pandas dataframe."""
+    dataset_profile_view = get_dataset_profile_view(pandas_df)
+    dataset_profile_view_proto = get_dataset_profile_proto(pandas_df, dataset_profile_view)
+    return get_column_profile_summaries_from_proto(dataset_profile_view_proto)
--- tests/ludwig/profiling/test_dataset_profile.py
+++ tests/ludwig/profiling/test_dataset_profile.py
@@ -1,32 +1,154 @@
-from ludwig.datasets import adult_census_income
+import pandas as pd
+import pytest
+
 from ludwig.profiling.dataset_profile import (
-    get_column_profile_views_from_proto,
+    get_column_profile_summaries,
+    get_column_profile_summaries_from_proto,
     get_dataset_profile_proto,
     get_dataset_profile_view,
+    get_distinct_values,
+    get_distinct_values_balance,
+    get_ludwig_type_map_from_column_profile_summaries,
+    get_num_distinct_values,
+    get_num_nonnull_values,
+    get_pct_distinct_values,
+    get_pct_null_values,
 )
 
 
 def test_get_dataset_profile_view_works():
-    train_df, _, _ = adult_census_income.load(split=True)
+    df = pd.DataFrame(
+        {
+            "animal": ["lion", "shark", "cat", "bear", "jellyfish", "kangaroo", "jellyfish", "jellyfish", "fish"],
+            "legs": [4, 0, 4, 4.0, None, 2, None, None, "fins"],
+            "weight": [14.3, 11.8, 4.3, 30.1, 2.0, 120.0, 2.7, 2.2, 1.2],
+        }
+    )
 
-    dataset_profile_view = get_dataset_profile_view(train_df)
-    dataset_profile_view_proto = get_dataset_profile_proto(dataset_profile_view)
-    column_profile_views = get_column_profile_views_from_proto(dataset_profile_view_proto)
+    dataset_profile_view = get_dataset_profile_view(df)
+    dataset_profile_view_proto = get_dataset_profile_proto(df, dataset_profile_view)
+    column_profile_summaries = get_column_profile_summaries_from_proto(dataset_profile_view_proto)
 
-    assert set(column_profile_views.keys()) == {
-        "hours-per-week",
-        "income",
-        "race",
-        "capital-loss",
-        "sex",
-        "capital-gain",
-        "occupation",
-        "relationship",
-        "native-country",
-        "workclass",
-        "education-num",
-        "age",
-        "education",
-        "marital-status",
-        "fnlwgt",
+    assert set(column_profile_summaries.keys()) == {
+        "animal",
+        "legs",
+        "weight",
+    }
+
+
+def test_get_column_profile_attributes():
+    df = pd.DataFrame(
+        {
+            "animal": ["lion", "shark", "cat", "bear", "jellyfish", "kangaroo", "jellyfish", "jellyfish", "fish"],
+            "legs": [4, 0, 4, 4.0, None, 2, None, None, "fins"],
+            "weight": [14.3, 11.8, 4.3, 30.1, 2.0, 120.0, 2.7, 2.2, 1.2],
+            "account_type": ["human", "bot", "human", "bot", "human", "bot", "human", "bot", "human"],  # Category
+            "finite_numbers_as_numbers": [0, 1, 2, 3, 4, 5, 6, 7, 8],  # Category
+            "finite_numbers_as_strings": ["0", "1", "2", "3", "4", "5", "6", "7", "8"],  # Category
+            "bools_as_bools": [True, False, True, False, True, False, True, False, True],  # Binary
+            "bools_as_strings": ["0", "1", "0", "1", "0", "1", "0", "1", "0"],  # Binary
+            "floats_as_strings": ["1.5", "3.7", "2.2", "3.1", "1.8", "1.4", "9.9", "8.7", "9.1"],  # Number
+        }
+    )
+    column_profile_summaries = get_column_profile_summaries(df)
+
+    assert get_num_distinct_values(column_profile_summaries["animal"]) == 7
+    assert get_distinct_values(column_profile_summaries["animal"]) == {
+        "fish",
+        "kangaroo",
+        "jellyfish",
+        "cat",
+        "bear",
+        "shark",
+        "lion",
+    }
+    # None doesn't count towards distinct values.
+    assert get_num_distinct_values(column_profile_summaries["legs"]) == 4
+    assert get_distinct_values(column_profile_summaries["legs"]) == {"2.000000", "0.000000", "fins", "4.000000"}
+
+    # True fractionals have no frequent items.
+    assert get_distinct_values(column_profile_summaries["weight"]) == {}
+    # Fractionals, as strings, do have frequent items.
+    assert get_distinct_values(column_profile_summaries["floats_as_strings"]) == {
+        "1.5",
+        "1.4",
+        "9.1",
+        "9.9",
+        "8.7",
+        "3.7",
+        "2.2",
+        "1.8",
+        "3.1",
+    }
+    # True integers have distinct values.
+    assert get_distinct_values(column_profile_summaries["finite_numbers_as_numbers"]) == {
+        "2",
+        "7",
+        "3",
+        "1",
+        "0",
+        "6",
+        "4",
+        "8",
+        "5",
+    }
+    # Integers as strings have distinct values.
+    assert get_distinct_values(column_profile_summaries["finite_numbers_as_strings"]) == {
+        "8",
+        "7",
+        "2",
+        "5",
+        "0",
+        "1",
+        "6",
+        "3",
+        "4",
+    }
+
+    # Booleans as booleans have no frequent items.
+    assert get_distinct_values(column_profile_summaries["bools_as_bools"]) == {}
+
+    # Booleans as strings have frequent items.
+    assert get_distinct_values(column_profile_summaries["bools_as_strings"]) == {"0", "1"}
+
+    assert get_distinct_values_balance(column_profile_summaries["animal"]) == pytest.approx(0.3333333333)
+    assert get_num_nonnull_values(column_profile_summaries["animal"]) == 9
+    assert get_num_nonnull_values(column_profile_summaries["legs"]) == 6
+    assert get_pct_null_values(column_profile_summaries["animal"]) == 0
+    assert get_pct_null_values(column_profile_summaries["legs"]) == pytest.approx(0.3333333333)
+    assert get_pct_distinct_values(column_profile_summaries["bools_as_strings"]) == pytest.approx(0.222222222)
+
+
+def test_get_ludwig_type_map_from_column_profile_summaries():
+    df = pd.DataFrame(
+        {
+            "animal": ["lion", "shark", "cat", "bear", "jellyfish", "kangaroo", "jellyfish", "jellyfish", "fish"],
+            "legs": [4, 0, 4, 4.0, None, 2, None, None, "fins"],
+            "weight": [14.3, 11.8, 4.3, 30.1, 2.0, 120.0, 2.7, 2.2, 1.2],
+            "account_type": ["human", "bot", "human", "bot", "human", "bot", "human", "bot", "human"],
+            "finite_numbers_as_numbers": [0, 1, 2, 3, 4, 5, 6, 7, 8],
+            "finite_numbers_as_strings": ["0", "1", "2", "3", "4", "5", "6", "7", "8"],
+            "handful_of_numbers": [0, 1, 2, 0, 1, 2, 0, 1, 2],
+            "handful_of_strings": ["human", "bot", "unknown", "human", "bot", "unknown", "human", "bot", "unknown"],
+            "bools_as_bools": [True, False, True, False, True, False, True, False, True],
+            "bools_as_strings": ["0", "1", "0", "1", "0", "1", "0", "1", "0"],
+            "floats_as_strings": ["1.5", "3.7", "2.2", "3.1", "1.8", "1.4", "9.9", "8.7", "9.1"],
+        }
+    )
+    column_profile_summaries = get_column_profile_summaries(df)
+
+    ludwig_type_map = get_ludwig_type_map_from_column_profile_summaries(column_profile_summaries)
+
+    assert ludwig_type_map == {
+        "account_type": "binary",
+        "animal": "text",
+        "bools_as_bools": "binary",
+        "bools_as_strings": "binary",
+        "finite_numbers_as_numbers": "number",
+        "finite_numbers_as_strings": "number",
+        "handful_of_numbers": "category",
+        "handful_of_strings": "category",
+        "floats_as_strings": "number",
+        "legs": "number",
+        "weight": "number",
     }
