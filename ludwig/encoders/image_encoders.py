8c18b9eed21aa9ef8264261cd87d71138e0f3673
Jim Thompson <jimthompson5802@gmail.com>
2022-11-07 11:14:13 -0500

    code clean up

--- ludwig/encoders/image_encoders.py
+++ ludwig/encoders/image_encoders.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import copy
 import logging
 import os
 from abc import abstractmethod
