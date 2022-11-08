dc3f08ea0ab0f3aa39d8fcdcbde20b27c5b8c835
Jeff Kinnison <jeffery.kinnison@predibase.com>
2022-11-08 14:50:58 -0500

    address reviews

--- tests/conftest.py
+++ tests/conftest.py
@@ -146,6 +146,13 @@ def ray_cluster_7cpu(request):
         yield
 
 
+@pytest.fixture(scope="module")
+def ray_cluster_small_object_store(request):
+    # Create a Ray cluster with the smallest possible object store.
+    with _ray_start(request, num_cpus=2, object_store_memory=78643200):
+        yield
+
+
 @contextlib.contextmanager
 def _ray_start(request, **kwargs):
     try:
