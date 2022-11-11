8ad1708666049ebd5e9a421f207f2fb3bf2033d1
Justin <justinxzhao@gmail.com>
2022-11-11 12:35:34 -0600

    Apply suggestions from code review
    Co-authored-by: Daniel Treiman <dan.treiman@gmail.com>

--- ludwig/datasets/loaders/sarcastic_headlines.py
+++ ludwig/datasets/loaders/sarcastic_headlines.py
@@ -12,11 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from typing import Optional
 
 import pandas as pd
 
-from ludwig.datasets.dataset_config import DatasetConfig
 from ludwig.datasets.loaders.dataset_loader import DatasetLoader
 
 
