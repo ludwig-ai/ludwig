43eca261c48763658f94a06621ca067d20603843
Kabir Brar <kabir@brar.org>
2022-11-12 17:40:21 -0800

    fix: Fix metadata object-to-JSON serialization for oneOf fields and add full schema serialization test. (#2758)
    * oneOf fix and try removing deepcopy for metadata
    
    * add test, remove print
    
    * add test, remove print
    
    * add test, remove print
    
    * fix json string

--- ludwig/schema/utils.py
+++ ludwig/schema/utils.py
@@ -921,7 +921,7 @@ def OneOfOptionsField(
                 "description": description,
                 "default": default,
                 "title": self.name,
-                "parameter_metadata": parameter_metadata,
+                "parameter_metadata": convert_metadata_to_json(parameter_metadata) if parameter_metadata else None,
             }
 
             for idx, option in enumerate(field_options):
