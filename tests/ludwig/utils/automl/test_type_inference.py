6381e2d7773a9d58364f1036b9583bc442fce206
Arnav Garg <arnav@predibase.com>
2022-11-16 11:21:04 +0000

    update test names

--- tests/ludwig/utils/automl/test_type_inference.py
+++ tests/ludwig/utils/automl/test_type_inference.py
@@ -98,13 +98,13 @@ def test_auto_type_inference_single_value_binary_feature():
 
 
 @pytest.mark.parametrize("missing_value", [(None), (np.nan)], ids=["none", "np.nan"])
-def test_bool_type_inference(missing_value):
+def test_bool_type_inference_from_object_dtype(missing_value):
     df = pd.DataFrame({"col1": ["a", "b", "a", "a", "b", missing_value]})
     info = get_dataset_info(df)
     assert info.fields[0].dtype == "bool"
 
 
-def test_object_type_inference():
+def test_object_type_inference_from_object_dtype():
     df = pd.DataFrame({"col1": ["a", "b", "c", "d", "e", "a", "b", "b"]})
     info = get_dataset_info(df)
     assert info.fields[0].dtype == "object"
