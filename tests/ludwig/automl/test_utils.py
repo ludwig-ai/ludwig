8e2aa63d3aab6d24164604d6e52639dd67519537
Arnav Garg <arnav@predibase.com>
2022-11-17 15:22:41 +0000

    consolidate Joppe's tests with my tests

--- tests/ludwig/automl/test_utils.py
+++ tests/ludwig/automl/test_utils.py
@@ -1,10 +1,7 @@
-import numpy as np
-import pandas as pd
 import pytest
 
 ray = pytest.importorskip("ray")  # noqa
 
-from ludwig.automl.base_config import get_dataset_info  # noqa
 from ludwig.utils.automl.utils import get_model_type  # noqa
 
 pytestmark = pytest.mark.distributed
@@ -28,19 +25,3 @@ def _features(*in_types, out):
 def test_get_model_type(config, expected):
     actual = get_model_type(config)
     assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "col,expected_dtype",
-    [
-        (["a", "b", "c", "d", "e", "a", "b", "b"], "object"),
-        (["a", "b", "a", "b", np.nan], "object"),
-        (["a", "b", "a", "b", None], "object"),
-        ([True, False, True, True, ""], "object"),
-        ([True, False, True, False, np.nan], "bool"),
-    ],
-)
-def test_object_and_bool_type_inference(col, expected_dtype):
-    df = pd.DataFrame({"col1": col})
-    info = get_dataset_info(df)
-    assert info.fields[0].dtype == expected_dtype
