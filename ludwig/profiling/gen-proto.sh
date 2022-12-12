1a11959e1f45398ab4d557c07bec2ff161b13722
Justin Zhao <justinxzhao@gmail.com>
2022-12-12 15:38:09 +0000

    Put pb2 files in /proto.

--- ludwig/profiling/gen-proto.sh
+++ ludwig/profiling/gen-proto.sh
@@ -3,6 +3,4 @@ protoc --go_out=. --python_out=. \
     ludwig/profiling/proto/dataset_profile.proto \
     --go_opt=paths=source_relative && {
     mv ludwig/profiling/proto/*.pb.go ludwig/profiling/go/
-} && {
-    mv ludwig/profiling/proto/*pb2.py ludwig/profiling/
 }
