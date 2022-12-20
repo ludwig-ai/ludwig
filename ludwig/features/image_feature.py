dcae650cf57fe7a84fd2f576cf097d9fa0b019b3
Arnav Garg <arnav@predibase.com>
2022-12-20 15:03:39 +0000

    force running checks

--- ludwig/features/image_feature.py
+++ ludwig/features/image_feature.py
@@ -273,6 +273,7 @@ class ImageFeatureMixin(BaseFeatureMixin):
                 f"Set image feature height and width to {width} to be compatible with"
                 f" {encoder_parameters[TYPE]} encoder."
             )
+            print("hi")
         return width, height
 
     @staticmethod
