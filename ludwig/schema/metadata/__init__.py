ca6ba03f56bc92c6dea955df82dc3faa40e89ee8
Travis Addair <tgaddair@gmail.com>
2022-12-26 12:26:37 -0800

    Fixed loading

--- ludwig/schema/metadata/__init__.py
+++ ludwig/schema/metadata/__init__.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 import yaml
 
@@ -10,8 +10,21 @@ _PATH_HERE = os.path.abspath(os.path.dirname(__file__))
 _CONFIG_DIR = os.path.join(_PATH_HERE, "configs")
 
 
+def _to_metadata(d: Dict[str, Any]) -> Union[ParameterMetadata, Dict[str, Any]]:
+    is_nested = False
+    for k, v in list(d.items()):
+        if isinstance(v, dict):
+            d[k] = _to_metadata(v)
+            is_nested = True
+
+    if is_nested:
+        return d
+
+    return ParameterMetadata.from_dict(d)
+
+
 def _load(fname: str) -> Dict[str, Any]:
-    return yaml.load(os.path.join(_CONFIG_DIR, fname))
+    return _to_metadata(yaml.load(os.path.join(_CONFIG_DIR, fname)))
 
 
 COMBINER_METADATA = _load("combiners.yaml")
@@ -20,7 +33,3 @@ ENCODER_METADATA = _load("encoders.yaml")
 FEATURE_METADATA = _load("features.yaml")
 PREPROCESSING_METADATA = _load("preprocessing.yaml")
 TRAINER_METADATA = _load("trainer.yaml")
-
-
-def to_metadata(d: Dict[str, Any]) -> ParameterMetadata:
-    return ParameterMetadata.from_dict(d)
