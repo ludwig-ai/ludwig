6eb8403051d89d97b59eb00d0b9e10b3af81ca21
Travis Addair <tgaddair@gmail.com>
2022-12-28 14:37:25 -0800

    WIP: add distributed interface

---
+++ ludwig/trainers/distributed/base.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+import torch
+from torch import nn
+from torch.optim import Optimizer
+
+
+class DistributedBase(ABC):
+    @abstractmethod
+    def wrap_model(self, model: nn.Module) -> nn.Module:
+        pass
+
+    @abstractmethod
+    def wrap_optimizer(self, optimizer: Optimizer) -> Optimizer:
+        pass
+
+    @abstractmethod
+    def size(self) -> int:
+        pass
+
+    @abstractmethod
+    def rank(self) -> int:
+        pass
+
+    @abstractmethod
+    def local_size(self) -> int:
+        pass
+
+    @abstractmethod
+    def local_rank(self) -> int:
+        pass
+
+    @abstractmethod
+    def barrier(self):
+        pass
+
+    @abstractmethod
+    def allreduce(self, t: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def broadcast(self, t: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def sync_model(self, model: nn.Module) -> nn.Module:
+        pass
+
+    @abstractmethod
+    def sync_optimizer(self, optimizer: Optimizer) -> Optimizer:
+        pass
+
+    @abstractmethod
+    def broadcast_object(self, v: Any) -> Any:
+        pass
