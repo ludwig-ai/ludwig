e643e7ac5be83f04547be9262d0fae4fbec82d5f
Travis Addair <tgaddair@gmail.com>
2022-12-28 21:01:22 -0800

    Cleanup

--- ludwig/utils/torch_utils.py
+++ ludwig/utils/torch_utils.py
@@ -10,6 +10,7 @@ from torch import nn
 from torch.nn import Module, ModuleDict
 
 from ludwig.api_annotations import DeveloperAPI
+from ludwig.distributed.base import DistributedStrategy
 from ludwig.utils.strings_utils import SpecialSymbol
 
 _TORCH_INIT_PARAMS: Optional[Tuple] = None
@@ -278,10 +279,10 @@ def initialize_pytorch(
     gpus: Optional[Union[int, str, List[int]]] = None,
     gpu_memory_limit: Optional[float] = None,
     allow_parallel_threads: bool = True,
-    horovod=None,  # Optional["horovod.torch"]
+    distributed: Optional[DistributedStrategy] = None,  # Optional["horovod.torch"]
 ):
-    use_horovod = horovod is not None
-    param_tuple = (gpus, gpu_memory_limit, allow_parallel_threads, use_horovod)
+    use_distributed = distributed is not None
+    param_tuple = (gpus, gpu_memory_limit, allow_parallel_threads, use_distributed)
     if _TORCH_INIT_PARAMS is not None:
         if _TORCH_INIT_PARAMS != param_tuple:
             warnings.warn(
@@ -301,17 +302,17 @@ def initialize_pytorch(
             torch.backends.cudnn.benchmark = False
 
     gpu_device_count = torch.cuda.device_count()
-    if horovod is not None and gpus is None:
-        if 0 < gpu_device_count < horovod.local_size():
+    if distributed is not None and gpus is None:
+        if 0 < gpu_device_count < distributed.local_size():
             warnings.warn(
                 f"Horovod: disabling GPU support! This host is running with "
-                f"{horovod.local_size()} worker processes but only {gpu_device_count} "
+                f"{distributed.local_size()} worker processes but only {gpu_device_count} "
                 f"GPUs. To enable GPU training, reduce the number of worker processes "
                 f"on this host to match the number of GPUs."
             )
             gpus = [-1]
         else:
-            gpus = [horovod.local_rank()]
+            gpus = [distributed.local_rank()]
 
     if isinstance(gpus, int):
         gpus = [gpus]
