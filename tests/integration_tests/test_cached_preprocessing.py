1ba42f8640f462680ea1bda42752f85ad932021f
Travis Addair <tgaddair@gmail.com>
2023-01-03 16:25:59 -0800

    Added distilbert test

--- tests/integration_tests/test_cached_preprocessing.py
+++ tests/integration_tests/test_cached_preprocessing.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 import pytest
 
@@ -9,7 +10,14 @@ from tests.integration_tests.utils import (
     category_feature,
     generate_data,
     number_feature,
+    text_feature,
 )
+
+
+def run_test_suite(config, dataset, backend):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        ludwig_model = LudwigModel(config, backend=backend)
+        ludwig_model.train(dataset=dataset, output_directory=tmpdir)
 
 
 @pytest.mark.parametrize(
@@ -27,9 +35,27 @@ def test_onehot_encoding(tmpdir, backend, ray_cluster_2cpu):
     output_features = [binary_feature()]
 
     data_csv_path = os.path.join(tmpdir, "dataset.csv")
-    training_data_csv_path = generate_data(input_features, output_features, data_csv_path)
+    dataset = generate_data(input_features, output_features, data_csv_path)
+    config = {"input_features": input_features, "output_features": output_features, TRAINER: {"epochs": 2}}
+    run_test_suite(config, dataset, backend)
+
+
+@pytest.mark.parametrize(
+    "backend",
+    [
+        pytest.param("local", id="local"),
+        pytest.param("ray", id="ray", marks=pytest.mark.distributed),
+    ],
+)
+def test_distilbert_embedding(tmpdir, backend, ray_cluster_2cpu):
+    input_features = [
+        number_feature(),
+        text_feature(encoder={"type": "distilbert"}),
+    ]
+    output_features = [binary_feature()]
+
+    data_csv_path = os.path.join(tmpdir, "dataset.csv")
+    dataset = generate_data(input_features, output_features, data_csv_path)
 
     config = {"input_features": input_features, "output_features": output_features, TRAINER: {"epochs": 2}}
-
-    ludwig_model = LudwigModel(config, backend=backend)
-    ludwig_model.train(dataset=training_data_csv_path)
+    run_test_suite(config, dataset, backend)
