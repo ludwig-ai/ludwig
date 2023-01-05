636e98bbff8b40becb473259b1292c8cbced3318
Geoffrey Angus <geoffrey@predibase.com>
2023-01-04 18:32:35 -0800

    move on_batch_end callback call so that eval is not counted as part of batch duration

--- ludwig/trainers/trainer.py
+++ ludwig/trainers/trainer.py
@@ -948,6 +948,8 @@ class Trainer(BaseTrainer):
                     f"{psutil.Process(os.getpid()).memory_info()[0] / 1e6:0.2f}MB"
                 )
 
+            self.callback(lambda c: c.on_batch_end(self, progress_tracker, save_path))
+
             if progress_tracker.steps % final_steps_per_checkpoint == 0:
                 # Checkpoint the model.
                 if self.is_coordinator() and not self.skip_save_progress:
@@ -972,8 +974,6 @@ class Trainer(BaseTrainer):
                 )
                 if should_break:
                     return should_break
-
-            self.callback(lambda c: c.on_batch_end(self, progress_tracker, save_path))
 
         return False
 
