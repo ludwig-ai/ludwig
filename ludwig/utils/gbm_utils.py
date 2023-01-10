2ad93c934d64e204e1fa1cdd827b03a874ec2050
Joppe Geluykens <joppe@predibase.com>
2023-01-10 23:49:26 +0000

    address review comments

--- ludwig/utils/gbm_utils.py
+++ ludwig/utils/gbm_utils.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, Iterable, Tuple
+from typing import Callable, Dict, Tuple
 
 import lightgbm as lgb
 import numpy as np
@@ -9,15 +9,7 @@ from numpy import typing as npt
 from ludwig.constants import NUMBER
 from ludwig.features.base_feature import BaseFeatureMixin, OutputFeature
 from ludwig.features.category_feature import CategoryOutputFeature
-from ludwig.features.feature_utils import LudwigFeatureDict
 from ludwig.models.base import BaseModel
-
-
-def iter_feature_metrics(features: LudwigFeatureDict) -> Iterable[Tuple[str, str]]:
-    """Helper for iterating feature names and metric names."""
-    for feature_name, feature in features.items():
-        for metric in feature.metric_functions:
-            yield feature_name, metric
 
 
 def get_single_output_feature(model: BaseModel) -> BaseFeatureMixin:
@@ -28,13 +20,11 @@ def get_single_output_feature(model: BaseModel) -> BaseFeatureMixin:
 def sigmoid(x: npt.NDArray) -> npt.NDArray:
     """Compute sigmoid function.
 
-    # Inputs
+    Args:
+        x: 1D array of shape (n_samples,).
 
-    :param x: 1D array of shape (n_samples,).
-
-    # Returns
-
-    :return: 1D array of shape (n_samples,).
+    Returns:
+        1D array of shape (n_samples,).
     """
     return 1.0 / (1.0 + np.exp(-x))
 
@@ -42,17 +32,14 @@ def sigmoid(x: npt.NDArray) -> npt.NDArray:
 def log_loss_objective(y_true: npt.NDArray, y_pred: npt.NDArray) -> Tuple[npt.NDArray, npt.NDArray]:
     """Binary objective function for LightGBM. Computes the logistic loss.
 
-    # Inputs
+    Args:
+        y_true: 1D array of true labels of shape (n_samples,).
+        y_pred: 1D array of raw predictions of shape (n_samples,).
 
-    :param y_true: 1D array of true labels of shape (n_samples,).
-    :param y_pred: 1D array of raw predictions of shape (n_samples,).
+    Returns:
+        1D array of gradients of shape (n_samples,) and 1D array of hessians of shape (n_samples,).
 
-    # Returns
-
-    :return: 1D array of gradients of shape (n_samples,) and 1D array of hessians of shape (n_samples,).
-
-    # References
-
+    References:
     - https://github.com/microsoft/LightGBM/issues/3312
     - https://github.com/microsoft/LightGBM/issues/5373#issuecomment-1188595889
     """
@@ -65,13 +52,11 @@ def log_loss_objective(y_true: npt.NDArray, y_pred: npt.NDArray) -> Tuple[npt.ND
 def softmax(x: npt.NDArray) -> npt.NDArray:
     """Compute softmax values for each sets of scores in x.
 
-    # Inputs
+    Args:
+        x: 2D array of shape (n_samples, n_classes).
 
-    :param x: 2D array of shape (n_samples, n_classes).
-
-    # Returns
-
-    :return: 2D array of shape (n_samples, n_classes).
+    Returns:
+        2D array of shape (n_samples, n_classes).
     """
     row_wise_max = np.max(x, axis=1).reshape(-1, 1)
     exp_x = np.exp(x - row_wise_max)
@@ -83,19 +68,16 @@ def multiclass_objective(
 ) -> Tuple[npt.NDArray, npt.NDArray]:
     """Multi-class objective function for LightGBM. Computes the softmax cross-entropy loss.
 
-    # Inputs
+    Args:
+        y_true: 1D array of true labels of shape (n_samples,).
+        y_pred: 1D array of raw predictions of shape (n_samples * n_classes,).
+        weight: 1D array of weights of shape (n_samples,).
 
-    :param y_true: 1D array of true labels of shape (n_samples,).
-    :param y_pred: 1D array of raw predictions of shape (n_samples * n_classes,).
-    :param weight: 1D array of weights of shape (n_samples,).
-
-    # Returns
-
-    :return: 1D array of gradients of shape (n_samples * n_classes,) and 1D array of hessians
+    Returns:
+        1D array of gradients of shape (n_samples * n_classes,) and 1D array of hessians
         of shape (n_samples * n_classes,).
 
-    # References
-
+    References:
     - https://github.com/microsoft/LightGBM/blob/9afd8b/tests/python_package_test/test_sklearn.py#L1296
     - https://github.com/microsoft/LightGBM/blob/9afd8b/tests/python_package_test/utils.py#L142
     """
@@ -124,13 +106,11 @@ def multiclass_objective(
 def store_predictions(train_logits_buffer: torch.Tensor) -> Callable:
     """Create a callback that records the predictions of the model on the training data in ``train_logits_buffer``.
 
-    # Inputs
+    Args:
+        train_logits_buffer: 2D tensor of shape (n_samples, n_classes) that stores the predictions of the model.
 
-    :param train_logits_buffer: 2D tensor of shape (n_samples, n_classes) that stores the predictions of the model.
-
-    # Returns
-
-    :return: a callback that records the predictions of the model in ``train_logits_buffer``.
+    Returns:
+        A callback that records the predictions of the model in ``train_logits_buffer``.
     """
 
     def _callback(env: CallbackEnv) -> None:
@@ -164,14 +144,12 @@ def reshape_logits(output_feature: OutputFeature, logits: torch.Tensor) -> torch
 def logits_to_predictions(model: BaseModel, train_logits: torch.Tensor) -> Dict[str, Dict[str, torch.Tensor]]:
     """Convert the logits of the model to Ludwig predictions.
 
-    # Inputs
+    Args:
+        model: the Ludwig model.
+        train_logits: 2D tensor of shape (n_samples, n_classes) that contains the predictions of the model.
 
-    :param model: the Ludwig model.
-    :param train_logits: 2D tensor of shape (n_samples, n_classes) that contains the predictions of the model.
-
-    # Returns
-
-    :return: a dictionary mapping the output feature name to the predictions.
+    Returns:
+        A dictionary mapping the output feature name to the predictions.
     """
     output_feature = get_single_output_feature(model)
     train_logits = reshape_logits(output_feature, train_logits)
@@ -181,14 +159,12 @@ def logits_to_predictions(model: BaseModel, train_logits: torch.Tensor) -> Dict[
 def get_targets(lgb_train: lgb.Dataset, output_feature: BaseFeatureMixin, device: str) -> Dict[str, torch.Tensor]:
     """Get the targets of the training data.
 
-    # Inputs
+    Args:
+        lgb_train: the training data.
+        output_feature: the output feature.
 
-    :param lgb_train: the training data.
-    :param output_feature: the output feature.
-
-    # Returns
-
-    :return: a dictionary mapping the output feature name to the targets.
+    Returns:
+        A dictionary mapping the output feature name to the targets.
     """
     is_regression = output_feature.type() == NUMBER
     targets = lgb_train.get_label().copy() if is_regression else lgb_train.get_label().copy().astype(int)
