bcc3a2c6e9bc9cd56c1b417f8abe0d6e5a72df80
Justin <justinxzhao@gmail.com>
2023-01-12 18:04:03 -0500

    Refactor metrics and metric tables and support adding more in-training metrics. (#2901)

--- ludwig/features/sequence_feature.py
+++ ludwig/features/sequence_feature.py
@@ -303,8 +303,6 @@ class SequenceInputFeature(SequenceFeatureMixin, InputFeature):
 
 
 class SequenceOutputFeature(SequenceFeatureMixin, OutputFeature):
-    metric_functions = SequenceOutputFeatureConfig.get_output_metric_functions()
-
     def __init__(
         self,
         output_feature_config: Union[SequenceOutputFeatureConfig, Dict],
