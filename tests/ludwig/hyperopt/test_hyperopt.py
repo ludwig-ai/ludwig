c9c2122c9c958547df5ef5a5d7913f24a8b98ebb
Joppe Geluykens <joppe@predibase.com>
2023-01-13 20:51:12 +0100

    add docstring

--- tests/ludwig/hyperopt/test_hyperopt.py
+++ tests/ludwig/hyperopt/test_hyperopt.py
@@ -90,6 +90,9 @@ def test_grid_search_more_than_one_sample():
 
 
 def test_hyperopt_config_gbm():
+    """This test was added due to a schema validation error when hyperopting GBMs:
+    ```jsonschema.exceptions.ValidationError: Additional properties are not allowed ('epochs' was unexpected)```
+    """
     config = {
         "input_features": [{"name": "Date received", "type": "category"}],
         "output_features": [{"name": "Product", "type": "category"}],
