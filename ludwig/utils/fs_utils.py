294728348d44b67dc929ba6da5f6c51e7c46b939
Arnav Garg <arnav@predibase.com>
2023-01-13 23:41:26 +0000

    Adds safe_move_directory to fs_utils

--- ludwig/utils/fs_utils.py
+++ ludwig/utils/fs_utils.py
@@ -183,6 +183,26 @@ def safe_move_file(src, dst):
 
 
 @DeveloperAPI
+def safe_move_directory(src, dst):
+    try:
+        os.replace(src, dst)
+    except OSError as err:
+        if err.errno == errno.EXDEV:
+            # Generate a unique ID, and copy `<src>` to the target directory with a temporary name `<dst>.<ID>.tmp`.
+            # Because we're copying across a filesystem boundary, this initial copy may not be atomic.  We insert a
+            # random UUID so if different processes are copying into `<dst>`, they don't overlap in their tmp copies.
+            copy_id = uuid.uuid4()
+            tmp_dst = f"{dst}.{copy_id}.tmp"
+            shutil.copyfile(src, tmp_dst)
+
+            # Atomic replace file onto the new name, and clean up original source file.
+            os.replace(tmp_dst, dst)
+            os.unlink(src)
+        else:
+            raise
+
+
+@DeveloperAPI
 def rename(src, tgt):
     protocol, _ = split_protocol(tgt)
     if protocol is not None:
