1f4df8bf04c3e9d837de6d491b25bfd2892285e3
Joppe Geluykens <joppe@predibase.com>
2023-01-17 13:00:09 +0000

    increase test coverage

--- tests/integration_tests/test_api.py
+++ tests/integration_tests/test_api.py
@@ -484,11 +484,17 @@ def test_api_callbacks(tmpdir, csv_filename, epochs, batch_size, num_examples, s
 
 
 @pytest.mark.parametrize("model_type", [MODEL_ECD, MODEL_GBM])
-def test_walltime_callback(model_type, tmpdir, csv_filename, caplog):
+@pytest.mark.parametrize(
+    "timeout_s, timeout_steps, should_timeout",
+    [
+        pytest.param(-1, -1, False, id="no-timeout"),
+        pytest.param(1e-6, -1, True, id="microsecond-timeout"),
+        pytest.param(1e-6, 1, True, id="microsecond-timeout-with-1-step-tolerance"),
+        pytest.param(1e-6, 2, True, id="microsecond-timeout-with-2-step-tolerance"),
+    ],
+)
+def test_early_stop_timeout(model_type, timeout_s, timeout_steps, should_timeout, tmpdir, csv_filename, caplog):
     num_examples = 3
-
-    # one microsecond timeout, 1 step buffer
-    callback = WalltimeEarlyStopCallback(timeout_s=1e-6, early_stopping_steps=1)
 
     input_features = [category_feature()]
     output_features = [category_feature(decoder={"vocab_size": num_examples}, reduce_input="sum")]
@@ -501,25 +507,48 @@ def test_walltime_callback(model_type, tmpdir, csv_filename, caplog):
     # map input to output, model should overfit and trigger early stopping
     df[output_features[0]["name"]] = df[input_features[0]["name"]]
 
+    additional_trainer_params = (
+        {
+            # Set boosting_rounds_per_checkpoint to 1 to check for early stopping at ev
+            "boosting_rounds_per_checkpoint": 1,
+            # Disable feature filtering to avoid having no features due to small test dataset,
+            # see https://stackoverflow.com/a/66405983/5222402
+            "feature_pre_filter": False,
+        }
+        if model_type == MODEL_GBM
+        else {}
+    )
     config = {
         "model_type": model_type,
         "input_features": input_features,
         "output_features": output_features,
         "combiner": {"type": "concat"},
         TRAINER: {
-            "validation_metric": "accuracy",
-            "validation_field": output_features[0]["name"],
+            **{
+                # Disable step-based early stopping to test time-based early stopping
+                "early_stop": -1,
+                "early_stop_timeout_s": timeout_s,
+                "early_stop_timeout_steps": timeout_steps,
+                "validation_metric": "accuracy",
+                "validation_field": output_features[0]["name"],
+            },
+            **additional_trainer_params,
         },
     }
-    model = LudwigModel(config, callbacks=[callback])
+    model = LudwigModel(config)
 
     with caplog.at_level(logging.INFO, logger="ludwig.trainers.trainer"), caplog.at_level(
         logging.INFO, logger="ludwig.trainers.trainer_lightgbm"
     ):
         model.train(training_set=df, validation_set=df, test_set=df)
 
-    # Make sure that early stopping was triggered
-    assert "EARLY STOPPING" in caplog.text
+    # Make sure that time-based early stopping was triggered as expected
+    if should_timeout:
+        assert "EARLY STOPPING" in caplog.text
+        if timeout_steps > -1:
+            assert f"It has been {timeout_steps} step(s) since last validation improvement." in caplog.text
+    else:
+        assert "EARLY STOPPING" not in caplog.text
 
 
 @pytest.mark.parametrize("epochs", [1, 2])
