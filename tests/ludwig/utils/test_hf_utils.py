efefa6cb97bb96571c6127519770fc60280da2d1
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-01-18 09:36:28 -0500

    reduce the number of models in the test

--- tests/ludwig/utils/test_hf_utils.py
+++ tests/ludwig/utils/test_hf_utils.py
@@ -2,68 +2,17 @@ import os
 from typing import Type
 
 import pytest
-from transformers import (  # CamembertModel,
-    AlbertModel,
-    BertModel,
-    BertTokenizer,
-    CTRLModel,
-    DistilBertModel,
-    ElectraModel,
-    FlaubertModel,
-    GPT2Model,
-    LongformerModel,
-    MT5EncoderModel,
-    OpenAIGPTModel,
-    RobertaModel,
-    T5Model,
-    TransfoXLModel,
-    XLMModel,
-    XLMRobertaModel,
-    XLNetModel,
-)
+from transformers import BertModel, BertTokenizer
 
-from ludwig.encoders.text_encoders import (  # CamemBERTEncoder,
-    ALBERTEncoder,
-    BERTEncoder,
-    CTRLEncoder,
-    DistilBERTEncoder,
-    ELECTRAEncoder,
-    FlauBERTEncoder,
-    GPT2Encoder,
-    GPTEncoder,
-    LongformerEncoder,
-    MT5Encoder,
-    RoBERTaEncoder,
-    T5Encoder,
-    TransformerXLEncoder,
-    XLMEncoder,
-    XLMRoBERTaEncoder,
-    XLNetEncoder,
-)
+from ludwig.encoders.text_encoders import BERTEncoder
 from ludwig.utils.hf_utils import load_pretrained_hf_model
 
 
 @pytest.mark.parametrize(
     ("model", "name"),
     [
-        (AlbertModel, ALBERTEncoder.DEFAULT_MODEL_NAME),
         (BertModel, BERTEncoder.DEFAULT_MODEL_NAME),
         (BertTokenizer, "bert-base-uncased"),
-        # (CamembertModel, CamemBERTEncoder.DEFAULT_MODEL_NAME),
-        (CTRLModel, CTRLEncoder.DEFAULT_MODEL_NAME),
-        (DistilBertModel, DistilBERTEncoder.DEFAULT_MODEL_NAME),
-        (ElectraModel, ELECTRAEncoder.DEFAULT_MODEL_NAME),
-        (FlaubertModel, FlauBERTEncoder.DEFAULT_MODEL_NAME),
-        (GPT2Model, GPT2Encoder.DEFAULT_MODEL_NAME),
-        (LongformerModel, LongformerEncoder.DEFAULT_MODEL_NAME),
-        (MT5EncoderModel, MT5Encoder.DEFAULT_MODEL_NAME),
-        (OpenAIGPTModel, GPTEncoder.DEFAULT_MODEL_NAME),
-        (RobertaModel, RoBERTaEncoder.DEFAULT_MODEL_NAME),
-        (T5Model, T5Encoder.DEFAULT_MODEL_NAME),
-        (TransfoXLModel, TransformerXLEncoder.DEFAULT_MODEL_NAME),
-        (XLMModel, XLMEncoder.DEFAULT_MODEL_NAME),
-        (XLMRobertaModel, XLMRoBERTaEncoder.DEFAULT_MODEL_NAME),
-        (XLNetModel, XLNetEncoder.DEFAULT_MODEL_NAME),
     ],
 )
 def test_load_hf_model(model: Type, name: str, tmpdir: os.PathLike):
