5a5a4a136a7d199d4b29ba76862fbfbc07d5e9fd
Justin Zhao <justinxzhao@gmail.com>
2023-01-19 02:35:58 +0000

    Remove irrelevant base feature diff.

--- ludwig/features/base_feature.py
+++ ludwig/features/base_feature.py
@@ -121,6 +121,9 @@ class BaseFeature:
 
     def __init__(self, feature: BaseFeatureConfig):
         super().__init__()
+
+        if not feature.name:
+            raise ValueError("Missing feature name")
         self.feature_name = feature.name
 
         if not feature.column:
