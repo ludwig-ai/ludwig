2a3f3849c30c046a9db67cc6a67447e805f1f1ba
Jim Thompson <jimthompson5802@gmail.com>
2023-01-20 14:28:08 -0500

    test: add Ludwig encoder test cases for augmentation

--- tests/ludwig/augmentation/test_augmentation_pipeline.py
+++ tests/ludwig/augmentation/test_augmentation_pipeline.py
@@ -7,6 +7,7 @@ import pytest
 import torch
 
 from ludwig.api import LudwigModel
+from ludwig.constants import IMAGENET1K
 from ludwig.data.dataset_synthesizer import cli_synthesize_dataset
 from ludwig.features.image_feature import ImageAugmentation
 
@@ -39,7 +40,7 @@ def train_data():
         input_features[0].update(
             {
                 "destination_folder": os.path.join(os.getcwd(), os.path.join(tmp_dir, "images")),
-                "preprocessing": {"height": 600, "width": 600, "num_channels": 3},
+                "preprocessing": {"height": 350, "width": 350, "num_channels": 3},
             }
         )
         feature_list = input_features + output_features
@@ -49,7 +50,7 @@ def train_data():
         os.makedirs(data_dir, exist_ok=True)
         train_fp = os.path.join(tmp_dir, "train.csv")
 
-        cli_synthesize_dataset(32, feature_list, train_fp)
+        cli_synthesize_dataset(16, feature_list, train_fp)
 
         # remove unneeded data generation parameters
         input_features[0].pop("destination_folder")
@@ -58,7 +59,45 @@ def train_data():
         yield train_fp, input_features, output_features
 
 
-# TODO: re-enable this test once refactoring image augmentation pipeline is fixed
+# common function to run model training with augmentation pipeline
+def run_augmentation_training(
+    train_data,
+    backend,
+    encoder,
+    preprocessing,
+    augmentation_pipeline_ops,
+):
+    # unpack training data
+    train_fp, input_features, output_features = train_data
+
+    # add encoder and preprocessing specification to input feature
+    input_features[0].update(
+        {
+            "encoder": encoder,
+            "preprocessing": preprocessing,
+        }
+    )
+
+    # add augmentation pipeline to input feature if specified
+    test_input_features = copy.deepcopy(input_features)
+    if augmentation_pipeline_ops:
+        test_input_features[0].update({"augmentation": augmentation_pipeline_ops})
+
+    config = {
+        "input_features": test_input_features,
+        "output_features": output_features,
+        "trainer": {"epochs": 2, "batch_size": 8, },
+        "backend": {"type": backend, },
+    }
+
+    model = LudwigModel(config, logging_level=logging.INFO)
+    model.experiment(
+        dataset=train_fp,
+        skip_save_processed_input=True,
+        skip_save_model=True,
+    )
+
+
 @pytest.mark.parametrize(
     "augmentation_pipeline_ops",
     [
@@ -86,53 +125,56 @@ def test_image_augmentation(test_image, augmentation_pipeline_ops):
     augmentation_pipeline(test_image)
 
 
-@pytest.mark.distributed
-@pytest.mark.parametrize(
-    "augmentation_pipeline_ops",
-    [
-        None,
-        [{"type": "random_horizontal_flip"}, {"type": "random_rotate"}],
-    ],
-)
-@pytest.mark.parametrize("backend", ["local"])  # TODO: re-enable #, "ray"])
-def test_model_training_with_augmentation_pipeline(
+AUGMENTATION_PIPELINE_OPS = [
+    None,
+    [{"type": "random_horizontal_flip"}, {"type": "random_rotate"}],
+]
+
+AUGMENTATION_ENCODER = [
+    {"type": "stacked_cnn"},
+    {"type": "alexnet", "model_cache_dir": os.path.join(os.getcwd(), "tv_cache")},
+]
+
+AUGMENTATION_PREPROCESSING = [
+    {"standardize_image": None, "width": 300, "height": 300, },
+    {"standardize_image": IMAGENET1K, "width": 300, "height": 300, },
+]
+
+
+@pytest.mark.parametrize("augmentation_pipeline_ops", AUGMENTATION_PIPELINE_OPS)
+@pytest.mark.parametrize("encoder", AUGMENTATION_ENCODER)
+@pytest.mark.parametrize("preprocessing", AUGMENTATION_PREPROCESSING)
+def test_local_model_training_with_augmentation_pipeline(
     train_data,
-    backend,
+    encoder,
+    preprocessing,
     augmentation_pipeline_ops,
     ray_cluster_2cpu,
 ):
-    # unpack training data
-    train_fp, input_features, output_features = train_data
-
-    # add encoder and preprocessing specification to input feature
-    input_features[0].update(
-        {
-            "encoder": {
-                "type": "alexnet",
-                "model_cache_dir": os.path.join(os.getcwd(), "tv_cache"),
-            },
-            "preprocessing": {
-                "standardize_image": "imagenet1k",
-                "width": 300,
-                "height": 300,
-            },
-        }
+    run_augmentation_training(
+        train_data,
+        "local",
+        encoder,
+        preprocessing,
+        augmentation_pipeline_ops,
     )
-    # add augmentation pipeline to input feature if specified
-    test_input_features = copy.deepcopy(input_features)
-    if augmentation_pipeline_ops:
-        test_input_features[0].update({"augmentation": augmentation_pipeline_ops})
 
-    config = {
-        "input_features": test_input_features,
-        "output_features": output_features,
-        "trainer": {"epochs": 2, "batch_size": 16},
-        "backend": {"type": backend},
-    }
 
-    model = LudwigModel(config, logging_level=logging.INFO)
-    model.experiment(
-        dataset=train_fp,
-        skip_save_processed_input=True,
-        skip_save_model=True,
+@pytest.mark.distributed
+@pytest.mark.parametrize("augmentation_pipeline_ops", AUGMENTATION_PIPELINE_OPS)
+@pytest.mark.parametrize("encoder", AUGMENTATION_ENCODER)
+@pytest.mark.parametrize("preprocessing", AUGMENTATION_PREPROCESSING)
+def test_ray_model_training_with_augmentation_pipeline(
+    train_data,
+    encoder,
+    preprocessing,
+    augmentation_pipeline_ops,
+    ray_cluster_2cpu,
+):
+    run_augmentation_training(
+        train_data,
+        "ray",
+        encoder,
+        preprocessing,
+        augmentation_pipeline_ops,
     )
