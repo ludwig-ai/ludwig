bc8a382547b304372e003867360a5d8ea1d3305f
ksbrar <kabir@brar.xyz>
2023-01-19 21:09:38 -0500

    fix

--- ludwig/schema/__init__.py
+++ ludwig/schema/__init__.py
@@ -39,6 +39,8 @@ def get_ludwig_version_jsonschema():
 @DeveloperAPI
 @lru_cache(maxsize=2)
 def get_schema(model_type: str = MODEL_ECD):
+    import ludwig.combiners.combiners  # noqa: F401
+
     schema = {
         "type": "object",
         "properties": {
