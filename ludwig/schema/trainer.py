8909684ba4f87625b3a8536058fd61cefeb6eff4
Joppe Geluykens <joppe@predibase.com>
2023-01-24 11:10:37 +0100

    GBM: disable goss boosting type (#2986)

--- ludwig/schema/trainer.py
+++ ludwig/schema/trainer.py
@@ -348,7 +348,8 @@ class GBMTrainerConfig(BaseTrainerConfig):
 
     # LightGBM core parameters (https://lightgbm.readthedocs.io/en/latest/Parameters.html)
     boosting_type: str = schema_utils.StringOptions(
-        ["gbdt", "dart", "goss"],
+        # TODO: Re-enable "goss" when supported: https://github.com/ludwig-ai/ludwig/issues/2988
+        ["gbdt", "dart"],
         default="gbdt",
         description="Type of boosting algorithm to use with GBM trainer.",
     )
