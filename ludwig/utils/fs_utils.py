683f618c08a2dc49bced64ae63691521a1badfa1
Arnav Garg <arnav@predibase.com>
2023-02-01 20:44:06 +0000

    Remove safe_move_dir

--- ludwig/utils/fs_utils.py
+++ ludwig/utils/fs_utils.py
@@ -182,41 +182,6 @@ def safe_move_file(src, dst):
 
 
 @DeveloperAPI
-def safe_move_directory(src, dst):
-    """Recursively moves files from src directory to dst directory and removes src directory.
-
-    If dst directory does not exist, it will be created.
-    """
-    try:
-        os.replace(src, dst)
-    except OSError as err:
-        # ENOTEMPTY: Directory not empty
-        # EXDEV: Cross-device link
-        if err.errno == errno.ENOTEMPTY or err.errno == errno.EXDEV:
-            # Generate a unique ID, and copy `<src>` to the target directory with a temporary name `<dst>.<ID>.tmp`.
-            # Because we're copying across a filesystem boundary, this initial copy may not be atomic.  We insert a
-            # random UUID so if different processes are copying into `<dst>`, they don't overlap in their tmp copies.
-            copy_id = uuid.uuid4()
-            tmp_dst = f"{dst}.{copy_id}.tmp"
-
-            # First copy from src to a temporary directory
-            shutil.copytree(src, tmp_dst)
-
-            # Then copy from temporary directory to dst
-            # If dirs_exist_ok is false (the default) and `dst` already exists, a
-            # `FileExistsError` is raised. If `dirs_exist_ok` is true, the copying
-            # operation will continue if it encounters existing directories, and files
-            # within the `dst` tree will be overwritten by corresponding files from the
-            # `src` tree.
-            shutil.copytree(tmp_dst, dst, dirs_exist_ok=True)
-
-            # Remove source directory after copy to dst is complete
-            shutil.rmtree(src)
-        else:
-            raise
-
-
-@DeveloperAPI
 def rename(src, tgt):
     protocol, _ = split_protocol(tgt)
     if protocol is not None:
