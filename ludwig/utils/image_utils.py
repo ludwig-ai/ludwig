bae4ca54210d85e45450f9c2159c66c0b0a628fe
Travis Addair <tgaddair@gmail.com>
2023-02-08 17:16:00 -0800

    Fix circular import

--- ludwig/utils/image_utils.py
+++ ludwig/utils/image_utils.py
@@ -29,7 +29,6 @@ from torchvision.models._api import WeightsEnum
 from ludwig.api_annotations import DeveloperAPI
 from ludwig.constants import CROP_OR_PAD, INTERPOLATE
 from ludwig.encoders.base import Encoder
-from ludwig.encoders.image.torchvision import TVBaseEncoder
 from ludwig.utils.data_utils import get_abs_path
 from ludwig.utils.fs_utils import get_bytes_obj_from_path
 from ludwig.utils.registry import Registry
@@ -96,6 +95,8 @@ def is_image_score(src_path, img_entry, column: str):
 @DeveloperAPI
 def is_torchvision_encoder(encoder_obj: Encoder) -> bool:
     # TODO(travis): do this through an interface rather than conditional logic
+    from ludwig.encoders.image.torchvision import TVBaseEncoder
+
     return isinstance(encoder_obj, TVBaseEncoder)
 
 
