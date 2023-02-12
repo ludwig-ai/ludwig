46c0a00a1c7bf688d7d48a074908decb2be54d3d
Travis Addair <tgaddair@gmail.com>
2023-02-12 15:00:29 -0800

    Remove previous ModelConfig implementation and refactor to use `__post_init__` (#3083)

--- ludwig/schema/utils.py
+++ ludwig/schema/utils.py
@@ -4,7 +4,7 @@ from dataclasses import field, Field
 from typing import Any
 from typing import Dict as TDict
 from typing import List as TList
-from typing import Optional, Set, Tuple, Type, Union
+from typing import Optional, Set, Tuple, Type, TypeVar, Union
 
 import marshmallow_dataclass
 import yaml
@@ -135,6 +135,9 @@ class ListSerializable(ABC):
         pass
 
 
+ConfigT = TypeVar("ConfigT", bound="BaseMarshmallowConfig")
+
+
 @DeveloperAPI
 class BaseMarshmallowConfig(ABC):
     """Base marshmallow class for common attributes and metadata."""
@@ -163,7 +166,7 @@ class BaseMarshmallowConfig(ABC):
         return convert_submodules(self.__dict__)
 
     @classmethod
-    def from_dict(cls, d: TDict[str, Any]):
+    def from_dict(cls: Type[ConfigT], d: TDict[str, Any]) -> ConfigT:
         schema = cls.get_class_schema()()
         return schema.load(d)
 
