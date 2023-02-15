8a0b6d32e83b73f312bbb43cfa887465404527bb
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-02-15 18:13:55 -0500

    add tests for transformer config validation

---
+++ tests/ludwig/config_validation/test_validate_config_encoder.py
@@ -0,0 +1,58 @@
+import pytest
+
+from ludwig.config_validation.validation import validate_config
+from ludwig.constants import DEFAULTS, ENCODER, INPUT_FEATURES, OUTPUT_FEATURES, SEQUENCE, TEXT, TIMESERIES, TYPE
+from ludwig.error import ConfigValidationError
+from tests.integration_tests.utils import (
+    binary_feature,
+    number_feature,
+    sequence_feature,
+    text_feature,
+    timeseries_feature,
+)
+
+
+@pytest.mark.parametrize("feature_type", [SEQUENCE, TEXT, TIMESERIES])
+def test_default_transformer_encoder(feature_type):
+    """Tests that a transformer hyperparameter divisibility error is correctly recognized in feature defaults.
+
+    Transformers require that `hidden_size % num_heads == 0`. 9 and 18 were selected as test values because they were
+    the values from the original error.
+    """
+    config = {
+        INPUT_FEATURES: [number_feature(), sequence_feature(), text_feature(), timeseries_feature()],
+        OUTPUT_FEATURES: [binary_feature()],
+        DEFAULTS: {feature_type: {ENCODER: {TYPE: "transformer", "hidden_size": 9, "num_heads": 18}}},
+    }
+
+    with pytest.raises(ConfigValidationError):
+        validate_config(config)
+
+    config[DEFAULTS][feature_type][ENCODER]["hidden_size"] = 18
+    config[DEFAULTS][feature_type][ENCODER]["num_heads"] = 9
+
+    validate_config(config)
+
+
+@pytest.mark.parametrize("feature_type", [sequence_feature, text_feature, timeseries_feature])
+def test_input_feature_transformer_encoder(feature_type):
+    """Tests that a transformer hyperparameter divisibility error is correctly recognized for a specific feature.
+
+    Transformers require that `hidden_size % num_heads == 0`. 9 and 18 were selected as test values because they were
+    the values from the original error.
+    """
+    config = {
+        INPUT_FEATURES: [
+            number_feature(),
+            feature_type(**{ENCODER: {TYPE: "transformer", "hidden_size": 9, "num_heads": 18}}),
+        ],
+        OUTPUT_FEATURES: [binary_feature()],
+    }
+
+    with pytest.raises(ConfigValidationError):
+        validate_config(config)
+
+    config[INPUT_FEATURES][1][ENCODER]["hidden_size"] = 18
+    config[INPUT_FEATURES][1][ENCODER]["num_heads"] = 9
+
+    validate_config(config)
