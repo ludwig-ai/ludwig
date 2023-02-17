9e7b84bdfcd70a403b169ddd1687d4cce7de78b5
Wael Abid <wael@predibase.com>
2023-02-17 11:30:35 -0800

    add excluded config back

--- tests/regression_tests/benchmark/test_model_performance.py
+++ tests/regression_tests/benchmark/test_model_performance.py
@@ -8,7 +8,6 @@ from ludwig.benchmarking.benchmark import benchmark
 from ludwig.utils.data_utils import load_yaml
 
 SKIPPED_CONFIG_ISSUES = {
-    "mercedes_benz_greener.ecd.yaml": "https://github.com/ludwig-ai/ludwig/issues/2978",
     "sarcos.ecd.yaml": "https://github.com/ludwig-ai/ludwig/issues/3019",
     "sarcos.gbm.yaml": "https://github.com/ludwig-ai/ludwig/issues/3019",
 }
