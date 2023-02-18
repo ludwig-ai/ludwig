e987d22fc6cfc77d652f9dad943a7b0a4e052f1e
Travis Addair <tgaddair@gmail.com>
2023-02-18 11:47:27 -0800

    Fixed schema

--- ludwig/schema/cross_validation.py
+++ ludwig/schema/cross_validation.py
@@ -14,3 +14,24 @@ class CrossValidationConfig(schema_utils.BaseMarshmallowConfig, ABC):
         default=10,
         description="Number of folds (k) used during k-fold cross validation.",
     )
+
+
+@DeveloperAPI
+def get_cv_jsonschema():
+    props = schema_utils.unload_jsonschema_from_marshmallow_class(CrossValidationField)["properties"]
+    return {
+        "type": ["object", "null"],
+        "properties": props,
+        "title": "cross_validation_options",
+        "description": "Settings for cross validation",
+    }
+
+
+@DeveloperAPI
+class CrossValidationField(schema_utils.DictMarshmallowField):
+    def __init__(self):
+        super().__init__(CrossValidationField, default_missing=True)
+
+    @staticmethod
+    def _jsonschema_type_mapping():
+        return get_cv_jsonschema()
