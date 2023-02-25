f25a2701fbfe75c9542267bf5252e23cbc8bddc0
Travis Addair <tgaddair@gmail.com>
2023-02-25 10:18:00 -0800

    Cache json conversion

--- ludwig/schema/metadata/parameter_metadata.py
+++ ludwig/schema/metadata/parameter_metadata.py
@@ -1,11 +1,12 @@
 import json
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, List, Union
+from typing import Any, Dict, List, Union
 
 from dataclasses_json import dataclass_json
 
 from ludwig.api_annotations import DeveloperAPI
+from ludwig.utils.misc_utils import memoized_method
 
 
 @DeveloperAPI
@@ -75,9 +76,13 @@ class ParameterMetadata:
     # Whether the parameter is used strictly internally.
     internal_only: bool = False
 
+    @memoized_method(maxsize=1)
+    def to_json_dict(self) -> Dict[str, Any]:
+        return json.loads(self.to_json())
+
 
 @DeveloperAPI
-def convert_metadata_to_json(pm: ParameterMetadata):
+def convert_metadata_to_json(pm: ParameterMetadata) -> Dict[str, Any]:
     """Converts a ParameterMetadata dict to a normal JSON dict.
 
     NOTE: Without the json.loads call, to_json() returns
@@ -85,7 +90,7 @@ def convert_metadata_to_json(pm: ParameterMetadata):
     """
     if not pm:
         return None
-    return json.loads(pm.to_json())
+    return pm.to_json_dict()
 
 
 # This is a quick way to flag schema parameters as internal only via the `parameter_metadata` argument
