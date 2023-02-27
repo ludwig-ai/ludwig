f185b26e64db921980bebbbab47cf6b80cd14c9d
Travis Addair <tgaddair@gmail.com>
2023-02-27 15:37:16 -0800

    Fixed confidence_penalty for newer versions of pytorch

--- tests/ludwig/modules/test_loss_modules.py
+++ tests/ludwig/modules/test_loss_modules.py
@@ -1,7 +1,12 @@
+from typing import Optional
 import pytest
 import torch
 
 from ludwig.modules import loss_modules
+
+
+def from_float(v: float) -> torch.Tensor:
+    return torch.tensor(v).float()
 
 
 @pytest.mark.parametrize("preds", [torch.arange(6).reshape(3, 2).float()])
@@ -44,11 +49,29 @@ def test_rmspe_loss_zero_targets(preds: torch.Tensor, target: torch.Tensor, outp
     assert torch.isclose(loss(preds, target), output, rtol=0.0001)
 
 
+@pytest.mark.parametrize(
+    "confidence_penalty,positive_class_weight,robust_lambda,output",
+    [
+        (0.0, None, 0, from_float(-21.4655)),
+        (2.0, None, 0, from_float(-21.1263)),
+        (0.0, 2.0, 0, from_float(-20.1222)),
+        (0.0, None, 2, from_float(22.4655)),
+        (2, 2, 2, from_float(21.4614)),
+    ],
+)
 @pytest.mark.parametrize("preds", [torch.arange(6).reshape(3, 2).float()])
 @pytest.mark.parametrize("target", [torch.arange(6, 12).reshape(3, 2).float()])
-@pytest.mark.parametrize("output", [torch.tensor(-21.4655).float()])
-def test_bwcew_loss(preds: torch.Tensor, target: torch.Tensor, output: torch.Tensor):
-    loss = loss_modules.BWCEWLoss()
+def test_bwcew_loss(
+    preds: torch.Tensor,
+    target: torch.Tensor,
+    confidence_penalty: float,
+    positive_class_weight: Optional[float],
+    robust_lambda: int,
+    output: torch.Tensor,
+):
+    loss = loss_modules.BWCEWLoss(
+        positive_class_weight=positive_class_weight, robust_lambda=robust_lambda, confidence_penalty=confidence_penalty
+    )
     assert torch.isclose(loss(preds, target), output)
 
 
