14b9671491594ee1ea03f3fb64d99bcc09f3def2
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-03-01 17:15:52 -0500

    package feature generator with feature name

--- tests/integration_tests/test_sequence_decoders.py
+++ tests/integration_tests/test_sequence_decoders.py
@@ -2,29 +2,29 @@ import os
 
 import pytest
 
-from ludwig.constants import COLUMN, DECODER, DEFAULTS, INPUT_FEATURES, NAME, OUTPUT_FEATURES, SEQUENCE, TEXT, TYPE
+from ludwig.constants import COLUMN, DECODER, DEFAULTS, INPUT_FEATURES, NAME, OUTPUT_FEATURES, TEXT, TYPE
 from tests.integration_tests.utils import (
-    category_feature,
     create_data_set_to_use,
     generate_data,
     RAY_BACKEND_CONFIG,
-    sequence_feature,
     text_feature,
     train_with_backend,
 )
 
 
-# TODO: Add sequence feature and additional deocders
-@pytest.mark.parametrize("feature_type", [TEXT])
-@pytest.mark.parametrize("use_default", [True, False])
+# TODO: Add sequence feature and additional decoders
+@pytest.mark.parametrize("feature_type,feature_gen", [(TEXT, text_feature)])
 @pytest.mark.parametrize("decoder_type", ["generator"])
+@pytest.mark.parametrize("use_defaults", [True, False])
 @pytest.mark.distributed
-def test_sequence_decoder_predictions(tmpdir, csv_filename, ray_cluster_2cpu, feature_type, use_default, decoder_type):
+def test_sequence_decoder_predictions(
+    tmpdir, csv_filename, ray_cluster_2cpu, feature_type, feature_gen, use_defaults, decoder_type
+):
     """Test that sequence decoders return the correct successfully predict."""
-    input_features = [category_feature()]
-    output_feature = (
-        sequence_feature(output_feature=True) if feature_type == "sequence" else text_feature(output_feature=True)
-    )
+    input_features = [feature_gen()]
+    output_feature = feature_gen(output_feature=True)
+    output_feature[DECODER] = {TYPE: decoder_type}
+
     dataset_path = generate_data(
         input_features=input_features,
         output_features=[output_feature],
@@ -35,7 +35,7 @@ def test_sequence_decoder_predictions(tmpdir, csv_filename, ray_cluster_2cpu, fe
     config = {INPUT_FEATURES: input_features}
 
     # Ensure that the decoder outputs the correct predictions through both the default and feature-specific configs.
-    if use_default:
+    if use_defaults:
         config[OUTPUT_FEATURES] = [{TYPE: feature_type, NAME: output_feature[NAME], COLUMN: output_feature[COLUMN]}]
         config[DEFAULTS] = {feature_type: {DECODER: {TYPE: decoder_type}}}
     else:
