7ac0d748cd88549694d9de4d9d6a441d5a78afe1
Geoffrey Angus <geoffrey@predibase.com>
2023-03-01 11:49:05 -0800

    factor out test

--- tests/integration_tests/test_preprocessing.py
+++ tests/integration_tests/test_preprocessing.py
@@ -565,14 +565,40 @@ def test_category_feature_vocab_size_1(feature_type, tmpdir) -> None:
         ludwig_model.train(dataset=training_data_csv_path)
 
 
-@pytest.mark.parametrize("encoder", ["efficientnet"])
-# @pytest.mark.parametrize("use_pretrained", [False, True], ids=["false", "true"])
-def test_image_encoder_different_dimension_image(tmpdir, csv_filename, encoder: str):
+@pytest.mark.parametrize("use_pretrained", [False, True], ids=["false", "true"])
+def test_image_encoder_different_dimension_image(tmpdir, csv_filename, use_pretrained: bool):
+    input_features = [
+        image_feature(
+            os.path.join(tmpdir, "generated_output"),
+            preprocessing={"in_memory": True, "height": 224, "width": 206, "num_channels": 3},
+            encoder={"type": "_vit_legacy", "use_pretrained": use_pretrained},
+        )
+    ]
+    output_features = [category_feature(decoder={"vocab_size": 5}, reduce_input="sum")]
+
+    data_csv = generate_data(
+        input_features, output_features, os.path.join(tmpdir, csv_filename), num_examples=NUM_EXAMPLES
+    )
+
+    config = {
+        "input_features": input_features,
+        "output_features": output_features,
+        "trainer": {"train_steps": 1},
+    }
+
+    model = LudwigModel(config)
+
+    # Failure happens post preprocessing but before training during the ECD model creation phase
+    # so make sure the model can be created properly and training can proceed
+    model.train(dataset=data_csv)
+
+
+def test_image_encoder_torchvision_different_num_channels(tmpdir, csv_filename):
     input_features = [
         image_feature(
             os.path.join(tmpdir, "generated_output"),
             preprocessing={"in_memory": True, "height": 224, "width": 206, "num_channels": 1},
-            encoder={"type": encoder},
+            encoder={"type": "efficientnet"},
         )
     ]
     output_features = [category_feature(decoder={"vocab_size": 5}, reduce_input="sum")]
