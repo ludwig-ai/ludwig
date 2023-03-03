694142a93b731948b6ad61667fff816564a75350
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-03-03 17:38:22 -0500

    add hebo schema

--- ludwig/schema/hyperopt/search_algorithm.py
+++ ludwig/schema/hyperopt/search_algorithm.py
@@ -372,6 +372,7 @@ class DragonflySAConfig(BaseSearchAlgorithmConfig):
             "dict passed to `tune.Tuner()`."
         )
     )
+
     metric: Optional[str] = schema_utils.String(
         default=None,
         allow_none=True,
@@ -421,6 +422,63 @@ class DragonflySAConfig(BaseSearchAlgorithmConfig):
 @ludwig_dataclass
 class HEBOSAConfig(BaseSearchAlgorithmConfig):
     type: str = schema_utils.ProtectedString("hebo")
+
+    dependencies: List[str] = ["hebo"]
+
+    space: Optional[List[Dict]] = schema_utils.DictList(
+        description=("A dict mapping parameter names to Tune search spaces or a HEBO DesignSpace object.")
+    )
+
+    metric: Optional[str] = schema_utils.String(
+        default=None,
+        allow_none=True,
+        description=(
+            "The training result objective value attribute. If None but a mode was passed, the anonymous metric "
+            "`_metric` will be used per default."
+        ),
+    )
+
+    mode: Optional[str] = schema_utils.StringOptions(
+        options=["min", "max"],
+        default=None,
+        allow_none=True,
+        description=(
+            "One of `{min, max}`. Determines whether objective is minimizing or maximizing the metric attribute."
+        ),
+    )
+
+    points_to_evaluate: Optional[List[Dict]] = schema_utils.DictList(
+        description=(
+            "Initial parameter suggestions to be run first. This is for when you already have some good parameters "
+            "you want to run first to help the algorithm make better suggestions for future parameters. Needs to be "
+            "a list of dicts containing the configurations."
+        )
+    )
+
+    evaluated_rewards: Optional[List] = schema_utils.List(
+        description=(
+            "If you have previously evaluated the parameters passed in as points_to_evaluate you can avoid re-running "
+            "those trials by passing in the reward attributes as a list so the optimiser can be told the results "
+            "without needing to re-compute the trial. Must be the same length as `points_to_evaluate`."
+        )
+    )
+
+    random_state_seed: Optional[int] = schema_utils.Integer(
+        default=None,
+        allow_none=True,
+        description=(
+            "Seed for reproducible results. Defaults to None. Please note that setting this to a value will change "
+            "global random state for `numpy` on initalization and loading from checkpoint."
+        ),
+    )
+
+    max_concurrent: int = schema_utils.NonNegativeInteger(
+        default=8,
+        description=(
+            "Number of maximum concurrent trials. If this Searcher is used in a `ConcurrencyLimiter`, the "
+            "`max_concurrent` value passed to it will override the value passed here."
+        ),
+    )
 
 
 @DeveloperAPI
