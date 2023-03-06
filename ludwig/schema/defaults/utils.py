79edd76e9eadc1c6fede93c0ecd1804aeba80a0d
Kabir Brar <kabir@brar.org>
2023-03-06 13:52:16 -0500

    fix: [REBASE] Streamline GBM defaults schema (#3188)

--- ludwig/schema/defaults/utils.py
+++ ludwig/schema/defaults/utils.py
@@ -4,11 +4,12 @@ from marshmallow import fields, ValidationError
 
 import ludwig.schema.utils as schema_utils
 from ludwig.api_annotations import DeveloperAPI
-from ludwig.schema.features.utils import defaults_config_registry
+from ludwig.schema.features.utils import ecd_defaults_config_registry
+from ludwig.utils.registry import Registry
 
 
 @DeveloperAPI
-def DefaultsDataclassField(feature_type: str):
+def DefaultsDataclassField(feature_type: str, defaults_registry: Registry = ecd_defaults_config_registry):
     """Custom dataclass field that when used inside a dataclass will allow the user to specify a nested default
     config for a specific feature type.
 
@@ -23,7 +24,7 @@ def DefaultsDataclassField(feature_type: str):
             if value is None:
                 return None
             if isinstance(value, dict):
-                defaults_class = defaults_config_registry[feature_type]
+                defaults_class = defaults_registry[feature_type]
                 try:
                     return defaults_class.Schema().load(value)
                 except (TypeError, ValidationError) as error:
@@ -31,7 +32,7 @@ def DefaultsDataclassField(feature_type: str):
             raise ValidationError(f"Invalid params: {value}")
 
         def _jsonschema_type_mapping(self):
-            defaults_cls = defaults_config_registry[feature_type]
+            defaults_cls = defaults_registry[feature_type]
             props = schema_utils.unload_jsonschema_from_marshmallow_class(defaults_cls)["properties"]
             return {
                 "type": "object",
@@ -41,7 +42,7 @@ def DefaultsDataclassField(feature_type: str):
             }
 
     try:
-        defaults_cls = defaults_config_registry[feature_type]
+        defaults_cls = defaults_registry[feature_type]
         dump_default = defaults_cls.Schema().dump({})
         load_default = lambda: defaults_cls.Schema().load({})
 
@@ -56,4 +57,6 @@ def DefaultsDataclassField(feature_type: str):
             default_factory=load_default,
         )
     except Exception as e:
-        raise ValidationError(f"Unsupported feature type: {feature_type}. See input_type_registry. " f"Details: {e}")
+        raise ValidationError(
+            f"Unsupported feature type: {feature_type}. Allowed: {defaults_registry.keys()}. " f"Details: {e}"
+        )
--- ludwig/schema/features/text_feature.py
+++ ludwig/schema/features/text_feature.py
@@ -11,7 +11,7 @@ from ludwig.schema.features.loss.utils import LossDataclassField
 from ludwig.schema.features.preprocessing.base import BasePreprocessingConfig
 from ludwig.schema.features.preprocessing.utils import PreprocessingDataclassField
 from ludwig.schema.features.utils import (
-    defaults_config_registry,
+    ecd_defaults_config_registry,
     ecd_input_config_registry,
     input_mixin_registry,
     output_config_registry,
@@ -109,7 +109,7 @@ class TextOutputFeatureConfig(BaseOutputFeatureConfig, TextOutputFeatureConfigMi
 
 
 @DeveloperAPI
-@defaults_config_registry.register(TEXT)
+@ecd_defaults_config_registry.register(TEXT)
 @ludwig_dataclass
 class TextDefaultsConfig(TextInputFeatureConfigMixin, TextOutputFeatureConfigMixin):
     loss: BaseLossConfig = LossDataclassField(
