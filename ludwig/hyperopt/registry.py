4c2b9837084bf1952be511eee381ff09a19ace47
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-03-09 10:40:08 -0500

    remove old search algos

--- ludwig/hyperopt/registry.py
+++
@@ -1,7 +0,0 @@
-from ludwig.schema.hyperopt.search_algorithm import get_search_algorithm_cls
-
-
-def instantiate_search_algorithm(search_alg: "BaseSearchAlgorithmConfig") -> "SearchAlgorithm":  # noqa: F821
-    search_alg_type = search_alg.type
-    cls = get_search_algorithm_cls(search_alg_type)
-    return cls(search_alg)
