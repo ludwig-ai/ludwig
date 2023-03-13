885127766bf5dd8e4e4dbf6884b18d871be48610
Travis Addair <tgaddair@gmail.com>
2023-03-13 16:03:28 -0700

    WIP

--- ludwig/schema/encoders/text_encoders.py
+++ ludwig/schema/encoders/text_encoders.py
@@ -4,6 +4,7 @@ from ludwig.api_annotations import DeveloperAPI
 from ludwig.constants import TEXT
 from ludwig.schema import utils as schema_utils
 from ludwig.schema.encoders.sequence_encoders import SequenceEncoderConfig
+from ludwig.schema.encoders.text.hf_model_params import DebertaModelParams, ModelParamsField
 from ludwig.schema.encoders.utils import register_encoder_config
 from ludwig.schema.features.preprocessing.text import TextPreprocessingConfig
 from ludwig.schema.metadata import ENCODER_METADATA
@@ -777,104 +778,7 @@ class DebertaV2Config(HFEncoderConfig):
         parameter_metadata=INTERNAL_ONLY,
     )
 
-    # Model architecture params for training from scratch
-    # TODO(travis): conditionally disable setting these when `use_pretrained=True`.
-    hidden_size: int = schema_utils.PositiveInteger(
-        default=1536,
-        description="Dimensionality of the encoder layers and the pooler layer.",
-    )
-
-    num_hidden_layers: int = schema_utils.PositiveInteger(
-        default=24,
-        description="Number of hidden layers in the Transformer encoder.",
-    )
-
-    num_attention_heads: int = schema_utils.PositiveInteger(
-        default=24,
-        description="Number of attention heads for each attention layer in the Transformer encoder.",
-    )
-
-    intermediate_size: int = schema_utils.PositiveInteger(
-        default=6144,
-        description="Dimensionality of the 'intermediate' (often named feed-forward) layer in the Transformer encoder.",
-    )
-
-    hidden_act: str = schema_utils.StringOptions(
-        options=["gelu", "relu", "silu", "gelu", "tanh", "gelu_fast", "mish", "linear", "sigmoid", "gelu_new"],
-        default="gelu",
-        description="The non-linear activation function (function or string) in the encoder and pooler.",
-    )
-
-    hidden_dropout_prob: float = schema_utils.NonNegativeFloat(
-        default=0.1,
-        description="The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.",
-    )
-
-    attention_probs_dropout_prob: float = schema_utils.NonNegativeFloat(
-        default=0.1,
-        description="The dropout ratio for the attention probabilities.",
-    )
-
-    max_position_embeddings: int = schema_utils.PositiveInteger(
-        default=512,
-        description=(
-            "The maximum sequence length that this model might ever be used with. Typically set this to something "
-            "large just in case (e.g., 512 or 1024 or 2048)."
-        ),
-    )
-
-    type_vocab_size: int = schema_utils.NonNegativeInteger(
-        default=0,
-        description=("The vocabulary size of the `token_type_ids`."),
-    )
-
-    initializer_range: float = schema_utils.NonNegativeFloat(
-        default=0.02,
-        description=(
-            "The standard deviation of the truncated_normal_initializer for initializing all weight matrices."
-        ),
-    )
-
-    layer_norm_eps: float = schema_utils.NonNegativeFloat(
-        default=1e-7,
-        description="The epsilon used by the layer normalization layers.",
-    )
-
-    relative_attention: bool = schema_utils.Boolean(
-        default=True,
-        description="Whether use relative position encoding.",
-    )
-
-    max_relative_positions: int = schema_utils.Integer(
-        default=-1,
-        description=(
-            "The range of relative positions `[-max_position_embeddings, max_position_embeddings]`. Use the same "
-            "value as `max_position_embeddings`."
-        ),
-    )
-
-    pad_token_id: int = schema_utils.Integer(
-        default=0,
-        description="The value used to pad input_ids.",
-    )
-
-    position_biased_input: bool = schema_utils.Boolean(
-        default=False,
-        description="Whether add absolute position embedding to content embedding.",
-    )
-
-    pos_att_type: List[str] = schema_utils.List(
-        default=["p2c", "c2p"],
-        description=(
-            "The type of relative position attention, it can be a combination of `['p2c', 'c2p']`, e.g. `['p2c']`, "
-            "`['p2c', 'c2p']`, `['p2c', 'c2p']`."
-        ),
-    )
-
-    layer_norm_eps: float = schema_utils.NonNegativeFloat(
-        default=1e-12,
-        description="The epsilon used by the layer normalization layers.",
-    )
+    model_params: DebertaModelParams = ModelParamsField(DebertaModelParams).get_default_field()
 
 
 # TODO: uncomment once we figure out host memory issue: https://github.com/ludwig-ai/ludwig/issues/3107
