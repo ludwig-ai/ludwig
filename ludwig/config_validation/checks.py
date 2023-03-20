3294c2488c0a87f21e01d0fca78fe7b6dcf6f3ae
Justin Zhao <justinxzhao@gmail.com>
2023-03-20 20:30:45 +0000

    Make sure that concat combiner aux check skips non-ecd models.

--- ludwig/config_validation/checks.py
+++ ludwig/config_validation/checks.py
@@ -326,6 +326,8 @@ def check_tagger_decoder_requirements(config: "ModelConfig") -> None:  # noqa: F
 def check_concat_combiner_requirements(config: "ModelConfig") -> None:  # noqa: F821
     """Checks that if the concat combiner receives a mixture of sequence and non-sequence features, that all
     sequence features are configured with reduce_output to be 2D tensors."""
+    if config.model_type != MODEL_ECD:
+        return
     if config.combiner.type != "concat":
         return
 
