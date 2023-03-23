29e40dae5089f7fee1356bef8c0f7cd0d43602ee
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-03-23 13:50:54 -0400

    use mocks instead of temp config objects

--- tests/ludwig/config_validation/test_validate_config_hyperopt.py
+++ tests/ludwig/config_validation/test_validate_config_hyperopt.py
@@ -1,9 +1,12 @@
 from itertools import repeat
+from unittest.mock import patch
 
 import pytest
 
 # Imported to populate the registry
 import ludwig.schema.hyperopt.parameter  # noqa: F401
+import ludwig.schema.hyperopt.scheduler  # noqa: F401
+import ludwig.schema.hyperopt.search_algorithm  # noqa: F401
 from ludwig.constants import (
     EXECUTOR,
     HYPEROPT,
@@ -16,106 +19,64 @@ from ludwig.constants import (
 )
 from ludwig.error import ConfigValidationError
 from ludwig.schema.hyperopt import utils
-from ludwig.schema.hyperopt.scheduler import BaseSchedulerConfig
-from ludwig.schema.hyperopt.search_algorithm import BaseSearchAlgorithmConfig
 from ludwig.schema.model_types.base import ModelConfig
-from ludwig.schema.utils import ludwig_dataclass, ProtectedString
 from tests.integration_tests.utils import binary_feature, text_feature
 
-CONFIG_DEPENDENCY_PARAMS = {  # Tuples of type, dependency list, whether it should raise an exception
-    "no_deps": {TYPE: "no_deps", "dependencies": None, "raises_exception": False},
-    "installed": {TYPE: "installed", "dependencies": [("ludwig", "ludwig")], "raises_exception": False},
-    "multiple_installed": {
-        TYPE: "multiple_installed",
-        "dependencies": [("ludwig", "ludwig"), ("marshmallow", "marshmallow")],
-        "raises_exception": False,
-    },
-    "not_installed": {
-        TYPE: "not_installed",
-        "dependencies": [("fake_dependency", "fake_dependency")],
-        "raises_exception": True,
-    },
-    "mixed_installed": {
-        TYPE: "mixed_installed",
-        "dependencies": [("ludwig", "ludwig"), ("fake_dependency", "fake_dependency")],
-        "raises_exception": True,
-    },
-}
 
-
-@pytest.fixture(scope="module")
-def register_schedulers():
-    for name, values in CONFIG_DEPENDENCY_PARAMS.items():
-
-        @utils.register_scheduler_config(name, dependencies=values["dependencies"])
-        @ludwig_dataclass
-        class DependencyCheckConfig(BaseSchedulerConfig):
-            type: str = ProtectedString(name)
-
-    yield
-
-    for name in CONFIG_DEPENDENCY_PARAMS.keys():
-        del utils.scheduler_config_registry[name]
-        del utils.scheduler_dependencies_registry[name]
-
-
-@pytest.fixture(scope="module")
-def register_search_algorithms():
-    for name, values in CONFIG_DEPENDENCY_PARAMS.items():
-
-        @utils.register_search_algorithm_config(name, dependencies=values["dependencies"])
-        @ludwig_dataclass
-        class DependencyCheckConfig(BaseSearchAlgorithmConfig):
-            type: str = ProtectedString(name)
-
-    yield
-
-    for name in CONFIG_DEPENDENCY_PARAMS.keys():
-        del utils.search_algorithm_config_registry[name]
-        del utils.search_algorithm_random_state_field_registry[name]
-        del utils.search_algorithm_dependencies_registry[name]
-
-
-@pytest.mark.parametrize("name", CONFIG_DEPENDENCY_PARAMS.keys())
-def test_check_scheduler_dependencies_installed(name, register_schedulers):
-    scheduler_name = CONFIG_DEPENDENCY_PARAMS[name][TYPE]
-    raises_exception = CONFIG_DEPENDENCY_PARAMS[name]["raises_exception"]
-
+@pytest.mark.parametrize(
+    "dependencies,raises_exception",
+    [
+        ([], False),
+        ([("ludwig", "ludwig")], False),
+        ([("ludwig", "ludwig"), ("marshmallow", "marshmallow")], False),
+        ([("fake_dependency", "fake_dependency")], True),
+        ([("ludwig", "ludwig"), ("fake_dependency", "fake_dependency")], True),
+    ],
+)
+def test_check_scheduler_dependencies_installed(dependencies, raises_exception):
     config = {
         INPUT_FEATURES: [text_feature()],
         OUTPUT_FEATURES: [binary_feature()],
         HYPEROPT: {
             PARAMETERS: {"trainer.learning_rate": {"space": "choice", "categories": [0.0001, 0.001, 0.01, 0.1]}},
-            EXECUTOR: {SCHEDULER: {TYPE: scheduler_name}},
+            EXECUTOR: {SCHEDULER: {TYPE: "fifo"}},
         },
     }
 
-    if raises_exception:
-        with pytest.raises(ConfigValidationError):
+    with patch("ludwig.schema.hyperopt.utils.get_scheduler_dependencies", return_value=dependencies):
+        if raises_exception:
+            with pytest.raises(ConfigValidationError):
+                ModelConfig.from_dict(config)
+        else:
             ModelConfig.from_dict(config)
-    else:
-        ModelConfig.from_dict(config)
 
 
-@pytest.mark.parametrize("name", CONFIG_DEPENDENCY_PARAMS.keys())
-def test_check_search_algorithm_dependencies_installed(name, register_search_algorithms):
-    search_algorithm_name = CONFIG_DEPENDENCY_PARAMS[name][TYPE]
-    raises_exception = CONFIG_DEPENDENCY_PARAMS[name]["raises_exception"]
-
+@pytest.mark.parametrize(
+    "dependencies,raises_exception",
+    [
+        ([], False),
+        ([("ludwig", "ludwig")], False),
+        ([("ludwig", "ludwig"), ("marshmallow", "marshmallow")], False),
+        ([("fake_dependency", "fake_dependency")], True),
+        ([("ludwig", "ludwig"), ("fake_dependency", "fake_dependency")], True),
+    ],
+)
+def test_check_search_algorithm_dependencies_installed(dependencies, raises_exception):
     config = {
         INPUT_FEATURES: [text_feature()],
         OUTPUT_FEATURES: [binary_feature()],
         HYPEROPT: {
             PARAMETERS: {"trainer.learning_rate": {"space": "choice", "categories": [0.0001, 0.001, 0.01, 0.1]}},
-            SEARCH_ALG: {TYPE: search_algorithm_name},
+            SEARCH_ALG: {TYPE: "random"},
         },
     }
 
-    if raises_exception:
-        with pytest.raises(ConfigValidationError):
+    with patch("ludwig.schema.hyperopt.utils.get_search_algorithm_dependencies", return_value=dependencies):
+        if raises_exception:
+            with pytest.raises(ConfigValidationError):
+                ModelConfig.from_dict(config)
+        else:
             ModelConfig.from_dict(config)
-    else:
-        ModelConfig.from_dict(config)
 
 
 @pytest.mark.parametrize(
@@ -138,7 +99,6 @@ def test_parameter_type_check(space, raises_exception):
             PARAMETERS: {
                 "trainer.learning_rate": {
                     "space": space,
-                    # "categories": [0.0001, 0.001, 0.01, 0.1]
                 }
             },
         },
