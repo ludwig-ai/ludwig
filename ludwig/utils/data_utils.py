14a09eb26eab1d93afc0c25a602976afe9e56455
ksbrar <kabir@brar.org>
2023-03-28 21:03:16 +0000

    fix

--- ludwig/utils/data_utils.py
+++ ludwig/utils/data_utils.py
@@ -252,10 +252,11 @@ def read_excel(data_fp, df_lib, **kwargs):
 def read_parquet(data_fp, df_lib, **kwargs):
     if "nrows" in kwargs:
         import pyarrow.parquet as pq
-        import s3fs
+        from ludwig.utils.fs_utils import get_fs_and_path
 
-        filesystem = s3fs.S3FileSystem() if "s3:" in data_fp else None
-        dataset = pq.ParquetDataset(data_fp, filesystem=filesystem, use_legacy_dataset=False).fragments[0]
+        fs, _ = get_fs_and_path(data_fp)
+        dataset = pq.ParquetDataset(data_fp, filesystem=fs, use_legacy_dataset=False).fragments[0]
+
         preview = dataset.head(kwargs["nrows"]).to_pandas()
 
         if is_dask_lib(df_lib):
