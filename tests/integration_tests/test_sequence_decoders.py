0ef7ac1b70abf5ede8dd3aa7f8c324563fe89467
Jeff Kinnison <jeffkinnison@users.noreply.github.com>
2023-04-12 11:20:09 -0400

    Fix sequence decoder attribute error (#3336)

--- tests/integration_tests/test_sequence_decoders.py
+++ tests/integration_tests/test_sequence_decoders.py
@@ -2,37 +2,50 @@ import os
 
 import pytest
 
-from ludwig.constants import COLUMN, DECODER, DEFAULTS, INPUT_FEATURES, NAME, OUTPUT_FEATURES, TEXT, TYPE
+from ludwig.constants import (
+    COLUMN,
+    DECODER,
+    DEFAULTS,
+    ENCODER,
+    INPUT_FEATURES,
+    NAME,
+    OUTPUT_FEATURES,
+    SEQUENCE,
+    TEXT,
+    TYPE,
+)
 from tests.integration_tests.utils import (
     create_data_set_to_use,
     generate_data,
     RAY_BACKEND_CONFIG,
+    sequence_feature,
     text_feature,
     train_with_backend,
 )
 
 
-# TODO: Add sequence feature and additional decoders
-@pytest.mark.parametrize("feature_type,feature_gen", [(TEXT, text_feature)])
-@pytest.mark.parametrize("decoder_type", ["generator"])
+@pytest.mark.parametrize("feature_type,feature_gen", [(TEXT, text_feature), (SEQUENCE, sequence_feature)])
+@pytest.mark.parametrize("decoder_type", ["generator", "tagger"])
 @pytest.mark.parametrize("use_defaults", [True, False])
 @pytest.mark.distributed
 def test_sequence_decoder_predictions(
     tmpdir, csv_filename, ray_cluster_2cpu, feature_type, feature_gen, use_defaults, decoder_type
 ):
     """Test that sequence decoders return the correct successfully predict."""
-    input_features = [feature_gen()]
+    input_feature = feature_gen()
     output_feature = feature_gen(output_feature=True)
+
+    input_feature[ENCODER] = {TYPE: "parallel_cnn", "reduce_output": None}
     output_feature[DECODER] = {TYPE: decoder_type}
 
     dataset_path = generate_data(
-        input_features=input_features,
+        input_features=[input_feature],
         output_features=[output_feature],
         filename=os.path.join(tmpdir, csv_filename),
     )
     dataset_path = create_data_set_to_use("csv", dataset_path)
 
-    config = {INPUT_FEATURES: input_features}
+    config = {INPUT_FEATURES: [input_feature]}
 
     # Ensure that the decoder outputs the correct predictions through both the default and feature-specific configs.
     if use_defaults:
