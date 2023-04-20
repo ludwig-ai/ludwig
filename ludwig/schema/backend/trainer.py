bb0bfe5d8a103db969a0c0ccd77ce073e73f58e3
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-04-20 11:52:57 -0400

    add trainer dataclass field

--- ludwig/schema/backend/trainer.py
+++ ludwig/schema/backend/trainer.py
@@ -1,3 +1,8 @@
+from dataclasses import field
+from typing import Dict
+
+from marshmallow import fields, ValidationError
+
 from ludwig.api_annotations import DeveloperAPI
 from ludwig.schema import utils as schema_utils
 from ludwig.schema.utils import ludwig_dataclass
@@ -5,7 +10,7 @@ from ludwig.schema.utils import ludwig_dataclass
 
 @DeveloperAPI
 @ludwig_dataclass
-class TrainerConfig(schema_utils.BaseMarshmallowConfig):
+class BaseBackendTrainerConfig(schema_utils.BaseMarshmallowConfig):
     """Configuration for distributed training (only supported by the `ray` backend)."""
 
     type: str = schema_utils.StringOptions(
@@ -37,4 +42,41 @@ class TrainerConfig(schema_utils.BaseMarshmallowConfig):
 
     max_retries: int = schema_utils.NonNegativeInteger(
         default=3, description="Number of retries when Ray actors fail (defaults to 3)."
+    )
+
+
+@DeveloperAPI
+def BackendTrainerDataclassField(description: str = "", default: Dict = {}):
+    class TrainerMarshmallowField(fields.Field):
+        def _deserialize(self, value, attr, data, **kwargs):
+            if isinstance(value, dict):
+                try:
+                    return BaseBackendTrainerConfig.Schema().load(value)
+                except (TypeError, ValidationError):
+                    raise ValidationError(f"Invalid params for trainer: {value}, see TrainerConfig class.")
+            raise ValidationError("Field should be dict")
+
+        def _jsonschema_type_mapping(self):
+            return {
+                "type": "object",
+                "title": "trainer_options",
+                "description": description,
+            }
+
+    if not isinstance(default, dict):
+        raise ValidationError(f"Invalid default: `{default}`")
+
+    load_default = lambda: BaseBackendTrainerConfig.Schema().load(default)
+    dump_default = BaseBackendTrainerConfig.Schema().dump(default)
+
+    return field(
+        metadata={
+            "marshmallow_field": TrainerMarshmallowField(
+                allow_none=False,
+                load_default=load_default,
+                dump_default=dump_default,
+                metadata={"description": description, "parameter_metadata": None},
+            )
+        },
+        default_factory=load_default,
     )
