5091a7a4a209dd0403684d49e844d298f1c51952
Jeff Kinnison <jeffery.kinnison@predibase.com>
2023-04-20 11:46:54 -0400

    add loader dataclass field

--- ludwig/schema/backend/loader.py
+++ ludwig/schema/backend/loader.py
@@ -1,4 +1,7 @@
-from typing import Union
+from dataclasses import field
+from typing import Dict, Union
+
+from marshmallow import fields, ValidationError
 
 from ludwig.api_annotations import DeveloperAPI
 from ludwig.schema import utils as schema_utils
@@ -7,7 +10,7 @@ from ludwig.schema.utils import ludwig_dataclass
 
 @DeveloperAPI
 @ludwig_dataclass
-class LoaderConfig(schema_utils.BaseMarshmallowConfig):
+class BaseLoaderConfig(schema_utils.BaseMarshmallowConfig):
     """Configuration for the "last mile" data ingest from processed data to tensor batches used for training the
     model."""
 
@@ -21,7 +24,10 @@ class LoaderConfig(schema_utils.BaseMarshmallowConfig):
 
     window_size_bytes: Union[int, str] = schema_utils.OneOfOptionsField(
         default=None,
-        field_options=[schema_utils.PositiveInteger(), schema_utils.String(default="auto")],
+        field_options=[
+            schema_utils.PositiveInteger("Dataset window size in bytes.", default=None),
+            schema_utils.String(default="auto"),
+        ],
         allow_none=True,
         description="Load and shuffle the preprocessed dataset in discrete windows of this size (defaults to null, "
         "meaning data will not be windowed). Try configuring this is if shuffling is taking a very long "
@@ -29,4 +35,41 @@ class LoaderConfig(schema_utils.BaseMarshmallowConfig):
         "window sizes result in more uniform shuffling (which can lead to better model performance in "
         "some cases), while smaller window sizes will be faster to load. This setting is particularly "
         "useful when running hyperopt over a large dataset.",
+    )
+
+
+@DeveloperAPI
+def LoaderDataclassField(description: str = "", default: Dict = {}):
+    class LoaderMarshmallowField(fields.Field):
+        def _deserialize(self, value, attr, data, **kwargs):
+            if isinstance(value, dict):
+                try:
+                    return BaseLoaderConfig.Schema().load(value)
+                except (TypeError, ValidationError):
+                    raise ValidationError(f"Invalid params for loader: {value}, see LoaderConfig class.")
+            raise ValidationError("Field should be dict")
+
+        def _jsonschema_type_mapping(self):
+            return {
+                "type": "object",
+                "title": "loader_options",
+                "description": description,
+            }
+
+    if not isinstance(default, dict):
+        raise ValidationError(f"Invalid default: `{default}`")
+
+    load_default = lambda: BaseLoaderConfig.Schema().load(default)
+    dump_default = BaseLoaderConfig.Schema().dump(default)
+
+    return field(
+        metadata={
+            "marshmallow_field": LoaderMarshmallowField(
+                allow_none=False,
+                load_default=load_default,
+                dump_default=dump_default,
+                metadata={"description": description, "parameter_metadata": None},
+            )
+        },
+        default_factory=load_default,
     )
