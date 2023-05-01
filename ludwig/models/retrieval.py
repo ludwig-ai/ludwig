31fe004918e7e6fd98e0d5ed52b660a5daad8a2e
Geoffrey Angus <geoffrey@predibase.com>
2023-05-01 10:47:28 -0600

    added simple example

--- ludwig/models/retrieval.py
+++ ludwig/models/retrieval.py
@@ -110,7 +110,7 @@ class SemanticRetrieval(RetrievalModel):
     """
 
     def __init__(self, model_name, **kwargs):
-        self.model_name = self.model_name
+        self.model_name = model_name
         self.model = get_semantic_retrieval_model(self.model_name)
         self.index: faiss.Index = None
         self.index_data: pd.DataFrame = None
