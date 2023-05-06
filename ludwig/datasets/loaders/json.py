4cef8cb5a10acd3c3b721090d588f2bd63cc9bf7
Travis Addair <tgaddair@gmail.com>
2023-05-06 14:40:06 -0700

    Fixed alpaca

--- ludwig/datasets/loaders/json.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-from ludwig.datasets.loaders.dataset_loader import DatasetLoader
-
-
-class JsonLoader(DatasetLoader):
-    def load_file_to_dataframe(self, file_path: str) -> pd.DataFrame:
-        return pd.read_json(file_path)
