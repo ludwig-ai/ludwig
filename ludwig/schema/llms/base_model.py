567e4736b104a00274c4bfdb7bfac2533f6568f4
Travis Addair <tgaddair@gmail.com>
2023-05-25 20:39:26 -0700

    Use remove_fields

--- ludwig/schema/llms/base_model.py
+++ ludwig/schema/llms/base_model.py
@@ -95,11 +95,7 @@ def get_base_model_conds():
     conds = []
     for base_model_type, base_model_cls in base_model_registry.items():
         other_props = schema_utils.unload_jsonschema_from_marshmallow_class(base_model_cls)["properties"]
-        if base_model_type == "none":
-            del other_props["preset"]
-        else:
-            del other_props["preset"]
-            del other_props["name"]
+        schema_utils.remove_duplicate_fields(other_props, fields=["preset"])
         preproc_cond = schema_utils.create_cond(
             {"preset": base_model_type},
             other_props,
