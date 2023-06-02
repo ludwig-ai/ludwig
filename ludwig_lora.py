b272ea0740f216cb8cc50e38f07c82be61facb1a
Arnav Garg <arnav@predibase.com>
2023-06-02 05:52:51 +0000

    Add comparison script

---
+++ ludwig_lora.py
@@ -0,0 +1,73 @@
+import logging
+import os
+
+import pandas as pd
+import yaml
+
+from ludwig.api import LudwigModel
+
+df = pd.read_csv("peft_twitter.csv")
+
+config = yaml.safe_load(
+    """
+        model_type: llm
+        model_name: hf-internal-testing/tiny-random-GPTJForCausalLM
+        adapter:
+            type: lora
+            r: 8
+            alpha: 16
+            dropout: 0.05
+        input_features:
+            - name: Tweet text
+              type: text
+              preprocessing:
+                max_sequence_length: 64
+        output_features:
+            - name: Label
+              type: text
+        prompt:
+            task: >-
+                Label :
+            template: >-
+                Tweet text : {__sample__} {__task__}
+        preprocessing:
+            split:
+                type: fixed
+                column: split
+        trainer:
+            type: finetune
+            batch_size: 8
+            learning_rate: 0.03
+            epochs: 10
+            early_stop: -1
+            optimizer:
+                type: adamw
+            regularization_type: null
+            should_shuffle: false
+            increase_batch_size_on_plateau: 0
+            gradient_clipping:
+                clipglobalnorm: 1.0
+            learning_rate_scheduler:
+                decay: null
+        backend:
+            type: local
+    """
+)
+
+model = LudwigModel(config=config, logging_level=logging.INFO)
+
+# initiate model training
+(
+    train_stats,  # dictionary containing training statistics
+    preprocessed_data,  # tuple Ludwig Dataset objects of pre-processed training data
+    output_directory,  # location of training results stored on disk
+) = model.train(
+    dataset=df,
+    experiment_name="ludwig_peft",
+    model_name="ludwig_peft",
+)
+
+# list contents of output directory
+print("contents of output directory:", output_directory)
+for item in os.listdir(output_directory):
+    print("\t", item)
