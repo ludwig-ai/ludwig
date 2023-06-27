72f7e15f04e64b735ca5e252978f0dc354056141
Kabir Brar <kabir@brar.org>
2023-06-26 22:16:14 -0400

    int: Add back `required` for input and output features to the Ludwig JSON schema (#3442)

--- ludwig/config_validation/validation.py
+++ ludwig/config_validation/validation.py
@@ -28,11 +28,17 @@ def get_schema(model_type: str = MODEL_ECD):
 
     cls = model_type_schema_registry[model_type]
     props = unload_jsonschema_from_marshmallow_class(cls)["properties"]
+
+    # TODO: Replace with more robust required logic later.
+    required = ["input_features", "output_features"]
+
     return {
         "type": "object",
         "properties": props,
         "title": "model_options",
         "description": "Settings for Ludwig configuration",
+        "required": required,
+        "additionalProperties": True,  # TODO: Set to false after 0.8 releases.
     }
 
 
