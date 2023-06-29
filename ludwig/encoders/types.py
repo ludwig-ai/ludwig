90f742e12af500bcf62bc2b396a75ba1d01d056f
Dennis Rall <dennis.rall@wogra.com>
2023-06-29 09:50:58 +0200

    fix: remove Required hint from EncoderOutputDict

--- ludwig/encoders/types.py
+++ ludwig/encoders/types.py
@@ -1,9 +1,9 @@
-from typing import Required, TypedDict
+from typing import TypedDict
 
 import torch
 
 
 class EncoderOutputDict(TypedDict, total=False):
-    encoder_output: Required[torch.Tensor]
+    encoder_output: torch.Tensor
     encoder_output_state: torch.Tensor
     attentions: torch.Tensor
