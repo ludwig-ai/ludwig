f600e764647135a07ce02df3e71af92862d37300
Dennis Rall <dennis.rall@wogra.com>
2023-07-06 10:04:50 +0200

    refactor: add comments to EncoderOutputDict

--- ludwig/encoders/types.py
+++ ludwig/encoders/types.py
@@ -5,5 +5,5 @@ import torch
 
 class EncoderOutputDict(TypedDict, total=False):
     encoder_output: torch.Tensor
-    encoder_output_state: torch.Tensor
-    attentions: torch.Tensor
+    encoder_output_state: torch.Tensor  # only used by sequence and h3 encoders
+    attentions: torch.Tensor  # only used by the vit legacy encoder
