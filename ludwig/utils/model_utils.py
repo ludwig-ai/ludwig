31b00745770db0654407933035cf0d52f343a0f6
Arnav Garg <arnav@predibase.com>
2023-07-21 18:02:39 +0000

    Coerce dtypes

--- ludwig/utils/model_utils.py
+++ ludwig/utils/model_utils.py
@@ -1,6 +1,23 @@
+from collections import OrderedDict
 from typing import Dict, List, Tuple
 
+import numpy as np
 import torch
+
+NUMPY_TO_TORCH_DTYPE = {
+    bool: torch.bool,
+    np.bool_: torch.bool,
+    np.uint8: torch.uint8,
+    np.int8: torch.int8,
+    np.int16: torch.int16,
+    np.int32: torch.int32,
+    np.int64: torch.int64,
+    np.float16: torch.float16,
+    np.float32: torch.float32,
+    np.float64: torch.float64,
+    np.complex64: torch.complex64,
+    np.complex128: torch.complex128,
+}
 
 
 def extract_tensors(model: torch.nn.Module) -> Tuple[torch.nn.Module, List[Dict]]:
@@ -16,8 +33,8 @@ def extract_tensors(model: torch.nn.Module) -> Tuple[torch.nn.Module, List[Dict]
         # Store the tensors as numpy arrays in Python dictionaries
         # Delete the same tensors since we no longer need them and we want to reduce memory pressure.
         # This ensures that throughout this process, we keep memory nearly linear w.r.t model parameters.
-        params = {}
-        buffers = {}
+        params = OrderedDict()
+        buffers = OrderedDict()
         for name, param in module.named_parameters(recurse=False):
             params[name] = torch.clone(param).detach().numpy()
             del param
@@ -47,7 +64,13 @@ def replace_tensors(m: torch.nn.Module, tensors: List[Dict], device: torch.devic
     for module, tensor_dict in zip(modules, tensors):
         # There are separate APIs to set parameters and buffers.
         for name, array in tensor_dict["params"].items():
-            module.register_parameter(name, torch.nn.Parameter(torch.as_tensor(array, device=device)))
+            module.register_parameter(
+                name,
+                torch.nn.Parameter(torch.as_tensor(array, device=device, dtype=NUMPY_TO_TORCH_DTYPE.get(array.dtype))),
+            )
 
         for name, array in tensor_dict["buffers"].items():
-            module.register_buffer(name, torch.as_tensor(array, device=device))
+            module.register_buffer(
+                name,
+                torch.as_tensor(array, device=device, dtype=NUMPY_TO_TORCH_DTYPE.get(array.dtype)),
+            )
