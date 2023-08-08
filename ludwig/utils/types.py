c8321039225212452d3fe97fe995d9f41adb7f86
Sumanth Datta Nanduri <sumanthnanduri@gmail.com>
2023-08-08 21:31:27 +0530

    Update types.py

--- ludwig/utils/types.py
+++ ludwig/utils/types.py
@@ -2,12 +2,15 @@ from typing import List, Tuple, Union
 
 import pandas as pd
 import torch
+import ray
+import ray.data.dataset as ray_dataset
 
 try:
     import dask.dataframe as dd
 
     DataFrame = Union[pd.DataFrame, dd.DataFrame]
     Series = Union[pd.Series, dd.Series]
+    RayDatasetType = Union[ray_dataset]
 except ImportError:
     DataFrame = pd.DataFrame
     Series = pd.Series
