cbc25cd0ff32c583df516ede22e4574031c4bf9b
Travis Addair <tgaddair@gmail.com>
2023-08-20 21:03:52 -0700

    Fixed signature

--- ludwig/backend/ray.py
+++ ludwig/backend/ray.py
@@ -1147,7 +1147,7 @@ class RayBackend(RemoteTrainingMixin, Backend):
 
 
 @ray.remote(max_calls=1)
-def _tune_batch_size_fn(evaluator_cls: Type[BatchSizeEvaluator], dataset_len: int, tune_for_training: bool) -> int:
+def _tune_batch_size_fn(evaluator_cls: Type[BatchSizeEvaluator], dataset_len: int) -> int:
     evaluator = evaluator_cls()
     return evaluator.select_best_batch_size(dataset_len)
 
