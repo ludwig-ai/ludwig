df434f058d4e9ca2348cd17ab9f6fc2d443a42d3
Justin Zhao <justinxzhao@gmail.com>
2023-09-11 23:34:54 +0000

    Undo non-change.

--- tests/ludwig/models/test_training_determinism.py
+++ tests/ludwig/models/test_training_determinism.py
@@ -25,8 +25,8 @@ from tests.integration_tests.utils import (
 )
 
 
-@pytest.mark.skip(reason="https://github.com/ludwig-ai/ludwig/issues/2686")
 @pytest.mark.distributed
+@pytest.mark.skip(reason="https://github.com/ludwig-ai/ludwig/issues/2686")
 def test_training_determinism_ray_backend(csv_filename, tmpdir, ray_cluster_4cpu):
     experiment_output_1, experiment_output_2 = train_twice("ray", csv_filename, tmpdir)
 
