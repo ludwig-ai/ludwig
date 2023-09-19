b607629aa67cad68c342b06fdc135ece077fdd23
Justin Zhao <justinxzhao@gmail.com>
2023-09-19 02:25:58 +0000

    Update ludwig version to 0.8.4.

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.8.3"
+LUDWIG_VERSION = "0.8.4"
 
 MODEL_WEIGHTS_FILE_NAME = "model_weights"
 MODEL_HYPERPARAMETERS_FILE_NAME = "model_hyperparameters.json"
