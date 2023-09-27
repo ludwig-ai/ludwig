071b7b8feb16011cdd138f1ad03a9ce48fbb969d
Infernaught <89timw@gmail.com>
2023-09-27 13:54:23 -0400

    Move hf loading into a helper function

--- ludwig/data/preprocessing.py
+++ ludwig/data/preprocessing.py
@@ -61,7 +61,7 @@ from ludwig.data.dataset.base import Dataset
 from ludwig.data.prompt import format_input_with_prompt, index_column
 from ludwig.data.split import get_splitter, split_dataset
 from ludwig.data.utils import get_input_and_output_features, set_fixed_split
-from ludwig.datasets import load_dataset_uris, load_hf_datasets
+from ludwig.datasets import load_dataset_uris
 from ludwig.features.feature_registries import get_base_type_registry
 from ludwig.models.embedder import create_embed_batch_size_evaluator, create_embed_transform_fn
 from ludwig.schema.encoders.utils import get_encoder_cls
@@ -1885,13 +1885,8 @@ def preprocess_for_training(
     if dataset is None and training_set is None:
         raise ValueError("No training data is provided!")
 
-    # preload ludwig datasets
+    # preload ludwig and HF datasets
     dataset, training_set, validation_set, test_set = load_dataset_uris(
-        dataset, training_set, validation_set, test_set, backend
-    )
-
-    # preload HF datasets
-    dataset, training_set, validation_set, test_set = load_hf_datasets(
         dataset, training_set, validation_set, test_set, backend
     )
 
@@ -2244,7 +2239,7 @@ def preprocess_for_prediction(
     if isinstance(dataset, Dataset):
         return dataset, training_set_metadata
 
-    # preload ludwig datasets
+    # preload ludwig and HF datasets
     dataset, _, _, _ = load_dataset_uris(dataset, None, None, None, backend)
 
     # determine data format if not provided or auto
