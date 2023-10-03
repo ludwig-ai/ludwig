6e9e2edffc46e33ea84a285fecea202498140755
Infernaught <89timw@gmail.com>
2023-10-03 12:41:31 -0400

    Clarify code by refactoring and shortening

--- ludwig/datasets/loaders/hugging_face.py
+++ ludwig/datasets/loaders/hugging_face.py
@@ -13,54 +13,46 @@
 # limitations under the License.
 # ==============================================================================
 import logging
+from typing import Dict
 
 import datasets
 import pandas as pd
 
+from ludwig.constants import TEST, TRAIN, VALIDATION
 from ludwig.datasets.loaders.dataset_loader import DatasetLoader
 
-TRAIN = "train"
-VALIDATION = "validation"
-TEST = "test"
-splits = [TRAIN, VALIDATION, TEST]
+SPLITS = [TRAIN, VALIDATION, TEST]
+logger = logging.getLogger(__name__)
 
 
 class HFLoader(DatasetLoader):
-    def load_hf_to_dict(self, hf_id: str, hf_subsample: str) -> dict:
-        # Convert from HF DatasetDict type to dict
+    def load_hf_to_dict(self, hf_id: str, hf_subsample: str) -> Dict[str, pd.DataFrame]:
+        # Convert from HF DatasetDict type to a dictionary of pandas dataframes
         dataset_dict = datasets.load_dataset(path=hf_id, name=hf_subsample)
         new_dict = {}
         for split in dataset_dict:
             new_dict[split] = dataset_dict[split].to_pandas()
         return new_dict
 
-    def load(self, split=False, kaggle_username=None, kaggle_key=None) -> pd.DataFrame:
-        logger = logging.getLogger(__name__)
+    def load(self, hf_id, hf_subsample, split=False) -> pd.DataFrame:
+        self.config.huggingface_dataset_id = hf_id
+        self.config.huggingface_subsample = hf_subsample
         dataset_dict = self.load_hf_to_dict(
-            hf_id=self.config.huggingface_dataset_id,
-            hf_subsample=self.config.huggingface_subsample,
+            hf_id=hf_id,
+            hf_subsample=hf_subsample,
         )
         if split:  # For each split, either return the appropriate dataframe or an empty dataframe
-            if TRAIN in dataset_dict:
-                train_df = dataset_dict[TRAIN]
-            else:
-                logger.warning("No training set found in provided Hugging Face dataset. Skipping training set.")
-                train_df = pd.DataFrame()
-            if VALIDATION in dataset_dict:
-                validation_df = dataset_dict[VALIDATION]
-            else:
-                logger.warning("No validation set found in provided Hugging Face dataset. Skipping validation set.")
-                validation_df = pd.DataFrame()
-            if TEST in dataset_dict:
-                test_df = dataset_dict[TEST]
-            else:
-                logger.warning("No test set found in provided Hugging Face dataset. Skipping test set.")
-                test_df = pd.DataFrame()
+            for spl in SPLITS:
+                if spl not in dataset_dict:
+                    logger.warning(f"No {spl} set found in provided Hugging Face dataset. Skipping {spl} set.")
+            train_df = dataset_dict[TRAIN] if TRAIN in dataset_dict else pd.DataFrame()
+            validation_df = dataset_dict[VALIDATION] if VALIDATION in dataset_dict else pd.DataFrame()
+            test_df = dataset_dict[TEST] if TEST in dataset_dict else pd.DataFrame()
 
             return train_df, validation_df, test_df
         else:
             dataset_list = []
-            for split in dataset_dict:
-                dataset_dict[split]["split"] = splits.index(split)
-                dataset_list.append(dataset_dict[split])
+            for spl in dataset_dict:
+                dataset_dict[spl]["split"] = SPLITS.index(spl)
+                dataset_list.append(dataset_dict[spl])
             return pd.concat(dataset_list)
