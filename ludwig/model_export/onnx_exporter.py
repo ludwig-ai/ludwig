d7fd119d456330994e5fdb7f5d412e922a8feeed
ethanreidel <ethanjreidel@gmail.com>
2023-12-11 19:06:34 -0800

    initial onnx quantization implementation

--- ludwig/model_export/onnx_exporter.py
+++ ludwig/model_export/onnx_exporter.py
@@ -7,7 +7,7 @@ from ludwig.api import LudwigModel
 from ludwig.model_export.base_model_exporter import BaseModelExporter, LudwigTorchWrapper
 
 
-# Copyright (c) 2023 Predibase, Inc., 2019 Uber Technologies, Inc.
+# Copyright (c) 2019 Uber Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,28 +24,46 @@ from ludwig.model_export.base_model_exporter import BaseModelExporter, LudwigTor
 class OnnxExporter(BaseModelExporter):
     """Class that abstracts the convertion of torch to onnx."""
 
-    def export(self, model_path, export_path, output_model_name):
+    def export(self, model_path, export_path, output_model_name, quantization: bool):
         ludwig_model = LudwigModel.load(model_path)
-        model = LudwigTorchWrapper(ludwig_model.model)  # Wrap the model
+        if quantization:
+            quantized_ludwig_model = self.quantize_model(ludwig_model)
+            model = LudwigTorchWrapper(quantized_ludwig_model.model)
+        else:
+            model = LudwigTorchWrapper(ludwig_model.model)
         model.eval()  # inference mode, is this needed.. I think onnx export does this for us
+
+        width = ludwig_model.config["input_features"][0]["preprocessing"]["width"]
+        height = ludwig_model.config["input_features"][0]["preprocessing"]["height"]
+        example_input = torch.randn(1, 3, width, height, requires_grad=True)
 
         torch.onnx.export(
             model,
-            # example_input,
+            example_input,
             os.path.join(export_path, output_model_name),
             opset_version=18,
             export_params=True,
             do_constant_folding=True,
             input_names=["input"],
-            output_names=["output"],
-            # output_names=["combiner_hidden_1", "output", "combiner_hidden_2"],
+            output_names=["combiner_hidden_1", "output", "combiner_hidden_2"],
         )
 
     def check_model_export(self, path):
         onnx_model = onnx.load(path)
         onnx.checker.check_model(onnx_model)
-        # width = ludwig_model.config["input_features"][0]["preprocessing"]["width"]
-        # height = ludwig_model.config["input_features"][0]["preprocessing"]["height"]
-        # example_input = torch.randn(1, 3, width, height, requires_grad=True)
-        # example_input = torch.randn(1, 1, 256, 256, requires_grad=True)
-        # example_input = torch.randn(1, 3, 224, 224)
+
+    def quantize_model(self, model, datatype=torch.qint8) -> LudwigModel:
+        """Converts float32 weighted model into qint8 weighted model. Utilizes dynamic quantization.
+
+        Args:
+            model: input Ludwig model
+            datatype: datatype to convert to
+                -Default is 'qint8.'
+        Returns:
+            quantized model
+        Notes:
+            Second parameter of quantize_dynamic specifies specific layers to be quantized.
+            If you want all layers to be quantized, comment that line out.
+        """
+        quantized_model = torch.quantization.quantize_dynamic(model, {torch.nn.Conv2d, torch.nn.Linear}, dtype=datatype)
+        return quantized_model
