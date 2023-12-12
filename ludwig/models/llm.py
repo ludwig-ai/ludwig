e4e599ddc5e4a17f5e4932eb15e487c468ee52fa
Arnav Garg <arnav@predibase.com>
2023-12-12 23:53:20 +0530

    Add comment to explain why we can't delete the key

--- ludwig/models/llm.py
+++ ludwig/models/llm.py
@@ -632,6 +632,9 @@ class LLM(BaseModel):
         logger.info("Done.")
 
         # Remove the quantization configuration from the model
+        # The reason we can't delete the quantization config is because it is a property of the model and
+        # HF does some weird serialization of the config that causes an error when trying to access `self.model.config`
+        # after you try and delete a key from the config: TypeError: Object of type dtype is not JSON serializable.
         self.model.config.quantization_config = {}
 
         # Override properties of the model to indicate that it is no longer quantized.
