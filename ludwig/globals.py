d30b06545039fb64dba8a55245a305a64e74de33
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2024-01-16 20:47:56 -0800

    Get trusted publishers approach for PyPi upload from 0.9.2 release branch.

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.9.2"
+LUDWIG_VERSION = "0.9.dev"
 
 MODEL_WEIGHTS_FILE_NAME = "model_weights"
 MODEL_HYPERPARAMETERS_FILE_NAME = "model_hyperparameters.json"
