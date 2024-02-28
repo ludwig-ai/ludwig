867d699a4f1afd0a320fa259ad8dc785f8b83aa3
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2024-02-28 11:09:07 -0800

    [MAINTENANCE] Update Ludwig development version. (#3947)

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.10.dev"
+LUDWIG_VERSION = "0.10.1.dev"
 
 MODEL_WEIGHTS_FILE_NAME = "model_weights"
 MODEL_HYPERPARAMETERS_FILE_NAME = "model_hyperparameters.json"
