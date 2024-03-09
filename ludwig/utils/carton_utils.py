c6ea057951011bf816da9e0f0b63fedde722a260
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2024-03-09 15:29:48 -0800

    Troubleshooting test failures.

--- ludwig/utils/carton_utils.py
+++ ludwig/utils/carton_utils.py
@@ -132,20 +132,28 @@ def export_carton(model: LudwigModel, carton_path: str, carton_model_name="ludwi
         # TODO: <Alex>ALEX</Alex>
         async def packster() -> str:
             time.sleep(1)
-            a: str = await carton.pack(
-                input_model_path,
-                runner_name="torchscript",
-                # Any 2.x.x version is okay
-                # TODO: improve this
-                required_framework_version="=2",
-                model_name=carton_model_name,
-                inputs=_get_input_spec(model),
-                outputs=_get_output_spec(model),
-            )
-            time.sleep(1)
-            print(f"\n[ALEX_TEST] [WOUTPUT] WOUTPUT:\n{a} ; TYPE: {str(type(a))}")
-            time.sleep(1)
-            return a
+            try:
+                a: str = await carton.pack(
+                    input_model_path,
+                    runner_name="torchscript",
+                    # Any 2.x.x version is okay
+                    # TODO: improve this
+                    required_framework_version="=2",
+                    model_name=carton_model_name,
+                    inputs=_get_input_spec(model),
+                    outputs=_get_output_spec(model),
+                )
+                time.sleep(1)
+                print(f"\n[ALEX_TEST] [WOUTPUT] WOUTPUT:\n{a} ; TYPE: {str(type(a))}")
+                time.sleep(1)
+                return a
+            except Exception as ie:
+                exception_message: str = "A Packster-Inside Exception occurred.\n"
+                exception_traceback: str = traceback.format_exc()
+                exception_message += f'{type(ie).__name__}: "{str(ie)}".  Traceback: "{exception_traceback}".'
+                sys.stderr.write(exception_message)
+                sys.stderr.flush()
+                raise ValueError(exception_message) from ie
 
         # TODO: <Alex>ALEX</Alex>
 
