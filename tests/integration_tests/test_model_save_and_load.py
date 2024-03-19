d242b5ec51a07683b627a895539cffa4206823d1
Geoffrey Angus <gdlangus@gmail.com>
2024-03-19 14:25:07 -0700

    add test

--- tests/integration_tests/test_model_save_and_load.py
+++ tests/integration_tests/test_model_save_and_load.py
@@ -32,6 +32,79 @@ from tests.integration_tests.utils import (
 )
 
 
+def test_model_load_from_checkpoint(tmpdir, csv_filename, tmp_path):
+    torch.manual_seed(1)
+    random.seed(1)
+    np.random.seed(1)
+
+    input_features = [
+        binary_feature(),
+        number_feature(),
+    ]
+
+    output_features = [
+        binary_feature(),
+    ]
+
+    data_csv_path = generate_data(input_features, output_features, csv_filename, num_examples=50)
+
+    config = {
+        "input_features": input_features,
+        "output_features": output_features,
+        TRAINER: {"epochs": 1, BATCH_SIZE: 2},
+    }
+    backend = LocalTestBackend()
+
+    # create sub-directory to store results
+    results_dir = tmp_path / "results"
+    results_dir.mkdir()
+
+    data_df = read_csv(data_csv_path)
+    splitter = get_splitter("random")
+    training_set, validation_set, test_set = splitter.split(data_df, backend)
+    ludwig_model1 = LudwigModel(config, backend=backend)
+    _, _, output_dir = ludwig_model1.train(
+        training_set=training_set,
+        validation_set=validation_set,
+        test_set=test_set,
+        output_directory="results",  # results_dir
+    )
+
+    model_dir = os.path.join(output_dir, "model")
+    ludwig_model_loaded = LudwigModel.load(model_dir, backend=backend, from_checkpoint=True)
+    preds_1, _ = ludwig_model1.predict(dataset=validation_set)
+
+    def check_model_equal(ludwig_model2):
+        # Compare model predictions
+        preds_2, _ = ludwig_model2.predict(dataset=validation_set)
+        assert set(preds_1.keys()) == set(preds_2.keys())
+        for key in preds_1:
+            assert preds_1[key].dtype == preds_2[key].dtype, key
+            assert np.all(a == b for a, b in zip(preds_1[key], preds_2[key])), key
+            # assert preds_2[key].dtype == preds_3[key].dtype, key
+            # assert list(preds_2[key]) == list(preds_3[key]), key
+
+        # Compare model weights
+        for if_name in ludwig_model1.model.input_features:
+            if1 = ludwig_model1.model.input_features.get(if_name)
+            if2 = ludwig_model2.model.input_features.get(if_name)
+            for if1_w, if2_w in zip(if1.encoder_obj.parameters(), if2.encoder_obj.parameters()):
+                assert torch.allclose(if1_w, if2_w)
+
+        c1 = ludwig_model1.model.combiner
+        c2 = ludwig_model2.model.combiner
+        for c1_w, c2_w in zip(c1.parameters(), c2.parameters()):
+            assert torch.allclose(c1_w, c2_w)
+
+        for of_name in ludwig_model1.model.output_features:
+            of1 = ludwig_model1.model.output_features.get(of_name)
+            of2 = ludwig_model2.model.output_features.get(of_name)
+            for of1_w, of2_w in zip(of1.decoder_obj.parameters(), of2.decoder_obj.parameters()):
+                assert torch.allclose(of1_w, of2_w)
+
+    check_model_equal(ludwig_model_loaded)
+
+
 def test_model_save_reload_api(tmpdir, csv_filename, tmp_path):
     torch.manual_seed(1)
     random.seed(1)
