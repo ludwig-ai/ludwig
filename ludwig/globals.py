e39e7d0060002dd8174251eb51e30fc5bc1bf4cf
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2024-03-21 13:08:09 -0700

    Update ludwig version to v0.10.2

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.10.1.dev"
+LUDWIG_VERSION = "0.10.2"
 
 MODEL_WEIGHTS_FILE_NAME = "model_weights"
 MODEL_HYPERPARAMETERS_FILE_NAME = "model_hyperparameters.json"
