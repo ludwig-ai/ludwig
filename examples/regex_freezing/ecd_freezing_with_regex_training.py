66fb3dec339dead3243e3254312965ef5bcf9b9c
ethanreidel <ethanjreidel@gmail.com>
2024-05-17 23:57:37 -0700

    spaces fix

--- examples/regex_freezing/ecd_freezing_with_regex_training.py
+++ examples/regex_freezing/ecd_freezing_with_regex_training.py
@@ -7,15 +7,12 @@ from datasets import load_dataset
 from ludwig.api import LudwigModel
 
 dataset = load_dataset("beans")
-
-
 train_df = pd.DataFrame(
     {"image_path": [f"train_{i}.jpg" for i in range(len(dataset["train"]))], "label": dataset["train"]["labels"]}
 )
 test_df = pd.DataFrame(
     {"image_path": [f"test_{i}.jpg" for i in range(len(dataset["test"]))], "label": dataset["test"]["labels"]}
 )
-
 
 os.makedirs("train_images", exist_ok=True)
 os.makedirs("test_images", exist_ok=True)
@@ -25,14 +22,11 @@ for i, img in enumerate(dataset["train"]["image"]):
 for i, img in enumerate(dataset["test"]["image"]):
     img.save(f"test_images/test_{i}.jpg")
 
-
 train_df["image_path"] = train_df["image_path"].apply(lambda x: os.path.join("train_images", x))
 test_df["image_path"] = test_df["image_path"].apply(lambda x: os.path.join("test_images", x))
 
-
 train_df.to_csv("beans_train.csv", index=False)
 test_df.to_csv("beans_test.csv", index=False)
-
 
 config = {
     "input_features": [
@@ -46,15 +40,9 @@ config = {
     "trainer": {"epochs": 1, "batch_size": 5, "layers_to_freeze_regex": r"(layer1\.0\.*|layer2\.0\.*)"},
 }
 
-
 model = LudwigModel(config, logging_level=logging.INFO)
-
-
 train_stats = model.train(dataset="beans_train.csv")
-
-
 model.save("beans_model")
-
 eval_stats, predictions, output_directory = model.evaluate(dataset="beans_test.csv")
 
 
