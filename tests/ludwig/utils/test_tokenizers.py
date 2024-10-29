01f308ef523d40dc668c68bc1bfc364463114df2
m.habedank <m.habedank@cartelsol.com>
2024-10-29 22:46:55 +0100

    Rewrote tests

--- tests/ludwig/utils/test_tokenizers.py
+++ tests/ludwig/utils/test_tokenizers.py
@@ -1,63 +1,9 @@
-import os
-
-import pytest
-import torch
-import torchtext
-
 from ludwig.utils.tokenizers import (
     EnglishLemmatizeFilterTokenizer,
+    get_tokenizer_from_registry,
     NgramTokenizer,
-    SentencePieceTokenizer,
     StringSplitTokenizer,
 )
-
-TORCHTEXT_0_14_0_HF_NAMES = [
-    "bert-base-uncased",
-    "distilbert-base-uncased",
-    "google/electra-small-discriminator",
-    "dbmdz/bert-base-italian-cased",  # Community model
-    "nreimers/MiniLM-L6-H384-uncased",  # Community model
-    "emilyalsentzer/Bio_ClinicalBERT",  # Community model
-    "bionlp/bluebert_pubmed_mimic_uncased_L-12_H-768_A-12",  # Community model
-]
-
-
-@pytest.mark.parametrize(
-    "pretrained_model_name_or_path",
-    [
-        pytest.param(
-            model_name,
-            marks=[
-                pytest.mark.skipif(
-                    torch.torch_version.TorchVersion(torchtext.__version__) < (0, 14, 0),
-                    reason="requires torchtext 0.14.0 or higher",
-                ),
-            ],
-        )
-        for model_name in TORCHTEXT_0_14_0_HF_NAMES
-    ],
-)
-def test_bert_hf_tokenizer_parity(tmpdir, pretrained_model_name_or_path):
-    """Tests the BERTTokenizer implementation.
-
-    Asserts both tokens and token IDs are the same by initializing the BERTTokenizer as a standalone tokenizer and as a
-    HF tokenizer.
-    """
-    from ludwig.utils.tokenizers import get_hf_tokenizer, HFTokenizer
-
-    inputs = "Hello, ``I'm'' ónë of 1,205,000 sentences!"
-    hf_tokenizer = HFTokenizer(pretrained_model_name_or_path)
-    torchtext_tokenizer = get_hf_tokenizer(pretrained_model_name_or_path)
-
-    # Ensure that the tokenizer is scriptable
-    tokenizer_path = os.path.join(tmpdir, "tokenizer.pt")
-    torch.jit.script(torchtext_tokenizer).save(tokenizer_path)
-    torchtext_tokenizer = torch.jit.load(tokenizer_path)
-
-    token_ids_expected = hf_tokenizer(inputs)
-    token_ids = torchtext_tokenizer(inputs)
-
-    assert token_ids_expected == token_ids
 
 
 def test_ngram_tokenizer():
@@ -94,6 +40,42 @@ def test_english_lemmatize_filter_tokenizer():
 
 def test_sentence_piece_tokenizer():
     inputs = "This is a sentence. And this is another one."
-    tokenizer = SentencePieceTokenizer()
+    tokenizer = get_tokenizer_from_registry("sentencepiece")()
     tokens = tokenizer(inputs)
     assert tokens == ["▁This", "▁is", "▁a", "▁sentence", ".", "▁And", "▁this", "▁is", "▁another", "▁one", "."]
+
+
+def test_clip_tokenizer():
+    inputs = "This is a sentence. And this is another one."
+    tokenizer = get_tokenizer_from_registry("clip")()
+    tokens = tokenizer(inputs)
+    print(tokens)
+    assert tokens == [
+        "this</w>",
+        "is</w>",
+        "a</w>",
+        "sentence</w>",
+        ".</w>",
+        "and</w>",
+        "this</w>",
+        "is</w>",
+        "another</w>",
+        "one</w>",
+        ".</w>",
+    ]
+
+
+def test_gpt2_bpe_tokenizer():
+    inputs = "This is a sentence. And this is another one."
+    tokenizer = get_tokenizer_from_registry("gpt2bpe")()
+    tokens = tokenizer(inputs)
+    print(tokens)
+    assert tokens == ["This", "Ġis", "Ġa", "Ġsentence", ".", "ĠAnd", "Ġthis", "Ġis", "Ġanother", "Ġone", "."]
+
+
+def test_bert_tokenizer():
+    inputs = "This is a sentence. And this is another one."
+    tokenizer = get_tokenizer_from_registry("bert")()
+    tokens = tokenizer(inputs)
+    print(tokens)
+    assert tokens == ["this", "is", "a", "sentence", ".", "and", "this", "is", "another", "one", "."]
