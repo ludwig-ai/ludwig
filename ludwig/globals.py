4bb00c70115f6a0d95cf4303e213cb4eb03db80f
w4nderlust <w4nderlust@gmail.com>
2026-04-12 20:35:39 -0700

    Release v0.14.0
    Bump LUDWIG_VERSION from 0.13.0 to 0.14.0.

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.13.0"
+LUDWIG_VERSION = "0.14.0"
 
 MODEL_FILE_NAME = "model"
 MODEL_WEIGHTS_FILE_NAME = "model_weights"  # legacy pickle format
