5877a432e82557cd228c668d13c0b2fbbc1dbdb1
w4nderlust <w4nderlust@gmail.com>
2026-05-04 22:28:28 -0700

    Release v0.15.1

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.15.0"
+LUDWIG_VERSION = "0.15.1"
 
 MODEL_FILE_NAME = "model"
 MODEL_WEIGHTS_FILE_NAME = "model_weights"  # legacy pickle format
