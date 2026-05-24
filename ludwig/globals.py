a0e3494070bc9ac206d84434b87a036e3acd2d28
w4nderlust <w4nderlust@gmail.com>
2026-05-24 09:29:44 -0700

    chore: bump version to 0.17.3

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.17.2"
+LUDWIG_VERSION = "0.17.3"
 
 MODEL_FILE_NAME = "model"
 MODEL_WEIGHTS_FILE_NAME = "model_weights"  # legacy pickle format
