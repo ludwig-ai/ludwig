ea38d2f32beb4fbcaa25fccd5b395deb79dd5990
w4nderlust <w4nderlust@gmail.com>
2026-05-24 22:06:37 -0700

    Release 0.17.4: loosen rich constraint to >=12.4.4

--- ludwig/globals.py
+++ ludwig/globals.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-LUDWIG_VERSION = "0.17.3"
+LUDWIG_VERSION = "0.17.4"
 
 MODEL_FILE_NAME = "model"
 MODEL_WEIGHTS_FILE_NAME = "model_weights"  # legacy pickle format
